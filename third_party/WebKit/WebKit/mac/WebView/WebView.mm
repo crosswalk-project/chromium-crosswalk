crosswalk-project/chromium-crosswalk

commit a7ec526016136213c8c4da51927fadcc7735d1ce

Reviewed by Tim Hatcher.
        - fix <rdar://problem/5725996> crash every time you open the Xcode documentation window

        * WebView/WebView.mm:
        (-[WebView _userAgentWithApplicationName:andWebKitVersion:]): Work around a bug in the
        garbage collector's Objective C++ support by not initializing a static to an object
        that needs to be marked when running under GC.



git-svn-id: svn://svn.chromium.org/blink/trunk@30119 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -4103,7 +4103,11 @@ static NSString *createMacOSXVersionString()
 
 - (NSString *)_userAgentWithApplicationName:(NSString *)applicationName andWebKitVersion:(NSString *)version
 {
-    static NSString *osVersion = createMacOSXVersionString();
+    // Note: Do *not* move the initialization of osVersion into the declaration.
+    // Garbage collection won't correctly mark the global variable in that case <rdar://problem/5733674>.
+    static NSString *osVersion;
+    if (!osVersion)
+        version = createMacOSXVersionString();
     NSString *language = [NSUserDefaults _webkit_preferredLanguageCode];
     if ([applicationName length])
         return [NSString stringWithFormat:@"Mozilla/5.0 (Macintosh; U; " PROCESSOR " Mac OS X %@; %@) AppleWebKit/%@ (KHTML, like Gecko) %@",

commit 0125365564376e73c8faf18717e414523efd9da0

- fix http://bugs.webkit.org/show_bug.cgi?id=17274           REGRESSION: User Agent string broken in r30119
        * WebView/WebView.mm:
        (-[WebView _userAgentWithApplicationName:andWebKitVersion:]):
        Fix wrong variable name. Doh!



git-svn-id: svn://svn.chromium.org/blink/trunk@30120 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -4107,7 +4107,7 @@ static NSString *createMacOSXVersionString()
     // Garbage collection won't correctly mark the global variable in that case <rdar://problem/5733674>.
     static NSString *osVersion;
     if (!osVersion)
-        version = createMacOSXVersionString();
+        osVersion = createMacOSXVersionString();
     NSString *language = [NSUserDefaults _webkit_preferredLanguageCode];
     if ([applicationName length])
         return [NSString stringWithFormat:@"Mozilla/5.0 (Macintosh; U; " PROCESSOR " Mac OS X %@; %@) AppleWebKit/%@ (KHTML, like Gecko) %@",