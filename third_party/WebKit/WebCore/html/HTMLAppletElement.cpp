crosswalk-project/chromium-crosswalk

commit 8609839cd91e328155dab3c1bf89ac2f8c3b5b65

2009-03-10  Sam Weinig  <sam@webkit.org>
        Reviewed by Geoffrey Garen.

        Fix for <rdar://problem/6166844>
        https://bugs.webkit.org/show_bug.cgi?id=24495

        Use same rule for loading java applets as we do for images.

        * html/HTMLAppletElement.cpp:
        (WebCore::HTMLAppletElement::createRenderer):
        * loader/FrameLoader.cpp:
        (WebCore::FrameLoader::loadItem):



git-svn-id: svn://svn.chromium.org/blink/trunk@41568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebCore/html/HTMLAppletElement.cpp
@@ -115,9 +115,13 @@ RenderObject* HTMLAppletElement::createRenderer(RenderArena*, RenderStyle* style
         HashMap<String, String> args;
 
         args.set("code", getAttribute(codeAttr));
+
         const AtomicString& codeBase = getAttribute(codebaseAttr);
-        if(!codeBase.isNull())
+        if (!codeBase.isNull())
             args.set("codeBase", codeBase);
+        else
+            args.set("codeBase", document()->baseURL().string());
+
         const AtomicString& name = getAttribute(document()->isHTMLDocument() ? nameAttr : idAttr);
         if (!name.isNull())
             args.set("name", name);

third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -55,6 +55,7 @@
 #include "FrameTree.h"
 #include "FrameView.h"
 #include "HTMLAnchorElement.h"
+#include "HTMLAppletElement.h"
 #include "HTMLFormElement.h"
 #include "HTMLFrameElement.h"
 #include "HTMLNames.h"
@@ -5068,12 +5069,15 @@ void FrameLoader::dispatchWindowObjectAvailable()
 Widget* FrameLoader::createJavaAppletWidget(const IntSize& size, HTMLAppletElement* element, const HashMap<String, String>& args)
 {
     String baseURLString;
+    String codeBaseURLString;
     Vector<String> paramNames;
     Vector<String> paramValues;
     HashMap<String, String>::const_iterator end = args.end();
     for (HashMap<String, String>::const_iterator it = args.begin(); it != end; ++it) {
         if (equalIgnoringCase(it->first, "baseurl"))
             baseURLString = it->second;
+        else if (equalIgnoringCase(it->first, "codebase"))
+            codeBaseURLString = it->second;
         paramNames.append(it->first);
         paramValues.append(it->second);
     }
@@ -5082,10 +5086,14 @@ Widget* FrameLoader::createJavaAppletWidget(const IntSize& size, HTMLAppletEleme
         baseURLString = m_frame->document()->baseURL().string();
     KURL baseURL = completeURL(baseURLString);
 
-    Widget* widget = m_client->createJavaAppletWidget(size, element, baseURL, paramNames, paramValues);
-    if (widget)
-        m_containsPlugIns = true;
-    
+    Widget* widget = 0;
+    KURL codeBaseURL = completeURL(codeBaseURLString);
+    if (canLoad(codeBaseURL, String(), element->document())) {
+        widget = m_client->createJavaAppletWidget(size, element, baseURL, paramNames, paramValues);
+        if (widget)
+            m_containsPlugIns = true;
+    }
+
     return widget;
 }
 

commit 109de9deca85e548f13d90148d216a212eea4138

2009-05-04  Brady Eidson  <beidson@apple.com>
        Reviewed by Sam 'The Belly' Weinig

        <rdar://problem/6828894> - Facebook photo uploader applet broken on Leopard only

        In r41568 we started adding a default codebase attribute to <applet> tags if they didn't specify one
        themselves.  Leopard's Java plug-in mishandles this case and fails to load code for the applet.

        The spirit of r41568 can be maintained by removing the "default codebase" piece and only performing
        the canLoad() check if a codebase was actually set on the applet tag.

        * html/HTMLAppletElement.cpp:
        (WebCore::HTMLAppletElement::createRenderer): Don't create a default codebase attribute.

        * loader/FrameLoader.cpp:
        (WebCore::FrameLoader::createJavaAppletWidget): Only perform the canLoad() check if the codebase was set.



git-svn-id: svn://svn.chromium.org/blink/trunk@43200 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebCore/html/HTMLAppletElement.cpp
@@ -120,8 +120,6 @@ RenderObject* HTMLAppletElement::createRenderer(RenderArena*, RenderStyle* style
         const AtomicString& codeBase = getAttribute(codebaseAttr);
         if (!codeBase.isNull())
             args.set("codeBase", codeBase);
-        else
-            args.set("codeBase", document()->baseURL().baseAsString());
 
         const AtomicString& name = getAttribute(document()->isHTMLDocument() ? nameAttr : idAttr);
         if (!name.isNull())

third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -5109,10 +5109,12 @@ Widget* FrameLoader::createJavaAppletWidget(const IntSize& size, HTMLAppletEleme
         paramValues.append(it->second);
     }
 
-    KURL codeBaseURL = completeURL(codeBaseURLString);
-    if (!canLoad(codeBaseURL, String(), element->document())) {
-        FrameLoader::reportLocalLoadFailed(m_frame, codeBaseURL.string());
-        return 0;
+    if (!codeBaseURLString.isEmpty()) {
+        KURL codeBaseURL = completeURL(codeBaseURLString);
+        if (!canLoad(codeBaseURL, String(), element->document())) {
+            FrameLoader::reportLocalLoadFailed(m_frame, codeBaseURL.string());
+            return 0;
+        }
     }
 
     if (baseURLString.isEmpty())