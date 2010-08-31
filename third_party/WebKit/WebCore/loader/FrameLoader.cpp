crosswalk-project/chromium-crosswalk

commit 33c9e8ad9332e45173e09b030ad4e8020567962d

2010-07-16  Nate Chapin  <japhet@chromium.org>
        Reviewed by Darin Fisher.

        Regression in r63100: Don't clear m_loadType in
        FrameLoader::handledOnloadEvents, as it is used
        to make some decisions after the load has actually
        completed.

        Tickling this bug requires manipulation
        via the API (layout tests don't appear to traverse
        the correct codepath), so no layout test.

        https://bugs.webkit.org/show_bug.cgi?id=42298

        * loader/FrameLoader.cpp:
        (WebCore::FrameLoader::handledOnloadEvents): Don't reset m_loadType.
        (WebCore::FrameLoader::addExtraFieldsToRequest): Set subresource cache
           policy in one place and don't depend on m_loadType.
2010-07-16  Nate Chapin  <japhet@chromium.org>

        Reviewed by Darin Fisher.

        Uncomment an assert that broke due to r63100.

        https://bugs.webkit.org/show_bug.cgi?id=42298

        * tests/testwebview.c:

git-svn-id: svn://svn.chromium.org/blink/trunk@63528 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -2609,8 +2609,6 @@ void FrameLoader::handledOnloadEvents()
 {
     m_client->dispatchDidHandleOnloadEvents();
 
-    m_loadType = FrameLoadTypeStandard;
-
 #if ENABLE(OFFLINE_WEB_APPLICATIONS)
     if (documentLoader())
         documentLoader()->applicationCacheHost()->stopDeferringEvents();
@@ -2690,7 +2688,14 @@ void FrameLoader::addExtraFieldsToRequest(ResourceRequest& request, FrameLoadTyp
     // 2. Delegates that modify the cache policy using willSendRequest: should
     //    not affect any other resources. Such changes need to be done
     //    per request.
-    if (loadType == FrameLoadTypeReload) {
+    if (!mainResource) {
+        if (request.isConditional())
+            request.setCachePolicy(ReloadIgnoringCacheData);
+        else if (documentLoader()->isLoadingInAPISense())
+            request.setCachePolicy(documentLoader()->originalRequest().cachePolicy());
+        else
+            request.setCachePolicy(UseProtocolCachePolicy);
+    } else if (loadType == FrameLoadTypeReload) {
         request.setCachePolicy(ReloadIgnoringCacheData);
         request.setHTTPHeaderField("Cache-Control", "max-age=0");
     } else if (loadType == FrameLoadTypeReloadFromOrigin) {
@@ -2701,8 +2706,6 @@ void FrameLoader::addExtraFieldsToRequest(ResourceRequest& request, FrameLoadTyp
         request.setCachePolicy(ReloadIgnoringCacheData);
     else if (isBackForwardLoadType(loadType) && m_stateMachine.committedFirstRealDocumentLoad() && !request.url().protocolIs("https"))
         request.setCachePolicy(ReturnCacheDataElseLoad);
-    else if (!mainResource && documentLoader()->isLoadingInAPISense())
-        request.setCachePolicy(documentLoader()->originalRequest().cachePolicy());
     
     if (mainResource)
         request.setHTTPAccept(defaultAcceptHeader);

third_party/WebKit/WebKit/gtk/tests/testwebview.c
@@ -266,10 +266,7 @@ static void do_test_webkit_web_view_adjustments(gboolean with_page_cache)
     g_assert_cmpfloat(lower, ==, gtk_adjustment_get_lower(adjustment));
     g_assert_cmpfloat(upper, ==, gtk_adjustment_get_upper(adjustment));
 
-    // This assert is temporarily disabled until we fix the following bug:
-    // https://bugs.webkit.org/show_bug.cgi?id=42114
-    // It should be re-enabled ASAP.
-    // g_assert_cmpfloat(gtk_adjustment_get_value(adjustment), ==, 100.0);
+    g_assert_cmpfloat(gtk_adjustment_get_value(adjustment), ==, 100.0);
 
     g_free(effective_uri);
     g_free(second_uri);

commit 10eaf676a52575776506ff225523977441b0c925

2010-08-31  Nate Chapin  <japhet@chromium.org>
        Reviewed by Darin Fisher.

        Regression in r63528: Custom http headers for loads of type
        FrameLoadTypeReloadFromOrigin are not being correctly
        propagated to subresource loads.

        https://bugs.webkit.org/show_bug.cgi?id=44987

        No tests, since DRT doesn't seem to have a way to trigger a
        FrameLoadTypeReloadFromOrigin load.

        * loader/FrameLoader.cpp:
        (WebCore::FrameLoader::addExtraFieldsToRequest):



git-svn-id: svn://svn.chromium.org/blink/trunk@66549 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -2645,17 +2645,19 @@ void FrameLoader::addExtraFieldsToRequest(ResourceRequest& request, FrameLoadTyp
             request.setCachePolicy(documentLoader()->originalRequest().cachePolicy());
         else
             request.setCachePolicy(UseProtocolCachePolicy);
-    } else if (loadType == FrameLoadTypeReload) {
-        request.setCachePolicy(ReloadIgnoringCacheData);
-        request.setHTTPHeaderField("Cache-Control", "max-age=0");
-    } else if (loadType == FrameLoadTypeReloadFromOrigin) {
-        request.setCachePolicy(ReloadIgnoringCacheData);
-        request.setHTTPHeaderField("Cache-Control", "no-cache");
-        request.setHTTPHeaderField("Pragma", "no-cache");
-    } else if (request.isConditional())
+    } else if (loadType == FrameLoadTypeReload || loadType == FrameLoadTypeReloadFromOrigin || request.isConditional())
         request.setCachePolicy(ReloadIgnoringCacheData);
     else if (isBackForwardLoadType(loadType) && m_stateMachine.committedFirstRealDocumentLoad() && !request.url().protocolIs("https"))
         request.setCachePolicy(ReturnCacheDataElseLoad);
+        
+    if (request.cachePolicy() == ReloadIgnoringCacheData) {
+        if (loadType == FrameLoadTypeReload)
+            request.setHTTPHeaderField("Cache-Control", "max-age=0");
+        else if (loadType == FrameLoadTypeReloadFromOrigin) {
+            request.setHTTPHeaderField("Cache-Control", "no-cache");
+            request.setHTTPHeaderField("Pragma", "no-cache");
+        }
+    }
     
     if (mainResource)
         request.setHTTPAccept(defaultAcceptHeader);