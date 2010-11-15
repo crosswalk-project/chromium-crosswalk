crosswalk-project/chromium-crosswalk

commit ed51f6467f9770447c6c3fe70096a75d9326f6ed

https://bugs.webkit.org/show_bug.cgi?id=49505
Reviewed by Dan Bernstein.

REGRESSION: @font-face doesn't work with the small-caps variant.  This was broken by the addition of SVG fonts
support long ago.  This patch fixes the SVG code so that it doesn't break non-SVG fonts.

Added fast/blockflow/broken-ideograph-small-caps.html

WebCore: 

* css/CSSFontFace.cpp:
(WebCore::CSSFontFace::hasSVGFontFaceSource):
* css/CSSFontFace.h:
* css/CSSFontFaceSource.cpp:
(WebCore::CSSFontFaceSource::getFontData):
* css/CSSFontFaceSource.h:
* css/CSSFontSelector.cpp:
(WebCore::CSSFontSelector::addFontFaceRule):
(WebCore::CSSFontSelector::getFontData):

LayoutTests: 

* fast/blockflow/broken-ideograph-small-caps.html: Added.
* platform/mac/fast/blockflow/broken-ideograph-small-caps-expected.checksum: Added.
* platform/mac/fast/blockflow/broken-ideograph-small-caps-expected.png: Added.
* platform/mac/fast/blockflow/broken-ideograph-small-caps-expected.txt: Added.



git-svn-id: svn://svn.chromium.org/blink/trunk@71979 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebCore/css/CSSFontSelector.cpp
@@ -224,7 +224,7 @@ void CSSFontSelector::addFontFaceRule(const CSSFontFaceRule* fontFaceRule)
             }
         }
     } else
-        traitsMask |= FontVariantNormalMask;
+        traitsMask |= FontVariantMask;
 
     // Each item in the src property's list is a single CSSFontFaceSource. Put them all into a CSSFontFace.
     RefPtr<CSSFontFace> fontFace;
@@ -316,13 +316,6 @@ void CSSFontSelector::addFontFaceRule(const CSSFontFaceRule* fontFaceRule)
 
         if (familyName.isEmpty())
             continue;
-
-#if ENABLE(SVG_FONTS)
-        // SVG allows several <font> elements with the same font-family, differing only
-        // in ie. font-variant. Be sure to pick up the right one - in getFontData below.
-        if (foundSVGFont && (traitsMask & FontVariantSmallCapsMask))
-            familyName += "-webkit-svg-small-caps";
-#endif
 
         Vector<RefPtr<CSSFontFace> >* familyFontFaces = m_fontFaces.get(familyName);
         if (!familyFontFaces) {
@@ -469,11 +462,6 @@ FontData* CSSFontSelector::getFontData(const FontDescription& fontDescription, c
 
     String family = familyName.string();
 
-#if ENABLE(SVG_FONTS)
-    if (fontDescription.smallCaps())
-        family += "-webkit-svg-small-caps";
-#endif
-
     Vector<RefPtr<CSSFontFace> >* familyFontFaces = m_fontFaces.get(family);
     // If no face was found, then return 0 and let the OS come up with its best match for the name.
     if (!familyFontFaces || familyFontFaces->isEmpty()) {
@@ -504,6 +492,12 @@ FontData* CSSFontSelector::getFontData(const FontDescription& fontDescription, c
                 continue;
             if ((traitsMask & FontVariantNormalMask) && !(candidateTraitsMask & FontVariantNormalMask))
                 continue;
+#if ENABLE(SVG_FONTS)
+            // For SVG Fonts that specify that they only support the "normal" variant, we will assume they are incapable
+            // of small-caps synthesis and just ignore the font face as a candidate.
+            if (candidate->hasSVGFontFaceSource() && (traitsMask & FontVariantSmallCapsMask) && !(candidateTraitsMask & FontVariantSmallCapsMask))
+                continue;
+#endif
             candidateFontFaces.append(candidate);
         }
 

third_party/WebKit/LayoutTests/fast/blockflow/broken-ideograph-small-caps.html
@@ -1,0 +1,73 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Broken Ideographic Font</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  
+<style type="text/css">
+@font-face {
+    src: url(resources/MakibaFont13.ttf);
+    font-family: MakibaFace;
+}
+
+body {
+  margin: 50px;
+  font-size: 20px;
+  font-family: MakibaFace;
+  font-variant: small-caps;
+}
+
+.lrblock {
+  writing-mode: tb-lr;  /* IE */
+  -webkit-writing-mode: vertical-lr;
+  height: 275px;
+  margin-bottom: 50px;
+  padding: 1px;
+  background-color: #eee;
+}
+
+.basic {
+  width: 275px;
+  margin: 0;
+  padding: 1px;
+  writing-mode: lr-tb;  /* IE */
+  -webkit-writing-mode: horizontal-tb;
+}
+
+div.d0 {
+  background-color: #fee;
+}
+
+div.d1 {
+  background-color: #ffe;
+}
+p {
+  margin: 10% 5% 10% 5%;
+  background-color: #faa;
+  border-left: 20px solid #f88;
+  border-right: 20px solid #f88;
+  line-height: 1em;
+  padding: 1px;
+}
+
+.vert {
+  writing-mode: tb-rl;  /* IE */
+  -webkit-writing-mode: vertical-rl;
+}
+
+</style>
+  
+<script type="text/javascript">
+
+</script>
+  
+</head>
+<body>
+
+<div class="lrblock">
+<div class="basic d0"><p>第一段落 Paragraph 1</p><p>第二段落 Paragraph 2</p></div>
+<div class="basic d1 vert"><p>第一段落 Paragraph 1</p><p>第二段落 Paragraph 2</p></div>
+</div>
+
+</body>
+</html>

third_party/WebKit/LayoutTests/platform/mac/fast/blockflow/broken-ideograph-small-caps-expected.checksum
@@ -1,0 +1,1 @@
+76d0c80c1f02ca72ea8fba3d166a98d1

third_party/WebKit/LayoutTests/platform/mac/fast/blockflow/broken-ideograph-small-caps-expected.txt
@@ -1,0 +1,20 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x377
+  RenderBlock {HTML} at (0,0) size 800x377
+    RenderBody {BODY} at (50,50) size 700x277
+      RenderBlock {DIV} at (0,0) size 556x277 [bgcolor=#EEEEEE]
+        RenderBlock {DIV} at (1,1) size 277x127 [bgcolor=#FFEEEE]
+          RenderBlock {P} at (14,28) size 249x22 [bgcolor=#FFAAAA] [border: (20px solid #FF8888) none (20px solid #FF8888)]
+            RenderText {#text} at (21,1) size 176x20
+              text run at (21,1) width 176: "\x{7B2C}\x{4E00}\x{6BB5}\x{843D} Paragraph 1"
+          RenderBlock {P} at (14,77) size 249x22 [bgcolor=#FFAAAA] [border: (20px solid #FF8888) none (20px solid #FF8888)]
+            RenderText {#text} at (21,1) size 176x20
+              text run at (21,1) width 176: "\x{7B2C}\x{4E8C}\x{6BB5}\x{843D} Paragraph 2"
+        RenderBlock {DIV} at (278,1) size 277x275 [bgcolor=#FFFFEE]
+          RenderBlock {P} at (14,28) size 62x219 [bgcolor=#FFAAAA] [border: (20px solid #FF8888) none (20px solid #FF8888)]
+            RenderText {#text} at (21,1) size 20x176
+              text run at (21,1) width 176: "\x{7B2C}\x{4E00}\x{6BB5}\x{843D} Paragraph 1"
+          RenderBlock {P} at (89,28) size 62x219 [bgcolor=#FFAAAA] [border: (20px solid #FF8888) none (20px solid #FF8888)]
+            RenderText {#text} at (21,1) size 20x176
+              text run at (21,1) width 176: "\x{7B2C}\x{4E8C}\x{6BB5}\x{843D} Paragraph 2"

third_party/WebKit/WebCore/css/CSSFontFace.cpp
@@ -118,5 +118,16 @@ SimpleFontData* CSSFontFace::getFontData(const FontDescription& fontDescription,
     return 0;
 }
 
+#if ENABLE(SVG_FONTS)
+bool CSSFontFace::hasSVGFontFaceSource() const
+{
+    for (unsigned i = 0; i < m_sources.size(); i++) {
+        if (m_sources[i]->isSVGFontFaceSource())
+            return true;
+    }
+    return false;
+}
+#endif
+
 }
 

third_party/WebKit/WebCore/css/CSSFontFace.h
@@ -79,6 +79,10 @@ public:
         UChar32 m_to;
     };
 
+#if ENABLE(SVG_FONTS)
+    bool hasSVGFontFaceSource() const;
+#endif
+
 private:
     CSSFontFace(FontTraitsMask traitsMask)
         : m_traitsMask(traitsMask)

third_party/WebKit/WebCore/css/CSSFontFaceSource.cpp
@@ -189,4 +189,11 @@ SimpleFontData* CSSFontFaceSource::getFontData(const FontDescription& fontDescri
     return fontDataRawPtr;
 }
 
+#if ENABLE(SVG_FONTS)
+bool CSSFontFaceSource::isSVGFontFaceSource() const
+{
+    return m_svgFontFaceElement || (m_font && m_font->isSVGFont());
+}
+#endif
+
 }

third_party/WebKit/WebCore/css/CSSFontFaceSource.h
@@ -65,6 +65,7 @@ public:
 #if ENABLE(SVG_FONTS)
     SVGFontFaceElement* svgFontFaceElement() const { return m_svgFontFaceElement; }
     void setSVGFontFaceElement(SVGFontFaceElement* element) { m_svgFontFaceElement = element; }
+    bool isSVGFontFaceSource() const;
 #endif
 
 private:

commit 1a2af2365cc30e2af1de753a7c462ca23b1f3912

https://bugs.webkit.org/show_bug.cgi?id=49521
Reviewed by Dan Bernstein.

REGRESSION svg/W3C-SVG-1.1/fonts-desc-02-t.svg broken by r71979.
        
Refine the font selection function for @font-face to be smarter about what fonts it prefers.  If
a font is labeled as only supporting small-caps, then prefer it to one that claims to support both
normal and small-caps.  The specialized font is more likely to be true small-caps and to not rely
on synthesis.
        
Added the same logic for italic as well.  Prefer the font that is specifically restricted to
italic to one that claims it can support anything.

* css/CSSFontSelector.cpp:
(WebCore::compareFontFaces):



git-svn-id: svn://svn.chromium.org/blink/trunk@71993 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebCore/css/CSSFontSelector.cpp
@@ -401,11 +401,29 @@ static inline bool compareFontFaces(CSSFontFace* first, CSSFontFace* second)
     if (firstHasDesiredVariant != secondHasDesiredVariant)
         return firstHasDesiredVariant;
 
+    if (desiredTraitsMaskForComparison & FontVariantSmallCapsMask) {
+        // Prefer a font that has indicated that it can only support small-caps to a font that claims to support
+        // all variants.  The specialized font is more likely to be true small-caps and not require synthesis.
+        bool firstRequiresSmallCaps = (firstTraitsMask & FontVariantSmallCapsMask) && !(firstTraitsMask & FontVariantNormalMask);
+        bool secondRequiresSmallCaps = (secondTraitsMask & FontVariantSmallCapsMask) && !(secondTraitsMask & FontVariantNormalMask);
+        if (firstRequiresSmallCaps != secondRequiresSmallCaps)
+            return firstRequiresSmallCaps;
+    }
+
     bool firstHasDesiredStyle = firstTraitsMask & desiredTraitsMaskForComparison & FontStyleMask;
     bool secondHasDesiredStyle = secondTraitsMask & desiredTraitsMaskForComparison & FontStyleMask;
 
     if (firstHasDesiredStyle != secondHasDesiredStyle)
         return firstHasDesiredStyle;
+
+    if (desiredTraitsMaskForComparison & FontStyleItalicMask) {
+        // Prefer a font that has indicated that it can only support italic to a font that claims to support
+        // all styles.  The specialized font is more likely to be what the author wants.
+        bool firstRequiresItalics = (firstTraitsMask & FontStyleItalicMask) && !(firstTraitsMask & FontStyleNormalMask);
+        bool secondRequiresItalics = (secondTraitsMask & FontStyleItalicMask) && !(secondTraitsMask & FontStyleNormalMask);
+        if (firstRequiresItalics != secondRequiresItalics)
+            return firstRequiresItalics;
+    }
 
     if (secondTraitsMask & desiredTraitsMaskForComparison & FontWeightMask)
         return false;