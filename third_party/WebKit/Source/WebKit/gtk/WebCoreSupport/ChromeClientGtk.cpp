crosswalk-project/chromium-crosswalk

commit 5b48704b9aaea26c58570d22449b4b85cb32243d

2011-04-29  Martin Robinson  <mrobinson@igalia.com>
        Reviewed by Gustavo Noronha Silva.

        [GTK] Untangle GtkAdjustments from WebCore
        https://bugs.webkit.org/show_bug.cgi?id=59821

        Completely remove the ability for WebKit to set GtkAdjustments on ScrollViews
        and, consequently, Scrollbars. Handling adjustment updates can now be managed
        in WebKit.

        No new tests. This is covered by existing Scrollbar tests.

        * platform/ScrollView.cpp: platformInit and platformDestroy can now be shared
        between ScrollView.cpp and ScrollViewGtk.cpp.
        * platform/ScrollView.h: Remove unused methods.
        * platform/gtk/MainFrameScrollbarGtk.cpp:
        (MainFrameScrollbarGtk::create): No longer need to pass in the adjustment.
        (MainFrameScrollbarGtk::MainFrameScrollbarGtk): Update the comments here.
        * platform/gtk/MainFrameScrollbarGtk.h: Lots of code removal.
        * platform/gtk/ScrollViewGtk.cpp: Ditto.
        (WebCore::ScrollView::createScrollbar): Ditto.
        (WebCore::ScrollView::setScrollbarModes): Ditto.
        * plugins/gtk/PluginViewGtk.cpp: This file needs the GRefPtrGtk.h include now
        that it doesn't receive it transitively.
2011-04-29  Martin Robinson  <mrobinson@igalia.com>

        Reviewed by Gustavo Noronha Silva.

        [GTK] Untangle GtkAdjustments from WebCore
        https://bugs.webkit.org/show_bug.cgi?id=59821

        Have ChromeClient manage the GtkAdjustments of the parent widget instead of passing
        them to WebCore to manage. This simplifies the code quite a bit and should be less
        crash prone in the future.

        * GNUmakefile.am: Added new GtkAdjustmentWatcher class to the sources list.
        * WebCoreSupport/ChromeClientGtk.cpp: Added new GtkAdjustmentWatcher member.
        (WebKit::ChromeClient::ChromeClient) Initialize the new member.
        (WebKit::ChromeClient::scroll): Poke the adjustment watcher to update the adjustments.

third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/ChromeClientGtk.cpp
@@ -72,6 +72,7 @@ namespace WebKit {
 
 ChromeClient::ChromeClient(WebKitWebView* webView)
     : m_webView(webView)
+    , m_adjustmentWatcher(webView)
     , m_closeSoonTimer(0)
     , m_pendingScrollInvalidations(false)
 {
@@ -449,6 +450,8 @@ void ChromeClient::scroll(const IntSize& delta, const IntRect& rectToScroll, con
     gdk_window_invalidate_region(window, invalidRegion, FALSE);
     cairo_region_destroy(invalidRegion);
 #endif
+
+    m_adjustmentWatcher.updateAdjustmentsFromScrollbarsLater();
 }
 
 IntRect ChromeClient::windowToScreen(const IntRect& rect) const
@@ -485,11 +488,18 @@ void ChromeClient::contentsSizeChanged(Frame* frame, const IntSize& size) const
         && (requisition.height != size.height())
         || (requisition.width != size.width()))
         gtk_widget_queue_resize_no_redraw(widget);
+
+    // If this was a main frame size change, update the scrollbars.
+    if (frame != frame->page()->mainFrame())
+        return;
+    m_adjustmentWatcher.updateAdjustmentsFromScrollbarsLater();
 }
 
 void ChromeClient::scrollbarsModeDidChange() const
 {
     WebKitWebFrame* webFrame = webkit_web_view_get_main_frame(m_webView);
+    if (!webFrame)
+        return;
 
     g_object_notify(G_OBJECT(webFrame), "horizontal-scrollbar-policy");
     g_object_notify(G_OBJECT(webFrame), "vertical-scrollbar-policy");
@@ -724,6 +734,5 @@ void ChromeClient::exitFullScreenForElement(WebCore::Element* element)
     element->document()->webkitDidExitFullScreenForElement(element);
 }
 #endif
-
 
 }

third_party/WebKit/Source/WebCore/platform/ScrollView.cpp
@@ -1245,7 +1245,7 @@ void ScrollView::setScrollOrigin(const IntPoint& origin, bool updatePositionAtAl
         updateScrollbars(scrollOffset());
 }
 
-#if !PLATFORM(WX) && !PLATFORM(GTK) && !PLATFORM(EFL)
+#if !PLATFORM(WX) && !PLATFORM(EFL)
 
 void ScrollView::platformInit()
 {

third_party/WebKit/Source/WebCore/platform/ScrollView.h
@@ -39,11 +39,6 @@
 @protocol WebCoreFrameScrollView;
 #endif
 
-#if PLATFORM(GTK)
-#include "GRefPtrGtk.h"
-typedef struct _GtkAdjustment GtkAdjustment;
-#endif
-
 #if PLATFORM(WX)
 class wxScrollWinEvent;
 #endif
@@ -400,18 +395,6 @@ public:
 
 private:
     NSScrollView<WebCoreFrameScrollView>* scrollView() const;
-#endif
-
-#if PLATFORM(GTK)
-public:
-    void setGtkAdjustments(GtkAdjustment* hadj, GtkAdjustment* vadj, bool resetValues = true);
-    void setHorizontalAdjustment(GtkAdjustment* hadj, bool resetValues = true);
-    void setVerticalAdjustment(GtkAdjustment* vadj, bool resetValues = true);
-    void setScrollOffset(const IntSize& offset) { m_scrollOffset = offset; }
-
-private:
-    GRefPtr<GtkAdjustment> m_horizontalAdjustment;
-    GRefPtr<GtkAdjustment> m_verticalAdjustment;
 #endif
 
 #if PLATFORM(WX)

third_party/WebKit/Source/WebCore/platform/gtk/MainFrameScrollbarGtk.cpp
@@ -2,7 +2,7 @@
  *  Copyright (C) 2007, 2009 Holger Hans Peter Freyther zecke@selfish.org
  *  Copyright (C) 2010 Gustavo Noronha Silva <gns@gnome.org>
  *  Copyright (C) 2010 Collabora Ltd.
- *  Copyright (C) 2010 Igalia S.L.
+ *  Copyright (C) 2010, 2011 Igalia S.L.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -22,7 +22,6 @@
 #include "config.h"
 #include "MainFrameScrollbarGtk.h"
 
-#include "GraphicsContext.h"
 #include "GtkVersioning.h"
 #include "IntRect.h"
 #include "ScrollableArea.h"
@@ -30,103 +29,22 @@
 
 using namespace WebCore;
 
-PassRefPtr<MainFrameScrollbarGtk> MainFrameScrollbarGtk::create(ScrollableArea* scrollableArea, ScrollbarOrientation orientation, GtkAdjustment* adj)
+PassRefPtr<MainFrameScrollbarGtk> MainFrameScrollbarGtk::create(ScrollableArea* scrollableArea, ScrollbarOrientation orientation)
 {
-    return adoptRef(new MainFrameScrollbarGtk(scrollableArea, orientation, adj));
+    return adoptRef(new MainFrameScrollbarGtk(scrollableArea, orientation));
 }
 
-// Main frame scrollbars are slaves to a GtkAdjustment. If a main frame
-// scrollbar has an m_adjustment, it belongs to the container (a GtkWidget such
-// as GtkScrolledWindow). The adjustment may also be null, in which case there
-// is no containing view or the parent ScrollView is in some sort of transition
-// state. These scrollbars are never painted, as the container takes care of
-// that. They exist only to shuttle data from the GtkWidget container into
-// WebCore and vice-versa.
-MainFrameScrollbarGtk::MainFrameScrollbarGtk(ScrollableArea* scrollableArea, ScrollbarOrientation orientation, GtkAdjustment* adjustment)
+// A MainFrameScrollbar is just a non-painting scrollbar. Otherwise it is fully
+// functional. A non-painting scrollbar allows a main-frame ScrollView to use
+// a containing GtkScrolledWindow as its user interface. The ChromeClient in the
+// WebKit layer just listens for scrolling and sizing changes and updates its
+// container (the GtkScrolledWindow) accordingly. The ScrollView is responsible
+// for deciding whether or not to create a MainFrameScrollbar or native scrollbar.
+MainFrameScrollbarGtk::MainFrameScrollbarGtk(ScrollableArea* scrollableArea, ScrollbarOrientation orientation)
     : Scrollbar(scrollableArea, orientation, RegularScrollbar)
-    , m_adjustment(0)
 {
-    attachAdjustment(adjustment);
-
-    // We have nothing to show as we are solely operating on the GtkAdjustment.
+    // We don't want to take up any space.
     resize(0, 0);
-}
-
-MainFrameScrollbarGtk::~MainFrameScrollbarGtk()
-{
-    if (m_adjustment)
-        detachAdjustment();
-}
-
-void MainFrameScrollbarGtk::attachAdjustment(GtkAdjustment* adjustment)
-{
-    if (m_adjustment.get() == adjustment)
-        return;
-    if (m_adjustment)
-        detachAdjustment();
-
-    m_adjustment = adjustment;
-    if (!m_adjustment)
-        return;
-
-    // In some cases this adjustment may still be attached to a living MainFrameScrollbar.
-    // If that's the case we want to force a disconnection now, before we modify the values.
-    g_signal_handlers_disconnect_matched(m_adjustment.get(), G_SIGNAL_MATCH_FUNC, 0, 0, 0,
-                                         reinterpret_cast<void*>(MainFrameScrollbarGtk::gtkValueChanged), 0);
-
-    updateThumbProportion();
-    updateThumbPosition();
-    g_signal_connect(m_adjustment.get(), "value-changed", G_CALLBACK(MainFrameScrollbarGtk::gtkValueChanged), this);
-}
-
-void MainFrameScrollbarGtk::detachAdjustment()
-{
-    if (!m_adjustment)
-        return;
-
-    g_signal_handlers_disconnect_by_func(G_OBJECT(m_adjustment.get()), (gpointer)MainFrameScrollbarGtk::gtkValueChanged, this);
-
-    // For the case where we only operate on the GtkAdjustment it is best to
-    // reset the values so that the surrounding scrollbar gets updated, or
-    // e.g. for a GtkScrolledWindow the scrollbar gets hidden.
-    gtk_adjustment_configure(m_adjustment.get(), 0, 0, 0, 0, 0, 0);
-
-    m_adjustment = 0;
-}
-
-void MainFrameScrollbarGtk::updateThumbPosition()
-{
-    if (!m_adjustment || gtk_adjustment_get_value(m_adjustment.get()) == m_currentPos)
-        return;
-    gtk_adjustment_set_value(m_adjustment.get(), m_currentPos);
-}
-
-void MainFrameScrollbarGtk::updateThumbProportion()
-{
-    if (!m_adjustment)
-        return;
-    gtk_adjustment_configure(m_adjustment.get(),
-                             gtk_adjustment_get_value(m_adjustment.get()),
-                             gtk_adjustment_get_lower(m_adjustment.get()),
-                             m_totalSize,
-                             m_lineStep,
-                             m_pageStep,
-                             m_visibleSize);
-}
-
-void MainFrameScrollbarGtk::gtkValueChanged(GtkAdjustment*, MainFrameScrollbarGtk* that)
-{
-    // If we've been removed from our parent, we no longer get to control the WebCore
-    // scrollbar. If this is the case, deactivate this signal handler. WebCore will
-    // create a fresh MainFrameScrollbar when the scrollbar reappears.
-    if (!that->parent()) {
-        that->detachAdjustment();
-        return;
-    }
-
-    int newValue = static_cast<int>(gtk_adjustment_get_value(that->m_adjustment.get()));
-    if (newValue != that->value())
-        that->scrollableArea()->scrollToOffsetWithoutAnimation(that->orientation(), newValue);
 }
 
 void MainFrameScrollbarGtk::paint(GraphicsContext* context, const IntRect& rect)

third_party/WebKit/Source/WebCore/platform/gtk/MainFrameScrollbarGtk.h
@@ -20,32 +20,18 @@
 #ifndef MainFrameScrollbarGtk_h
 #define MainFrameScrollbarGtk_h
 
-#include "GRefPtrGtk.h"
 #include "Scrollbar.h"
 #include <wtf/PassRefPtr.h>
-
-typedef struct _GtkAdjustment GtkAdjustment;
 
 namespace WebCore {
 
 class MainFrameScrollbarGtk : public Scrollbar {
 public:
-    static PassRefPtr<MainFrameScrollbarGtk> create(ScrollableArea*, ScrollbarOrientation, GtkAdjustment*);
-
-    ~MainFrameScrollbarGtk();
+    static PassRefPtr<MainFrameScrollbarGtk> create(ScrollableArea*, ScrollbarOrientation);
     virtual void paint(GraphicsContext*, const IntRect&);
-    void detachAdjustment();
-    void attachAdjustment(GtkAdjustment*);
-
-protected:
-    virtual void updateThumbPosition();
-    virtual void updateThumbProportion();
 
 private:
-    MainFrameScrollbarGtk(ScrollableArea*, ScrollbarOrientation, GtkAdjustment*);
-    static void gtkValueChanged(GtkAdjustment*, MainFrameScrollbarGtk*);
-
-    GRefPtr<GtkAdjustment> m_adjustment;
+    MainFrameScrollbarGtk(ScrollableArea*, ScrollbarOrientation);
 };
 
 }

third_party/WebKit/Source/WebCore/platform/gtk/ScrollViewGtk.cpp
@@ -31,34 +31,14 @@
 #include "config.h"
 #include "ScrollView.h"
 
-#include "ChromeClient.h"
-#include "FloatRect.h"
-#include "Frame.h"
-#include "FrameView.h"
-#include "GraphicsContext.h"
-#include "GtkVersioning.h"
 #include "HostWindow.h"
-#include "IntRect.h"
 #include "MainFrameScrollbarGtk.h"
-#include "Page.h"
-#include "PlatformMouseEvent.h"
-#include "PlatformWheelEvent.h"
 #include "ScrollbarTheme.h"
 #include <gtk/gtk.h>
 
 using namespace std;
 
 namespace WebCore {
-
-void ScrollView::platformInit()
-{
-}
-
-void ScrollView::platformDestroy()
-{
-    m_horizontalAdjustment = 0;
-    m_verticalAdjustment = 0;
-}
 
 PassRefPtr<Scrollbar> ScrollView::createScrollbar(ScrollbarOrientation orientation)
 {
@@ -71,94 +51,10 @@ PassRefPtr<Scrollbar> ScrollView::createScrollbar(ScrollbarOrientation orientati
     // and defers to our GtkAdjustment for all of its state. Note that the GtkAdjustment
     // may be null here.
     if (orientation == HorizontalScrollbar)
-        return MainFrameScrollbarGtk::create(this, orientation, m_horizontalAdjustment.get());
+        return MainFrameScrollbarGtk::create(this, orientation);
 
     // VerticalScrollbar
-    return MainFrameScrollbarGtk::create(this, orientation, m_verticalAdjustment.get());
-}
-
-void ScrollView::setHorizontalAdjustment(GtkAdjustment* hadj, bool resetValues)
-{
-    ASSERT(!parent() || !hadj);
-    if (parent())
-        return;
-
-    m_horizontalAdjustment = hadj;
-
-    if (!m_horizontalAdjustment) {
-        MainFrameScrollbarGtk* hScrollbar = reinterpret_cast<MainFrameScrollbarGtk*>(horizontalScrollbar());
-        if (hScrollbar)
-            hScrollbar->detachAdjustment();
-
-        return;
-    }
-
-    // We may be lacking scrollbars when returning to a cached
-    // page, this kicks the page to recreate the scrollbars.
-    setHasHorizontalScrollbar(true);
-
-    MainFrameScrollbarGtk* hScrollbar = reinterpret_cast<MainFrameScrollbarGtk*>(horizontalScrollbar());
-    hScrollbar->attachAdjustment(m_horizontalAdjustment.get());
-
-    // We used to reset everything to 0 here, but when page cache
-    // is enabled we reuse FrameViews that are cached. Since their
-    // size is not going to change when being restored, (which is
-    // what would cause the upper limit in the adjusments to be
-    // set in the normal case), we make sure they are up-to-date
-    // here. This is needed for the parent scrolling widget to be
-    // able to report correct values.
-    int horizontalPageStep = max(max<int>(frameRect().width() * Scrollbar::minFractionToStepWhenPaging(), frameRect().width() - Scrollbar::maxOverlapBetweenPages()), 1);
-    gtk_adjustment_configure(m_horizontalAdjustment.get(),
-                             resetValues ? 0 : scrollOffset().width(), 0,
-                             resetValues ? 0 : contentsSize().width(),
-                             resetValues ? 0 : Scrollbar::pixelsPerLineStep(),
-                             resetValues ? 0 : horizontalPageStep,
-                             resetValues ? 0 : frameRect().width());
-}
-
-void ScrollView::setVerticalAdjustment(GtkAdjustment* vadj, bool resetValues)
-{
-    ASSERT(!parent() || !vadj);
-    if (parent())
-        return;
-
-    m_verticalAdjustment = vadj;
-
-    if (!m_verticalAdjustment) {
-        MainFrameScrollbarGtk* vScrollbar = reinterpret_cast<MainFrameScrollbarGtk*>(verticalScrollbar());
-        if (vScrollbar)
-            vScrollbar->detachAdjustment();
-
-        return;
-    }
-
-    // We may be lacking scrollbars when returning to a cached
-    // page, this kicks the page to recreate the scrollbars.
-    setHasVerticalScrollbar(true);
-
-    MainFrameScrollbarGtk* vScrollbar = reinterpret_cast<MainFrameScrollbarGtk*>(verticalScrollbar());
-    vScrollbar->attachAdjustment(m_verticalAdjustment.get());
-
-    // We used to reset everything to 0 here, but when page cache
-    // is enabled we reuse FrameViews that are cached. Since their
-    // size is not going to change when being restored, (which is
-    // what would cause the upper limit in the adjusments to be
-    // set in the normal case), we make sure they are up-to-date
-    // here. This is needed for the parent scrolling widget to be
-    // able to report correct values.
-    int verticalPageStep = max(max<int>(frameRect().width() * Scrollbar::minFractionToStepWhenPaging(), frameRect().width() - Scrollbar::maxOverlapBetweenPages()), 1);
-    gtk_adjustment_configure(m_verticalAdjustment.get(),
-                             resetValues ? 0 : scrollOffset().height(), 0,
-                             resetValues ? 0 : contentsSize().height(),
-                             resetValues ? 0 : Scrollbar::pixelsPerLineStep(),
-                             resetValues ? 0 : verticalPageStep,
-                             resetValues ? 0 : frameRect().height());
-}
-
-void ScrollView::setGtkAdjustments(GtkAdjustment* hadj, GtkAdjustment* vadj, bool resetValues)
-{
-    setHorizontalAdjustment(hadj, resetValues);
-    setVerticalAdjustment(vadj, resetValues);
+    return MainFrameScrollbarGtk::create(this, orientation);
 }
 
 IntRect ScrollView::visibleContentRect(bool includeScrollbars) const
@@ -216,7 +112,7 @@ void ScrollView::setScrollbarModes(ScrollbarMode horizontalMode, ScrollbarMode v
 
     // We don't need to report policy changes on ScrollView's unless this
     // one has an adjustment attached and it is a main frame.
-    if (!m_horizontalAdjustment || parent() || !isFrameView())
+    if (parent() || !isFrameView())
         return;
 
     // For frames that do have adjustments attached, we want to report

third_party/WebKit/Source/WebCore/plugins/gtk/PluginViewGtk.cpp
@@ -39,6 +39,7 @@
 #include "FrameTree.h"
 #include "Frame.h"
 #include "FrameView.h"
+#include "GRefPtrGtk.h"
 #include "GraphicsContext.h"
 #include "GtkVersioning.h"
 #include "HTMLNames.h"

third_party/WebKit/Source/WebKit/gtk/GNUmakefile.am
@@ -190,6 +190,8 @@ webkitgtk_sources += \
 	Source/WebKit/gtk/WebCoreSupport/FrameNetworkingContextGtk.h \
 	Source/WebKit/gtk/WebCoreSupport/FullscreenVideoController.cpp \
 	Source/WebKit/gtk/WebCoreSupport/FullscreenVideoController.h \
+	Source/WebKit/gtk/WebCoreSupport/GtkAdjustmentWatcher.cpp \
+	Source/WebKit/gtk/WebCoreSupport/GtkAdjustmentWatcher.h \
 	Source/WebKit/gtk/WebCoreSupport/InspectorClientGtk.cpp \
 	Source/WebKit/gtk/WebCoreSupport/InspectorClientGtk.h \
 	Source/WebKit/gtk/WebCoreSupport/PasteboardHelperGtk.cpp \

commit 0aa8edb5045cbc579906ce812c4c6bfc051c8d80

2011-05-16  Martin Robinson  <mrobinson@igalia.com>
        Reviewed by Xan Lopez.

        [GTK] Scrolling in Twitter is broken after r86102
        https://bugs.webkit.org/show_bug.cgi?id=60922

        Fix adjustment handling for pages that trigger the slow scrolling path.
        The slow scrolling path is triggered by WebCore when it determines that
        doing a simple invalidation is quicker than doing a normal scroll. This
        typically happens when there are large elements with fixed positions.

        * WebCoreSupport/ChromeClientGtk.cpp:
        (WebKit::ChromeClient::invalidateContentsForSlowScroll): Poke the adjustment
        watcher to update its adjustments when a page triggers the slow scrolling path.

git-svn-id: svn://svn.chromium.org/blink/trunk@86627 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/ChromeClientGtk.cpp
@@ -403,6 +403,7 @@ void ChromeClient::invalidateContentsAndWindow(const IntRect& updateRect, bool i
 void ChromeClient::invalidateContentsForSlowScroll(const IntRect& updateRect, bool immediate)
 {
     invalidateContentsAndWindow(updateRect, immediate);
+    m_adjustmentWatcher.updateAdjustmentsFromScrollbarsLater();
 }
 
 void ChromeClient::scroll(const IntSize& delta, const IntRect& rectToScroll, const IntRect& clipRect)