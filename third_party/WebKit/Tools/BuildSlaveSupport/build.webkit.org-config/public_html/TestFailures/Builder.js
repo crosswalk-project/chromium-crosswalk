crosswalk-project/chromium-crosswalk

commit 605599825dc8207cd9648c29e9d07111aca17e9d

Make TestFailures correctly remember whether old-run-webkit-tests exited early
Fixes <http://webkit.org/b/63342> TestFailures page incorrectly claims test run ran to
completion after reload

Reviewed by David Kilzer.

* BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Builder.js:
(Builder.prototype.getNumberOfFailingTests): Instead of just storing the number of failures
in the PeristentCache, store an object that contains both the number of failures and whether
old-run-webkit-tests exited early.

git-svn-id: svn://svn.chromium.org/blink/trunk@89696 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Builder.js
@@ -93,38 +93,45 @@ Builder.prototype = {
     getNumberOfFailingTests: function(buildNumber, callback) {
         var cacheKey = this.name + '_getNumberOfFailingTests_' + buildNumber;
         if (PersistentCache.contains(cacheKey)) {
-            callback(PersistentCache.get(cacheKey));
-            return;
+            var cachedData = PersistentCache.get(cacheKey);
+            // Old versions of this function used to cache a number instead of an object, so we have
+            // to check to see what type we have.
+            if (typeof cachedData === 'object') {
+                callback(cachedData.failureCount, cachedData.tooManyFailures);
+                return;
+            }
         }
+
+        var result = { failureCount: -1, tooManyFailures: false };
 
         var self = this;
         self._getBuildJSON(buildNumber, function(data) {
             var layoutTestStep = data.steps.findFirst(function(step) { return step.name === 'layout-test'; });
             if (!layoutTestStep) {
-                PersistentCache.set(cacheKey, -1);
+                PersistentCache.set(cacheKey, result);
                 callback(PersistentCache.get(cacheKey), false);
                 return;
             }
 
             if (!('isStarted' in layoutTestStep)) {
                 // run-webkit-tests never even ran.
-                PersistentCache.set(cacheKey, -1);
+                PersistentCache.set(cacheKey, result);
                 callback(PersistentCache.get(cacheKey), false);
                 return;
             }
 
             if (!('results' in layoutTestStep) || layoutTestStep.results[0] === 0) {
                 // All tests passed.
-                PersistentCache.set(cacheKey, -1);
+                result.failureCount = 0;
+                PersistentCache.set(cacheKey, result);
                 callback(PersistentCache.get(cacheKey), false);
                 return;
             }
 
-            var tooManyFailures = false;
             if (/^Exiting early/.test(layoutTestStep.results[1][0]))
-                tooManyFailures = true;
+                result.tooManyFailures = true;
 
-            var failureCount = layoutTestStep.results[1].reduce(function(sum, outputLine) {
+            result.failureCount = layoutTestStep.results[1].reduce(function(sum, outputLine) {
                 var match = /^(\d+) test case/.exec(outputLine);
                 if (!match)
                     return sum;
@@ -134,8 +141,8 @@ Builder.prototype = {
                 return sum + parseInt(match[1], 10);
             }, 0);
 
-            PersistentCache.set(cacheKey, failureCount);
-            callback(failureCount, tooManyFailures);
+            PersistentCache.set(cacheKey, result);
+            callback(result.failureCount, result.tooManyFailures);
         });
     },
 

commit 00a19be347d9113daba08c084c0e677f67a90d25

Fix typos introduced in r89696
* BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Builder.js:
(Builder.prototype.getNumberOfFailingTests): Pass the result data's constituent parts to the
callback, since that's what it expects.

git-svn-id: svn://svn.chromium.org/blink/trunk@89698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Builder.js
@@ -109,14 +109,14 @@ Builder.prototype = {
             var layoutTestStep = data.steps.findFirst(function(step) { return step.name === 'layout-test'; });
             if (!layoutTestStep) {
                 PersistentCache.set(cacheKey, result);
-                callback(PersistentCache.get(cacheKey), false);
+                callback(result.failureCount, result.tooManyFailures);
                 return;
             }
 
             if (!('isStarted' in layoutTestStep)) {
                 // run-webkit-tests never even ran.
                 PersistentCache.set(cacheKey, result);
-                callback(PersistentCache.get(cacheKey), false);
+                callback(result.failureCount, result.tooManyFailures);
                 return;
             }
 
@@ -124,7 +124,7 @@ Builder.prototype = {
                 // All tests passed.
                 result.failureCount = 0;
                 PersistentCache.set(cacheKey, result);
-                callback(PersistentCache.get(cacheKey), false);
+                callback(result.failureCount, result.tooManyFailures);
                 return;
             }
 