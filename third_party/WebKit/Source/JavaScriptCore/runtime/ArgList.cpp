crosswalk-project/chromium-crosswalk

commit 6a215801bb0a527a7256168fee2e965d0eee3a9f

v8 benchmark takes 12-13 million function call slow paths due to extra arguments https://bugs.webkit.org/show_bug.cgi?id=74244
Reviewed by Filip Pizlo.
        
.arguments function of order the Reversed
        
10% speedup on v8-raytrace, 1.7% speedup on v8 overall, neutral on Kraken
and SunSpider.

* bytecode/CodeBlock.h:
(JSC::CodeBlock::valueProfileForArgument): Clarified that the interface
to this function is an argument number.

* bytecompiler/BytecodeGenerator.cpp:
(JSC::BytecodeGenerator::BytecodeGenerator):
(JSC::BytecodeGenerator::emitCall):
(JSC::BytecodeGenerator::emitConstruct):
(JSC::BytecodeGenerator::isArgumentNumber): Switched to using CallFrame
helper functions for computing offsets for arguments, rather than doing
the math by hand.
        
Switched to iterating argument offsets backwards (--) instead of forwards (++).

* bytecompiler/BytecodeGenerator.h:
(JSC::CallArguments::thisRegister):
(JSC::CallArguments::argumentRegister):
(JSC::CallArguments::registerOffset): Updated for arguments being reversed.

* bytecompiler/NodesCodegen.cpp: Allocate arguments in reverse order.

* dfg/DFGByteCodeParser.cpp:
(JSC::DFG::ByteCodeParser::getArgument):
(JSC::DFG::ByteCodeParser::setArgument):
(JSC::DFG::ByteCodeParser::flush):
(JSC::DFG::ByteCodeParser::addCall):
(JSC::DFG::ByteCodeParser::handleCall):
(JSC::DFG::ByteCodeParser::handleInlining):
(JSC::DFG::ByteCodeParser::handleMinMax):
(JSC::DFG::ByteCodeParser::handleIntrinsic):
(JSC::DFG::ByteCodeParser::parseBlock):
(JSC::DFG::ByteCodeParser::processPhiStack): Use abstract argument indices
that just-in-time convert to bytecode operands (i.e., indexes in the register
file) through helper functions. This means only one piece of code needs
to know how arguments are laid out in the register file.

* dfg/DFGGraph.cpp:
(JSC::DFG::Graph::dump): Ditto.

* dfg/DFGGraph.h:
(JSC::DFG::Graph::valueProfileFor): Ditto.

* dfg/DFGJITCompiler.cpp:

third_party/WebKit/Source/JavaScriptCore/runtime/ArgList.cpp
@@ -32,12 +32,12 @@ namespace JSC {
 
 void ArgList::getSlice(int startIndex, ArgList& result) const
 {
-    if (startIndex <= 0 || static_cast<unsigned>(startIndex) >= m_argCount) {
+    if (startIndex <= 0 || startIndex >= m_argCount) {
         result = ArgList();
         return;
     }
 
-    result.m_args = m_args + startIndex;
+    result.m_args = m_args - startIndex;
     result.m_argCount =  m_argCount - startIndex;
 }
 
@@ -52,32 +52,37 @@ void MarkedArgumentBuffer::markLists(HeapRootVisitor& heapRootVisitor, ListSet& 
 
 void MarkedArgumentBuffer::slowAppend(JSValue v)
 {
+    int newCapacity = m_capacity * 4;
+    EncodedJSValue* newBuffer = &(new EncodedJSValue[newCapacity])[newCapacity - 1];
+    for (int i = 0; i < m_capacity; ++i)
+        newBuffer[-i] = m_buffer[-i];
+
+    if (m_capacity != inlineCapacity)
+        delete [] &m_buffer[-(m_capacity - 1)];
+
+    m_buffer = newBuffer;
+    m_capacity = newCapacity;
+
+    m_buffer[-m_size] = JSValue::encode(v);
+    ++m_size;
+
+    if (m_markSet)
+        return;
+
     // As long as our size stays within our Vector's inline 
     // capacity, all our values are allocated on the stack, and 
     // therefore don't need explicit marking. Once our size exceeds
     // our Vector's inline capacity, though, our values move to the 
     // heap, where they do need explicit marking.
-    if (!m_markSet) {
-        // FIXME: Even if v is not a JSCell*, if previous values in the buffer
-        // are, then they won't be marked!
-        if (Heap* heap = Heap::heap(v)) {
-            ListSet& markSet = heap->markListSet();
-            markSet.add(this);
-            m_markSet = &markSet;
-        }
-    }
+    for (int i = 0; i < m_size; ++i) {
+        Heap* heap = Heap::heap(JSValue::decode(m_buffer[-i]));
+        if (!heap)
+            continue;
 
-    if (m_vector.size() < m_vector.capacity()) {
-        m_vector.uncheckedAppend(v);
-        return;
+        m_markSet = &heap->markListSet();
+        m_markSet->add(this);
+        break;
     }
-
-    // 4x growth would be excessive for a normal vector, but it's OK for Lists 
-    // because they're short-lived.
-    m_vector.reserveCapacity(m_vector.capacity() * 4);
-    
-    m_vector.uncheckedAppend(v);
-    m_buffer = m_vector.data();
 }
 
 } // namespace JSC

third_party/WebKit/Source/JavaScriptCore/runtime/ArgList.h
@@ -38,7 +38,7 @@ namespace JSC {
         friend class ArgList;
 
     private:
-        static const unsigned inlineCapacity = 8;
+        static const size_t inlineCapacity = 8;
         typedef Vector<Register, inlineCapacity> VectorType;
         typedef HashSet<MarkedArgumentBuffer*> ListSet;
 
@@ -46,90 +46,57 @@ namespace JSC {
         // Constructor for a read-write list, to which you may append values.
         // FIXME: Remove all clients of this API, then remove this API.
         MarkedArgumentBuffer()
-            : m_isUsingInlineBuffer(true)
+            : m_size(0)
+            , m_capacity(inlineCapacity)
+            , m_buffer(&m_inlineBuffer[m_capacity - 1])
             , m_markSet(0)
-#ifndef NDEBUG
-            , m_isReadOnly(false)
-#endif
         {
-            m_buffer = m_vector.data();
-            m_size = 0;
-        }
-
-        // Constructor for a read-only list whose data has already been allocated elsewhere.
-        MarkedArgumentBuffer(Register* buffer, size_t size)
-            : m_buffer(buffer)
-            , m_size(size)
-            , m_isUsingInlineBuffer(true)
-            , m_markSet(0)
-#ifndef NDEBUG
-            , m_isReadOnly(true)
-#endif
-        {
-        }
-
-        void initialize(WriteBarrier<Unknown>* buffer, size_t size)
-        {
-            ASSERT(!m_markSet);
-            ASSERT(isEmpty());
-
-            m_buffer = reinterpret_cast<Register*>(buffer);
-            m_size = size;
-#ifndef NDEBUG
-            m_isReadOnly = true;
-#endif
         }
 
         ~MarkedArgumentBuffer()
         {
             if (m_markSet)
                 m_markSet->remove(this);
+
+            if (m_capacity != inlineCapacity)
+                delete [] &m_buffer[-(m_capacity - 1)];
         }
 
         size_t size() const { return m_size; }
         bool isEmpty() const { return !m_size; }
 
-        JSValue at(size_t i) const
+        JSValue at(int i) const
         {
-            if (i < m_size)
-                return m_buffer[i].jsValue();
-            return jsUndefined();
+            if (i >= m_size)
+                return jsUndefined();
+
+            return JSValue::decode(m_buffer[-i]);
         }
 
         void clear()
         {
-            m_vector.clear();
-            m_buffer = 0;
             m_size = 0;
         }
 
         void append(JSValue v)
         {
-            ASSERT(!m_isReadOnly);
+            if (m_size >= m_capacity)
+                return slowAppend(v);
 
-            if (m_isUsingInlineBuffer && m_size < inlineCapacity) {
-                m_vector.uncheckedAppend(v);
-                ++m_size;
-            } else {
-                // Putting this case all in one function measurably improves
-                // the performance of the fast "just append to inline buffer" case.
-                slowAppend(v);
-                ++m_size;
-                m_isUsingInlineBuffer = false;
-            }
+            m_buffer[-m_size] = JSValue::encode(v);
+            ++m_size;
         }
 
         void removeLast()
         { 
             ASSERT(m_size);
             m_size--;
-            m_vector.removeLast();
         }
 
         JSValue last() 
         {
             ASSERT(m_size);
-            return m_buffer[m_size - 1].jsValue();
+            return JSValue::decode(m_buffer[-(m_size - 1)]);
         }
         
         static void markLists(HeapRootVisitor&, ListSet&);
@@ -137,15 +104,11 @@ namespace JSC {
     private:
         void slowAppend(JSValue);
         
-        Register* m_buffer;
-        size_t m_size;
-        bool m_isUsingInlineBuffer;
-
-        VectorType m_vector;
+        int m_size;
+        int m_capacity;
+        EncodedJSValue m_inlineBuffer[inlineCapacity];
+        EncodedJSValue* m_buffer;
         ListSet* m_markSet;
-#ifndef NDEBUG
-        bool m_isReadOnly;
-#endif
 
     private:
         // Prohibits new / delete, which would break GC.
@@ -173,24 +136,24 @@ namespace JSC {
             , m_argCount(0)
         {
         }
-        
+
         ArgList(ExecState* exec)
-            : m_args(reinterpret_cast<JSValue*>(&exec[exec->hostThisRegister() + 1]))
+            : m_args(reinterpret_cast<JSValue*>(&exec[CallFrame::argumentOffset(0)]))
             , m_argCount(exec->argumentCount())
         {
         }
-        
+
         ArgList(const MarkedArgumentBuffer& args)
             : m_args(reinterpret_cast<JSValue*>(args.m_buffer))
             , m_argCount(args.size())
         {
         }
 
-        JSValue at(size_t idx) const
+        JSValue at(int i) const
         {
-            if (idx < m_argCount)
-                return m_args[idx];
-            return jsUndefined();
+            if (i >= m_argCount)
+                return jsUndefined();
+            return m_args[-i];
         }
 
         bool isEmpty() const { return !m_argCount; }
@@ -200,7 +163,7 @@ namespace JSC {
 
     private:
         JSValue* m_args;
-        size_t m_argCount;
+        int m_argCount;
     };
 
 } // namespace JSC

third_party/WebKit/Source/JavaScriptCore/Configurations/Base.xcconfig
@@ -24,7 +24,7 @@
 #include "CompilerVersion.xcconfig"
 
 COMPILER_SPECIFIC_WARNING_CFLAGS = $(COMPILER_SPECIFIC_WARNING_CFLAGS_$(TARGET_GCC_VERSION));
-COMPILER_SPECIFIC_WARNING_CFLAGS_LLVM_COMPILER = -Wglobal-constructors -Wexit-time-destructors;
+COMPILER_SPECIFIC_WARNING_CFLAGS_LLVM_COMPILER = ;
 
 CLANG_WARN_CXX0X_EXTENSIONS = NO;
 DEBUG_INFORMATION_FORMAT = dwarf;

third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.h
@@ -651,12 +651,12 @@ namespace JSC {
         {
             return WTF::genericBinarySearch<ValueProfile, int, getValueProfileBytecodeOffset>(m_valueProfiles, m_valueProfiles.size(), bytecodeOffset);
         }
-        ValueProfile* valueProfileForArgument(int argumentIndex)
+        ValueProfile* valueProfileForArgument(int argument)
         {
-            int index = argumentIndex;
-            if (static_cast<unsigned>(index) >= m_valueProfiles.size())
+            size_t index = argument;
+            if (index >= m_valueProfiles.size())
                 return 0;
-            ValueProfile* result = valueProfile(argumentIndex);
+            ValueProfile* result = valueProfile(index);
             if (result->m_bytecodeOffset != -1)
                 return 0;
             return result;

third_party/WebKit/Source/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
@@ -202,7 +202,7 @@ BytecodeGenerator::BytecodeGenerator(ProgramNode* programNode, ScopeChainNode* s
     , m_symbolTable(symbolTable)
     , m_scopeNode(programNode)
     , m_codeBlock(codeBlock)
-    , m_thisRegister(RegisterFile::ProgramCodeThisRegister)
+    , m_thisRegister(CallFrame::thisArgumentOffset())
     , m_finallyDepth(0)
     , m_dynamicScopeDepth(0)
     , m_baseScopeDepth(0)
@@ -396,16 +396,15 @@ BytecodeGenerator::BytecodeGenerator(FunctionBodyNode* functionBody, ScopeChainN
         codeBlock->m_numCapturedVars = codeBlock->m_numVars;
 
     FunctionParameters& parameters = *functionBody->parameters();
-    size_t parameterCount = parameters.size();
-    int nextParameterIndex = -RegisterFile::CallFrameHeaderSize - parameterCount - 1;
-    m_parameters.grow(1 + parameterCount); // reserve space for "this"
+    m_parameters.grow(parameters.size() + 1); // reserve space for "this"
 
     // Add "this" as a parameter
-    m_thisRegister.setIndex(nextParameterIndex);
+    int nextParameterIndex = CallFrame::thisArgumentOffset();
+    m_thisRegister.setIndex(nextParameterIndex--);
     ++m_codeBlock->m_numParameters;
     
-    for (size_t i = 0; i < parameterCount; ++i)
-        addParameter(parameters[i], ++nextParameterIndex);
+    for (size_t i = 0; i < parameters.size(); ++i)
+        addParameter(parameters[i], nextParameterIndex--);
 
     preserveLastVar();
 
@@ -435,7 +434,7 @@ BytecodeGenerator::BytecodeGenerator(EvalNode* evalNode, ScopeChainNode* scopeCh
     , m_symbolTable(symbolTable)
     , m_scopeNode(evalNode)
     , m_codeBlock(codeBlock)
-    , m_thisRegister(RegisterFile::ProgramCodeThisRegister)
+    , m_thisRegister(CallFrame::thisArgumentOffset())
     , m_finallyDepth(0)
     , m_dynamicScopeDepth(0)
     , m_baseScopeDepth(codeBlock->baseScopeDepth())
@@ -1801,9 +1800,9 @@ RegisterID* BytecodeGenerator::emitCall(OpcodeID opcodeID, RegisterID* dst, Regi
         emitMove(callArguments.profileHookRegister(), func);
 
     // Generate code for arguments.
-    unsigned argumentIndex = 0;
+    unsigned argument = 0;
     for (ArgumentListNode* n = callArguments.argumentsNode()->m_listNode; n; n = n->m_next)
-        emitNode(callArguments.argumentRegister(argumentIndex++), n);
+        emitNode(callArguments.argumentRegister(argument++), n);
 
     // Reserve space for call frame.
     Vector<RefPtr<RegisterID>, RegisterFile::CallFrameHeaderSize> callFrame;
@@ -1820,7 +1819,7 @@ RegisterID* BytecodeGenerator::emitCall(OpcodeID opcodeID, RegisterID* dst, Regi
     // Emit call.
     emitOpcode(opcodeID);
     instructions().append(func->index()); // func
-    instructions().append(callArguments.count()); // argCount
+    instructions().append(callArguments.argumentCountIncludingThis()); // argCount
     instructions().append(callArguments.registerOffset()); // registerOffset
     if (dst != ignoredResult()) {
         emitOpcode(op_call_put_result);
@@ -1900,10 +1899,10 @@ RegisterID* BytecodeGenerator::emitConstruct(RegisterID* dst, RegisterID* func, 
         emitMove(callArguments.profileHookRegister(), func);
 
     // Generate code for arguments.
-    unsigned argumentIndex = 0;
+    unsigned argument = 0;
     if (ArgumentsNode* argumentsNode = callArguments.argumentsNode()) {
         for (ArgumentListNode* n = argumentsNode->m_listNode; n; n = n->m_next)
-            emitNode(callArguments.argumentRegister(argumentIndex++), n);
+            emitNode(callArguments.argumentRegister(argument++), n);
     }
 
     if (m_shouldEmitProfileHooks) {
@@ -1920,7 +1919,7 @@ RegisterID* BytecodeGenerator::emitConstruct(RegisterID* dst, RegisterID* func, 
 
     emitOpcode(op_construct);
     instructions().append(func->index()); // func
-    instructions().append(callArguments.count()); // argCount
+    instructions().append(callArguments.argumentCountIncludingThis()); // argCount
     instructions().append(callArguments.registerOffset()); // registerOffset
     if (dst != ignoredResult()) {
         emitOpcode(op_call_put_result);
@@ -2362,7 +2361,7 @@ bool BytecodeGenerator::isArgumentNumber(const Identifier& ident, int argumentNu
     RegisterID* registerID = registerFor(ident);
     if (!registerID || registerID->index() >= 0)
          return 0;
-    return registerID->index() - m_thisRegister.index() - 1 == argumentNumber;
+    return registerID->index() == CallFrame::argumentOffset(argumentNumber);
 }
 
 } // namespace JSC

third_party/WebKit/Source/JavaScriptCore/bytecompiler/BytecodeGenerator.h
@@ -55,8 +55,8 @@ namespace JSC {
 
         RegisterID* thisRegister() { return m_argv[0].get(); }
         RegisterID* argumentRegister(unsigned i) { return m_argv[i + 1].get(); }
-        unsigned registerOffset() { return thisRegister()->index() + count() + RegisterFile::CallFrameHeaderSize; }
-        unsigned count() { return m_argv.size(); }
+        unsigned registerOffset() { return m_argv.last()->index() + CallFrame::offsetFor(argumentCountIncludingThis()); }
+        unsigned argumentCountIncludingThis() { return m_argv.size(); }
         RegisterID* profileHookRegister() { return m_profileHookRegister.get(); }
         ArgumentsNode* argumentsNode() { return m_argumentsNode; }
 

third_party/WebKit/Source/JavaScriptCore/bytecompiler/NodesCodegen.cpp
@@ -319,11 +319,17 @@ inline CallArguments::CallArguments(BytecodeGenerator& generator, ArgumentsNode*
     if (generator.shouldEmitProfileHooks())
         m_profileHookRegister = generator.newTemporary();
 
-    newArgument(generator); // 'this' register.
-    if (!argumentsNode)
-        return;
-    for (ArgumentListNode* n = argumentsNode->m_listNode; n; n = n->m_next)
-        newArgument(generator);
+    size_t argumentCountIncludingThis = 1; // 'this' register.
+    if (argumentsNode) {
+        for (ArgumentListNode* node = argumentsNode->m_listNode; node; node = node->m_next)
+            ++argumentCountIncludingThis;
+    }
+
+    m_argv.grow(argumentCountIncludingThis);
+    for (int i = argumentCountIncludingThis - 1; i >= 0; --i) {
+        m_argv[i] = generator.newTemporary();
+        ASSERT(static_cast<size_t>(i) == m_argv.size() - 1 || m_argv[i]->index() == m_argv[i + 1]->index() + 1);
+    }
 }
 
 inline void CallArguments::newArgument(BytecodeGenerator& generator)

commit 011b147117f58959609a4cecf2f8dfe89c65b42f

<rdar://problem/10577239> GC Crash introduced in r102545
Reviewed by Gavin Barraclough.
        
MarkedArgumentBuffer was still marking items in forwards order, even though
the argument order has been reversed.
        
I fixed this bug, and replaced address calculation code with some helper
functions -- mallocBase() and slotFor() -- so it stays fixed everywhere.

* runtime/ArgList.cpp:
(JSC::MarkedArgumentBuffer::markLists):
(JSC::MarkedArgumentBuffer::slowAppend):
* runtime/ArgList.h:
(JSC::MarkedArgumentBuffer::~MarkedArgumentBuffer):
(JSC::MarkedArgumentBuffer::at):
(JSC::MarkedArgumentBuffer::append):
(JSC::MarkedArgumentBuffer::last):
(JSC::MarkedArgumentBuffer::slotFor):
(JSC::MarkedArgumentBuffer::mallocBase):



git-svn-id: svn://svn.chromium.org/blink/trunk@102728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/JavaScriptCore/runtime/ArgList.cpp
@@ -46,7 +46,8 @@ void MarkedArgumentBuffer::markLists(HeapRootVisitor& heapRootVisitor, ListSet& 
     ListSet::iterator end = markSet.end();
     for (ListSet::iterator it = markSet.begin(); it != end; ++it) {
         MarkedArgumentBuffer* list = *it;
-        heapRootVisitor.visit(reinterpret_cast<JSValue*>(list->m_buffer), list->m_size);
+        for (int i = 0; i < list->m_size; ++i)
+            heapRootVisitor.visit(reinterpret_cast<JSValue*>(&list->slotFor(i)));
     }
 }
 
@@ -57,13 +58,13 @@ void MarkedArgumentBuffer::slowAppend(JSValue v)
     for (int i = 0; i < m_capacity; ++i)
         newBuffer[-i] = m_buffer[-i];
 
-    if (m_capacity != static_cast<int>(inlineCapacity))
-        delete [] &m_buffer[-(m_capacity - 1)];
+    if (EncodedJSValue* base = mallocBase())
+        delete [] base;
 
     m_buffer = newBuffer;
     m_capacity = newCapacity;
 
-    m_buffer[-m_size] = JSValue::encode(v);
+    slotFor(m_size) = JSValue::encode(v);
     ++m_size;
 
     if (m_markSet)
@@ -75,7 +76,7 @@ void MarkedArgumentBuffer::slowAppend(JSValue v)
     // our Vector's inline capacity, though, our values move to the 
     // heap, where they do need explicit marking.
     for (int i = 0; i < m_size; ++i) {
-        Heap* heap = Heap::heap(JSValue::decode(m_buffer[-i]));
+        Heap* heap = Heap::heap(JSValue::decode(slotFor(i)));
         if (!heap)
             continue;
 

third_party/WebKit/Source/JavaScriptCore/runtime/ArgList.h
@@ -58,8 +58,8 @@ namespace JSC {
             if (m_markSet)
                 m_markSet->remove(this);
 
-            if (m_capacity != static_cast<int>(inlineCapacity))
-                delete [] &m_buffer[-(m_capacity - 1)];
+            if (EncodedJSValue* base = mallocBase())
+                delete [] base;
         }
 
         size_t size() const { return m_size; }
@@ -70,7 +70,7 @@ namespace JSC {
             if (i >= m_size)
                 return jsUndefined();
 
-            return JSValue::decode(m_buffer[-i]);
+            return JSValue::decode(slotFor(i));
         }
 
         void clear()
@@ -83,7 +83,7 @@ namespace JSC {
             if (m_size >= m_capacity)
                 return slowAppend(v);
 
-            m_buffer[-m_size] = JSValue::encode(v);
+            slotFor(m_size) = JSValue::encode(v);
             ++m_size;
         }
 
@@ -96,13 +96,25 @@ namespace JSC {
         JSValue last() 
         {
             ASSERT(m_size);
-            return JSValue::decode(m_buffer[-(m_size - 1)]);
+            return JSValue::decode(slotFor(m_size - 1));
         }
         
         static void markLists(HeapRootVisitor&, ListSet&);
 
     private:
         void slowAppend(JSValue);
+        
+        EncodedJSValue& slotFor(int item) const
+        {
+            return m_buffer[-item];
+        }
+        
+        EncodedJSValue* mallocBase()
+        {
+            if (m_capacity == static_cast<int>(inlineCapacity))
+                return 0;
+            return &slotFor(m_capacity - 1);
+        }
         
         int m_size;
         int m_capacity;