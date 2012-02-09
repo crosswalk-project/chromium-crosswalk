crosswalk-project/chromium-crosswalk

commit f24eb563838d0194483148d62ee6eb56bdbcff0c

Unreviewed. Fixed typo in the following files. [TreatUndefinedAs] => [TreatAsUndefined].
* bindings/scripts/CodeGeneratorJS.pm:
(GenerateParametersCheck):

* bindings/scripts/test/TestObj.idl:

* bindings/scripts/test/CPP/WebDOMTestObj.cpp: Updated run-bindings-tests results.
(WebDOMTestObj::methodWithOptionalIsUndefinedString):
* bindings/scripts/test/CPP/WebDOMTestObj.h:
* bindings/scripts/test/GObject/WebKitDOMTestObj.cpp:
(webkit_dom_test_obj_method_with_optional_is_undefined_string):
* bindings/scripts/test/GObject/WebKitDOMTestObj.h:
* bindings/scripts/test/JS/JSTestNamedConstructor.cpp:
(WebCore::JSTestNamedConstructorNamedConstructor::constructJSTestNamedConstructor):
* bindings/scripts/test/JS/JSTestObj.cpp:
(WebCore):
(WebCore::jsTestObjPrototypeFunctionMethodWithOptionalIsUndefinedString):
(WebCore::jsTestObjPrototypeFunctionMethodWithOptionalIsNullStringString):
(WebCore::jsTestObjPrototypeFunctionConvert5):
* bindings/scripts/test/JS/JSTestObj.h:
(WebCore):
* bindings/scripts/test/ObjC/DOMTestObj.h:
* bindings/scripts/test/ObjC/DOMTestObj.mm:
(-[DOMTestObj methodWithOptionalIsUndefinedString:]):
* bindings/scripts/test/V8/V8TestObj.cpp:
(WebCore::TestObjInternal::methodWithOptionalIsUndefinedStringCallback):
(WebCore):

git-svn-id: svn://svn.chromium.org/blink/trunk@107191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -2496,7 +2496,7 @@ sub GenerateParametersCheck
             }
 
             my $parameterMissingPolicy = "MissingIsUndefinedValue";
-            if ($parameter->extendedAttributes->{"TreatAsUndefined"} and $parameter->extendedAttributes->{"TreatAsUndefined"} eq "NullString") {
+            if ($parameter->extendedAttributes->{"TreatUndefinedAs"} and $parameter->extendedAttributes->{"TreatUndefinedAs"} eq "NullString") {
                 $parameterMissingPolicy = "MissingIsNullValue";
             }
 

third_party/WebKit/Source/WebCore/bindings/scripts/test/CPP/WebDOMTestObj.cpp
@@ -951,12 +951,12 @@ void WebDOMTestObj::methodWithOptionalString(const WebDOMString& str)
     impl()->methodWithOptionalString(str);
 }
 
-void WebDOMTestObj::methodWithOptionalIsTreatAsUndefinedString(const WebDOMString& str)
+void WebDOMTestObj::methodWithOptionalIsUndefinedString(const WebDOMString& str)
 {
     if (!impl())
         return;
 
-    impl()->methodWithOptionalIsTreatAsUndefinedString(str);
+    impl()->methodWithOptionalIsUndefinedString(str);
 }
 
 void WebDOMTestObj::methodWithOptionalIsNullStringString(const WebDOMString& str)

third_party/WebKit/Source/WebCore/bindings/scripts/test/CPP/WebDOMTestObj.h
@@ -187,7 +187,7 @@ public:
     void methodWithNonOptionalArgAndOptionalArg(int nonOpt, int opt);
     void methodWithNonOptionalArgAndTwoOptionalArgs(int nonOpt, int opt1, int opt2);
     void methodWithOptionalString(const WebDOMString& str);
-    void methodWithOptionalIsTreatAsUndefinedString(const WebDOMString& str);
+    void methodWithOptionalIsUndefinedString(const WebDOMString& str);
     void methodWithOptionalIsNullStringString(const WebDOMString& str);
     WebDOMString conditionalMethod1();
     void conditionalMethod2();

third_party/WebKit/Source/WebCore/bindings/scripts/test/GObject/WebKitDOMTestObj.cpp
@@ -346,14 +346,14 @@ webkit_dom_test_obj_method_with_optional_string(WebKitDOMTestObj* self, const gc
 }
 
 void
-webkit_dom_test_obj_method_with_optional_is_treat_as_undefined_string(WebKitDOMTestObj* self, const gchar* str)
+webkit_dom_test_obj_method_with_optional_is_undefined_string(WebKitDOMTestObj* self, const gchar* str)
 {
     g_return_if_fail(self);
     WebCore::JSMainThreadNullState state;
     WebCore::TestObj * item = WebKit::core(self);
     g_return_if_fail(str);
     WTF::String converted_str = WTF::String::fromUTF8(str);
-    item->methodWithOptionalIsTreatAsUndefinedString(converted_str);
+    item->methodWithOptionalIsUndefinedString(converted_str);
 }
 
 void

third_party/WebKit/Source/WebCore/bindings/scripts/test/GObject/WebKitDOMTestObj.h
@@ -273,7 +273,7 @@ WEBKIT_API void
 webkit_dom_test_obj_method_with_optional_string(WebKitDOMTestObj* self, const gchar* str);
 
 /**
- * webkit_dom_test_obj_method_with_optional_is_treat_as_undefined_string:
+ * webkit_dom_test_obj_method_with_optional_is_undefined_string:
  * @self: A #WebKitDOMTestObj
  * @str: A #gchar
  *
@@ -281,7 +281,7 @@ webkit_dom_test_obj_method_with_optional_string(WebKitDOMTestObj* self, const gc
  *
 **/
 WEBKIT_API void
-webkit_dom_test_obj_method_with_optional_is_treat_as_undefined_string(WebKitDOMTestObj* self, const gchar* str);
+webkit_dom_test_obj_method_with_optional_is_undefined_string(WebKitDOMTestObj* self, const gchar* str);
 
 /**
  * webkit_dom_test_obj_method_with_optional_is_null_string_string:

third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestNamedConstructor.cpp
@@ -103,7 +103,7 @@ EncodedJSValue JSC_HOST_CALL JSTestNamedConstructorNamedConstructor::constructJS
     const String& str2(ustringToString(MAYBE_MISSING_PARAMETER(exec, 1, MissingIsUndefinedValue).isEmpty() ? UString() : MAYBE_MISSING_PARAMETER(exec, 1, MissingIsUndefinedValue).toString(exec)->value(exec)));
     if (exec->hadException())
         return JSValue::encode(jsUndefined());
-    const String& str3(ustringToString(MAYBE_MISSING_PARAMETER(exec, 2, MissingIsUndefinedValue).isEmpty() ? UString() : MAYBE_MISSING_PARAMETER(exec, 2, MissingIsUndefinedValue).toString(exec)->value(exec)));
+    const String& str3(ustringToString(MAYBE_MISSING_PARAMETER(exec, 2, MissingIsNullValue).isEmpty() ? UString() : MAYBE_MISSING_PARAMETER(exec, 2, MissingIsNullValue).toString(exec)->value(exec)));
     if (exec->hadException())
         return JSValue::encode(jsUndefined());
     RefPtr<TestNamedConstructor> object = TestNamedConstructor::createForJSConstructor(jsConstructor->document(), str1, str2, str3, ec);

third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestObj.cpp
@@ -270,7 +270,7 @@ static const HashTableValue JSTestObjPrototypeTableValues[] =
     { "methodWithNonOptionalArgAndOptionalArg", DontDelete | JSC::Function, (intptr_t)static_cast<NativeFunction>(jsTestObjPrototypeFunctionMethodWithNonOptionalArgAndOptionalArg), (intptr_t)2, NoIntrinsic },
     { "methodWithNonOptionalArgAndTwoOptionalArgs", DontDelete | JSC::Function, (intptr_t)static_cast<NativeFunction>(jsTestObjPrototypeFunctionMethodWithNonOptionalArgAndTwoOptionalArgs), (intptr_t)3, NoIntrinsic },
     { "methodWithOptionalString", DontDelete | JSC::Function, (intptr_t)static_cast<NativeFunction>(jsTestObjPrototypeFunctionMethodWithOptionalString), (intptr_t)1, NoIntrinsic },
-    { "methodWithOptionalIsTreatAsUndefinedString", DontDelete | JSC::Function, (intptr_t)static_cast<NativeFunction>(jsTestObjPrototypeFunctionMethodWithOptionalIsTreatAsUndefinedString), (intptr_t)1, NoIntrinsic },
+    { "methodWithOptionalIsUndefinedString", DontDelete | JSC::Function, (intptr_t)static_cast<NativeFunction>(jsTestObjPrototypeFunctionMethodWithOptionalIsUndefinedString), (intptr_t)1, NoIntrinsic },
     { "methodWithOptionalIsNullStringString", DontDelete | JSC::Function, (intptr_t)static_cast<NativeFunction>(jsTestObjPrototypeFunctionMethodWithOptionalIsNullStringString), (intptr_t)1, NoIntrinsic },
     { "methodWithCallbackArg", DontDelete | JSC::Function, (intptr_t)static_cast<NativeFunction>(jsTestObjPrototypeFunctionMethodWithCallbackArg), (intptr_t)1, NoIntrinsic },
     { "methodWithNonCallbackArgAndCallbackArg", DontDelete | JSC::Function, (intptr_t)static_cast<NativeFunction>(jsTestObjPrototypeFunctionMethodWithNonCallbackArgAndCallbackArg), (intptr_t)2, NoIntrinsic },
@@ -298,7 +298,7 @@ static const HashTableValue JSTestObjPrototypeTableValues[] =
     { 0, 0, 0, 0, NoIntrinsic }
 };
 
-static const HashTable JSTestObjPrototypeTable = { 265, 255, JSTestObjPrototypeTableValues, 0 };
+static const HashTable JSTestObjPrototypeTable = { 266, 255, JSTestObjPrototypeTableValues, 0 };
 const ClassInfo JSTestObjPrototype::s_info = { "TestObjPrototype", &Base::s_info, &JSTestObjPrototypeTable, 0, CREATE_METHOD_TABLE(JSTestObjPrototype) };
 
 JSObject* JSTestObjPrototype::self(ExecState* exec, JSGlobalObject* globalObject)
@@ -1629,7 +1629,7 @@ EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithOptionalString(
     return JSValue::encode(jsUndefined());
 }
 
-EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithOptionalIsTreatAsUndefinedString(ExecState* exec)
+EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithOptionalIsUndefinedString(ExecState* exec)
 {
     JSValue thisValue = exec->hostThisValue();
     if (!thisValue.inherits(&JSTestObj::s_info))
@@ -1640,7 +1640,7 @@ EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithOptionalIsTreat
     const String& str(ustringToString(MAYBE_MISSING_PARAMETER(exec, 0, MissingIsUndefinedValue).isEmpty() ? UString() : MAYBE_MISSING_PARAMETER(exec, 0, MissingIsUndefinedValue).toString(exec)->value(exec)));
     if (exec->hadException())
         return JSValue::encode(jsUndefined());
-    impl->methodWithOptionalIsTreatAsUndefinedString(str);
+    impl->methodWithOptionalIsUndefinedString(str);
     return JSValue::encode(jsUndefined());
 }
 
@@ -1652,7 +1652,7 @@ EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithOptionalIsNullS
     JSTestObj* castedThis = static_cast<JSTestObj*>(asObject(thisValue));
     ASSERT_GC_OBJECT_INHERITS(castedThis, &JSTestObj::s_info);
     TestObj* impl = static_cast<TestObj*>(castedThis->impl());
-    const String& str(ustringToString(MAYBE_MISSING_PARAMETER(exec, 0, MissingIsUndefinedValue).isEmpty() ? UString() : MAYBE_MISSING_PARAMETER(exec, 0, MissingIsUndefinedValue).toString(exec)->value(exec)));
+    const String& str(ustringToString(MAYBE_MISSING_PARAMETER(exec, 0, MissingIsNullValue).isEmpty() ? UString() : MAYBE_MISSING_PARAMETER(exec, 0, MissingIsNullValue).toString(exec)->value(exec)));
     if (exec->hadException())
         return JSValue::encode(jsUndefined());
     impl->methodWithOptionalIsNullStringString(str);
@@ -2080,7 +2080,7 @@ EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionConvert5(ExecState* exec)
     TestObj* impl = static_cast<TestObj*>(castedThis->impl());
     if (exec->argumentCount() < 1)
         return throwVMError(exec, createTypeError(exec, "Not enough arguments"));
-    e* (toe(MAYBE_MISSING_PARAMETER(exec, 0, MissingIsUndefinedValue)));
+    e* (toe(MAYBE_MISSING_PARAMETER(exec, 0, MissingIsNullValue)));
     if (exec->hadException())
         return JSValue::encode(jsUndefined());
     impl->convert5();

third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestObj.h
@@ -175,7 +175,7 @@ JSC::EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithOptionalAr
 JSC::EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithNonOptionalArgAndOptionalArg(JSC::ExecState*);
 JSC::EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithNonOptionalArgAndTwoOptionalArgs(JSC::ExecState*);
 JSC::EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithOptionalString(JSC::ExecState*);
-JSC::EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithOptionalIsTreatAsUndefinedString(JSC::ExecState*);
+JSC::EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithOptionalIsUndefinedString(JSC::ExecState*);
 JSC::EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithOptionalIsNullStringString(JSC::ExecState*);
 JSC::EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithCallbackArg(JSC::ExecState*);
 JSC::EncodedJSValue JSC_HOST_CALL jsTestObjPrototypeFunctionMethodWithNonCallbackArgAndCallbackArg(JSC::ExecState*);

commit 4198f745a9bf614e656328ea80e64130e33983fa

Unreviewed. Fix build failure caused by r107191.
* bindings/scripts/CodeGeneratorJS.pm:
(GenerateParametersCheck):
* bindings/scripts/CodeGeneratorV8.pm:
(GenerateParametersCheck):

git-svn-id: svn://svn.chromium.org/blink/trunk@107199 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -2496,7 +2496,7 @@ sub GenerateParametersCheck
             }
 
             my $parameterMissingPolicy = "MissingIsUndefinedValue";
-            if ($parameter->extendedAttributes->{"TreatUndefinedAs"} and $parameter->extendedAttributes->{"TreatUndefinedAs"} eq "NullString") {
+            if ($parameter->extendedAttributes->{"Optional"} and $parameter->extendedAttributes->{"Optional"} eq "TreatAsUndefined" and $parameter->extendedAttributes->{"TreatUndefinedAs"} and $parameter->extendedAttributes->{"TreatUndefinedAs"} eq "NullString") {
                 $parameterMissingPolicy = "MissingIsNullValue";
             }
 

third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -1537,7 +1537,7 @@ sub GenerateParametersCheck
         }
 
         my $parameterMissingPolicy = "MissingIsUndefinedValue";
-        if ($parameter->extendedAttributes->{"TreatUndefinedAs"} and $parameter->extendedAttributes->{"TreatUndefinedAs"} eq "NullString") {
+        if ($parameter->extendedAttributes->{"Optional"} and $parameter->extendedAttributes->{"Optional"} eq "TreatAsUndefined" and $parameter->extendedAttributes->{"TreatUndefinedAs"} and $parameter->extendedAttributes->{"TreatUndefinedAs"} eq "NullString") {
             $parameterMissingPolicy = "MissingIsNullValue";
         }
 