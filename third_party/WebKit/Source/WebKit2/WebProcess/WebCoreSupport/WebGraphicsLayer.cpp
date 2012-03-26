crosswalk-project/chromium-crosswalk

commit 9e4f5c4f029196d96d58a87e201f7ac4eb968000

[Qt] WebGraphicsLayer: Untie the layer updates and tile updates. https://bugs.webkit.org/show_bug.cgi?id=81771
Reviewed by Kenneth Rohde Christiansen.

The DidRenderFrame message needs to be sent for every UpdateTileForLayer
message, but this currently has to be triggered by m_shouldSyncFrame which
is only set after sending a SyncCompositingLayerState message.
This patch makes sure that tile updates won't trigger sending the whole
layer info if it didn't change, and send the DidRenderFrame message directly.

It also makes sure that the layer info is sent before any tile update so that
the layer can be created on the UI process before any tile update is handled.

* UIProcess/WebLayerTreeRenderer.cpp:
(WebKit::WebLayerTreeRenderer::getBackingStore):
* WebProcess/WebCoreSupport/WebGraphicsLayer.cpp:
(WebCore::WebGraphicsLayer::syncCompositingStateForThisLayerOnly):
(WebCore::WebGraphicsLayer::tiledBackingStorePaint):
(WebCore::WebGraphicsLayer::createTile):
(WebCore::WebGraphicsLayer::updateTile):
(WebCore::WebGraphicsLayer::removeTile):
* WebProcess/WebPage/qt/LayerTreeHostQt.cpp:
(WebKit::LayerTreeHostQt::createTile):
(WebKit::LayerTreeHostQt::updateTile):
(WebKit::LayerTreeHostQt::removeTile):

git-svn-id: svn://svn.chromium.org/blink/trunk@111567 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebGraphicsLayer.cpp
@@ -430,35 +430,37 @@ WebGraphicsLayer* toWebGraphicsLayer(GraphicsLayer* layer)
 
 void WebGraphicsLayer::syncCompositingStateForThisLayerOnly()
 {
+
+    if (m_modified) {
+        computeTransformedVisibleRect();
+
+        m_layerInfo.name = name();
+        m_layerInfo.anchorPoint = anchorPoint();
+        m_layerInfo.backfaceVisible = backfaceVisibility();
+        m_layerInfo.childrenTransform = childrenTransform();
+        m_layerInfo.contentsOpaque = contentsOpaque();
+        m_layerInfo.contentsRect = contentsRect();
+        m_layerInfo.drawsContent = drawsContent();
+        m_layerInfo.mask = toWebLayerID(maskLayer());
+        m_layerInfo.masksToBounds = masksToBounds();
+        m_layerInfo.opacity = opacity();
+        m_layerInfo.parent = toWebLayerID(parent());
+        m_layerInfo.pos = position();
+        m_layerInfo.preserves3D = preserves3D();
+        m_layerInfo.replica = toWebLayerID(replicaLayer());
+        m_layerInfo.size = size();
+        m_layerInfo.transform = transform();
+        m_contentNeedsDisplay = false;
+        m_layerInfo.children.clear();
+
+        for (size_t i = 0; i < children().size(); ++i)
+            m_layerInfo.children.append(toWebLayerID(children()[i]));
+
+        m_webGraphicsLayerClient->didSyncCompositingStateForLayer(m_layerInfo);
+    }
+
     updateContentBuffers();
 
-    if (!m_modified)
-        return;
-
-    computeTransformedVisibleRect();
-    m_layerInfo.name = name();
-    m_layerInfo.anchorPoint = anchorPoint();
-    m_layerInfo.backfaceVisible = backfaceVisibility();
-    m_layerInfo.childrenTransform = childrenTransform();
-    m_layerInfo.contentsOpaque = contentsOpaque();
-    m_layerInfo.contentsRect = contentsRect();
-    m_layerInfo.drawsContent = drawsContent();
-    m_layerInfo.mask = toWebLayerID(maskLayer());
-    m_layerInfo.masksToBounds = masksToBounds();
-    m_layerInfo.opacity = opacity();
-    m_layerInfo.parent = toWebLayerID(parent());
-    m_layerInfo.pos = position();
-    m_layerInfo.preserves3D = preserves3D();
-    m_layerInfo.replica = toWebLayerID(replicaLayer());
-    m_layerInfo.size = size();
-    m_layerInfo.transform = transform();
-    m_contentNeedsDisplay = false;
-    m_layerInfo.children.clear();
-
-    for (size_t i = 0; i < children().size(); ++i)
-        m_layerInfo.children.append(toWebLayerID(children()[i]));
-
-    m_webGraphicsLayerClient->didSyncCompositingStateForLayer(m_layerInfo);
     m_modified = false;
     m_layerInfo.imageIsUpdated = false;
     if (m_hasPendingAnimations)
@@ -514,7 +516,6 @@ void WebGraphicsLayer::tiledBackingStorePaint(GraphicsContext* context, const In
 {
     if (rect.isEmpty())
         return;
-    m_modified = true;
     paintGraphicsLayerContents(*context, rect);
 }
 
@@ -558,19 +559,16 @@ Color WebGraphicsLayer::tiledBackingStoreBackgroundColor() const
 }
 void WebGraphicsLayer::createTile(int tileID, const UpdateInfo& updateInfo)
 {
-    m_modified = true;
     m_webGraphicsLayerClient->createTile(id(), tileID, updateInfo);
 }
 
 void WebGraphicsLayer::updateTile(int tileID, const UpdateInfo& updateInfo)
 {
-    m_modified = true;
     m_webGraphicsLayerClient->updateTile(id(), tileID, updateInfo);
 }
 
 void WebGraphicsLayer::removeTile(int tileID)
 {
-    m_modified = true;
     m_webGraphicsLayerClient->removeTile(id(), tileID);
 }
 

third_party/WebKit/Source/WebKit2/UIProcess/WebLayerTreeRenderer.cpp
@@ -274,8 +274,8 @@ void WebLayerTreeRenderer::setRootLayerID(WebLayerID layerID)
 
 PassRefPtr<LayerBackingStore> WebLayerTreeRenderer::getBackingStore(WebLayerID id)
 {
-    ensureLayer(id);
     TextureMapperLayer* layer = toTextureMapperLayer(layerByID(id));
+    ASSERT(layer);
     RefPtr<LayerBackingStore> backingStore = static_cast<LayerBackingStore*>(layer->backingStore().get());
     if (!backingStore) {
         backingStore = LayerBackingStore::create();

third_party/WebKit/Source/WebKit2/WebProcess/WebPage/qt/LayerTreeHostQt.cpp
@@ -400,16 +400,19 @@ bool LayerTreeHost::supportsAcceleratedCompositing()
 
 void LayerTreeHostQt::createTile(WebLayerID layerID, int tileID, const UpdateInfo& updateInfo)
 {
+    m_shouldSyncFrame = true;
     m_webPage->send(Messages::LayerTreeHostProxy::CreateTileForLayer(layerID, tileID, updateInfo));
 }
 
 void LayerTreeHostQt::updateTile(WebLayerID layerID, int tileID, const UpdateInfo& updateInfo)
 {
+    m_shouldSyncFrame = true;
     m_webPage->send(Messages::LayerTreeHostProxy::UpdateTileForLayer(layerID, tileID, updateInfo));
 }
 
 void LayerTreeHostQt::removeTile(WebLayerID layerID, int tileID)
 {
+    m_shouldSyncFrame = true;
     m_webPage->send(Messages::LayerTreeHostProxy::RemoveTileForLayer(layerID, tileID));
 }
 

commit 0e039480c728507ab32096af2057d3a212544af0

[Qt] Make sure that purged directly composited images are re-created before a layer's sync. https://bugs.webkit.org/show_bug.cgi?id=81771
Reviewed by Noam Rosenthal.

This fixes a regression in tst_qquickwebview introduced by r111567.

* WebProcess/WebCoreSupport/WebGraphicsLayer.cpp:
(WebCore::WebGraphicsLayer::syncCompositingStateForThisLayerOnly):
(WebCore::WebGraphicsLayer::updateContentBuffers):

git-svn-id: svn://svn.chromium.org/blink/trunk@112096 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebGraphicsLayer.cpp
@@ -430,6 +430,13 @@ WebGraphicsLayer* toWebGraphicsLayer(GraphicsLayer* layer)
 
 void WebGraphicsLayer::syncCompositingStateForThisLayerOnly()
 {
+    // The remote image might have been released by purgeBackingStores.
+    if (m_image) {
+        if (!m_layerInfo.imageBackingStoreID) {
+            m_layerInfo.imageBackingStoreID = m_webGraphicsLayerClient->adoptImageBackingStore(m_image.get());
+            m_layerInfo.imageIsUpdated = true;
+        }
+    }
 
     if (m_modified) {
         computeTransformedVisibleRect();
@@ -574,14 +581,6 @@ void WebGraphicsLayer::removeTile(int tileID)
 
 void WebGraphicsLayer::updateContentBuffers()
 {
-    // The remote image might have been released by purgeBackingStores.
-    if (m_image) {
-        if (!m_layerInfo.imageBackingStoreID) {
-            m_layerInfo.imageBackingStoreID = m_webGraphicsLayerClient->adoptImageBackingStore(m_image.get());
-            m_layerInfo.imageIsUpdated = true;
-        }
-    }
-
     if (!drawsContent()) {
         m_mainBackingStore.clear();
         m_previousBackingStore.clear();