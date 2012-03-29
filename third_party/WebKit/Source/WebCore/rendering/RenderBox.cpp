crosswalk-project/chromium-crosswalk

commit af37df67528dfe7e97f7fa20e65bf64a868a3852

Lazily allocate overflow: hidden layers if we have overflowing content https://bugs.webkit.org/show_bug.cgi?id=75568
Reviewed by David Hyatt.

Source/WebCore:

Change covered by the existing tests and the tons of rebaselines.

This change makes us lazily allocate our RenderLayer for overflow: hidden layers only.

Apart from saving some memory, it will also speed up the rendering as we don't need to
go through the layer's machinery when painting and hit testing.

On http://dglazkov.github.com/performance-tests/biggrid.html benchmark, this puts the
overflow: hidden case in par with the overflow: visible case when scrolling that is a
very-smooth scrolling vs a jerky one currently (mostly due to the painting speedup).

* rendering/RenderBlock.cpp:
(WebCore::RenderBlock::updateScrollInfoAfterLayout):
Changed this method to update our size cache if needed.

* rendering/RenderBlock.cpp:
(WebCore::RenderBlock::layoutBlock):
(WebCore::RenderBlock::paint):
(WebCore::RenderBlock::isPointInOverflowControl):
* rendering/RenderBlock.h:
(RenderBlock):
* rendering/RenderBox.cpp:
(WebCore::RenderBox::willBeDestroyed):
(WebCore::RenderBox::styleDidChange):
(WebCore::RenderBox::layout):
(WebCore::RenderBox::scrollWidth):
(WebCore::RenderBox::scrollHeight):
(WebCore::RenderBox::scrollLeft):
(WebCore::RenderBox::scrollTop):
(WebCore::RenderBox::setScrollLeft):
(WebCore::RenderBox::setScrollTop):
(WebCore::RenderBox::includeVerticalScrollbarSize):
(WebCore::RenderBox::includeHorizontalScrollbarSize):
(WebCore::RenderBox::scrolledContentOffset):
(WebCore::RenderBox::pushContentsClip):
(WebCore::RenderBox::popContentsClip):
(WebCore::RenderBox::addLayoutOverflow):
Added layer() check to the previous call sites.

(WebCore::cachedSizeForOverflowClipMap):
(WebCore::RenderBox::cachedSizeForOverflowClip):
(WebCore::RenderBox::updateCachedSizeForOverflowClip):
(WebCore::RenderBox::clearCachedSizeForOverflowClip):
This logic stores the size information for later repainting.

third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
@@ -263,6 +263,9 @@ void RenderBox::willBeDestroyed()
     // value during laying out. It causes a use-after-free bug.
     ASSERT(!RenderBlock::hasPercentHeightDescendant(this));
 
+    if (hasOverflowClip() && everHadLayout() && !hasLayer())
+        clearCachedSizeForOverflowClip();
+
     RenderBoxModelObject::willBeDestroyed();
 }
 
@@ -360,7 +363,7 @@ void RenderBox::styleDidChange(StyleDifference diff, const RenderStyle* oldStyle
 
     // If our zoom factor changes and we have a defined scrollLeft/Top, we need to adjust that value into the
     // new zoomed coordinate space.
-    if (hasOverflowClip() && oldStyle && newStyle && oldStyle->effectiveZoom() != newStyle->effectiveZoom()) {
+    if (hasOverflowClipWithLayer() && oldStyle && newStyle && oldStyle->effectiveZoom() != newStyle->effectiveZoom()) {
         if (int left = layer()->scrollXOffset()) {
             left = (left / oldStyle->effectiveZoom()) * newStyle->effectiveZoom();
             layer()->scrollToXOffset(left);
@@ -463,6 +466,10 @@ void RenderBox::layout()
         child = child->nextSibling();
     }
     statePusher.pop();
+
+    if (hasOverflowClip() && !hasLayer())
+        updateCachedSizeForOverflowClip();
+
     setNeedsLayout(false);
 }
 
@@ -490,7 +497,7 @@ int RenderBox::pixelSnappedClientHeight() const
 
 int RenderBox::scrollWidth() const
 {
-    if (hasOverflowClip())
+    if (hasOverflowClipWithLayer())
         return layer()->scrollWidth();
     // For objects with visible overflow, this matches IE.
     // FIXME: Need to work right with writing modes.
@@ -501,7 +508,7 @@ int RenderBox::scrollWidth() const
 
 int RenderBox::scrollHeight() const
 {
-    if (hasOverflowClip())
+    if (hasOverflowClipWithLayer())
         return layer()->scrollHeight();
     // For objects with visible overflow, this matches IE.
     // FIXME: Need to work right with writing modes.
@@ -510,23 +517,23 @@ int RenderBox::scrollHeight() const
 
 int RenderBox::scrollLeft() const
 {
-    return hasOverflowClip() ? layer()->scrollXOffset() : 0;
+    return hasOverflowClipWithLayer() ? layer()->scrollXOffset() : 0;
 }
 
 int RenderBox::scrollTop() const
 {
-    return hasOverflowClip() ? layer()->scrollYOffset() : 0;
+    return hasOverflowClipWithLayer() ? layer()->scrollYOffset() : 0;
 }
 
 void RenderBox::setScrollLeft(int newLeft)
 {
-    if (hasOverflowClip())
+    if (hasOverflowClipWithLayer())
         layer()->scrollToXOffset(newLeft, RenderLayer::ScrollOffsetClamped);
 }
 
 void RenderBox::setScrollTop(int newTop)
 {
-    if (hasOverflowClip())
+    if (hasOverflowClipWithLayer())
         layer()->scrollToYOffset(newTop, RenderLayer::ScrollOffsetClamped);
 }
 
@@ -651,13 +658,13 @@ bool RenderBox::fixedElementLaysOutRelativeToFrame(Frame* frame, FrameView* fram
 
 bool RenderBox::includeVerticalScrollbarSize() const
 {
-    return hasOverflowClip() && !layer()->hasOverlayScrollbars()
+    return hasOverflowClipWithLayer() && !layer()->hasOverlayScrollbars()
         && (style()->overflowY() == OSCROLL || style()->overflowY() == OAUTO);
 }
 
 bool RenderBox::includeHorizontalScrollbarSize() const
 {
-    return hasOverflowClip() && !layer()->hasOverlayScrollbars()
+    return hasOverflowClipWithLayer() && !layer()->hasOverlayScrollbars()
         && (style()->overflowX() == OSCROLL || style()->overflowX() == OAUTO);
 }
 
@@ -749,16 +756,56 @@ bool RenderBox::needsPreferredWidthsRecalculation() const
 IntSize RenderBox::scrolledContentOffset() const
 {
     ASSERT(hasOverflowClip());
-    ASSERT(hasLayer());
-    return layer()->scrolledContentOffset();
+
+    if (hasLayer())
+        return layer()->scrolledContentOffset();
+
+    // If we have no layer, it means that we have no overflowing content as we lazily
+    // allocate it on demand. Thus we don't have any scroll offset.
+    ASSERT(!requiresLayerForOverflowClip());
+    return LayoutSize();
+}
+
+typedef HashMap<const RenderBox*, LayoutSize> RendererSizeCache;
+static RendererSizeCache& cachedSizeForOverflowClipMap()
+{
+    DEFINE_STATIC_LOCAL(RendererSizeCache, cachedSizeForOverflowClipMap, ());
+    return cachedSizeForOverflowClipMap;
 }
 
 IntSize RenderBox::cachedSizeForOverflowClip() const
 {
     ASSERT(hasOverflowClip());
-    ASSERT(hasLayer());
+    if (hasLayer())
+        return layer()->size();
 
-    return layer()->size();
+    ASSERT(!requiresLayerForOverflowClip());
+    RendererSizeCache::iterator it = cachedSizeForOverflowClipMap().find(this);
+    if (it == cachedSizeForOverflowClipMap().end())
+        return LayoutSize();
+
+    return it->second;
+}
+
+void RenderBox::updateCachedSizeForOverflowClip()
+{
+    ASSERT(hasOverflowClip());
+    ASSERT(!requiresLayerForOverflowClip());
+    ASSERT(!hasLayer());
+
+    cachedSizeForOverflowClipMap().set(this, size());
+}
+
+void RenderBox::clearCachedSizeForOverflowClip()
+{
+    ASSERT(hasOverflowClip());
+    ASSERT(!requiresLayerForOverflowClip());
+    ASSERT(!hasLayer());
+
+    // FIXME: We really would like to enable this ASSERT. However the current updateScrollInfoAfterLayout
+    // is not bullet-proof and it triggers in non-obvious ways under NRWT.
+    // ASSERT(cachedSizeForOverflowClipMap().contains(this));
+    cachedSizeForOverflowClipMap().remove(this);
 }
 
 LayoutUnit RenderBox::minPreferredLogicalWidth() const
@@ -1190,7 +1237,7 @@ bool RenderBox::pushContentsClip(PaintInfo& paintInfo, const LayoutPoint& accumu
         return false;
         
     bool isControlClip = hasControlClip();
-    bool isOverflowClip = hasOverflowClip() && !layer()->isSelfPaintingLayer();
+    bool isOverflowClip = hasOverflowClip() && !hasSelfPaintingLayer();
     
     if (!isControlClip && !isOverflowClip)
         return false;
@@ -1212,7 +1259,7 @@ bool RenderBox::pushContentsClip(PaintInfo& paintInfo, const LayoutPoint& accumu
 
 void RenderBox::popContentsClip(PaintInfo& paintInfo, PaintPhase originalPhase, const LayoutPoint& accumulatedOffset)
 {
-    ASSERT(hasControlClip() || (hasOverflowClip() && !layer()->isSelfPaintingLayer()));
+    ASSERT(hasControlClip() || (hasOverflowClip() && !hasSelfPaintingLayer()));
 
     paintInfo.context->restore();
     if (originalPhase == PaintPhaseOutline) {
@@ -3686,6 +3733,10 @@ void RenderBox::addLayoutOverflow(const LayoutRect& rect)
     if (!m_overflow)
         m_overflow = adoptPtr(new RenderOverflow(clientBox, borderBoxRect()));
     
+    // Lazily allocate our layer as we will need it to hold our scroll information.
+    if (hasOverflowClip())
+        ensureLayer();
+
     m_overflow->addLayoutOverflow(overflowRect);
 }
 

commit 0f77caa5fb54a7886b03ac403ccdd4eabf3d29fc

REGRESSION (r110065-r110080): Content drawing outside overflow: hidden at ynet.co.il https://bugs.webkit.org/show_bug.cgi?id=82129
Reviewed by Ojan Vafai.

Source/WebCore:

Tests: fast/overflow/before-after-overflow-hidden-horizontal-writing-mode-tb-expected.html
       fast/overflow/before-after-overflow-hidden-horizontal-writing-mode-tb.html
       fast/overflow/before-after-overflow-hidden-vertical-writing-mode-rl-expected.html
       fast/overflow/before-after-overflow-hidden-vertical-writing-mode-rl.html
       fast/overflow/start-end-overflow-hidden-horizontal-writing-mode-tb-expected.html
       fast/overflow/start-end-overflow-hidden-horizontal-writing-mode-tb.html
       fast/overflow/start-end-overflow-hidden-vertical-writing-mode-rl-expected.html
       fast/overflow/start-end-overflow-hidden-vertical-writing-mode-rl.html

This is a regression from r110072. I wrongly thought we should call ensureLayer if we create our RenderOverflow.
However the current overflow code removes the before and start overflows (like in horizontal writing mode with ltr direction,
we never have a top or a left overflow). Because of that we would not get a RenderLayer as expected and the overflow clip rects
would be wrong on our RenderLayer children.

* rendering/RenderBox.cpp:
(WebCore::RenderBox::addLayoutOverflow):
Moved the ensureLayer() call after the check that we do have some overflow but before we remove the overflow in some directions.

LayoutTests:

* fast/overflow/before-after-overflow-hidden-horizontal-writing-mode-tb-expected.html: Added.
* fast/overflow/before-after-overflow-hidden-horizontal-writing-mode-tb.html: Added.
* fast/overflow/before-after-overflow-hidden-vertical-writing-mode-rl-expected.html: Added.
* fast/overflow/before-after-overflow-hidden-vertical-writing-mode-rl.html: Added.
* fast/overflow/start-end-overflow-hidden-horizontal-writing-mode-tb-expected.html: Added.
* fast/overflow/start-end-overflow-hidden-horizontal-writing-mode-tb.html: Added.

third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
@@ -3724,6 +3724,12 @@ void RenderBox::addLayoutOverflow(const LayoutRect& rect)
     if (clientBox.contains(rect) || rect.isEmpty())
         return;
     
+    // Lazily allocate our layer as we will need it to hold our scroll information
+    // and for the clipping logic to work properly. Note that we *do* need a layer
+    // if we have some left overflow on an horizontal writing mode with ltr direction.
+    if (hasOverflowClip())
+        ensureLayer();
+
     // For overflow clip objects, we don't want to propagate overflow into unreachable areas.
     LayoutRect overflowRect(rect);
     if (hasOverflowClip() || isRenderView()) {
@@ -3758,10 +3764,6 @@ void RenderBox::addLayoutOverflow(const LayoutRect& rect)
     if (!m_overflow)
         m_overflow = adoptPtr(new RenderOverflow(clientBox, borderBoxRect()));
     
-    // Lazily allocate our layer as we will need it to hold our scroll information.
-    if (hasOverflowClip())
-        ensureLayer();
-
     m_overflow->addLayoutOverflow(overflowRect);
 }
 

third_party/WebKit/LayoutTests/fast/overflow/before-after-overflow-hidden-horizontal-writing-mode-tb-expected.html
@@ -1,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<style>
+    div {
+        height: 100px;
+        width: 100px;
+    }
+
+    div#wrapper {
+        margin-left: 100px;
+        margin-top: 100px;
+        background-color: green;
+        overflow: hidden;
+    }
+
+    div#afterOverflow {
+        background-color: red;
+        position: relative;
+        top: 100px;
+    }
+</style>
+<body>
+<a href="https://bugs.webkit.org/show_bug.cgi?id=82129">82129</a>: REGRESSION (r110065-r110080): Content drawing outside overflow: hidden at ynet.co.il</a>
+<p>Your shouldn't see any red on this page, just a green square.</p>
+<div id="wrapper">
+    <div id="afterOverflow"></div>
+</div>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/overflow/before-after-overflow-hidden-horizontal-writing-mode-tb.html
@@ -1,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<style>
+    div {
+        height: 100px;
+        width: 100px;
+    }
+
+    div#wrapper {
+        margin-left: 100px;
+        margin-top: 100px;
+        background-color: green;
+        overflow: hidden;
+    }
+
+    div#beforeOverflow {
+        background-color: red;
+        position: relative;
+        top: -100px;
+    }
+</style>
+<body>
+<a href="https://bugs.webkit.org/show_bug.cgi?id=82129">82129</a>: REGRESSION (r110065-r110080): Content drawing outside overflow: hidden at ynet.co.il</a>
+<p>Your shouldn't see any red on this page, just a green square.</p>
+<div id="wrapper">
+    <div id="beforeOverflow"></div>
+</div>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/overflow/before-after-overflow-hidden-vertical-writing-mode-rl-expected.html
@@ -1,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<style>
+    div {
+        height: 100px;
+        width: 100px;
+    }
+
+    div#wrapper {
+        -webkit-writing-mode: vertical-lr;
+        margin-left: 100px;
+        margin-top: 100px;
+        background-color: green;
+        overflow: hidden;
+    }
+
+    div#afterOverflow {
+        background-color: red;
+        position: relative;
+        left: 100px;
+    }
+</style>
+<body>
+<a href="https://bugs.webkit.org/show_bug.cgi?id=82129">82129</a>: REGRESSION (r110065-r110080): Content drawing outside overflow: hidden at ynet.co.il</a>
+<p>Your shouldn't see any red on this page, just a green square.</p>
+<div id="wrapper">
+    <div id="afterOverflow"></div>
+</div>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/overflow/before-after-overflow-hidden-vertical-writing-mode-rl.html
@@ -1,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<style>
+    div {
+        height: 100px;
+        width: 100px;
+    }
+
+    div#wrapper {
+        -webkit-writing-mode: vertical-lr;
+        margin-left: 100px;
+        margin-top: 100px;
+        background-color: green;
+        overflow: hidden;
+    }
+
+    div#beforeOverflow {
+        background-color: red;
+        position: relative;
+        left: -100px;
+    }
+</style>
+<body>
+<a href="https://bugs.webkit.org/show_bug.cgi?id=82129">82129</a>: REGRESSION (r110065-r110080): Content drawing outside overflow: hidden at ynet.co.il</a>
+<p>Your shouldn't see any red on this page, just a green square.</p>
+<div id="wrapper">
+    <div id="beforeOverflow"></div>
+</div>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/overflow/start-end-overflow-hidden-horizontal-writing-mode-tb-expected.html
@@ -1,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<style>
+    div {
+        height: 100px;
+        width: 100px;
+    }
+
+    div#wrapper {
+        margin-left: 100px;
+        margin-top: 100px;
+        background-color: green;
+        overflow: hidden;
+    }
+
+    div#endOverflow {
+        background-color: red;
+        position: relative;
+        left: 100px;
+    }
+</style>
+<body>
+<a href="https://bugs.webkit.org/show_bug.cgi?id=82129">82129</a>: REGRESSION (r110065-r110080): Content drawing outside overflow: hidden at ynet.co.il</a>
+<p>Your shouldn't see any red on this page, just a green square.</p>
+<div id="wrapper">
+    <div id="endOverflow"></div>
+</div>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/overflow/start-end-overflow-hidden-horizontal-writing-mode-tb.html
@@ -1,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<style>
+    div {
+        height: 100px;
+        width: 100px;
+    }
+
+    div#wrapper {
+        margin-left: 100px;
+        margin-top: 100px;
+        background-color: green;
+        overflow: hidden;
+    }
+
+    div#startOverflow {
+        background-color: red;
+        position: relative;
+        left: -100px;
+    }
+</style>
+<body>
+<a href="https://bugs.webkit.org/show_bug.cgi?id=82129">82129</a>: REGRESSION (r110065-r110080): Content drawing outside overflow: hidden at ynet.co.il</a>
+<p>Your shouldn't see any red on this page, just a green square.</p>
+<div id="wrapper">
+    <div id="startOverflow"></div>
+</div>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/overflow/start-end-overflow-hidden-vertical-writing-mode-rl-expected.html
@@ -1,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<style>
+    div {
+        height: 100px;
+        width: 100px;
+    }
+
+    div#wrapper {
+        -webkit-writing-mode: vertical-lr;
+        margin-left: 100px;
+        margin-top: 100px;
+        background-color: green;
+        overflow: hidden;
+    }
+
+    div#endOverflow {
+        background-color: red;
+        position: relative;
+        top: 100px;
+    }
+</style>
+<body>
+<a href="https://bugs.webkit.org/show_bug.cgi?id=82129">82129</a>: REGRESSION (r110065-r110080): Content drawing outside overflow: hidden at ynet.co.il</a>
+<p>Your shouldn't see any red on this page, just a green square.</p>
+<div id="wrapper">
+    <div id="endOverflow"></div>
+</div>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/overflow/start-end-overflow-hidden-vertical-writing-mode-rl.html
@@ -1,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<style>
+    div {
+        height: 100px;
+        width: 100px;
+    }
+
+    div#wrapper {
+        -webkit-writing-mode: vertical-lr;
+        margin-left: 100px;
+        margin-top: 100px;
+        background-color: green;
+        overflow: hidden;
+    }
+
+    div#startOverflow {
+        background-color: red;
+        position: relative;
+        top: -100px;
+    }
+</style>
+<body>
+<a href="https://bugs.webkit.org/show_bug.cgi?id=82129">82129</a>: REGRESSION (r110065-r110080): Content drawing outside overflow: hidden at ynet.co.il</a>
+<p>Your shouldn't see any red on this page, just a green square.</p>
+<div id="wrapper">
+    <div id="startOverflow"></div>
+</div>
+</body>
+</html>

third_party/WebKit/LayoutTests/platform/chromium-linux/fast/box-shadow/shadow-buffer-partial-expected.txt
@@ -4,5 +4,6 @@ layer at (0,0) size 800x192
   RenderBlock {HTML} at (0,0) size 800x192
     RenderBody {BODY} at (8,30) size 784x132
       RenderBlock {DIV} at (30,0) size 200x50
-      RenderBlock {DIV} at (30,80) size 202x52 [border: (1px solid #000000)]
-        RenderBlock {DIV} at (1,-39) size 202x32 [border: (1px solid #0000FF)]
+layer at (38,110) size 202x52 clip at (39,111) size 200x50
+  RenderBlock {DIV} at (30,80) size 202x52 [border: (1px solid #000000)]
+    RenderBlock {DIV} at (1,-39) size 202x32 [border: (1px solid #0000FF)]

third_party/WebKit/LayoutTests/platform/chromium-win/fast/block/lineboxcontain/block-font-expected.txt
@@ -7,17 +7,18 @@ layer at (0,0) size 800x210
         RenderText {#text} at (0,0) size 471x19
           text run at (0,0) width 248: "You should see a green rectangle below. "
           text run at (248,0) width 223: "If you see any red, the test has failed."
-      RenderBlock {DIV} at (0,20) size 384x128 [bgcolor=#FF0000]
-        RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
-          RenderText {#text} at (0,0) size 320x64
-            text run at (0,0) width 320: "xxxxx"
-        RenderInline {SPAN} at (0,0) size 64x128 [bgcolor=#008000]
-        RenderBR {BR} at (384,0) size 0x64
-        RenderBlock {DIV} at (0,0) size 64x128 [bgcolor=#008000]
-        RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
-          RenderText {#text} at (64,64) size 320x64
-            text run at (64,64) width 320: "xxxxx"
-        RenderText {#text} at (0,0) size 0x0
       RenderBlock {P} at (0,164) size 784x22 [border: (1px solid #000000)]
         RenderText {#text} at (1,1) size 172x19
           text run at (1,1) width 172: "This line should look normal."
+layer at (8,28) size 384x128
+  RenderBlock {DIV} at (0,20) size 384x128 [bgcolor=#FF0000]
+    RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
+      RenderText {#text} at (0,0) size 320x64
+        text run at (0,0) width 320: "xxxxx"
+    RenderInline {SPAN} at (0,0) size 64x128 [bgcolor=#008000]
+    RenderBR {BR} at (384,0) size 0x64
+    RenderBlock {DIV} at (0,0) size 64x128 [bgcolor=#008000]
+    RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
+      RenderText {#text} at (64,64) size 320x64
+        text run at (64,64) width 320: "xxxxx"
+    RenderText {#text} at (0,0) size 0x0

third_party/WebKit/LayoutTests/platform/chromium-win/fast/block/lineboxcontain/block-glyphs-expected.txt
@@ -7,17 +7,18 @@ layer at (0,0) size 800x210
         RenderText {#text} at (0,0) size 471x19
           text run at (0,0) width 248: "You should see a green rectangle below. "
           text run at (248,0) width 223: "If you see any red, the test has failed."
-      RenderBlock {DIV} at (0,20) size 384x128 [bgcolor=#FF0000]
-        RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
-          RenderText {#text} at (0,0) size 320x64
-            text run at (0,0) width 320: "xxxxx"
-        RenderInline {SPAN} at (0,0) size 64x128 [bgcolor=#008000]
-        RenderBR {BR} at (384,0) size 0x64
-        RenderBlock {DIV} at (0,0) size 64x128 [bgcolor=#008000]
-        RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
-          RenderText {#text} at (64,64) size 320x64
-            text run at (64,64) width 320: "xxxxx"
-        RenderText {#text} at (0,0) size 0x0
       RenderBlock {P} at (0,164) size 784x22 [border: (1px solid #000000)]
         RenderText {#text} at (1,1) size 172x19
           text run at (1,1) width 172: "This line should look normal."
+layer at (8,28) size 384x128
+  RenderBlock {DIV} at (0,20) size 384x128 [bgcolor=#FF0000]
+    RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
+      RenderText {#text} at (0,0) size 320x64
+        text run at (0,0) width 320: "xxxxx"
+    RenderInline {SPAN} at (0,0) size 64x128 [bgcolor=#008000]
+    RenderBR {BR} at (384,0) size 0x64
+    RenderBlock {DIV} at (0,0) size 64x128 [bgcolor=#008000]
+    RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
+      RenderText {#text} at (64,64) size 320x64
+        text run at (64,64) width 320: "xxxxx"
+    RenderText {#text} at (0,0) size 0x0

third_party/WebKit/LayoutTests/platform/chromium-win/fast/block/lineboxcontain/font-expected.txt
@@ -7,14 +7,15 @@ layer at (0,0) size 800x164
         RenderText {#text} at (0,0) size 471x19
           text run at (0,0) width 248: "You should see a green rectangle below. "
           text run at (248,0) width 223: "If you see any red, the test has failed."
-      RenderBlock {DIV} at (0,20) size 384x128 [bgcolor=#FF0000]
-        RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
-          RenderText {#text} at (0,0) size 320x64
-            text run at (0,0) width 320: "xxxxx"
-        RenderInline {SPAN} at (0,0) size 64x128 [bgcolor=#008000]
-        RenderBR {BR} at (384,0) size 0x64
-        RenderBlock {DIV} at (0,0) size 64x128 [bgcolor=#008000]
-        RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
-          RenderText {#text} at (64,64) size 320x64
-            text run at (64,64) width 320: "xxxxx"
-        RenderText {#text} at (0,0) size 0x0
+layer at (8,28) size 384x128
+  RenderBlock {DIV} at (0,20) size 384x128 [bgcolor=#FF0000]
+    RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
+      RenderText {#text} at (0,0) size 320x64
+        text run at (0,0) width 320: "xxxxx"
+    RenderInline {SPAN} at (0,0) size 64x128 [bgcolor=#008000]
+    RenderBR {BR} at (384,0) size 0x64
+    RenderBlock {DIV} at (0,0) size 64x128 [bgcolor=#008000]
+    RenderInline {SPAN} at (0,0) size 320x64 [color=#008000]
+      RenderText {#text} at (64,64) size 320x64
+        text run at (64,64) width 320: "xxxxx"
+    RenderText {#text} at (0,0) size 0x0

third_party/WebKit/LayoutTests/platform/efl/Skipped
@@ -2705,3 +2705,10 @@ fast/table/colspanMinWidth-vertical.html
 
 # Transparent image being produced
 fast/frames/transparent-scrollbar.html
+
+# This should only be a layer change.
+# http://webkit.org/b/82129
+fast/box-shadow/shadow-buffer-partial.html
+fast/block/lineboxcontain/block-font.html
+fast/block/lineboxcontain/block-glyphs.html
+fast/block/lineboxcontain/font.html

third_party/WebKit/LayoutTests/platform/gtk/Skipped
@@ -1644,3 +1644,10 @@ fast/table/colspanMinWidth-vertical.html
 # https://bugs.webkit.org/show_bug.cgi?id=82309
 http/tests/xmlhttprequest/basic-auth-default.html
 http/tests/xmlhttprequest/re-login-async.html
+
+# This should only be a layer change.
+# http://webkit.org/b/82129
+fast/box-shadow/shadow-buffer-partial.html
+fast/block/lineboxcontain/block-font.html
+fast/block/lineboxcontain/block-glyphs.html
+fast/block/lineboxcontain/font.html

third_party/WebKit/LayoutTests/platform/mac/Skipped
@@ -653,3 +653,10 @@ fast/layers/video-layer.html
 media/video-empty-source.html
 media/video-playing-and-pause.html
 media/controls-after-reload.html
+
+# This should only be a layer change.
+# http://webkit.org/b/82129
+fast/box-shadow/shadow-buffer-partial.html
+fast/block/lineboxcontain/block-font.html
+fast/block/lineboxcontain/block-glyphs.html
+fast/block/lineboxcontain/font.html

third_party/WebKit/LayoutTests/platform/qt/Skipped
@@ -2622,3 +2622,10 @@ fast/js/large-expressions.html
 # [Qt] css2.1/20110323/c543-txt-decor-000.html is failing
 # https://bugs.webkit.org/show_bug.cgi?id=82225
 css2.1/20110323/c543-txt-decor-000.html
+
+# This should only be a layer change.
+# http://webkit.org/b/82129
+fast/box-shadow/shadow-buffer-partial.html
+fast/block/lineboxcontain/block-font.html
+fast/block/lineboxcontain/block-glyphs.html
+fast/block/lineboxcontain/font.html

third_party/WebKit/LayoutTests/platform/win/Skipped
@@ -1878,3 +1878,10 @@ fast/table/cell-pref-width-invalidation.html
 
 # The inner <input> should now grow to take the full cell's height.
 fast/table/colspanMinWidth-vertical.html
+
+# This should only be a layer change.
+# http://webkit.org/b/82129
+fast/box-shadow/shadow-buffer-partial.html
+fast/block/lineboxcontain/block-font.html
+fast/block/lineboxcontain/block-glyphs.html
+fast/block/lineboxcontain/font.html

third_party/WebKit/LayoutTests/platform/wk2/Skipped
@@ -1108,6 +1108,13 @@ fast/events/wheelevent-direction-inverted-from-device.html
 # eventSender.clearKillRing() is unimplemented
 editing/pasteboard/emacs-cntl-y-001.html
 
+# This should only be a layer change.
+# http://webkit.org/b/82129
+fast/box-shadow/shadow-buffer-partial.html
+fast/block/lineboxcontain/block-font.html
+fast/block/lineboxcontain/block-glyphs.html
+fast/block/lineboxcontain/font.html
+
 ### END OF (2) Classified failures without bug reports (yet)
 ########################################
 