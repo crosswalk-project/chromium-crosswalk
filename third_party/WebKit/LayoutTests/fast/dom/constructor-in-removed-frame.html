crosswalk-project/chromium-crosswalk

commit 88875f3b5ee81872222485ddc1f13e2f5d8745bf

[V8] Add a per context data store and use that for caching boiler plates as well as constructor functions https://bugs.webkit.org/show_bug.cgi?id=83093
Reviewed by Adam Barth.

Source/WebCore:

This introduces a V8BindingPerContextData class. After creating a new v8::Context we also create a new
per context data instance. This instance maintains the boiler plate and constructor caches. V8DOMWrapper
now delegates the caching to this new class. Previously the caching was only done in V8DOMWindowShell which
made workers and isolated worlds behave slightly different.

Tests: fast/dom/constructor-proto.html
       fast/workers/constructor-proto.html

* WebCore.gypi:
* bindings/scripts/CodeGeneratorV8.pm:
(GenerateConstructorGetter): Renamed getConstructor to constructorForType.
* bindings/v8/V8BindingPerContextData.cpp: Added.
(WebCore):
(WebCore::V8BindingPerContextData::dispose): This clears the persistent handles stored in the maps.
(WebCore::V8BindingPerContextData::init): This installs the hidden prototype which is used as the
[[Prototype]] for the constructor functions (for HotMail compat).
(WebCore::V8BindingPerContextData::createWrapperFromCacheSlowCase): Instantiates a new object and
stores it in the boilerplate map, and returns a clone.
(WebCore::V8BindingPerContextData::constructorForTypeSlowCase): Creates the function for the constructor
and stores it in the constructor map.
* bindings/v8/V8BindingPerContextData.h: Added.
(WebCore):
(V8BindingPerContextData):
(WebCore::V8BindingPerContextData::create):
(WebCore::V8BindingPerContextData::~V8BindingPerContextData):
(WebCore::V8BindingPerContextData::createWrapperFromCache): Checks the cache and clones the wrapper in
the cache. If no boiler plate exists one is created and cached.
(WebCore::V8BindingPerContextData::constructorForType): If the constructor already exists the cached
constructor function is returned, otherwise a new constructor function is created and cached.

third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWrapper.cpp
@@ -41,6 +41,7 @@
 #include "StylePropertySet.h"
 #include "V8AbstractEventListener.h"
 #include "V8Binding.h"
+#include "V8BindingPerContextData.h"
 #include "V8Collection.h"
 #include "V8EventListener.h"
 #include "V8EventListenerList.h"
@@ -94,67 +95,37 @@ void V8DOMWrapper::setJSWrapperForDOMNode(Node* node, v8::Persistent<v8::Object>
         getDOMNodeMap().set(node, wrapper);
 }
 
-v8::Local<v8::Function> V8DOMWrapper::getConstructor(WrapperTypeInfo* type, v8::Handle<v8::Value> objectPrototype)
-{
-    // A DOM constructor is a function instance created from a DOM constructor
-    // template. There is one instance per context. A DOM constructor is
-    // different from a normal function in two ways:
-    //   1) it cannot be called as constructor (aka, used to create a DOM object)
-    //   2) its __proto__ points to Object.prototype rather than
-    //      Function.prototype.
-    // The reason for 2) is that, in Safari, a DOM constructor is a normal JS
-    // object, but not a function. Hotmail relies on the fact that, in Safari,
-    // HTMLElement.__proto__ == Object.prototype.
-    v8::Handle<v8::FunctionTemplate> functionTemplate = type->getTemplate();
-    // Getting the function might fail if we're running out of
-    // stack or memory.
-    v8::TryCatch tryCatch;
-    v8::Local<v8::Function> value = functionTemplate->GetFunction();
-    if (value.IsEmpty())
-        return v8::Local<v8::Function>();
-    // Hotmail fix, see comments above.
-    if (!objectPrototype.IsEmpty())
-        value->SetPrototype(objectPrototype);
-    return value;
-}
-
-v8::Local<v8::Function> V8DOMWrapper::getConstructorForContext(WrapperTypeInfo* type, v8::Handle<v8::Context> context)
-{
-    // Enter the scope for this context to get the correct constructor.
-    v8::Context::Scope scope(context);
-
-    return getConstructor(type, V8DOMWindowShell::getHiddenObjectPrototype(context));
-}
-
-v8::Local<v8::Function> V8DOMWrapper::getConstructor(WrapperTypeInfo* type, DOMWindow* window)
+v8::Local<v8::Function> V8DOMWrapper::constructorForType(WrapperTypeInfo* type, DOMWindow* window)
 {
     Frame* frame = window->frame();
     if (!frame)
         return v8::Local<v8::Function>();
-
-    v8::Handle<v8::Context> context = V8Proxy::context(frame);
-    if (context.IsEmpty())
-        return v8::Local<v8::Function>();
-
-    return getConstructorForContext(type, context);
+    return V8Proxy::retrievePerContextData(frame)->constructorForType(type);
 }
 
 #if ENABLE(WORKERS)
-v8::Local<v8::Function> V8DOMWrapper::getConstructor(WrapperTypeInfo* type, WorkerContext*)
+v8::Local<v8::Function> V8DOMWrapper::constructorForType(WrapperTypeInfo* type, WorkerContext*)
 {
     WorkerScriptController* controller = WorkerScriptController::controllerForContext();
     WorkerContextExecutionProxy* proxy = controller ? controller->proxy() : 0;
-    if (!proxy)
-        return v8::Local<v8::Function>();
-
-    v8::Handle<v8::Context> context = proxy->context();
-    if (context.IsEmpty())
-        return v8::Local<v8::Function>();
-
-    return getConstructorForContext(type, context);
+    return proxy ? proxy->perContextData()->constructorForType(type) : v8::Local<v8::Function>();
 }
 #endif
 
+V8BindingPerContextData* V8DOMWrapper::perContextData(V8Proxy* proxy)
+{
+    V8DOMWindowShell* shell = proxy->windowShell();
+    return shell ? shell->perContextData() : 0;
+}
+
+#if ENABLE(WORKERS)
+V8BindingPerContextData* V8DOMWrapper::perContextData(WorkerContext*)
+{
+    WorkerScriptController* controller = WorkerScriptController::controllerForContext();
+    WorkerContextExecutionProxy* proxy = controller ? controller->proxy() : 0;
+    return proxy ? proxy->perContextData() : 0;
+}
+#endif
 
 void V8DOMWrapper::setNamedHiddenReference(v8::Handle<v8::Object> parent, const char* name, v8::Handle<v8::Value> child)
 {
@@ -202,12 +173,10 @@ v8::Local<v8::Object> V8DOMWrapper::instantiateV8Object(V8Proxy* proxy, WrapperT
 #if ENABLE(WORKERS)
     WorkerContext* workerContext = 0;
 #endif
-    if (V8IsolatedContext::getEntered()) {
-        // This effectively disables the wrapper cache for isolated worlds.
-        proxy = 0;
-        // FIXME: Do we need a wrapper cache for the isolated world?  We should
-        //        see if the performance gains are worth while.
-        // We'll get one once we give the isolated context a proper window shell.
+    V8BindingPerContextData* contextData = 0;
+    V8IsolatedContext* isolatedContext;
+    if (UNLIKELY(!!(isolatedContext = V8IsolatedContext::getEntered()))) {
+        contextData = isolatedContext->perContextData();
     } else if (!proxy) {
         v8::Handle<v8::Context> context = v8::Context::GetCurrent();
         if (!context.IsEmpty()) {
@@ -225,19 +194,22 @@ v8::Local<v8::Object> V8DOMWrapper::instantiateV8Object(V8Proxy* proxy, WrapperT
     }
 
     v8::Local<v8::Object> instance;
-    if (proxy)
-        // FIXME: Fix this to work properly with isolated worlds (see above).
-        instance = proxy->windowShell()->createWrapperFromCache(type);
-    else {
-        v8::Local<v8::Function> function;
+    if (!contextData) {
+        if (proxy)
+            contextData = perContextData(proxy);
 #if ENABLE(WORKERS)
-        if (workerContext)
-            function = getConstructor(type, workerContext);
-        else
+        else if (workerContext)
+            contextData = perContextData(workerContext);
 #endif
-            function = type->getTemplate()->GetFunction();
+    }
+
+    if (contextData)
+        instance = contextData->createWrapperFromCache(type);
+    else {
+        v8::Local<v8::Function> function = type->getTemplate()->GetFunction();
         instance = SafeAllocation::newInstance(function);
     }
+
     if (!instance.IsEmpty()) {
         // Avoid setting the DOM wrapper for failed allocations.
         setDOMWrapper(instance, type, impl);
@@ -332,5 +304,6 @@ RefPtr<XPathNSResolver> V8DOMWrapper::getXPathNSResolver(v8::Handle<v8::Value> v
         resolver = V8CustomXPathNSResolver::create(value->ToObject());
     return resolver;
 }
+
 
 }  // namespace WebCore

third_party/WebKit/LayoutTests/fast/dom/constructor-proto-expected.txt
@@ -1,0 +1,11 @@
+This tests that setting the proto of a built in constructor is not reset
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS Object.getPrototypeOf(HTMLElement) is Object.prototype
+PASS Object.getPrototypeOf(HTMLElement) is proto
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

third_party/WebKit/LayoutTests/fast/dom/constructor-proto.html
@@ -1,0 +1,15 @@
+<!DOCTYPE html>
+<script src="../js/resources/js-test-pre.js"></script>
+<script>
+
+description('This tests that setting the proto of a built in constructor is not reset');
+
+// Hotmail depends on the [[Prototype]] of DOM constructors to be Object.prototype.
+shouldBe('Object.getPrototypeOf(HTMLElement)', 'Object.prototype');
+
+var proto = new Number(42);
+HTMLElement.__proto__ = proto;
+shouldBe('Object.getPrototypeOf(HTMLElement)', 'proto');
+
+</script>
+<script src="../js/resources/js-test-post.js"></script>

third_party/WebKit/LayoutTests/fast/workers/constructor-proto-expected.txt
@@ -1,0 +1,13 @@
+This tests that setting the proto of a built in constructor is not reset
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+proto = new Number(42): 42
+Object.getPrototypeOf(WorkerLocation): [object Object]
+WorkerLocation.__proto__ = proto: 42
+Object.getPrototypeOf(WorkerLocation): 42
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

third_party/WebKit/LayoutTests/fast/workers/constructor-proto.html
@@ -1,0 +1,25 @@
+<!DOCTYPE html>
+<script src="../js/resources/js-test-pre.js"></script>
+<script src="resources/worker-create-common.js"></script>
+<script>
+
+var jsTestIsAsync = true;
+
+description('This tests that setting the proto of a built in constructor is not reset');
+
+var worker = createWorker();
+worker.postMessage("eval proto = new Number(42)");
+worker.postMessage("eval Object.getPrototypeOf(WorkerLocation)");
+worker.postMessage("eval WorkerLocation.__proto__ = proto");
+worker.postMessage("eval Object.getPrototypeOf(WorkerLocation)");
+worker.postMessage("eval DONE");
+
+worker.onmessage = function(evt) {
+    if (!/DONE/.test(evt.data))
+        debug(evt.data.replace(new RegExp("/.*LayoutTests"), "<...>"));
+    else
+        finishJSTest();
+};
+
+</script>
+<script src="../js/resources/js-test-post.js"></script>

third_party/WebKit/Source/WebCore/WebCore.gypi
@@ -2132,6 +2132,8 @@
             'bindings/v8/V8AbstractEventListener.h',
             'bindings/v8/V8Binding.cpp',
             'bindings/v8/V8Binding.h',
+            'bindings/v8/V8BindingPerContextData.cpp',
+            'bindings/v8/V8BindingPerContextData.h',
             'bindings/v8/V8Collection.cpp',
             'bindings/v8/V8Collection.h',
             'bindings/v8/V8DOMMap.cpp',

third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -743,11 +743,11 @@ END
     // Get the proxy corresponding to the DOMWindow if possible to
     // make sure that the constructor function is constructed in the
     // context of the DOMWindow and not in the context of the caller.
-    return V8DOMWrapper::getConstructor(type, V8DOMWindow::toNative(info.Holder()));
+    return V8DOMWrapper::constructorForType(type, V8DOMWindow::toNative(info.Holder()));
 END
     } elsif ($dataNode->extendedAttributes->{"IsWorkerContext"}) {
         push(@implContentDecls, <<END);
-    return V8DOMWrapper::getConstructor(type, V8WorkerContext::toNative(info.Holder()));
+    return V8DOMWrapper::constructorForType(type, V8WorkerContext::toNative(info.Holder()));
 END
     } else {
         push(@implContentDecls, "    return v8::Handle<v8::Value>();");

third_party/WebKit/Source/WebCore/bindings/v8/V8BindingPerContextData.cpp
@@ -1,0 +1,112 @@
+/*
+ * Copyright (C) 2012 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "V8BindingPerContextData.h"
+
+#include "SafeAllocation.h"
+
+namespace WebCore {
+
+void V8BindingPerContextData::dispose()
+{
+    {
+        WrapperBoilerplateMap::iterator it = m_wrapperBoilerplates.begin();
+        for (; it != m_wrapperBoilerplates.end(); ++it) {
+            v8::Persistent<v8::Object> wrapper = it->second;
+            wrapper.Dispose();
+        }
+        m_wrapperBoilerplates.clear();
+    }
+
+    {
+        ConstructorMap::iterator it = m_constructorMap.begin();
+        for (; it != m_constructorMap.end(); ++it) {
+            v8::Persistent<v8::Function> wrapper = it->second;
+            wrapper.Dispose();
+        }
+        m_constructorMap.clear();
+    }
+}
+
+bool V8BindingPerContextData::init()
+{
+    ASSERT(m_objectPrototype.get().IsEmpty());
+
+    v8::Handle<v8::String> objectString = v8::String::New("Object");
+    v8::Handle<v8::String> prototypeString = v8::String::New("prototype");
+    if (objectString.IsEmpty() || prototypeString.IsEmpty())
+        return false;
+
+    v8::Handle<v8::Object> object = v8::Handle<v8::Object>::Cast(m_context->Global()->Get(objectString));
+    if (object.IsEmpty())
+        return false;
+    v8::Handle<v8::Value> objectPrototype = object->Get(prototypeString);
+    if (objectPrototype.IsEmpty())
+        return false;
+
+    m_objectPrototype.set(objectPrototype);
+    return true;
+}
+
+v8::Local<v8::Object> V8BindingPerContextData::createWrapperFromCacheSlowCase(WrapperTypeInfo* type)
+{
+    ASSERT(!m_objectPrototype.get().IsEmpty());
+
+    v8::Context::Scope scope(m_context);
+    v8::Local<v8::Function> function = constructorForType(type);
+    v8::Local<v8::Object> instance = SafeAllocation::newInstance(function);
+    if (!instance.IsEmpty()) {
+        m_wrapperBoilerplates.set(type, v8::Persistent<v8::Object>::New(instance));
+        return instance->Clone();
+    }
+    return v8::Local<v8::Object>();
+}
+
+v8::Local<v8::Function> V8BindingPerContextData::constructorForTypeSlowCase(WrapperTypeInfo* type)
+{
+    ASSERT(!m_objectPrototype.get().IsEmpty());
+
+    v8::Context::Scope scope(m_context);
+    v8::Handle<v8::FunctionTemplate> functionTemplate = type->getTemplate();
+    // Getting the function might fail if we're running out of stack or memory.
+    v8::TryCatch tryCatch;
+    v8::Local<v8::Function> function = functionTemplate->GetFunction();
+    if (function.IsEmpty())
+        return v8::Local<v8::Function>();
+
+    function->SetPrototype(m_objectPrototype.get());
+
+    m_constructorMap.set(type, v8::Persistent<v8::Function>::New(function));
+
+    return function;
+}
+
+} // namespace WebCore

commit 2dc4a0db9d49d8b3e567bbccaac2f21b0a28bef9

[V8] Fix issue with trying to access a constructor in a frame that has been removed https://bugs.webkit.org/show_bug.cgi?id=84640
Reviewed by Kentaro Hara.

Source/WebCore:

This regressed in r113250. Now we do what we did before and return undefined if the frame
does not have a context.

Test: fast/dom/constructor-in-removed-frame.html

* bindings/v8/V8DOMWrapper.cpp:
(WebCore::V8DOMWrapper::constructorForType):

LayoutTests:

* fast/dom/constructor-in-removed-frame-expected.txt: Added.
* fast/dom/constructor-in-removed-frame.html: Added.


git-svn-id: svn://svn.chromium.org/blink/trunk@114989 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWrapper.cpp
@@ -87,7 +87,11 @@ v8::Local<v8::Function> V8DOMWrapper::constructorForType(WrapperTypeInfo* type, 
     Frame* frame = window->frame();
     if (!frame)
         return v8::Local<v8::Function>();
-    return V8Proxy::retrievePerContextData(frame)->constructorForType(type);
+
+    if (V8BindingPerContextData* contextData = V8Proxy::retrievePerContextData(frame))
+        return contextData->constructorForType(type);
+
+    return v8::Local<v8::Function>();
 }
 
 #if ENABLE(WORKERS)

third_party/WebKit/LayoutTests/fast/dom/constructor-in-removed-frame-expected.txt
@@ -1,0 +1,1 @@
+PASS

third_party/WebKit/LayoutTests/fast/dom/constructor-in-removed-frame.html
@@ -1,0 +1,28 @@
+<!DOCTYPE html>
+<p>FAIL</p>
+<iframe src="data:text/html,FAIL"></iframe>
+<script>
+
+window.onload = function() {
+    if (window.layoutTestController) {
+        layoutTestController.waitUntilDone();
+        layoutTestController.dumpAsText();
+
+        var iframeElement = document.querySelector('iframe');
+        iframeElement.onload = function() {
+            var frame = window.frames[0];
+            iframeElement.parentNode.removeChild(iframeElement);
+
+            // The V8 bindings does not keep the frame constructor alive but at least it should not crash!
+            frame.Window;
+
+            document.body.textContent = 'PASS';
+            layoutTestController.notifyDone();
+        };
+        iframeElement.src = 'data:text/html,PASS';
+    }
+};
+
+</script>
+</body>
+</html>