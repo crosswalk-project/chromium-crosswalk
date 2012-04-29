crosswalk-project/chromium-crosswalk

commit 53028f7f5693e29db091cad3814f68725351189e

[v8] Fix memory leak in V8LazyEventListener https://bugs.webkit.org/show_bug.cgi?id=83057
Reviewed by Ojan Vafai.

Source/WebCore:

This also brings the V8 and JSC implementation closer. The timing when we first lookup
the form element is now same in JSC and V8 (but different from Mozilla).

This also clears the strings once the code has been parsed and the function created.

Tests: fast/dom/inline-event-attributes-moved.html
       fast/dom/inline-event-attributes-release.html

* bindings/v8/V8LazyEventListener.cpp:
(WebCore::V8LazyEventListener::V8LazyEventListener):
(WebCore::V8LazyEventListener::prepareListenerObject):
* bindings/v8/V8LazyEventListener.h:
(WebCore::V8LazyEventListener::create):
(V8LazyEventListener):

LayoutTests:

* fast/dom/inline-event-attributes-moved-expected.txt: Added.
* fast/dom/inline-event-attributes-moved.html: Added.
* fast/dom/inline-event-attributes-release-expected.txt: Added.
* fast/dom/inline-event-attributes-release.html: Added.

git-svn-id: svn://svn.chromium.org/blink/trunk@113086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/bindings/v8/V8LazyEventListener.cpp
@@ -50,18 +50,15 @@
 
 namespace WebCore {
 
-V8LazyEventListener::V8LazyEventListener(const AtomicString& functionName, const AtomicString& eventParameterName, const String& code, const String sourceURL, const TextPosition& position, PassRefPtr<Node> node, const WorldContextHandle& worldContext)
+V8LazyEventListener::V8LazyEventListener(const AtomicString& functionName, const AtomicString& eventParameterName, const String& code, const String sourceURL, const TextPosition& position, Node* node, const WorldContextHandle& worldContext)
     : V8AbstractEventListener(true, worldContext)
     , m_functionName(functionName)
     , m_eventParameterName(eventParameterName)
     , m_code(code)
     , m_sourceURL(sourceURL)
     , m_node(node)
-    , m_formElement(0)
     , m_position(position)
 {
-    if (m_node && m_node->isHTMLElement())
-        m_formElement = static_cast<HTMLElement*>(m_node.get())->form();
 }
 
 template<typename T>
@@ -165,12 +162,13 @@ void V8LazyEventListener::prepareListenerObject(ScriptExecutionContext* context)
     ASSERT(value->IsFunction());
     v8::Local<v8::Function> intermediateFunction = value.As<v8::Function>();
 
-    v8::Handle<v8::Object> nodeWrapper = toObjectWrapper<Node>(m_node.get());
-    v8::Handle<v8::Object> formWrapper = toObjectWrapper<HTMLFormElement>(m_formElement.get());
-    v8::Handle<v8::Object> documentWrapper = toObjectWrapper<Document>(m_node ? m_node->ownerDocument() : 0);
+    HTMLFormElement* formElement = 0;
+    if (m_node && m_node->isHTMLElement())
+        formElement = static_cast<HTMLElement*>(m_node)->form();
 
-    m_node.clear();
-    m_formElement.clear();
+    v8::Handle<v8::Object> nodeWrapper = toObjectWrapper<Node>(m_node);
+    v8::Handle<v8::Object> formWrapper = toObjectWrapper<HTMLFormElement>(formElement);
+    v8::Handle<v8::Object> documentWrapper = toObjectWrapper<Document>(m_node ? m_node->ownerDocument() : 0);
 
     v8::Handle<v8::Value> parameters[3] = { nodeWrapper, formWrapper, documentWrapper };
 
@@ -211,6 +209,12 @@ void V8LazyEventListener::prepareListenerObject(ScriptExecutionContext* context)
     }
 
     wrappedFunction->SetName(v8::String::New(fromWebCoreString(m_functionName), m_functionName.length()));
+
+    // Since we only parse once, there's no need to keep data used for parsing around anymore.
+    m_functionName = String();
+    m_code = String();
+    m_eventParameterName = String();
+    m_sourceURL = String();
 
     setListenerObject(wrappedFunction);
 }

third_party/WebKit/Source/WebCore/bindings/v8/V8LazyEventListener.h
@@ -48,7 +48,7 @@ namespace WebCore {
     // A V8LazyEventListener is either a HTML or SVG event handler.
     class V8LazyEventListener : public V8AbstractEventListener {
     public:
-        static PassRefPtr<V8LazyEventListener> create(const AtomicString& functionName, const AtomicString& eventParameterName, const String& code, const String& sourceURL, const TextPosition& position, PassRefPtr<Node> node, const WorldContextHandle& worldContext)
+        static PassRefPtr<V8LazyEventListener> create(const AtomicString& functionName, const AtomicString& eventParameterName, const String& code, const String& sourceURL, const TextPosition& position, Node* node, const WorldContextHandle& worldContext)
         {
             return adoptRef(new V8LazyEventListener(functionName, eventParameterName, code, sourceURL, position, node, worldContext));
         }
@@ -59,7 +59,7 @@ namespace WebCore {
         virtual void prepareListenerObject(ScriptExecutionContext*);
 
     private:
-        V8LazyEventListener(const AtomicString& functionName, const AtomicString& eventParameterName, const String& code, const String sourceURL, const TextPosition&, PassRefPtr<Node>, const WorldContextHandle&);
+        V8LazyEventListener(const AtomicString& functionName, const AtomicString& eventParameterName, const String& code, const String sourceURL, const TextPosition&, Node*, const WorldContextHandle&);
 
         virtual v8::Local<v8::Value> callListenerFunction(ScriptExecutionContext*, v8::Handle<v8::Value> jsEvent, Event*);
 
@@ -73,8 +73,7 @@ namespace WebCore {
         AtomicString m_eventParameterName;
         String m_code;
         String m_sourceURL;
-        RefPtr<Node> m_node;
-        RefPtr<HTMLFormElement> m_formElement;
+        Node* m_node;
         TextPosition m_position;
     };
 

third_party/WebKit/LayoutTests/fast/dom/inline-event-attributes-moved-expected.txt
@@ -1,0 +1,10 @@
+Tests that we have the expected form in scope
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS expected is "undefined"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

third_party/WebKit/LayoutTests/fast/dom/inline-event-attributes-moved.html
@@ -1,0 +1,25 @@
+<!DOCTYPE html>
+<script src="../js/resources/js-test-pre.js"></script>
+<script>
+
+description('Tests that we have the expected form in scope');
+
+function dispatchClick(element)
+{
+    var clickEvent = document.createEvent('MouseEvent');
+    clickEvent.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    element.dispatchEvent(clickEvent);
+}
+
+var expected;
+var f = document.createElement('form');
+var i = f.appendChild(document.createElement('input'));
+i.setAttribute('onclick', 'expected = typeof action');
+f.removeChild(i);
+f = null;
+gc();
+dispatchClick(i);
+shouldBeEqualToString('expected', 'undefined');
+
+</script>
+<script src="../js/resources/js-test-post.js"></script>

third_party/WebKit/LayoutTests/fast/dom/inline-event-attributes-release-expected.txt
@@ -1,0 +1,10 @@
+Tests that we do not hold on to any nodes
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS afterCount - beforeCount is 0
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

third_party/WebKit/LayoutTests/fast/dom/inline-event-attributes-release.html
@@ -1,0 +1,28 @@
+<!DOCTYPE html>
+<script src="../js/resources/js-test-pre.js"></script>
+<script>
+
+description('Tests that we do not hold on to any nodes');
+
+gc();
+
+function numberOfLiveNodes() {
+    return window.internals && window.internals.numberOfLiveNodes && window.internals.numberOfLiveNodes();
+}
+
+var beforeCount = numberOfLiveNodes();
+
+var f = document.createElement('form');
+var i = f.appendChild(document.createElement('input'));
+i.setAttribute('onclick', '');
+f.removeChild(i);
+f = null;
+i = null;
+gc();
+
+var afterCount = numberOfLiveNodes();
+
+shouldBe('afterCount - beforeCount', '0');
+
+</script>
+<script src="../js/resources/js-test-post.js"></script>

commit 00d0b7ab6f65acfcf6ddef13caeaab7ca9e651dd

REGRESSION(r113086): onresize event handler can be deleted in popup window https://bugs.webkit.org/show_bug.cgi?id=84908
Reviewed by Ojan Vafai.

In a nutshell, an onresize event handler in the popup window
can be non-deterministically deleted. For more details, please
look at Chromium issue 123642:
http://code.google.com/p/chromium/issues/detail?id=123642

I confirmed that this bug is the regression caused by r113086.

r113086 introduced the following code:

void V8LazyEventListener::prepareListenerObject(...) {
    if (hasExistingListenerObject())
        return;
    ...;
    // Since we only parse once, there's no need to keep data
    // used for parsing around anymore.
    m_functionName = String();
    m_code = String();
    m_eventParameterName = String();
    m_sourceURL = String();

    setListenerObject(wrappedFunction);
}

This is not correct. The parsing can be done more than once,
and thus we cannot clear data. This patch removes the above code.

Consider the following situation:

(1) Assume '<body onresize="f()"></body>'.
(2) prepareListenerObject() runs.
(3) Since this is the first parsing, hasExistingListenerObject()
returns false. After the parsing, the listener object is set
by setListenerObject().
(4) GC runs. Since there is no strong reference to the listener
object, weakEventListenerCallback() is called back, and the listener
object is disposed.
(5) A resize event is triggered.
(6) prepareListenerObject() is called again. Since the listener object
is already disposed, hasExistingListenerObject() returns false,
and the second parsing starts.

In my investigation, the above situation is happening in the reported
Chromium bug. Anyway, I am sure that potentially the parsing can be
done more than once, and thus we must keep m_xxxx data.

However, this is just a temporary fix. We should fix the code so that
an alive event listener object is never reclaimed.
See https://bugs.webkit.org/show_bug.cgi?id=85152 for more details.

third_party/WebKit/Source/WebCore/bindings/v8/V8LazyEventListener.cpp
@@ -59,6 +59,9 @@ V8LazyEventListener::V8LazyEventListener(const AtomicString& functionName, const
     , m_sourceURL(sourceURL)
     , m_node(node)
     , m_position(position)
+#ifndef NDEBUG
+    , m_prepared(false)
+#endif
 {
 }
 
@@ -216,11 +219,20 @@ void V8LazyEventListener::prepareListenerObject(ScriptExecutionContext* context)
 
     wrappedFunction->SetName(v8::String::New(fromWebCoreString(m_functionName), m_functionName.length()));
 
-    // Since we only parse once, there's no need to keep data used for parsing around anymore.
-    m_functionName = String();
-    m_code = String();
-    m_eventParameterName = String();
-    m_sourceURL = String();
+    // FIXME: Remove m_prepared and the following comment-outs.
+    // See https://bugs.webkit.org/show_bug.cgi?id=85152 for more details.
+#ifndef NDEBUG
+    // Checks if the second parsing never happens. Currently the second parsing can happen
+    // in a popup window.
+    ASSERT(!m_prepared);
+    m_prepared = true;
+#endif
+    // Comments out the following code since the second parsing can happen.
+    // // Since we only parse once, there's no need to keep data used for parsing around anymore.
+    // m_functionName = String();
+    // m_code = String();
+    // m_eventParameterName = String();
+    // m_sourceURL = String();
 
     setListenerObject(wrappedFunction);
 }

third_party/WebKit/Source/WebCore/bindings/v8/V8LazyEventListener.h
@@ -75,6 +75,9 @@ namespace WebCore {
         String m_sourceURL;
         Node* m_node;
         TextPosition m_position;
+#ifndef NDEBUG
+        bool m_prepared;
+#endif
     };
 
 } // namespace WebCore