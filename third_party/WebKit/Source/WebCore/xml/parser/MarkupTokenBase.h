crosswalk-project/chromium-crosswalk

commit 7fd621132eaf81ed0c0c95a3e2b7dddf132de753

Clear the external characters pointer of an AtomicHTMLToken before the raw token is cleared. https://bugs.webkit.org/show_bug.cgi?id=92056
Reviewed by Adam Barth.

AtomicHTMLToken keeps a pointer to the HTMLToken's buffer instead of copying the
characters for performance. Clear the external characters pointer before the raw token
is cleared to make sure that we won't have a dangling pointer.

No new tests - no functional changes.

* html/parser/HTMLTreeBuilder.cpp:
(WebCore::HTMLTreeBuilder::constructTreeFromToken):
* xml/parser/MarkupTokenBase.h:
(WebCore::AtomicMarkupTokenBase::clearExternalCharacters):
(AtomicMarkupTokenBase):

git-svn-id: svn://svn.chromium.org/blink/trunk@123536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/xml/parser/MarkupTokenBase.h
@@ -482,6 +482,12 @@ public:
         return m_doctypeData->m_systemIdentifier;
     }
 
+    void clearExternalCharacters()
+    {
+        ASSERT(m_type == Token::Type::Character);
+        m_externalCharacters = 0;
+    }
+
 protected:
     typename Token::Type::Type m_type;
 

third_party/WebKit/Source/WebCore/html/parser/HTMLTreeBuilder.cpp
@@ -451,6 +451,12 @@ void HTMLTreeBuilder::constructTreeFromToken(HTMLToken& rawToken)
 
     constructTreeFromAtomicToken(token.get());
 
+    // AtomicHTMLToken keeps a pointer to the HTMLToken's buffer instead
+    // of copying the characters for performance.
+    // Clear the external characters pointer before the raw token is cleared
+    // to make sure that we won't have a dangling pointer.
+    token->clearExternalCharacters();
+
     if (!rawToken.isUninitialized()) {
         ASSERT(rawToken.type() == HTMLTokenTypes::Character);
         rawToken.clear();

commit 1254f32afbe32732b55becbee58069572d109c68

Remove an invalid assertion introduced in r123536. Unreviewed.
* xml/parser/MarkupTokenBase.h:
(WebCore::AtomicMarkupTokenBase::clearExternalCharacters):

git-svn-id: svn://svn.chromium.org/blink/trunk@123542 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/xml/parser/MarkupTokenBase.h
@@ -484,7 +484,6 @@ public:
 
     void clearExternalCharacters()
     {
-        ASSERT(m_type == Token::Type::Character);
         m_externalCharacters = 0;
     }
 