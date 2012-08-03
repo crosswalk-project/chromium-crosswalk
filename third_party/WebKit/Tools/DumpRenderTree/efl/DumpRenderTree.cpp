crosswalk-project/chromium-crosswalk

commit 6a7e3e303fc1813963e62a6a0a13b700d8994588

All ports should support per test switching of pixel testing https://bugs.webkit.org/show_bug.cgi?id=92398
Reviewed by Dirk Pranke.

Teach all test drivers to accept a per test control of whether
to dump pixels. Drivers now accept a -p/--pixel-test argument
on the standart input that means that running the current
test as pixel test is allowed (even if the expected hash is
missing). Removed the --pixel-tests command line option since
there is no need for it anymore.

* DumpRenderTree/DumpRenderTree.gyp/DumpRenderTree.gyp:
* DumpRenderTree/DumpRenderTree.gypi:
* DumpRenderTree/DumpRenderTree.h:
(TestCommand::TestCommand):
(TestCommand):
* DumpRenderTree/DumpRenderTree.xcodeproj/project.pbxproj:
* DumpRenderTree/DumpRenderTreeCommon.cpp: Added.
(CommandTokenizer):
(CommandTokenizer::CommandTokenizer):
(CommandTokenizer::pump):
(CommandTokenizer::next):
(CommandTokenizer::hasNext):
(die):
(parseInputLine):
Common logic to parse the input line from the standard input (or from the command line
in standalone mode). Made it somewhat general so we can easily add more arguments if
there is a need.

* DumpRenderTree/chromium/DumpRenderTree.cpp:
(runTest):
(main):

* DumpRenderTree/chromium/TestRunner/AccessibilityControllerChromium.cpp: Renamed from Tools/DumpRenderTree/chromium/TestRunner/AccessibilityController.cpp.
* DumpRenderTree/chromium/TestRunner/AccessibilityControllerChromium.h: Renamed from Tools/DumpRenderTree/chromium/TestRunner/AccessibilityController.h.
* DumpRenderTree/chromium/TestRunner/AccessibilityUIElementChromium.cpp: Renamed from Tools/DumpRenderTree/chromium/TestRunner/AccessibilityUIElement.cpp.
* DumpRenderTree/chromium/TestRunner/AccessibilityUIElementChromium.h: Renamed from Tools/DumpRenderTree/chromium/TestRunner/AccessibilityUIElement.h.
Renamed these files with a Chromium suffix because they were clashing with the common ones used by other ports after adding the root
DumpRenderTree directory to the include path.

third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTree.cpp
@@ -65,7 +65,7 @@ extern Ewk_History_Item* prevTestBFItem;
 RefPtr<LayoutTestController> gLayoutTestController;
 volatile bool done = false;
 
-static int dumpPixels = false;
+static bool dumpPixelsForCurrentTest;
 static int dumpTree = true;
 static int printSeparators = true;
 static int useX11Window = false;
@@ -193,7 +193,6 @@ static bool parseCommandLineOptions(int argc, char** argv)
 {
     static const option options[] = {
         {"notree", no_argument, &dumpTree, false},
-        {"pixel-tests", no_argument, &dumpPixels, true},
         {"tree", no_argument, &dumpTree, true},
         {"gui", no_argument, &useX11Window, true},
         {0, 0, 0, 0}
@@ -209,32 +208,6 @@ static bool parseCommandLineOptions(int argc, char** argv)
     }
 
     return true;
-}
-
-static String getFinalTestURL(const String& testURL)
-{
-    const size_t hashSeparatorPos = testURL.find("'");
-    if (hashSeparatorPos != notFound)
-        return getFinalTestURL(testURL.left(hashSeparatorPos));
-
-    // Convert the path into a full file URL if it does not look
-    // like an HTTP/S URL (doesn't start with http:// or https://).
-    if (!testURL.startsWith("http://") && !testURL.startsWith("https://")) {
-        char* cFilePath = ecore_file_realpath(testURL.utf8().data());
-        const String filePath = String::fromUTF8(cFilePath);
-        free(cFilePath);
-
-        if (ecore_file_exists(filePath.utf8().data()))
-            return String("file://") + filePath;
-    }
-
-    return testURL;
-}
-
-static String getExpectedPixelHash(const String& testURL)
-{
-    const size_t hashSeparatorPos = testURL.find("'");
-    return (hashSeparatorPos != notFound) ? testURL.substring(hashSeparatorPos + 1) : String();
 }
 
 static inline bool isGlobalHistoryTest(const String& cTestPathOrURL)
@@ -268,13 +241,13 @@ static void createLayoutTestController(const String& testURL, const String& expe
     }
 }
 
-static void runTest(const char* cTestPathOrURL)
+static void runTest(const char* inputLine)
 {
-    const String testPathOrURL = String::fromUTF8(cTestPathOrURL);
-    ASSERT(!testPathOrURL.isEmpty());
-
-    const String testURL = getFinalTestURL(testPathOrURL);
-    const String expectedPixelHash = getExpectedPixelHash(testPathOrURL);
+    TestCommand command = parseInputLine(inputLine);
+    const String testURL(command.pathOrURL.c_str());
+    ASSERT(!testURL.isEmpty());
+    dumpPixelsForCurrentTest = command.shouldDumpPixels;
+    const String expectedPixelHash(command.expectedPixelHash.c_str());
 
     browser->resetDefaultsToConsistentValues();
     createLayoutTestController(testURL, expectedPixelHash);
@@ -352,7 +325,7 @@ static bool shouldDumpFrameScrollPosition()
 
 static bool shouldDumpPixelsAndCompareWithExpected()
 {
-    return dumpPixels && gLayoutTestController->generatePixelResults() && !gLayoutTestController->dumpDOMAsWebArchive() && !gLayoutTestController->dumpSourceAsWebArchive();
+    return dumpPixelsForCurrentTest && gLayoutTestController->generatePixelResults() && !gLayoutTestController->dumpDOMAsWebArchive() && !gLayoutTestController->dumpSourceAsWebArchive();
 }
 
 static bool shouldDumpBackForwardList()
@@ -461,7 +434,7 @@ int main(int argc, char** argv)
         printSeparators = true;
         runTestingServerLoop();
     } else {
-        printSeparators = (optind < argc - 1 || (dumpPixels && dumpTree));
+        printSeparators = (optind < argc - 1 || (dumpPixelsForCurrentTest && dumpTree));
         for (int i = optind; i != argc; ++i)
             runTest(argv[i]);
     }

third_party/WebKit/Tools/DumpRenderTree/chromium/DumpRenderTree.cpp
@@ -29,6 +29,7 @@
  */
 
 #include "config.h"
+#include "DumpRenderTree.h"
 
 #include "MockWebKitPlatformSupport.h"
 #include "TestShell.h"
@@ -44,7 +45,6 @@ using namespace std;
 static const char optionComplexText[] = "--complex-text";
 static const char optionDumpAllPixels[] = "--dump-all-pixels";
 static const char optionNotree[] = "--notree";
-static const char optionPixelTests[] = "--pixel-tests";
 static const char optionThreaded[] = "--threaded";
 static const char optionDebugRenderTree[] = "--debug-render-tree";
 static const char optionDebugLayerTree[] = "--debug-layer-tree";
@@ -85,17 +85,12 @@ private:
     OwnPtr<MockWebKitPlatformSupport> m_mockPlatform;
 };
 
-static void runTest(TestShell& shell, TestParams& params, const string& testName)
+static void runTest(TestShell& shell, TestParams& params, const string& inputLine)
 {
     int oldTimeoutMsec = shell.layoutTestTimeout();
-    params.pixelHash = "";
-    string pathOrURL = testName;
-    string::size_type separatorPosition = pathOrURL.find("'");
-    if (separatorPosition != string::npos) {
-        params.pixelHash = pathOrURL.substr(separatorPosition + 1);
-        pathOrURL.erase(separatorPosition);
-    }
-    params.testUrl = webkit_support::CreateURLForPathOrURL(pathOrURL);
+    TestCommand command = parseInputLine(inputLine);
+    params.testUrl = webkit_support::CreateURLForPathOrURL(command.pathOrURL);
+    params.pixelHash = command.shouldDumpPixels;
     webkit_support::SetCurrentDirectoryForFileURL(params.testUrl);
     v8::V8::SetFlagsFromString(shell.javaScriptFlags().c_str(), shell.javaScriptFlags().length());
     if (shell.stressOpt() || shell.stressDeopt()) {
@@ -108,11 +103,11 @@ static void runTest(TestShell& shell, TestParams& params, const string& testName
           bool isLastLoad = (i == (v8::Testing::GetStressRuns() - 1));
           shell.setDumpWhenFinished(isLastLoad);
           shell.resetTestController();
-          shell.runFileTest(params);
+          shell.runFileTest(params, command.shouldDumpPixels);
       }
     } else {
       shell.resetTestController();
-      shell.runFileTest(params);
+      shell.runFileTest(params, command.shouldDumpPixels);
     }
     shell.setLayoutTestTimeout(oldTimeoutMsec);
 }
@@ -146,8 +141,6 @@ int main(int argc, char* argv[])
             serverMode = true;
         else if (argument == optionNotree)
             params.dumpTree = false;
-        else if (argument == optionPixelTests)
-            params.dumpPixels = true;
         else if (argument == optionDebugRenderTree)
             params.debugRenderTree = true;
         else if (argument == optionDebugLayerTree)

third_party/WebKit/Tools/DumpRenderTree/gtk/DumpRenderTree.cpp
@@ -73,7 +73,7 @@ extern gchar* webkit_web_frame_get_response_mime_type(WebKitWebFrame* frame);
 
 volatile bool done;
 static bool printSeparators;
-static int dumpPixels;
+static bool dumpPixelsForCurrentTest;
 static int dumpTree = 1;
 static int useTimeoutWatchdog = 1;
 
@@ -544,7 +544,6 @@ static void initializeGlobalsFromCommandLineOptions(int argc, char *argv[])
 {
     struct option options[] = {
         {"notree", no_argument, &dumpTree, false},
-        {"pixel-tests", no_argument, &dumpPixels, true},
         {"tree", no_argument, &dumpTree, true},
         {"no-timeout", no_argument, &useTimeoutWatchdog, false},
         {NULL, 0, NULL, 0}
@@ -624,7 +623,7 @@ void dump()
         }
     }
 
-    if (dumpPixels
+    if (dumpPixelsForCurrentTest
      && gLayoutTestController->generatePixelResults()
      && !gLayoutTestController->dumpDOMAsWebArchive()
      && !gLayoutTestController->dumpSourceAsWebArchive()) {
@@ -687,18 +686,13 @@ static void sendPixelResultsEOF()
     fflush(stderr);
 }
 
-static void runTest(const string& testPathOrURL)
+static void runTest(const string& inputLine)
 {
     ASSERT(!testPathOrURL.empty());
 
-    // Look for "'" as a separator between the path or URL, and the pixel dump hash that follows.
-    string testURL(testPathOrURL);
-    string expectedPixelHash;
-    size_t separatorPos = testURL.find("'");
-    if (separatorPos != string::npos) {
-        testURL = string(testPathOrURL, 0, separatorPos);
-        expectedPixelHash = string(testPathOrURL, separatorPos + 1);
-    }
+    TestCommand command = parseInputLine(inputLine);
+    string& testURL = command.pathOrURL;
+    dumpPixelsForCurrentTest = command.shouldDumpPixels;
 
     // Convert the path into a full file URL if it does not look
     // like an HTTP/S URL (doesn't start with http:// or https://).
@@ -712,7 +706,7 @@ static void runTest(const string& testPathOrURL)
 
     resetDefaultsToConsistentValues();
 
-    gLayoutTestController = LayoutTestController::create(testURL, expectedPixelHash);
+    gLayoutTestController = LayoutTestController::create(testURL, command.expectedPixelHash);
     topLoadingFrame = 0;
     done = false;
 
@@ -1447,7 +1441,7 @@ int main(int argc, char* argv[])
         printSeparators = true;
         runTestingServerLoop();
     } else {
-        printSeparators = (optind < argc-1 || (dumpPixels && dumpTree));
+        printSeparators = (optind < argc-1 || (dumpPixelsForCurrentTest && dumpTree));
         for (int i = optind; i != argc; ++i)
             runTest(argv[i]);
     }

third_party/WebKit/Tools/DumpRenderTree/win/DumpRenderTree.cpp
@@ -80,7 +80,7 @@ static LPCWSTR fontsEnvironmentVariable = L"WEBKIT_TESTFONTS";
 const LPCWSTR kDumpRenderTreeClassName = L"DumpRenderTreeWindow";
 
 static bool dumpTree = true;
-static bool dumpPixels;
+static bool dumpPixelsForCurrentTest;
 static bool dumpAllPixels;
 static bool printSeparators;
 static bool leakChecking = false;
@@ -759,7 +759,7 @@ void dump()
         fflush(stderr);
     }
 
-    if (dumpPixels
+    if (dumpPixelsForCurrentTest
      && gLayoutTestController->generatePixelResults()
      && !gLayoutTestController->dumpDOMAsWebArchive()
      && !gLayoutTestController->dumpSourceAsWebArchive())
@@ -948,20 +948,14 @@ static void sizeWebViewForCurrentTest()
     ::SetWindowPos(webViewWindow, 0, 0, 0, width, height, SWP_NOMOVE);
 }
 
-static void runTest(const string& testPathOrURL)
+static void runTest(const string& inputLine)
 {
+    TestCommand command = parseInputLine(inputLine);
+    const string& pathOrURL = command.pathOrURL;
+    dumpPixelsForCurrentTest = command.shouldDumpPixels;
+
     static BSTR methodBStr = SysAllocString(TEXT("GET"));
 
-    // Look for "'" as a separator between the path or URL, and the pixel dump hash that follows.
-    string pathOrURL(testPathOrURL);
-    string expectedPixelHash;
-    
-    size_t separatorPos = pathOrURL.find("'");
-    if (separatorPos != string::npos) {
-        pathOrURL = string(testPathOrURL, 0, separatorPos);
-        expectedPixelHash = string(testPathOrURL, separatorPos + 1);
-    }
-    
     BSTR urlBStr;
  
     CFStringRef str = CFStringCreateWithCString(0, pathOrURL.c_str(), kCFStringEncodingWindowsLatin1);
@@ -983,7 +977,7 @@ static void runTest(const string& testPathOrURL)
 
     CFRelease(url);
 
-    ::gLayoutTestController = LayoutTestController::create(pathOrURL, expectedPixelHash);
+    ::gLayoutTestController = LayoutTestController::create(pathOrURL, command.expectedPixelHash);
     done = false;
     topLoadingFrame = 0;
 
@@ -1334,11 +1328,6 @@ extern "C" __declspec(dllexport) int WINAPI dllLauncherEntryPoint(int argc, cons
 
         if (!stricmp(argv[i], "--dump-all-pixels")) {
             dumpAllPixels = true;
-            continue;
-        }
-
-        if (!stricmp(argv[i], "--pixel-tests")) {
-            dumpPixels = true;
             continue;
         }
 

third_party/WebKit/Tools/DumpRenderTree/DumpRenderTree.gyp/DumpRenderTree.gyp
@@ -133,6 +133,7 @@
             'include_dirs': [
                 '<(chromium_src_dir)',
                 '<(source_dir)/WebKit/chromium/public',
+                '<(tools_dir)/DumpRenderTree',
                 '<(DEPTH)',
             ],
             'defines': [

third_party/WebKit/Tools/DumpRenderTree/DumpRenderTree.gypi
@@ -54,10 +54,12 @@
             'chromium/WebViewHost.h',
         ],
         'test_runner_files': [
-            'chromium/TestRunner/AccessibilityController.cpp',
-            'chromium/TestRunner/AccessibilityController.h',
-            'chromium/TestRunner/AccessibilityUIElement.cpp',
-            'chromium/TestRunner/AccessibilityUIElement.h',
+            'DumpRenderTree.h',
+            'DumpRenderTreeCommon.cpp',
+            'chromium/TestRunner/AccessibilityControllerChromium.cpp',
+            'chromium/TestRunner/AccessibilityControllerChromium.h',
+            'chromium/TestRunner/AccessibilityUIElementChromium.cpp',
+            'chromium/TestRunner/AccessibilityUIElementChromium.h',
             'chromium/TestRunner/CppBoundClass.cpp',
             'chromium/TestRunner/CppBoundClass.h',
             'chromium/TestRunner/CppVariant.cpp',

third_party/WebKit/Tools/DumpRenderTree/DumpRenderTree.h
@@ -65,4 +65,14 @@ extern RefPtr<LayoutTestController> gLayoutTestController;
 void dump();
 void displayWebView();
 
+struct TestCommand {
+    TestCommand() : shouldDumpPixels(false) { }
+
+    std::string pathOrURL;
+    bool shouldDumpPixels;
+    std::string expectedPixelHash;
+};
+
+TestCommand parseInputLine(const std::string&);
+
 #endif // DumpRenderTree_h

third_party/WebKit/Tools/DumpRenderTree/DumpRenderTree.xcodeproj/project.pbxproj
@@ -89,6 +89,7 @@
 		8CCDA82A151A72D10003F937 /* SampleFont.sfont in Copy Font Files */ = {isa = PBXBuildFile; fileRef = 8CCDA81F151A56550003F937 /* SampleFont.sfont */; };
 		9340994C08540CAE007F3BC8 /* DumpRenderTreePrefix.h in Headers */ = {isa = PBXBuildFile; fileRef = 32A70AAB03705E1F00C91783 /* DumpRenderTreePrefix.h */; };
 		9340995108540CAE007F3BC8 /* WebKit.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 9335435F03D75502008635CE /* WebKit.framework */; };
+		9830F31F15C81181005AB206 /* DumpRenderTreeCommon.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 9830F31E15C81181005AB206 /* DumpRenderTreeCommon.cpp */; };
 		A817090008B163EF00CCB9FB /* Cocoa.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = A84F608908B136DA00E9745F /* Cocoa.framework */; };
 		A817090408B164D300CCB9FB /* JavaScriptCore.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = A817090308B164D300CCB9FB /* JavaScriptCore.framework */; };
 		A84F608A08B136DA00E9745F /* Cocoa.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = A84F608908B136DA00E9745F /* Cocoa.framework */; };
@@ -289,6 +290,7 @@
 		8CCDA81F151A56550003F937 /* SampleFont.sfont */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xml; name = SampleFont.sfont; path = fonts/SampleFont.sfont; sourceTree = "<group>"; };
 		9335435F03D75502008635CE /* WebKit.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; path = WebKit.framework; sourceTree = BUILT_PRODUCTS_DIR; };
 		9340995408540CAF007F3BC8 /* DumpRenderTree */ = {isa = PBXFileReference; explicitFileType = "compiled.mach-o.executable"; includeInIndex = 0; path = DumpRenderTree; sourceTree = BUILT_PRODUCTS_DIR; };
+		9830F31E15C81181005AB206 /* DumpRenderTreeCommon.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = DumpRenderTreeCommon.cpp; sourceTree = "<group>"; };
 		A803FF7409CAAD08009B2A37 /* DumpRenderTree.h */ = {isa = PBXFileReference; explicitFileType = sourcecode.cpp.h; fileEncoding = 4; path = DumpRenderTree.h; sourceTree = "<group>"; };
 		A817090308B164D300CCB9FB /* JavaScriptCore.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; path = JavaScriptCore.framework; sourceTree = BUILT_PRODUCTS_DIR; };
 		A84F608908B136DA00E9745F /* Cocoa.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = Cocoa.framework; path = /System/Library/Frameworks/Cocoa.framework; sourceTree = "<absolute>"; };
@@ -421,6 +423,7 @@
 		08FB7794FE84155DC02AAC07 /* DumpRenderTree */ = {
 			isa = PBXGroup;
 			children = (
+				9830F31E15C81181005AB206 /* DumpRenderTreeCommon.cpp */,
 				32A70AAB03705E1F00C91783 /* DumpRenderTreePrefix.h */,
 				1422A2750AF6F4BD00E1A883 /* Delegates */,
 				1422A2690AF6F45200E1A883 /* Controllers */,
@@ -955,6 +958,7 @@
 				29CFBA2E12273A1000BC30C0 /* AccessibilityTextMarkerMac.mm in Sources */,
 				3A5626CB131CA02A002BE6D9 /* StorageTrackerDelegate.mm in Sources */,
 				80045AEE147718E7008290A8 /* AccessibilityNotificationHandler.mm in Sources */,
+				9830F31F15C81181005AB206 /* DumpRenderTreeCommon.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

commit 0999075871e7b6ab876619d9ea826f06605c62a0

[EFL] (REGRESSION 124581) : EFL 64-bit Debug bot is red and build is exiting early after 20 test crashes https://bugs.webkit.org/show_bug.cgi?id=93084
Patch by Sudarsana Nagineni <sudarsana.nagineni@linux.intel.com> on 2012-08-03
Reviewed by Csaba Osztrogonác.

Fix regression introduced in r124581.

* DumpRenderTree/efl/DumpRenderTree.cpp:
(getFinalTestURL): Convert the path into a full file URL.
(runTest):

git-svn-id: svn://svn.chromium.org/blink/trunk@124595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTree.cpp
@@ -241,13 +241,31 @@ static void createLayoutTestController(const String& testURL, const String& expe
     }
 }
 
+static String getFinalTestURL(const String& testURL)
+{
+    if (!testURL.startsWith("http://") && !testURL.startsWith("https://")) {
+        char* cFilePath = ecore_file_realpath(testURL.utf8().data());
+        const String filePath = String::fromUTF8(cFilePath);
+        free(cFilePath);
+
+        if (ecore_file_exists(filePath.utf8().data()))
+            return String("file://") + filePath;
+    }
+
+    return testURL;
+}
+
 static void runTest(const char* inputLine)
 {
     TestCommand command = parseInputLine(inputLine);
-    const String testURL(command.pathOrURL.c_str());
-    ASSERT(!testURL.isEmpty());
+    const String testPathOrURL(command.pathOrURL.c_str());
+    ASSERT(!testPathOrURL.isEmpty());
     dumpPixelsForCurrentTest = command.shouldDumpPixels;
     const String expectedPixelHash(command.expectedPixelHash.c_str());
+
+    // Convert the path into a full file URL if it does not look
+    // like an HTTP/S URL (doesn't start with http:// or https://).
+    const String testURL = getFinalTestURL(testPathOrURL);
 
     browser->resetDefaultsToConsistentValues();
     createLayoutTestController(testURL, expectedPixelHash);