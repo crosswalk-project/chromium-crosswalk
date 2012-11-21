crosswalk-project/chromium-crosswalk

commit 85e3fdb282a4770d3b2ae82bae3237c4a14111cd

Remove initiallyFitToViewport attribute https://bugs.webkit.org/show_bug.cgi?id=102392
Reviewed by Noam Rosenthal.

Remove the initiallyFitToViewport which shouldn't have been
added in the first place. We now reset userScalable to auto (-1)
in case it was not explicitly set by the web author.

Same behavior, covered by existing tests.

Source/WebCore:

* dom/ViewportArguments.cpp:
(WebCore::ViewportArguments::resolve):
* dom/ViewportArguments.h:
(ViewportAttributes):

Source/WebKit2:

* UIProcess/PageViewportController.cpp:
(WebKit::PageViewportController::PageViewportController):
(WebKit::PageViewportController::pageTransitionViewportReady):
* WebProcess/WebPage/WebPage.cpp:
(WebKit::WebPage::sendViewportAttributesChanged):


git-svn-id: svn://svn.chromium.org/blink/trunk@134801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebKit2/UIProcess/PageViewportController.cpp
@@ -56,7 +56,6 @@ PageViewportController::PageViewportController(WebKit::WebPageProxy* proxy, Page
     m_rawAttributes.minimumScale = 1;
     m_rawAttributes.maximumScale = 1;
     m_rawAttributes.userScalable = m_allowsUserScaling;
-    m_rawAttributes.initiallyFitToViewport = true;
 
     ASSERT(m_client);
     m_client->setController(this);
@@ -145,7 +144,7 @@ void PageViewportController::pageTransitionViewportReady()
 {
     if (!m_rawAttributes.layoutSize.isEmpty()) {
         m_hadUserInteraction = false;
-        float initialScale = m_rawAttributes.initiallyFitToViewport ? m_minimumScaleToFit : m_rawAttributes.initialScale;
+        float initialScale = (m_rawAttributes.initialScale < 0) ? m_minimumScaleToFit : m_rawAttributes.initialScale;
         applyScaleAfterRenderingContents(innerBoundedViewportScale(toViewportScale(initialScale)));
     }
 

third_party/WebKit/Source/WebCore/dom/ViewportArguments.cpp
@@ -74,8 +74,6 @@ ViewportAttributes ViewportArguments::resolve(const FloatSize& initialViewportSi
     float resultMaxZoom = maxZoom;
     float resultUserZoom = userZoom;
 
-    bool resultAutoFit = resultZoom == ViewportArguments::ValueAuto;
-
     switch (int(resultWidth)) {
     case ViewportArguments::ValueDeviceWidth:
         resultWidth = deviceSize.width();
@@ -102,7 +100,6 @@ ViewportAttributes ViewportArguments::resolve(const FloatSize& initialViewportSi
     resultMaxZoom = clampScaleValue(resultMaxZoom);
 
     ViewportAttributes result;
-    result.initiallyFitToViewport = resultAutoFit;
 
     // Resolve minimum-scale and maximum-scale values according to spec.
     if (resultMinZoom == ViewportArguments::ValueAuto)
@@ -152,7 +149,7 @@ ViewportAttributes ViewportArguments::resolve(const FloatSize& initialViewportSi
     result.layoutSize.setWidth(resultWidth);
     result.layoutSize.setHeight(resultHeight);
 
-    // FIXME: Remove initiallyFitToViewport and use the below.
+    // FIXME: This might affect some ports, but is the right thing to do.
     // Only set initialScale to a value if it was explicitly set.
     // if (resultZoom == ViewportArguments::ValueAuto)
     //    result.initialScale = ViewportArguments::ValueAuto;

third_party/WebKit/Source/WebCore/dom/ViewportArguments.h
@@ -50,7 +50,6 @@ struct ViewportAttributes {
     float maximumScale;
 
     float userScalable;
-    bool initiallyFitToViewport;
 };
 
 struct ViewportArguments {

third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -1021,6 +1021,7 @@ void WebPage::sendViewportAttributesChanged()
     int deviceHeight = (settings->deviceHeight() > 0) ? settings->deviceHeight() : m_viewportSize.height();
 
     ViewportAttributes attr = computeViewportAttributes(m_page->viewportArguments(), minimumLayoutFallbackWidth, deviceWidth, deviceHeight, m_page->deviceScaleFactor(), m_viewportSize);
+    attr.initialScale = m_page->viewportArguments().zoom; // Resets auto (-1) if no value was set by user.
 
     // This also takes care of the relayout.
     setFixedLayoutSize(IntSize(static_cast<int>(attr.layoutSize.width()), static_cast<int>(attr.layoutSize.height())));

commit 02e96880aee3c66cf57cb9c76c66688702a23f70

[WK2] Viewport meta tag broken after r134801 https://bugs.webkit.org/show_bug.cgi?id=102801
Reviewed by Kenneth Rohde Christiansen.

If the initial-scale attribute is not explicitly specified it is left -1 by the
viewport meta algorithm and if the content is not scalable the min and max scales
are restricted by restrictScaleFactorToInitialScaleIfNotUserScalable() which resulted
in negative scales on the UI side.
Make sure that the minimum scale to fit the viewport is calculated before restricting
the scales and use this scale as the initial scale if no explicit initial scale was set.

* UIProcess/PageViewportController.cpp:
(WebKit::PageViewportController::PageViewportController):
(WebKit::PageViewportController::pageTransitionViewportReady):
(WebKit::PageViewportController::didChangeViewportAttributes):
(WebKit::PageViewportController::updateMinimumScaleToFit):

git-svn-id: svn://svn.chromium.org/blink/trunk@135399 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebKit2/UIProcess/PageViewportController.cpp
@@ -52,10 +52,14 @@ PageViewportController::PageViewportController(WebKit::WebPageProxy* proxy, Page
 {
     // Initializing Viewport Raw Attributes to avoid random negative or infinity scale factors
     // if there is a race condition between the first layout and setting the viewport attributes for the first time.
-    m_rawAttributes.initialScale = 1;
     m_rawAttributes.minimumScale = 1;
     m_rawAttributes.maximumScale = 1;
     m_rawAttributes.userScalable = m_allowsUserScaling;
+
+    // The initial scale might be implicit and set to -1, in this case we have to infer it
+    // using the viewport size and the final layout size.
+    // To be able to assert for valid scale we initialize it to -1.
+    m_rawAttributes.initialScale = -1;
 
     ASSERT(m_client);
     m_client->setController(this);
@@ -145,8 +149,8 @@ void PageViewportController::pageTransitionViewportReady()
 {
     if (!m_rawAttributes.layoutSize.isEmpty()) {
         m_hadUserInteraction = false;
-        float initialScale = (m_rawAttributes.initialScale < 0) ? m_minimumScaleToFit : m_rawAttributes.initialScale;
-        applyScaleAfterRenderingContents(innerBoundedViewportScale(toViewportScale(initialScale)));
+        ASSERT(m_rawAttributes.initialScale > 0);
+        applyScaleAfterRenderingContents(innerBoundedViewportScale(toViewportScale(m_rawAttributes.initialScale)));
     }
 
     // At this point we should already have received the first viewport arguments and the requested scroll
@@ -213,10 +217,18 @@ void PageViewportController::didChangeViewportAttributes(const WebCore::Viewport
         return;
 
     m_rawAttributes = newAttributes;
-    WebCore::restrictScaleFactorToInitialScaleIfNotUserScalable(m_rawAttributes);
-
     m_allowsUserScaling = !!m_rawAttributes.userScalable;
-    updateMinimumScaleToFit();
+
+    if (!updateMinimumScaleToFit())
+        return;
+
+    ASSERT(m_minimumScaleToFit > 0);
+
+    // Set the initial scale if it was not specified in the viewport meta tag.
+    if (m_rawAttributes.initialScale < 0)
+        m_rawAttributes.initialScale = m_minimumScaleToFit;
+
+    WebCore::restrictScaleFactorToInitialScaleIfNotUserScalable(m_rawAttributes);
 
     m_client->didChangeViewportAttributes();
 }
@@ -262,10 +274,13 @@ void PageViewportController::applyPositionAfterRenderingContents(const FloatPoin
 
 bool PageViewportController::updateMinimumScaleToFit()
 {
-    if (m_viewportSize.isEmpty())
+    if (m_viewportSize.isEmpty() || m_contentsSize.isEmpty())
         return false;
 
     float minimumScale = WebCore::computeMinimumScaleFactorForContentContained(m_rawAttributes, WebCore::roundedIntSize(m_viewportSize), WebCore::roundedIntSize(m_contentsSize), devicePixelRatio());
+
+    if (minimumScale <= 0)
+        return false;
 
     if (!fuzzyCompare(minimumScale, m_minimumScaleToFit, 0.001)) {
         m_minimumScaleToFit = minimumScale;