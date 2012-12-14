crosswalk-project/chromium-crosswalk

commit db973925324abeea0de22fbe0e89e61c62d70081

Fix target element handling in SVGSMILElement. https://bugs.webkit.org/show_bug.cgi?id=104495
Reviewed by Abhishek Arya.

Source/WebCore:

This patch fixes two bugs that led to stale target elements in SVGSMILElement.

r122449 changed the <use> code to call removeAllElementReferencesForTarget to rebuild
dependent <use> trees. This contained a bug where element references would be incorrectly
removed. removeAllElementReferencesForTarget calls svgAttributeChanged(href) on all
references, which can then re-add the references back. The bug is that
removeAllElementReferencesForTarget removes a newly-added reference as the last step.

r136906 changed targetElement() to no longer be lazily resolved so any changes to an
animation's href attribute needed to be tracked. r136906 refactored the code but did not
fix a bug where animation elements not in the document would not react to
svgAttributeChanged(href).

The combination of these two bugs allowed stale targetElement references to exist. A
target element could be removed from the document without dependent animation elements
receiving a notification (bug 1) and not reacting to notifications they received (bug 2).

This patch updates SVGUseElement::buildInstanceTree to only rebuild (not remove) the
elements referencing the target by adding rebuildAllElementReferencesForTarget. This patch
also updates SVGSMILElement to correctly react to svgAttributeChanged() notifications
for href changes even when not in the document.

Test: svg/animations/target-condition-crash.html

* svg/SVGDocumentExtensions.cpp:
(WebCore::SVGDocumentExtensions::rebuildAllElementReferencesForTarget):
(WebCore::SVGDocumentExtensions::removeAllElementReferencesForTarget):

    Refactored so that we can notify element references to update without removing
    references.

* svg/SVGDocumentExtensions.h:
(SVGDocumentExtensions):
* svg/SVGElement.cpp:
(WebCore::SVGElement::~SVGElement):
(WebCore::SVGElement::removedFrom):

third_party/WebKit/Source/WebCore/svg/animation/SVGSMILElement.cpp
@@ -154,8 +154,12 @@ void SVGSMILElement::clearResourceReferences()
 void SVGSMILElement::buildPendingResource()
 {
     clearResourceReferences();
-    if (!inDocument())
+
+    if (!inDocument()) {
+        // Reset the target element if we are no longer in the document.
+        setTargetElement(0);
         return;
+    }
 
     String id;
     String href = getAttribute(XLinkNames::hrefAttr);
@@ -165,6 +169,9 @@ void SVGSMILElement::buildPendingResource()
     else
         target = SVGURIReference::targetElementFromIRIString(href, document(), &id);
     SVGElement* svgTarget = target && target->isSVGElement() ? static_cast<SVGElement*>(target) : 0;
+
+    if (svgTarget && !svgTarget->inDocument())
+        svgTarget = 0;
 
     if (svgTarget != targetElement())
         setTargetElement(svgTarget);
@@ -266,17 +273,8 @@ Node::InsertionNotificationRequest SVGSMILElement::insertedInto(ContainerNode* r
 void SVGSMILElement::removedFrom(ContainerNode* rootParent)
 {
     if (rootParent->inDocument()) {
-        if (m_timeContainer) {
-            if (m_targetElement && hasValidAttributeName())
-                m_timeContainer->unschedule(this, m_targetElement, m_attributeName);
-            m_timeContainer = 0;
-        }
-        // Calling disconnectConditions() may kill us if there are syncbase conditions.
-        // OK, but we don't want to die inside the call.
-        RefPtr<SVGSMILElement> keepAlive(this);
-        disconnectConditions();
-
         clearResourceReferences();
+        disconnectConditions();
         setTargetElement(0);
         setAttributeName(anyQName());
         animationAttributeChanged();
@@ -504,17 +502,16 @@ void SVGSMILElement::svgAttributeChanged(const QualifiedName& attrName)
         m_cachedMin = invalidCachedTime;
     else if (attrName == SVGNames::maxAttr)
         m_cachedMax = invalidCachedTime;
-    else if (inDocument()) {
+    else if (attrName == SVGNames::attributeNameAttr)
+        setAttributeName(constructQualifiedName(this, fastGetAttribute(SVGNames::attributeNameAttr)));
+    else if (attrName.matches(XLinkNames::hrefAttr)) {
+        SVGElementInstance::InvalidationGuard invalidationGuard(this);
+        buildPendingResource();
+    } else if (inDocument()) {
         if (attrName == SVGNames::beginAttr)
             beginListChanged(elapsed());
         else if (attrName == SVGNames::endAttr)
             endListChanged(elapsed());
-        else if (attrName == SVGNames::attributeNameAttr)
-            setAttributeName(constructQualifiedName(this, fastGetAttribute(SVGNames::attributeNameAttr)));
-        else if (attrName.matches(XLinkNames::hrefAttr)) {
-            SVGElementInstance::InvalidationGuard invalidationGuard(this);
-            buildPendingResource();
-        }
     }
 
     animationAttributeChanged();
@@ -609,9 +606,11 @@ void SVGSMILElement::setTargetElement(SVGElement* target)
             m_timeContainer->schedule(this, target, m_attributeName);
     }
 
-    // Only clear the animated type, if we had a target before.
-    if (m_targetElement)
+    if (m_targetElement) {
+        // Clear values that may depend on the previous target.
         clearAnimatedType(m_targetElement);
+        disconnectConditions();
+    }
 
     // If the animation state is not Inactive, always reset to a clear state before leaving the old target element.
     if (m_activeState != Inactive)

third_party/WebKit/LayoutTests/svg/animations/target-condition-crash-expected.txt
@@ -1,0 +1,1 @@
+PASS: Test did not crash.

third_party/WebKit/LayoutTests/svg/animations/target-condition-crash.html
@@ -1,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+Test that target conditions are correctly removed on document destruction.<br/>
+This test passes if it does not crash<br/>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="100" height="100">
+  <path id="path"></path>
+  <use xlink:href="#path">
+    <animateMotion begin="click"></animateMotion>
+  </use>
+</svg>
+<script type="text/javascript">
+  if (window.testRunner)
+    testRunner.waitUntilDone();
+
+  window.setTimeout(function(){
+    if (location.hash != '#5') {
+      if (window.GCController)
+        window.GCController.collect();
+      else if (window.gc)
+        gc();
+      if (location.hash == '')
+        location.hash = '#1';
+      else
+        location.hash = "#" + (parseInt(location.hash.slice(1)) + 1).toString();
+      location.reload();
+    } else {
+      document.write('PASS: Test did not crash.');
+      if (window.testRunner) {
+        testRunner.dumpAsText();
+        testRunner.notifyDone();
+      }
+    }
+  }, 0);
+</script>
+</body>
+</html>

third_party/WebKit/Source/WebCore/svg/SVGDocumentExtensions.cpp
@@ -340,7 +340,7 @@ void SVGDocumentExtensions::removeAllTargetReferencesForElement(SVGElement* refe
         m_elementDependencies.remove(*it);
 }
 
-void SVGDocumentExtensions::removeAllElementReferencesForTarget(SVGElement* referencedElement)
+void SVGDocumentExtensions::rebuildAllElementReferencesForTarget(SVGElement* referencedElement)
 {
     ASSERT(referencedElement);
     HashMap<SVGElement*, OwnPtr<HashSet<SVGElement*> > >::iterator it = m_elementDependencies.find(referencedElement);
@@ -363,8 +363,17 @@ void SVGDocumentExtensions::removeAllElementReferencesForTarget(SVGElement* refe
                 (*vectorIt)->svgAttributeChanged(XLinkNames::hrefAttr);
         }
     }
+}
 
-    m_elementDependencies.remove(referencedElement);
+void SVGDocumentExtensions::removeAllElementReferencesForTarget(SVGElement* referencedElement)
+{
+    ASSERT(referencedElement);
+    HashMap<SVGElement*, OwnPtr<HashSet<SVGElement*> > >::iterator it = m_elementDependencies.find(referencedElement);
+    if (it == m_elementDependencies.end())
+        return;
+    ASSERT(it->key == referencedElement);
+
+    m_elementDependencies.remove(it);
 }
 
 #if ENABLE(SVG_FONTS)

third_party/WebKit/Source/WebCore/svg/SVGDocumentExtensions.h
@@ -67,8 +67,9 @@ public:
 
     HashSet<SVGElement*>* setOfElementsReferencingTarget(SVGElement* referencedElement) const;
     void addElementReferencingTarget(SVGElement* referencingElement, SVGElement* referencedElement);
-    void removeAllTargetReferencesForElement(SVGElement* referencingElement);
-    void removeAllElementReferencesForTarget(SVGElement* referencedElement);
+    void removeAllTargetReferencesForElement(SVGElement*);
+    void rebuildAllElementReferencesForTarget(SVGElement*);
+    void removeAllElementReferencesForTarget(SVGElement*);
 
 #if ENABLE(SVG_FONTS)
     const HashSet<SVGFontFaceElement*>& svgFontFaceElements() const { return m_svgFontFaceElements; }

third_party/WebKit/Source/WebCore/svg/SVGElement.cpp
@@ -83,6 +83,7 @@ SVGElement::~SVGElement()
         rareDataMap.remove(it);
     }
     ASSERT(document());
+    document()->accessSVGExtensions()->rebuildAllElementReferencesForTarget(this);
     document()->accessSVGExtensions()->removeAllElementReferencesForTarget(this);
 }
 
@@ -182,6 +183,7 @@ void SVGElement::removedFrom(ContainerNode* rootParent)
     StyledElement::removedFrom(rootParent);
 
     if (wasInDocument) {
+        document()->accessSVGExtensions()->rebuildAllElementReferencesForTarget(this);
         document()->accessSVGExtensions()->removeAllElementReferencesForTarget(this);
         document()->accessSVGExtensions()->removeElementFromPendingResources(this);
     }
@@ -536,7 +538,7 @@ void SVGElement::attributeChanged(const QualifiedName& name, const AtomicString&
     StyledElement::attributeChanged(name, newValue);
 
     if (isIdAttributeName(name))
-        document()->accessSVGExtensions()->removeAllElementReferencesForTarget(this);
+        document()->accessSVGExtensions()->rebuildAllElementReferencesForTarget(this);
 
     // Changes to the style attribute are processed lazily (see Element::getAttribute() and related methods),
     // so we don't want changes to the style attribute to result in extra work here.

third_party/WebKit/Source/WebCore/svg/SVGUseElement.cpp
@@ -525,7 +525,7 @@ void SVGUseElement::buildShadowAndInstanceTree(SVGElement* target)
 
     // Rebuild all dependent use elements.
     ASSERT(document());
-    document()->accessSVGExtensions()->removeAllElementReferencesForTarget(this);
+    document()->accessSVGExtensions()->rebuildAllElementReferencesForTarget(this);
 
     // Eventually dump instance tree
 #ifdef DUMP_INSTANCE_TREE
@@ -608,7 +608,7 @@ void SVGUseElement::buildInstanceTree(SVGElement* target, SVGElementInstance* ta
         if (foundProblem)
             return;
 
-        // We only need to track fist degree <use> dependencies. Indirect references are handled
+        // We only need to track first degree <use> dependencies. Indirect references are handled
         // as the invalidation bubbles up the dependency chain.
         if (!foundUse) {
             ASSERT(document());

commit 165b66cc5528179f112fd43c87acb8f632f238a6

Clear m_timeContainer on SVGSMILElement removal. https://bugs.webkit.org/show_bug.cgi?id=104972
Reviewed by Abhishek Arya.

Source/WebCore:

This patch fixes a regression introduced by r137509 where we did not clear
m_timeContainer in SVGSMILElement::removedFrom.

Test: svg/custom/removed-from-animation-crash.html

* svg/animation/SVGSMILElement.cpp:
(WebCore::SVGSMILElement::removedFrom):

LayoutTests:

* svg/custom/removed-from-animation-crash-expected.txt: Added.
* svg/custom/removed-from-animation-crash.html: Added.


git-svn-id: svn://svn.chromium.org/blink/trunk@137701 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/svg/animation/SVGSMILElement.cpp
@@ -278,6 +278,7 @@ void SVGSMILElement::removedFrom(ContainerNode* rootParent)
         setTargetElement(0);
         setAttributeName(anyQName());
         animationAttributeChanged();
+        m_timeContainer = 0;
     }
 
     SVGElement::removedFrom(rootParent);

third_party/WebKit/LayoutTests/svg/custom/removed-from-animation-crash-expected.txt
@@ -1,0 +1,1 @@
+PASS: test did not crash

third_party/WebKit/LayoutTests/svg/custom/removed-from-animation-crash.html
@@ -1,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<!-- Test for WK104972: this test passes if it does not crash. -->
+<body id="body" onload="crash()">
+  <svg width="100" height="100">
+    <rect id="rect1">
+      <animateMotion>
+        <g id="g1"></g>
+        <g id="g2"></g>
+        <set id="animate"></set>
+      </animateMotion>
+    </rect>
+  </svg>
+<script>
+  if (window.testRunner) {
+    testRunner.waitUntilDone();
+    testRunner.dumpAsText();
+  }
+
+  function crash() {
+    var notInDocRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    setTimeout(function() {
+      var g2 = document.getElementById('g2');
+      g2.parentNode.removeChild(g2);
+      document.getElementById('g1').appendChild(document.getElementById('animate'));
+      notInDocRect.appendChild(document.getElementById('rect1'));
+      document.getElementById('body').innerHTML = "PASS: test did not crash";
+
+      if (window.testRunner)
+        testRunner.notifyDone();
+    }, 1);
+  }
+</script>
+</body>
+</html>