crosswalk-project/chromium-crosswalk

commit 8d64cb394f1183fe90d40a608f01c45da44e3736

Add a USE() macro for content filtering code https://bugs.webkit.org/show_bug.cgi?id=107098
Reviewed by Mark Rowe.

Source/WebCore:

* WebCore.exp.in: Use USE(CONTENT_FILTERING).
* loader/MainResourceLoader.cpp:
(WebCore::MainResourceLoader::MainResourceLoader): Ditto.
(WebCore::MainResourceLoader::~MainResourceLoader): Ditto.
(WebCore::MainResourceLoader::cancel): Ditto.
(WebCore::MainResourceLoader::responseReceived): Ditto.
(WebCore::MainResourceLoader::dataReceived): Ditto.
(WebCore::MainResourceLoader::didFinishLoading): Ditto.
(WebCore::MainResourceLoader::notifyFinished): Ditto.
* loader/MainResourceLoader.h:
(MainResourceLoader): Ditto.
* platform/mac/WebCoreSystemInterface.h: Ditto.
* platform/mac/WebCoreSystemInterface.mm: Ditto.

Source/WebKit/mac:

* WebCoreSupport/WebSystemInterface.mm:
(InitWebCoreSystemInterface): Use USE(CONTENT_FILTERING).

Source/WebKit2:

* WebProcess/WebCoreSupport/mac/WebSystemInterface.mm:
(InitWebCoreSystemInterface): Use USE(CONTENT_FILTERING).

Source/WTF:

* wtf/Platform.h: Define WTF_USE_CONTENT_FILTERING on Mac platforms
more recent than Lion.

git-svn-id: svn://svn.chromium.org/blink/trunk@140366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WTF/wtf/Platform.h
@@ -1209,4 +1209,8 @@
 #endif
 #endif
 
+#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
+#define WTF_USE_CONTENT_FILTERING 1
+#endif
+
 #endif /* WTF_Platform_h */

third_party/WebKit/Source/WebCore/WebCore.exp.in
@@ -2150,14 +2150,17 @@ _wkExecutableWasLinkedOnOrBeforeSnowLeopard
 _wkRecommendedScrollerStyle
 #endif
 
-#if !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
-_wkExecutableWasLinkedOnOrBeforeLion
+#if USE(CONTENT_FILTERING)
 _wkFilterAddData
 _wkFilterCreateInstance
 _wkFilterDataComplete
 _wkFilterIsManagedSession
 _wkFilterRelease
 _wkFilterWasBlocked
+#endif
+
+#if !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+_wkExecutableWasLinkedOnOrBeforeLion
 _wkNSElasticDeltaForReboundDelta
 _wkNSElasticDeltaForTimeDelta
 _wkNSReboundDeltaForElasticDelta

third_party/WebKit/Source/WebCore/loader/MainResourceLoader.cpp
@@ -61,7 +61,7 @@
 #include "PluginDatabase.h"
 #endif
 
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
 #include "WebCoreSystemInterface.h"
 #endif
 
@@ -74,7 +74,7 @@ MainResourceLoader::MainResourceLoader(DocumentLoader* documentLoader)
     , m_waitingForContentPolicy(false)
     , m_timeOfLastDataReceived(0.0)
     , m_substituteDataLoadIdentifier(0)
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
     , m_filter(0)
 #endif
 {
@@ -83,7 +83,7 @@ MainResourceLoader::MainResourceLoader(DocumentLoader* documentLoader)
 MainResourceLoader::~MainResourceLoader()
 {
     clearResource();
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
     ASSERT(!m_filter);
 #endif
 }
@@ -136,7 +136,7 @@ void MainResourceLoader::cancel(const ResourceError& error)
     clearResource();
     receivedError(resourceError);
 
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
     if (m_filter) {
         wkFilterRelease(m_filter);
         m_filter = 0;
@@ -453,7 +453,7 @@ void MainResourceLoader::responseReceived(CachedResource* resource, const Resour
     }
 #endif
 
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
     if (r.url().protocolIs("https") && wkFilterIsManagedSession())
         m_filter = wkFilterCreateInstance(r.nsURLResponse());
 #endif
@@ -483,7 +483,7 @@ void MainResourceLoader::dataReceived(CachedResource* resource, const char* data
     ASSERT(!defersLoading());
 #endif
 
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
     if (m_filter) {
         ASSERT(!wkFilterWasBlocked(m_filter));
         const char* blockedData = wkFilterAddData(m_filter, data, &length);
@@ -511,7 +511,7 @@ void MainResourceLoader::dataReceived(CachedResource* resource, const char* data
 
     documentLoader()->receivedData(data, length);
 
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
     if (WebFilterEvaluator *filter = m_filter) {
         // If we got here, it means we know if we were blocked or not. If we were blocked, we're
         // done loading the page altogether. Either way, we don't need the filter anymore.
@@ -543,7 +543,7 @@ void MainResourceLoader::didFinishLoading(double finishTime)
         m_substituteDataLoadIdentifier = 0;
     }
 
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
     if (m_filter) {
         int length;
         const char* data = wkFilterDataComplete(m_filter, &length);
@@ -579,7 +579,7 @@ void MainResourceLoader::notifyFinished(CachedResource* resource)
         return;
     }
 
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
     if (m_filter) {
         wkFilterRelease(m_filter);
         m_filter = 0;

third_party/WebKit/Source/WebCore/loader/MainResourceLoader.h
@@ -36,7 +36,7 @@
 #include "SubstituteData.h"
 #include <wtf/Forward.h>
 
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
 OBJC_CLASS WebFilterEvaluator;
 #endif
 
@@ -130,7 +130,7 @@ private:
     double m_timeOfLastDataReceived;
     unsigned long m_substituteDataLoadIdentifier;
 
-#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if USE(CONTENT_FILTERING)
     WebFilterEvaluator *m_filter;
 #endif
 };

third_party/WebKit/Source/WebCore/platform/mac/WebCoreSystemInterface.h
@@ -336,14 +336,16 @@ extern void (*wkCGPathAddRoundedRect)(CGMutablePathRef path, const CGAffineTrans
 extern void (*wkCFURLRequestAllowAllPostCaching)(CFURLRequestRef);
 #endif
 
-#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
+#if USE(CONTENT_FILTERING)
 extern BOOL (*wkFilterIsManagedSession)(void);
 extern WebFilterEvaluator *(*wkFilterCreateInstance)(NSURLResponse *);
 extern void (*wkFilterRelease)(WebFilterEvaluator *);
 extern BOOL (*wkFilterWasBlocked)(WebFilterEvaluator *);
 extern const char* (*wkFilterAddData)(WebFilterEvaluator *, const char* data, int* length);
 extern const char* (*wkFilterDataComplete)(WebFilterEvaluator *, int* length);
+#endif
 
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
 extern CGFloat (*wkNSElasticDeltaForTimeDelta)(CGFloat initialPosition, CGFloat initialVelocity, CGFloat elapsedTime);
 extern CGFloat (*wkNSElasticDeltaForReboundDelta)(CGFloat delta);
 extern CGFloat (*wkNSReboundDeltaForElasticDelta)(CGFloat delta);

third_party/WebKit/Source/WebCore/platform/mac/WebCoreSystemInterface.mm
@@ -208,14 +208,16 @@ void (*wkCGPathAddRoundedRect)(CGMutablePathRef path, const CGAffineTransform* m
 void (*wkCFURLRequestAllowAllPostCaching)(CFURLRequestRef);
 #endif
 
-#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
+#if USE(CONTENT_FILTERING)
 BOOL (*wkFilterIsManagedSession)(void);
 WebFilterEvaluator *(*wkFilterCreateInstance)(NSURLResponse *);
 void (*wkFilterRelease)(WebFilterEvaluator *);
 BOOL (*wkFilterWasBlocked)(WebFilterEvaluator *);
 const char* (*wkFilterAddData)(WebFilterEvaluator *, const char* data, int* length);
 const char* (*wkFilterDataComplete)(WebFilterEvaluator *, int* length);
+#endif
 
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
 CGFloat (*wkNSElasticDeltaForTimeDelta)(CGFloat initialPosition, CGFloat initialVelocity, CGFloat elapsedTime);
 CGFloat (*wkNSElasticDeltaForReboundDelta)(CGFloat delta);
 CGFloat (*wkNSReboundDeltaForElasticDelta)(CGFloat delta);

third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebSystemInterface.mm
@@ -205,14 +205,16 @@ void InitWebCoreSystemInterface(void)
     INIT(CFURLRequestAllowAllPostCaching);
 #endif
 
-#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
+#if USE(CONTENT_FILTERING)
     INIT(FilterIsManagedSession);
     INIT(FilterCreateInstance);
     INIT(FilterRelease);
     INIT(FilterWasBlocked);
     INIT(FilterAddData);
     INIT(FilterDataComplete);
+#endif
 
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
     INIT(NSElasticDeltaForTimeDelta);
     INIT(NSElasticDeltaForReboundDelta);
     INIT(NSReboundDeltaForElasticDelta);

third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/mac/WebSystemInterface.mm
@@ -168,14 +168,16 @@ void InitWebCoreSystemInterface(void)
         INIT(CGPathAddRoundedRect);
         INIT(CFURLRequestAllowAllPostCaching);
 
-#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
+#if USE(CONTENT_FILTERING)
         INIT(FilterIsManagedSession);
         INIT(FilterCreateInstance);
         INIT(FilterRelease);
         INIT(FilterWasBlocked);
         INIT(FilterAddData);
         INIT(FilterDataComplete);
+#endif
 
+#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
         INIT(NSElasticDeltaForTimeDelta);
         INIT(NSElasticDeltaForReboundDelta);
         INIT(NSReboundDeltaForElasticDelta);

commit 0368252e78619dfb2682490a0ffb51036deb42bb

BUILD FIX: Platform.h:1212:22: error: '__MAC_OS_X_VERSION_MIN_REQUIRED' is not defined, evaluates to 0 [-Werror,-Wundef]
This fixes the following build error introduced in r140366 for
Bug 107098:

    Platform.h:1212:22: error: '__MAC_OS_X_VERSION_MIN_REQUIRED' is not defined, evaluates to 0 [-Werror,-Wundef]
    #if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
                         ^
    1 error generated.

* wtf/Platform.h: Check !PLATFORM(IOS) first so that iOS builds
do not try to evaluate __MAC_OS_X_VERSION_MIN_REQUIRED.

git-svn-id: svn://svn.chromium.org/blink/trunk@140988 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WTF/wtf/Platform.h
@@ -1205,7 +1205,7 @@
 #endif
 #endif
 
-#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080 && !PLATFORM(IOS)
+#if !PLATFORM(IOS) && PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
 #define WTF_USE_CONTENT_FILTERING 1
 #endif
 