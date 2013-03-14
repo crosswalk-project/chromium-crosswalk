crosswalk-project/chromium-crosswalk

commit d0f9562df8b33950be97453e6edfc19236c986ed

Coordinated Graphics: CoordinatedGraphicsLayer makes CoordinatedGraphicsScene perform via CoordinatedGraphicsState. https://bugs.webkit.org/show_bug.cgi?id=108294
Patch by Gwang Yoon Hwang <ryumiel@company100.net> on 2013-03-05
Reviewed by Anders Carlsson.

Source/WebCore:

CoordinatedLayerTreeHostProxy has too many IPC messages (e.g. SyncCanvas and
CreateTile), and there is a long function chain from CoordinatedGraphicsLayer to
CoordinatedGraphicsScene (4 classes).
If we want to add a new message, we need to add similar functions into 4 classes.

Now CoordinatedLayerTreeHost has only one IPC message for CoordinatedGraphicsLayer:
CommitCoordinatedGraphicsState.
CoordinatedGraphicsLayer makes CoordinatedGraphicsScene run as follows:
1. CoordinatedGraphicsLayer makes a CoordinatedGraphicsLayerState.
2. CoordinatedLayerTreeHost stores all LayerStates.
3. CoordinatedLayerTreeHost sends GraphicsState includes LayerStates to CoordinatedLayerTreeHostProxy
at the moment of flushing via CommitCoordinatedGraphicsState message.
4. CoordinatedGraphicsScene applies all state changes to layers.

There is one big behavior change. All states changes (e.g.
UpdateTiles, SetLayerChildren, and etc..) are performed at the same time, when
CoordinatedGraphicsScene::commitCoordinatedGraphicsState is called.

This patch is based on Dongsung Huang, and Noam's work in
https://bugs.webkit.org/show_bug.cgi?id=108294

No new tests, covered by existing tests.

* Target.pri:
* platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp:
(WebCore::CoordinatedGraphicsLayer::setPosition):
(WebCore::CoordinatedGraphicsLayer::setAnchorPoint):
(WebCore::CoordinatedGraphicsLayer::setSize):
(WebCore::CoordinatedGraphicsLayer::setTransform):
(WebCore::CoordinatedGraphicsLayer::setChildrenTransform):
(WebCore::CoordinatedGraphicsLayer::setPreserves3D):
(WebCore::CoordinatedGraphicsLayer::setMasksToBounds):
(WebCore::CoordinatedGraphicsLayer::setDrawsContent):

third_party/WebKit/Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp
@@ -192,6 +192,7 @@ void CoordinatedGraphicsLayer::setPosition(const FloatPoint& p)
         return;
 
     GraphicsLayer::setPosition(p);
+    m_layerState.positionChanged = true;
     didChangeGeometry();
 }
 
@@ -201,6 +202,7 @@ void CoordinatedGraphicsLayer::setAnchorPoint(const FloatPoint3D& p)
         return;
 
     GraphicsLayer::setAnchorPoint(p);
+    m_layerState.anchorPointChanged = true;
     didChangeGeometry();
 }
 
@@ -210,6 +212,7 @@ void CoordinatedGraphicsLayer::setSize(const FloatSize& size)
         return;
 
     GraphicsLayer::setSize(size);
+    m_layerState.sizeChanged = true;
 
     if (maskLayer())
         maskLayer()->setSize(size);
@@ -222,6 +225,8 @@ void CoordinatedGraphicsLayer::setTransform(const TransformationMatrix& t)
         return;
 
     GraphicsLayer::setTransform(t);
+    m_layerState.transformChanged = true;
+
     didChangeGeometry();
 }
 
@@ -231,6 +236,8 @@ void CoordinatedGraphicsLayer::setChildrenTransform(const TransformationMatrix& 
         return;
 
     GraphicsLayer::setChildrenTransform(t);
+    m_layerState.childrenTransformChanged = true;
+
     didChangeGeometry();
 }
 
@@ -240,6 +247,9 @@ void CoordinatedGraphicsLayer::setPreserves3D(bool b)
         return;
 
     GraphicsLayer::setPreserves3D(b);
+    m_layerState.preserves3D = b;
+    m_layerState.flagsChanged = true;
+
     didChangeGeometry();
 }
 
@@ -248,6 +258,9 @@ void CoordinatedGraphicsLayer::setMasksToBounds(bool b)
     if (masksToBounds() == b)
         return;
     GraphicsLayer::setMasksToBounds(b);
+    m_layerState.masksToBounds = b;
+    m_layerState.flagsChanged = true;
+
     didChangeGeometry();
 }
 
@@ -256,6 +269,8 @@ void CoordinatedGraphicsLayer::setDrawsContent(bool b)
     if (drawsContent() == b)
         return;
     GraphicsLayer::setDrawsContent(b);
+    m_layerState.drawsContent = b;
+    m_layerState.flagsChanged = true;
 
     didChangeLayerState();
 }
@@ -265,6 +280,9 @@ void CoordinatedGraphicsLayer::setContentsVisible(bool b)
     if (contentsAreVisible() == b)
         return;
     GraphicsLayer::setContentsVisible(b);
+    m_layerState.contentsVisible = b;
+    m_layerState.flagsChanged = true;
+
     if (maskLayer())
         maskLayer()->setContentsVisible(b);
 
@@ -278,6 +296,9 @@ void CoordinatedGraphicsLayer::setContentsOpaque(bool b)
     if (m_mainBackingStore)
         m_mainBackingStore->setSupportsAlpha(!b);
     GraphicsLayer::setContentsOpaque(b);
+    m_layerState.contentsOpaque = b;
+    m_layerState.flagsChanged = true;
+
     didChangeLayerState();
 }
 
@@ -287,6 +308,9 @@ void CoordinatedGraphicsLayer::setBackfaceVisibility(bool b)
         return;
 
     GraphicsLayer::setBackfaceVisibility(b);
+    m_layerState.backfaceVisible = b;
+    m_layerState.flagsChanged = true;
+
     didChangeLayerState();
 }
 
@@ -296,6 +320,9 @@ void CoordinatedGraphicsLayer::setOpacity(float opacity)
         return;
 
     GraphicsLayer::setOpacity(opacity);
+    m_layerState.opacity = opacity;
+    m_layerState.opacityChanged = true;
+
     didChangeLayerState();
 }
 
@@ -305,6 +332,9 @@ void CoordinatedGraphicsLayer::setContentsRect(const IntRect& r)
         return;
 
     GraphicsLayer::setContentsRect(r);
+    m_layerState.contentsRect = r;
+    m_layerState.contentsRectChanged = true;
+
     didChangeLayerState();
 }
 
@@ -356,16 +386,23 @@ bool CoordinatedGraphicsLayer::setFilters(const FilterOperations& newFilters)
 {
     if (filters() == newFilters)
         return true;
+
+    if (!GraphicsLayer::setFilters(newFilters))
+        return false;
+
     didChangeFilters();
-    return GraphicsLayer::setFilters(newFilters);
+    return true;
 }
 #endif
 
 void CoordinatedGraphicsLayer::setContentsToSolidColor(const Color& color)
 {
-    if (m_layerInfo.solidColor == color)
+    if (m_layerState.solidColor == color)
         return;
-    m_layerInfo.solidColor = color;
+
+    m_layerState.solidColor = color;
+    m_layerState.solidColorChanged = true;
+
     didChangeLayerState();
 }
 
@@ -375,6 +412,9 @@ void CoordinatedGraphicsLayer::setShowDebugBorder(bool show)
         return;
 
     GraphicsLayer::setShowDebugBorder(show);
+    m_layerState.showDebugBorders = true;
+    m_layerState.flagsChanged = true;
+
     didChangeLayerState();
 }
 
@@ -384,6 +424,9 @@ void CoordinatedGraphicsLayer::setShowRepaintCounter(bool show)
         return;
 
     GraphicsLayer::setShowRepaintCounter(show);
+    m_layerState.showRepaintCounter = true;
+    m_layerState.flagsChanged = true;
+
     didChangeLayerState();
 }
 
@@ -421,6 +464,10 @@ void CoordinatedGraphicsLayer::setMaskLayer(GraphicsLayer* layer)
     layer->setContentsVisible(contentsAreVisible());
     CoordinatedGraphicsLayer* coordinatedLayer = toCoordinatedGraphicsLayer(layer);
     coordinatedLayer->didChangeLayerState();
+
+    m_layerState.mask = coordinatedLayer->id();
+    m_layerState.maskChanged = true;
+
     didChangeLayerState();
 }
 
@@ -442,6 +489,8 @@ void CoordinatedGraphicsLayer::setReplicatedByLayer(GraphicsLayer* layer)
         return;
 
     GraphicsLayer::setReplicatedByLayer(layer);
+    m_layerState.replica = toCoordinatedLayerID(layer);
+    m_layerState.replicaChanged = true;
     didChangeLayerState();
 }
 
@@ -471,6 +520,9 @@ void CoordinatedGraphicsLayer::setFixedToViewport(bool isFixed)
         return;
 
     m_fixedToViewport = isFixed;
+    m_layerState.fixedToViewport = isFixed;
+    m_layerState.flagsChanged = true;
+
     didChangeLayerState();
 }
 
@@ -504,11 +556,10 @@ void CoordinatedGraphicsLayer::syncChildren()
     if (!m_shouldSyncChildren)
         return;
     m_shouldSyncChildren = false;
-    Vector<CoordinatedLayerID> childIDs;
+    m_layerState.childrenChanged = true;
+    m_layerState.children.clear();
     for (size_t i = 0; i < children().size(); ++i)
-        childIDs.append(toCoordinatedLayerID(children()[i]));
-
-    m_coordinator->syncLayerChildren(m_id, childIDs);
+        m_layerState.children.append(toCoordinatedLayerID(children()[i]));
 }
 
 #if ENABLE(CSS_FILTERS)
@@ -517,7 +568,9 @@ void CoordinatedGraphicsLayer::syncFilters()
     if (!m_shouldSyncFilters)
         return;
     m_shouldSyncFilters = false;
-    m_coordinator->syncLayerFilters(m_id, filters());
+
+    m_layerState.filters = GraphicsLayer::filters();
+    m_layerState.filtersChanged = true;
 }
 #endif
 
@@ -538,14 +591,15 @@ void CoordinatedGraphicsLayer::syncImageBacking()
         if (!m_coordinatedImageBacking) {
             m_coordinatedImageBacking = m_coordinator->createImageBackingIfNeeded(m_compositedImage.get());
             m_coordinatedImageBacking->addHost(this);
-            m_layerInfo.imageID = m_coordinatedImageBacking->id();
+            m_layerState.imageID = m_coordinatedImageBacking->id();
         }
 
         m_coordinatedImageBacking->markDirty();
+        m_layerState.imageChanged = true;
     } else
         releaseImageBackingIfNeeded();
 
-    // syncImageBacking() changed m_layerInfo.imageID.
+    // syncImageBacking() changed m_layerState.imageID.
     didChangeLayerState();
 }
 
@@ -554,38 +608,34 @@ void CoordinatedGraphicsLayer::syncLayerState()
     if (!m_shouldSyncLayerState)
         return;
     m_shouldSyncLayerState = false;
-    m_layerInfo.fixedToViewport = fixedToViewport();
 
-    m_layerInfo.backfaceVisible = backfaceVisibility();
-    m_layerInfo.childrenTransform = childrenTransform();
-    m_layerInfo.contentsOpaque = contentsOpaque();
-    m_layerInfo.contentsRect = contentsRect();
-    m_layerInfo.drawsContent = drawsContent();
-    m_layerInfo.contentsVisible = contentsAreVisible();
-    m_layerInfo.mask = toCoordinatedLayerID(maskLayer());
-    m_layerInfo.masksToBounds = masksToBounds();
-    m_layerInfo.opacity = opacity();
-    m_layerInfo.preserves3D = preserves3D();
-    m_layerInfo.replica = toCoordinatedLayerID(replicaLayer());
-    m_layerInfo.transform = transform();
+    m_layerState.childrenTransform = childrenTransform();
+    m_layerState.contentsRect = contentsRect();
+    m_layerState.mask = toCoordinatedLayerID(maskLayer());
+    m_layerState.opacity = opacity();
+    m_layerState.replica = toCoordinatedLayerID(replicaLayer());
+    m_layerState.transform = transform();
 
-    m_layerInfo.anchorPoint = m_adjustedAnchorPoint;
-    m_layerInfo.pos = m_adjustedPosition;
-    m_layerInfo.size = m_adjustedSize;
+    m_layerState.anchorPoint = m_adjustedAnchorPoint;
+    m_layerState.pos = m_adjustedPosition;
+    m_layerState.size = m_adjustedSize;
 
-    m_layerInfo.showDebugBorders = isShowingDebugBorder();
-    if (m_layerInfo.showDebugBorders)
+    if (m_layerState.showDebugBorders)
         updateDebugIndicators();
-    m_layerInfo.showRepaintCounter = isShowingRepaintCounter();
-
-    m_coordinator->syncLayerState(m_id, m_layerInfo);
 }
 
 void CoordinatedGraphicsLayer::setDebugBorder(const Color& color, float width)
 {
-    ASSERT(m_layerInfo.showDebugBorders);
-    m_layerInfo.debugBorderColor = color;
-    m_layerInfo.debugBorderWidth = width;
+    ASSERT(m_layerState.showDebugBorders);
+    if (m_layerState.debugBorderColor != color) {
+        m_layerState.debugBorderColor = color;
+        m_layerState.debugBorderColorChanged = true;
+    }
+
+    if (m_layerState.debugBorderWidth != width) {
+        m_layerState.debugBorderWidth = width;
+        m_layerState.debugBorderWidthChanged = true;
+    }
 }
 
 void CoordinatedGraphicsLayer::syncAnimations()
@@ -594,8 +644,8 @@ void CoordinatedGraphicsLayer::syncAnimations()
         return;
 
     m_shouldSyncAnimations = false;
-
-    m_coordinator->setLayerAnimations(m_id, m_animations);
+    m_layerState.animations = m_animations.getActiveAnimations();
+    m_layerState.animationsChanged = true;
 }
 
 #if USE(GRAPHICS_SURFACE)
@@ -612,7 +662,9 @@ void CoordinatedGraphicsLayer::syncCanvas()
     if (!m_isValidCanvas)
         return;
 
-    m_coordinator->syncCanvas(m_id, m_canvasPlatformLayer);
+    ASSERT(m_canvasPlatformLayer);
+    m_layerState.canvasFrontBuffer = m_canvasPlatformLayer->copyToGraphicsSurface();
+    m_layerState.canvasShouldSwapBuffers = true;
 }
 
 void CoordinatedGraphicsLayer::destroyCanvasIfNeeded()
@@ -621,8 +673,9 @@ void CoordinatedGraphicsLayer::destroyCanvasIfNeeded()
         return;
 
     if (m_isValidCanvas) {
-        m_coordinator->destroyCanvas(m_id);
         m_isValidCanvas = false;
+        m_layerState.canvasToken = GraphicsSurfaceToken();
+        m_layerState.canvasChanged = true;
     }
 
     m_pendingCanvasOperation &= ~DestroyCanvas;
@@ -635,7 +688,9 @@ void CoordinatedGraphicsLayer::createCanvasIfNeeded()
 
     ASSERT(m_canvasPlatformLayer);
     if (!m_isValidCanvas) {
-        m_coordinator->createCanvas(m_id, m_canvasPlatformLayer);
+        m_layerState.canvasSize = m_canvasPlatformLayer->platformLayerSize();
+        m_layerState.canvasToken = m_canvasPlatformLayer->graphicsSurfaceToken();
+        m_layerState.canvasChanged = true;
         m_isValidCanvas = true;
     }
 
@@ -667,9 +722,21 @@ void CoordinatedGraphicsLayer::flushCompositingStateForThisLayerOnly()
 #if USE(GRAPHICS_SURFACE)
     syncCanvas();
 #endif
+
+    m_coordinator->syncLayerState(m_id, m_layerState);
+    resetLayerState();
+
     // Only unset m_movingVisibleRect after we have updated the visible rect after the animation stopped.
     if (!hasActiveTransformAnimation)
         m_movingVisibleRect = false;
+}
+
+void CoordinatedGraphicsLayer::resetLayerState()
+{
+    m_layerState.changeMask = 0;
+    m_layerState.tilesToCreate.clear();
+    m_layerState.tilesToRemove.clear();
+    m_layerState.tilesToUpdate.clear();
 }
 
 bool CoordinatedGraphicsLayer::imageBackingVisible()
@@ -686,7 +753,8 @@ void CoordinatedGraphicsLayer::releaseImageBackingIfNeeded()
     ASSERT(m_coordinator);
     m_coordinatedImageBacking->removeHost(this);
     m_coordinatedImageBacking.clear();
-    m_layerInfo.imageID = InvalidCoordinatedImageBackingID;
+    m_layerState.imageID = InvalidCoordinatedImageBackingID;
+    m_layerState.imageChanged = true;
 }
 
 void CoordinatedGraphicsLayer::tiledBackingStorePaintBegin()
@@ -695,7 +763,8 @@ void CoordinatedGraphicsLayer::tiledBackingStorePaintBegin()
 
 void CoordinatedGraphicsLayer::setRootLayer(bool isRoot)
 {
-    m_layerInfo.isRootLayer = isRoot;
+    m_layerState.isRootLayer = isRoot;
+    m_layerState.flagsChanged = true;
     didChangeLayerState();
 }
 
@@ -751,8 +820,11 @@ void CoordinatedGraphicsLayer::tiledBackingStorePaint(GraphicsContext* context, 
 
 void CoordinatedGraphicsLayer::tiledBackingStorePaintEnd(const Vector<IntRect>& updatedRects)
 {
-    if (isShowingRepaintCounter() && !updatedRects.isEmpty())
-        m_coordinator->setLayerRepaintCount(id(), incrementRepaintCount());
+    if (!isShowingRepaintCounter() || updatedRects.isEmpty())
+        return;
+
+    m_layerState.repaintCount = incrementRepaintCount();
+    m_layerState.repaintCountChanged = true;
 }
 
 void CoordinatedGraphicsLayer::tiledBackingStoreHasPendingTileCreation()
@@ -811,21 +883,32 @@ void CoordinatedGraphicsLayer::createTile(uint32_t tileID, const SurfaceUpdateIn
 {
     ASSERT(m_coordinator);
     ASSERT(m_coordinator->isFlushingLayerChanges());
-    m_coordinator->createTile(id(), tileID, updateInfo, tileRect);
+
+    TileCreationInfo creationInfo;
+    creationInfo.tileID = tileID;
+    creationInfo.scale = updateInfo.scaleFactor;
+
+    m_layerState.tilesToCreate.append(creationInfo);
+    updateTile(tileID, updateInfo, tileRect);
 }
 
 void CoordinatedGraphicsLayer::updateTile(uint32_t tileID, const SurfaceUpdateInfo& updateInfo, const IntRect& tileRect)
 {
     ASSERT(m_coordinator);
     ASSERT(m_coordinator->isFlushingLayerChanges());
-    m_coordinator->updateTile(id(), tileID, updateInfo, tileRect);
+
+    TileUpdateInfo tileUpdateInfo;
+    tileUpdateInfo.tileID = tileID;
+    tileUpdateInfo.tileRect = tileRect;
+    tileUpdateInfo.updateInfo = updateInfo;
+    m_layerState.tilesToUpdate.append(tileUpdateInfo);
 }
 
 void CoordinatedGraphicsLayer::removeTile(uint32_t tileID)
 {
     ASSERT(m_coordinator);
     ASSERT(m_coordinator->isFlushingLayerChanges() || m_isPurging);
-    m_coordinator->removeTile(id(), tileID);
+    m_layerState.tilesToRemove.append(tileID);
 }
 
 void CoordinatedGraphicsLayer::updateContentBuffers()

third_party/WebKit/Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsState.h
@@ -1,0 +1,179 @@
+/*
+ * Copyright (C) 2013 Nokia Corporation and/or its subsidiary(-ies)
+ * Copyright (C) 2013 Company 100, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CoordinatedGraphicsState_h
+#define CoordinatedGraphicsState_h
+
+#if USE(COORDINATED_GRAPHICS)
+
+#include "Color.h"
+#include "FloatRect.h"
+#include "FloatSize.h"
+#include "GraphicsLayerAnimation.h"
+#include "IntRect.h"
+#include "IntSize.h"
+#include "SurfaceUpdateInfo.h"
+#include "TransformationMatrix.h"
+
+#if ENABLE(CSS_FILTERS)
+#include "FilterOperations.h"
+#endif
+
+#if USE(GRAPHICS_SURFACE)
+#include "GraphicsSurfaceToken.h"
+#endif
+
+namespace WebCore {
+
+class CoordinatedSurface;
+
+typedef uint32_t CoordinatedLayerID;
+enum { InvalidCoordinatedLayerID = 0 };
+
+typedef uint64_t CoordinatedImageBackingID;
+enum { InvalidCoordinatedImageBackingID = 0 };
+
+struct TileUpdateInfo {
+    uint32_t tileID;
+    IntRect tileRect;
+    WebCore::SurfaceUpdateInfo updateInfo;
+};
+
+struct TileCreationInfo {
+    uint32_t tileID;
+    float scale;
+};
+
+struct CoordinatedGraphicsLayerState {
+    union {
+        struct {
+            bool positionChanged: 1;
+            bool anchorPointChanged: 1;
+            bool sizeChanged: 1;
+            bool transformChanged: 1;
+            bool childrenTransformChanged: 1;
+            bool contentsRectChanged: 1;
+            bool opacityChanged: 1;
+            bool solidColorChanged: 1;
+            bool debugBorderColorChanged: 1;
+            bool debugBorderWidthChanged: 1;
+            bool replicaChanged: 1;
+            bool maskChanged: 1;
+            bool imageChanged: 1;
+            bool flagsChanged: 1;
+            bool animationsChanged: 1;
+            bool filtersChanged: 1;
+            bool childrenChanged: 1;
+            bool repaintCountChanged : 1;
+            bool canvasChanged: 1;
+            bool canvasShouldSwapBuffers: 1;
+        };
+        unsigned changeMask;
+    };
+    union {
+        struct {
+            bool contentsOpaque : 1;
+            bool drawsContent : 1;
+            bool contentsVisible : 1;
+            bool backfaceVisible : 1;
+            bool masksToBounds : 1;
+            bool preserves3D : 1;
+            bool isRootLayer: 1;
+            bool fixedToViewport : 1;
+            bool showDebugBorders : 1;
+            bool showRepaintCounter : 1;
+        };
+        unsigned flags;
+    };
+
+    CoordinatedGraphicsLayerState()
+        : changeMask(0)
+        , contentsOpaque(false)
+        , drawsContent(false)
+        , contentsVisible(true)
+        , backfaceVisible(true)
+        , masksToBounds(false)
+        , preserves3D(false)
+        , isRootLayer(false)
+        , fixedToViewport(false)
+        , showDebugBorders(false)
+        , showRepaintCounter(false)
+        , opacity(0)
+        , debugBorderWidth(0)
+        , replica(InvalidCoordinatedLayerID)
+        , mask(InvalidCoordinatedLayerID)
+        , imageID(InvalidCoordinatedImageBackingID)
+        , canvasFrontBuffer(0)
+    {
+    }
+
+    FloatPoint pos;
+    FloatPoint3D anchorPoint;
+    FloatSize size;
+    TransformationMatrix transform;
+    TransformationMatrix childrenTransform;
+    IntRect contentsRect;
+    float opacity;
+    Color solidColor;
+    Color debugBorderColor;
+    float debugBorderWidth;
+#if ENABLE(CSS_FILTERS)
+    FilterOperations filters;
+#endif
+    GraphicsLayerAnimations animations;
+    Vector<uint32_t> children;
+    Vector<TileCreationInfo> tilesToCreate;
+    Vector<uint32_t> tilesToRemove;
+    CoordinatedLayerID replica;
+    CoordinatedLayerID mask;
+    CoordinatedImageBackingID imageID;
+
+    unsigned repaintCount;
+    Vector<TileUpdateInfo> tilesToUpdate;
+
+#if USE(GRAPHICS_SURFACE)
+    IntSize canvasSize;
+    GraphicsSurfaceToken canvasToken;
+    uint32_t canvasFrontBuffer;
+#endif
+};
+
+struct CoordinatedGraphicsState {
+    uint32_t rootCompositingLayer;
+    Color backgroundColor;
+    FloatPoint scrollPosition;
+    IntSize contentsSize;
+    IntRect coveredRect;
+
+    Vector<std::pair<CoordinatedLayerID, CoordinatedGraphicsLayerState> > layersToUpdate;
+    Vector<std::pair<CoordinatedImageBackingID, RefPtr<CoordinatedSurface> > > imagesToUpdate;
+};
+
+} // namespace WebCore
+
+#endif // USE(COORDINATED_GRAPHICS)
+
+#endif // CoordinatedGraphicsState_h

third_party/WebKit/Source/WebCore/Target.pri
@@ -4117,8 +4117,8 @@ use?(3D_GRAPHICS) {
         platform/graphics/texmap/coordinated/CoordinatedCustomFilterProgram.h \
         platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.h \
         platform/graphics/texmap/coordinated/CoordinatedGraphicsScene.h \
+        platform/graphics/texmap/coordinated/CoordinatedGraphicsState.h \
         platform/graphics/texmap/coordinated/CoordinatedImageBacking.h \
-        platform/graphics/texmap/coordinated/CoordinatedLayerInfo.h \
         platform/graphics/texmap/coordinated/CoordinatedSurface.h \
         platform/graphics/texmap/coordinated/CoordinatedTile.h \
         platform/graphics/texmap/coordinated/SurfaceUpdateInfo.h \

third_party/WebKit/Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.h
@@ -21,8 +21,8 @@
 #ifndef CoordinatedGraphicsLayer_h
 #define CoordinatedGraphicsLayer_h
 
+#include "CoordinatedGraphicsState.h"
 #include "CoordinatedImageBacking.h"
-#include "CoordinatedLayerInfo.h"
 #include "CoordinatedTile.h"
 #include "FloatPoint3D.h"
 #include "GraphicsLayer.h"
@@ -48,31 +48,12 @@ class GraphicsLayerAnimations;
 class CoordinatedGraphicsLayerClient {
 public:
     virtual bool isFlushingLayerChanges() const = 0;
-
-    // CoordinatedTileClient
-    virtual void createTile(CoordinatedLayerID, uint32_t tileID, const SurfaceUpdateInfo&, const IntRect&) = 0;
-    virtual void updateTile(CoordinatedLayerID, uint32_t tileID, const SurfaceUpdateInfo&, const IntRect&) = 0;
-    virtual void removeTile(CoordinatedLayerID, uint32_t tileID) = 0;
-
     virtual FloatRect visibleContentsRect() const = 0;
     virtual PassRefPtr<CoordinatedImageBacking> createImageBackingIfNeeded(Image*) = 0;
-    virtual void syncLayerState(CoordinatedLayerID, const CoordinatedLayerInfo&) = 0;
-    virtual void syncLayerChildren(CoordinatedLayerID, const Vector<CoordinatedLayerID>&) = 0;
-#if ENABLE(CSS_FILTERS)
-    virtual void syncLayerFilters(CoordinatedLayerID, const FilterOperations&) = 0;
-#endif
-#if USE(GRAPHICS_SURFACE)
-    virtual void createCanvas(CoordinatedLayerID, PlatformLayer*) = 0;
-    virtual void syncCanvas(CoordinatedLayerID, PlatformLayer*) = 0;
-    virtual void destroyCanvas(CoordinatedLayerID) = 0;
-#endif
-
-    virtual void setLayerRepaintCount(CoordinatedLayerID, int) = 0;
-
-    virtual void setLayerAnimations(CoordinatedLayerID, const GraphicsLayerAnimations&) = 0;
-
     virtual void detachLayer(CoordinatedGraphicsLayer*) = 0;
     virtual PassOwnPtr<GraphicsContext> beginContentUpdate(const IntSize&, CoordinatedSurface::Flags, uint32_t& atlasID, IntPoint&) = 0;
+
+    virtual void syncLayerState(CoordinatedLayerID, CoordinatedGraphicsLayerState&) = 0;
 };
 
 class CoordinatedGraphicsLayer : public GraphicsLayer
@@ -190,6 +171,7 @@ private:
 #endif
     void didChangeImageBacking();
 
+    void resetLayerState();
     void syncLayerState();
     void syncAnimations();
     void syncChildren();
@@ -216,7 +198,7 @@ private:
     void animationStartedTimerFired(Timer<CoordinatedGraphicsLayer>*);
 
     CoordinatedLayerID m_id;
-    CoordinatedLayerInfo m_layerInfo;
+    CoordinatedGraphicsLayerState m_layerState;
     GraphicsLayerTransform m_layerTransform;
     TransformationMatrix m_cachedInverseTransform;
     FloatSize m_pixelAlignmentOffset;

commit 306807ba5dac1991a10d7be90e63a6bfb86ba228

[Texmap] Synchronize layers only if the layer has been changed. https://bugs.webkit.org/show_bug.cgi?id=112095
Patch by No'am Rosenthal <noam@webkit.org> on 2013-03-14
Reviewed by Allan Sandfeld Jensen.

A regression was introduced in r144787, causing an infinite IPC commitState/renderNextFrame
loop.
This patch fixes this by making sure we only commit layer states that have actual pending
changes.

No new tests, this is an optimization.

* platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp:
(WebCore::CoordinatedGraphicsLayer::flushCompositingStateForThisLayerOnly):
* platform/graphics/texmap/coordinated/CoordinatedGraphicsState.h:
(CoordinatedGraphicsLayerState):
(WebCore::CoordinatedGraphicsLayerState::hasPendingChanges):

git-svn-id: svn://svn.chromium.org/blink/trunk@145806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp
@@ -760,8 +760,10 @@ void CoordinatedGraphicsLayer::flushCompositingStateForThisLayerOnly()
     syncCanvas();
 #endif
 
-    m_coordinator->syncLayerState(m_id, m_layerState);
-    resetLayerState();
+    if (m_layerState.hasPendingChanges()) {
+        m_coordinator->syncLayerState(m_id, m_layerState);
+        resetLayerState();
+    }
 
     // Only unset m_movingVisibleRect after we have updated the visible rect after the animation stopped.
     if (!hasActiveTransformAnimation)

third_party/WebKit/Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsState.h
@@ -165,6 +165,11 @@ struct CoordinatedGraphicsLayerState {
 #endif
 
     IntSize committedScrollOffset;
+
+    bool hasPendingChanges() const
+    {
+        return changeMask || tilesToUpdate.size() || tilesToRemove.size() || tilesToCreate.size();
+    }
 };
 
 struct CoordinatedGraphicsState {