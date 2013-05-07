crosswalk-project/chromium-crosswalk

commit b3088996994cfdaf1cb9b13c98ee2d4ae5c77c49

Round when converting percentage length to LayoutUnit
In minimumValueForLength we currently floor when converting from float
to LayoutUnit for percentage lengths. This can lead to differences in
width or height for elements using percentage lengths vs auto lengths.

BUG=234652
TEST=fast/sub-pixel/height-from-top-bottom.html
R=leviw@chromium.org

Review URL: https://chromiumcodereview.appspot.com/14495018

git-svn-id: svn://svn.chromium.org/blink/trunk@149449 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/css/LengthFunctions.cpp
@@ -49,7 +49,7 @@ LayoutUnit minimumValueForLength(const Length& length, LayoutUnit maximumValue, 
         if (roundPercentages)
             return static_cast<LayoutUnit>(round(maximumValue * length.percent() / 100.0f));
         // Don't remove the extra cast to float. It is needed for rounding on 32-bit Intel machines that use the FPU stack.
-        return static_cast<LayoutUnit>(static_cast<float>(maximumValue * length.percent() / 100.0f));
+        return LayoutUnit::fromFloatRound(static_cast<float>(maximumValue * length.percent() / 100.0f));
     case Calculated:
         return length.nonNanCalculatedValue(maximumValue);
     case ViewportPercentageWidth:

third_party/WebKit/LayoutTests/TestExpectations
@@ -2679,6 +2679,12 @@ crbug.com/169374 transforms/3d/point-mapping/3d-point-mapping-coplanar.html [ Im
 crbug.com/169374 transforms/3d/point-mapping/3d-point-mapping-overlapping.html [ ImageOnlyFailure ]
 crbug.com/169374 transitions/default-timing-function.html [ ImageOnlyFailure ]
 
+# Needs rebaselines once bots cyclce.
+crbug.com/234652 fast/css/getComputedStyle/getComputedStyle-margin-percentage.html [ Pass Failure ImageOnlyFailure ]
+crbug.com/234652 fast/css/percentage-non-integer.html [ Pass Failure ImageOnlyFailure ]
+crbug.com/234652 svg/wicd/rightsizing-grid.xhtml [ Pass Failure ImageOnlyFailure ]
+crbug.com/234652 svg/wicd/test-rightsizing-b.xhtml [ Pass Failure ImageOnlyFailure ]
+
 # started failing in r149321/r149322
 crbug.com/236555 fast/forms/calendar-picker/week-picker-key-operations.html [ Failure ]
 

third_party/WebKit/LayoutTests/fast/css/getComputedStyle/getComputedStyle-margin-percentage-expected.txt
@@ -1,7 +1,7 @@
 Test calling getPropertyValue on computed margin styles.
 
 margin-left: 258.71875px
-margin-top: 78.390625px
+margin-top: 78.40625px
 margin-right: 113.28125px
 margin-bottom: 0px
 Test

third_party/WebKit/LayoutTests/platform/chromium-win/fast/css/percentage-non-integer-expected.txt
@@ -29,7 +29,7 @@ layer at (0,0) size 800x585
         RenderBlock {DIV} at (0,44) size 910x4
           RenderBlock {DIV} at (0,0) size 101x4 [bgcolor=#0000007F]
         RenderBlock {DIV} at (0,48) size 910x4
-          RenderBlock {DIV} at (0,0) size 101x4 [bgcolor=#0000007F]
+          RenderBlock {DIV} at (0,0) size 102x4 [bgcolor=#0000007F]
         RenderBlock {DIV} at (0,52) size 900x4
           RenderBlock {DIV} at (0,0) size 101x4 [bgcolor=#0000007F]
         RenderBlock {DIV} at (0,56) size 890x4

third_party/WebKit/LayoutTests/platform/chromium-win/svg/wicd/test-rightsizing-b-expected.txt
@@ -50,7 +50,7 @@ layer at (0,0) size 785x856
                 RenderSVGContainer {g} at (9,22) size 56x31
                   RenderSVGText {text} at (26,59) size 148x83 contains 1 chunk(s)
                     RenderSVGInlineText {#text} at (0,0) size 148x83
-                      chunk 1 (middle anchor) text run 1 at (26.22,125.00) startOffset 0 endOffset 3 width 147.56: "SVG"
+                      chunk 1 (middle anchor) text run 1 at (26.23,125.00) startOffset 0 endOffset 3 width 147.53: "SVG"
           RenderText {#text} at (524,280) size 5x18
             text run at (524,280) width 5: " "
           RenderEmbeddedObject {object} at (528,258) size 38x37 [bgcolor=#FF0000]

commit 2f49438bdcf275971720d90e56f2d701a6d6871b

Percentage widths adds up to more than 100%.
Due to rounding float and inline blocks sized in percentages can add up
to just over 100% and thus overflow or cause wrapping.
Regression from r149449.

R=leviw@chromium.org
BUG=237751
TEST=LayoutTests/fast/sub-pixel/float-percentage-widths.html

Review URL: https://chromiumcodereview.appspot.com/15026009

git-svn-id: svn://svn.chromium.org/blink/trunk@149912 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/css/LengthFunctions.cpp
@@ -49,7 +49,7 @@ LayoutUnit minimumValueForLength(const Length& length, LayoutUnit maximumValue, 
         if (roundPercentages)
             return static_cast<LayoutUnit>(round(maximumValue * length.percent() / 100.0f));
         // Don't remove the extra cast to float. It is needed for rounding on 32-bit Intel machines that use the FPU stack.
-        return LayoutUnit::fromFloatRound(static_cast<float>(maximumValue * length.percent() / 100.0f));
+        return static_cast<float>(maximumValue * length.percent() / 100.0f);
     case Calculated:
         return length.nonNanCalculatedValue(maximumValue);
     case ViewportPercentageWidth:

third_party/WebKit/LayoutTests/TestExpectations
@@ -1606,6 +1606,12 @@ crbug.com/237270 http/tests/history/replacestate-post-to-get.html [ Pass Timeout
 crbug.com/237270 http/tests/images/jpeg-partial-load.html [ Pass Timeout ]
 crbug.com/237270 http/tests/images/png-partial-load-no-alpha.html [ Pass Timeout ]
 
+# Needs rebaselines once bots cyclce.
+crbug.com/237751 fast/css/getComputedStyle/getComputedStyle-margin-percentage.html [ Pass Failure ImageOnlyFailure ]
+crbug.com/237751 fast/css/percentage-non-integer.html [ Pass Failure ImageOnlyFailure ]
+crbug.com/237751 svg/wicd/rightsizing-grid.xhtml [ Pass Failure ImageOnlyFailure ]
+crbug.com/237751 svg/wicd/test-rightsizing-b.xhtml [ Pass Failure ImageOnlyFailure ]
+
 # Layout root not getting laid out before renderer is destroyed. See bug 238363.
 crbug.com/238363 compositing/overflow/overflow-hidden-canvas-layer.html [ Crash ]
 crbug.com/238363 fast/dom/node-move-to-new-document-crash-main.html [ Crash ]

third_party/WebKit/LayoutTests/fast/sub-pixel/float-percentage-widths-expected.txt
@@ -1,0 +1,4 @@
+PASS All boxes are on the same line.
+The three boxes below should all appear on the same line.
+
+   

third_party/WebKit/LayoutTests/fast/sub-pixel/float-percentage-widths.html
@@ -1,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            .container { width: 249px; margin: 0 auto; }
+            .child { width: 30%; margin-right: 5%; margin-bottom: 10px; background: black; float: left; color: white; }
+            .child.last { margin-right: 0; }
+        </style>
+        <script src="../js/resources/js-test-pre.js"></script>
+    </head>
+    <body>
+        <p>The three boxes below should all appear on the same line.</p>
+        <div class="container">
+            <div class="child">&nbsp;</div>
+            <div class="child">&nbsp;</div>
+            <div class="child last">&nbsp;</div>
+        </div>
+        <script>
+            var children = document.getElementsByClassName('child');
+            var firstChildTop = children[0].getBoundingClientRect().top;
+            var lastChildTop = children[2].getBoundingClientRect().top;
+            if (firstChildTop == lastChildTop)
+                testPassed('All boxes are on the same line.');
+            else
+                shouldBe('children[0].getBoundingClientRect().top', "children[2].getBoundingClientRect().top");
+        </script>
+    </body>
+</html>