76b959476329df5b7d262a58e69e882f376bccc4
jered@chromium.org <jered@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2013-05-22 16:20:01 +0000

    Merge 201131 "Revert "Revert 200954 "Revert "InstantExtended: Pr..."
    > Revert "Revert 200954 "Revert "InstantExtended: Prevent spurious themech...""
    >
    > This reverts commit 2671a2c404d0b5eeb6af9d19f375c85caaa9305b.
    >
    > BUG=238671
    > TBR=jered@chromium.org
    >
    > Review URL: https://chromiumcodereview.appspot.com/15469005
    
    TBR=jered@chromium.org
    
    Review URL: https://codereview.chromium.org/15747005
    
    git-svn-id: http://src.chromium.org/svn/branches/1500/src@201551 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- chrome/common/instant_types.h
+++ chrome/common/instant_types.h
@@ -123,8 +123,6 @@ enum ThemeBackgroundImageTiling {
   THEME_BKGRND_IMAGE_REPEAT,
 };
 
-// Update IsThemeInfoEqual in chrome/renderer/searchbox/searchbox.cc
-// whenever any fields are added/removed.
 struct ThemeBackgroundInfo {
   ThemeBackgroundInfo();
   ~ThemeBackgroundInfo();
@@ -160,8 +158,6 @@ struct ThemeBackgroundInfo {
   bool has_attribution;
 };
 
-// Update AreMostVisitedItemsEqual in chrome/renderer/searchbox/searchbox.cc
-// whenever any fields are added/removed.
 struct InstantMostVisitedItem {
   // The URL of the Most Visited item.
   GURL url;
