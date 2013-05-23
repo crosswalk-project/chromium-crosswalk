crosswalk-project/chromium-crosswalk

commit 6c148ea231554ccfefe9f90146fdef65e4f1d11c

Reland "Unifies ScrollView and RenderLayer to use non-shi..."
DO NOT REVERT THIS if AutomationRendererHelperTest.RTLSnapshot fails when
blink is rolled. A chromium patch is reviewed and ready for dcommit.
https://chromiumcodereview.appspot.com/15006004/

> Unifies ScrollView and RenderLayer to use non-shifted [minPos, maxPos] scroll offset
>
> For pages with negative overflow (due to RTL or writing mode), there are
> two sets of scroll offset defined. One that ranges from [0, maxPos - minPos]
> and the other one ranges from [minPos, maxPos]. To reduce confusion, this
> patch unifies ScrollView and RenderLayer to use the second definition of
> scroll offset.
>
> BUG=232063
> TEST=manual
> R=jamesr@chromium.org,jchaffraix@chromium.org
>
> Review URL: https://chromiumcodereview.appspot.com/14807003

TBR=jamesr@chromium.org,jchaffraix@chromium.org

Review URL: https://codereview.chromium.org/15093011

git-svn-id: svn://svn.chromium.org/blink/trunk@150264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/rendering/RenderLayer.cpp
@@ -2202,10 +2202,10 @@ void RenderLayer::scrollToOffset(const IntSize& scrollOffset, ScrollOffsetClampi
 {
     IntSize newScrollOffset = clamp == ScrollOffsetClamped ? clampScrollOffset(scrollOffset) : scrollOffset;
     if (newScrollOffset != adjustedScrollOffset())
-        scrollToOffsetWithoutAnimation(IntPoint(newScrollOffset));
+        scrollToOffsetWithoutAnimation(-scrollOrigin() + newScrollOffset);
 }
 
-void RenderLayer::scrollTo(int x, int y)
+void RenderLayer::setScrollOffset(const IntPoint& newScrollOffset)
 {
     RenderBox* box = renderBox();
     if (!box)
@@ -2217,15 +2217,9 @@ void RenderLayer::scrollTo(int x, int y)
             computeScrollDimensions();
     }
     
-    // FIXME: Eventually, we will want to perform a blit.  For now never
-    // blit, since the check for blitting is going to be very
-    // complicated (since it will involve testing whether our layer
-    // is either occluded by another layer or clipped by an enclosing
-    // layer or contains fixed backgrounds, etc.).
-    IntSize newScrollOffset = IntSize(x - scrollOrigin().x(), y - scrollOrigin().y());
-    if (m_scrollOffset == newScrollOffset)
+    if (m_scrollOffset == toIntSize(newScrollOffset))
         return;
-    m_scrollOffset = newScrollOffset;
+    m_scrollOffset = toIntSize(newScrollOffset);
 
     Frame* frame = renderer()->frame();
     InspectorInstrumentation::willScrollLayer(frame);
@@ -2581,11 +2575,6 @@ int RenderLayer::scrollSize(ScrollbarOrientation orientation) const
     return scrollbar ? (scrollbar->totalSize() - scrollbar->visibleSize()) : 0;
 }
 
-void RenderLayer::setScrollOffset(const IntPoint& offset)
-{
-    scrollTo(offset.x(), offset.y());
-}
-
 int RenderLayer::scrollPosition(Scrollbar* scrollbar) const
 {
     if (scrollbar->orientation() == HorizontalScrollbar)
@@ -2607,8 +2596,13 @@ IntPoint RenderLayer::minimumScrollPosition() const
 
 IntPoint RenderLayer::maximumScrollPosition() const
 {
-    // FIXME: m_scrollSize may not be up-to-date if m_scrollDimensionsDirty is true.
-    return -scrollOrigin() + roundedIntSize(m_scrollSize) - visibleContentRect(IncludeScrollbars).size();
+    RenderBox* box = renderBox();
+    if (!box)
+        return -scrollOrigin();
+
+    LayoutRect overflowRect(box->layoutOverflowRect());
+    box->flipForWritingMode(overflowRect);
+    return -scrollOrigin() + enclosingIntRect(overflowRect).size() - enclosingIntRect(box->clientBoxRect()).size();
 }
 
 IntRect RenderLayer::visibleContentRect(VisibleContentRectIncludesScrollbars scrollbarInclusion) const
@@ -3250,7 +3244,7 @@ void RenderLayer::updateScrollInfoAfterLayout()
     updateScrollbarsAfterLayout();
 
     if (originalScrollOffset != adjustedScrollOffset())
-        scrollToOffsetWithoutAnimation(IntPoint(adjustedScrollOffset()));
+        scrollToOffsetWithoutAnimation(-scrollOrigin() + adjustedScrollOffset());
 
     // Composited scrolling may need to be enabled or disabled if the amount of overflow changed.
     if (renderer()->view() && compositor()->updateLayerCompositingState(this))

third_party/WebKit/LayoutTests/TestExpectations
@@ -1699,3 +1699,7 @@ crbug.com/238858 [ Win7 Debug ] virtual/gpu/fast/hidpi/video-controls-in-hidpi.h
 crbug.com/239077 fast/canvas/webgl/index-validation-with-resized-buffer.html  [ Crash ]
 
 crbug.com/240400 fast/dom/console-log-stack-overflow.html [ Pass Failure ]
+
+# Transient. PNGs need to be uploaded separately.
+crbug.com/232063 virtual/gpu/compositedscrolling/scrollbars/scrollbars-on-positioned-content.html [ ImageOnlyFailure ]
+crbug.com/232063 virtual/gpu/compositedscrolling/scrollbars/custom-scrollbar-with-incomplete-style.html [ ImageOnlyFailure ]

third_party/WebKit/LayoutTests/compositing/overflow/composited-scrolling-creates-a-stacking-container-expected.txt
@@ -12,7 +12,7 @@
           (children 4
             (GraphicsLayer
               (position 1.00 1.00)
-              (bounds 200.00 200.00)
+              (bounds 185.00 185.00)
               (children 1
                 (GraphicsLayer
                   (bounds 200.00 300.00)

third_party/WebKit/LayoutTests/compositing/overflow/composited-scrolling-paint-phases-expected.txt
@@ -27,7 +27,7 @@
           (children 4
             (GraphicsLayer
               (position 1.00 1.00)
-              (bounds 200.00 200.00)
+              (bounds 185.00 185.00)
               (paintingPhases
                 GraphicsLayerPaintBackground
                 GraphicsLayerPaintForeground

third_party/WebKit/LayoutTests/compositing/overflow/scrolling-content-clip-to-viewport-expected.txt
@@ -9,7 +9,7 @@
           (bounds 320.00 340.00)
           (children 4
             (GraphicsLayer
-              (bounds 320.00 340.00)
+              (bounds 305.00 325.00)
               (children 1
                 (GraphicsLayer
                   (bounds 305.00 1224.00)

third_party/WebKit/LayoutTests/compositing/overflow/scrolling-without-painting-expected.txt
@@ -12,7 +12,7 @@
           (children 4
             (GraphicsLayer
               (position 1.00 1.00)
-              (bounds 200.00 200.00)
+              (bounds 185.00 185.00)
               (children 1
                 (GraphicsLayer
                   (position 0.00 -25.00)

third_party/WebKit/LayoutTests/compositing/overflow/textarea-scroll-touch-expected.txt
@@ -15,7 +15,7 @@
           (children 3
             (GraphicsLayer
               (position 1.00 1.00)
-              (bounds 204.00 124.00)
+              (bounds 189.00 124.00)
               (children 1
                 (GraphicsLayer
                   (position 0.00 -50.00)
@@ -45,7 +45,7 @@
           (children 3
             (GraphicsLayer
               (position 1.00 1.00)
-              (bounds 204.00 124.00)
+              (bounds 189.00 124.00)
               (children 1
                 (GraphicsLayer
                   (position 0.00 -50.00)

third_party/WebKit/LayoutTests/compositing/overflow/updating-scrolling-content-expected.txt
@@ -10,7 +10,7 @@
           (bounds 200.00 200.00)
           (children 4
             (GraphicsLayer
-              (bounds 200.00 200.00)
+              (bounds 185.00 185.00)
               (children 1
                 (GraphicsLayer
                   (bounds 185.00 1200.00)

commit 3c84eccacf5b489d7d48aeea8688b517a5dc6ba7

A RenderLayer shouldn't report scroll range if it doesn't scroll
This fixes a regression introduced in r150264, that scrolling a frame feels
choppy with pages which use <html style="height:100%">.

The cause of this bug is that both FrameView and the RenderLayer owned by the
RenderView reports scroll range, however the RenderView doesn't actually
handle scrolling. Therefore some of the scroll delta get absorbed by the
RenderView.

BUG=242113
R=jchaffraix@chromium.org

Review URL: https://codereview.chromium.org/15663002

git-svn-id: svn://svn.chromium.org/blink/trunk@150938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/rendering/RenderLayer.cpp
@@ -2580,7 +2580,7 @@ IntPoint RenderLayer::minimumScrollPosition() const
 IntPoint RenderLayer::maximumScrollPosition() const
 {
     RenderBox* box = renderBox();
-    if (!box)
+    if (!box || !box->hasOverflowClip())
         return -scrollOrigin();
 
     LayoutRect overflowRect(box->layoutOverflowRect());

third_party/WebKit/LayoutTests/fast/events/mouse-wheel-main-frame-scroll-expected.html
@@ -1,0 +1,10 @@
+<!DOCTYPE HTML>
+<html style="height:100%; overflow:scroll;">
+<body>
+<div style="height:1000px;">
+</div>
+<script>
+window.scrollTo(0, 100);
+</script>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/events/mouse-wheel-main-frame-scroll.html
@@ -1,0 +1,17 @@
+<!DOCTYPE HTML>
+<html style="height:100%; overflow:scroll;">
+<head>
+<script>
+function test() {
+    if (window.eventSender) {
+        eventSender.mouseMoveTo(50, 50);
+        eventSender.continuousMouseScrollBy(0, -100);
+    }
+}
+</script>
+</head>
+<body onload="test()">
+<div style="height:1000px;">
+</div>
+</body>
+</html>