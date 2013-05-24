475af496ada908b586f8b5dd56c77622ff30bd7f
mmenke@chromium.org <mmenke@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2013-05-24 16:17:21 +0000

    Merge 200356 "net:  Return LoadTiming information in the case of a cache hit"
    > net:  Return LoadTiming information in the case of a cache hit.
    > If the request went over the wire before headers were returned,
    > return information about the last over the wire request.  Otherwise,
    > set send times to be when the cache entry was opened, and
    > receive_header_start to be when the headers were read.
    >
    > BUG=239842
    >
    > Review URL: https://chromiumcodereview.appspot.com/14625012
    
    TBR=mmenke@chromium.org
    Review URL: https://codereview.chromium.org/15983006
    
    git-svn-id: http://src.chromium.org/svn/branches/1500/src@202103 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- net/url_request/url_request_http_job.cc
+++ net/url_request/url_request_http_job.cc
@@ -415,6 +415,7 @@ void URLRequestHttpJob::DestroyTransaction() {
   DoneWithRequest(ABORTED);
   transaction_.reset();
   response_info_ = NULL;
+  receive_headers_end_ = base::TimeTicks();
 }
 
 void URLRequestHttpJob::StartTransaction() {
@@ -818,6 +819,8 @@ void URLRequestHttpJob::OnStartCompleted(int result) {
   if (!transaction_.get())
     return;
 
+  receive_headers_end_ = base::TimeTicks::Now();
+
   // Clear the IO_PENDING status
   SetStatus(URLRequestStatus());
 
@@ -917,6 +920,7 @@ void URLRequestHttpJob::RestartTransactionWithAuth(
 
   // These will be reset in OnStartCompleted.
   response_info_ = NULL;
+  receive_headers_end_ = base::TimeTicks();
   response_cookies_.clear();
 
   ResetTimer();
@@ -981,8 +985,12 @@ void URLRequestHttpJob::GetResponseInfo(HttpResponseInfo* info) {
 
 void URLRequestHttpJob::GetLoadTimingInfo(
     LoadTimingInfo* load_timing_info) const {
-  if (transaction_)
-    transaction_->GetLoadTimingInfo(load_timing_info);
+  // If haven't made it far enough to receive any headers, don't return
+  // anything.  This makes for more consistent behavior in the case of errors.
+  if (!transaction_ || receive_headers_end_.is_null())
+    return;
+  if (transaction_->GetLoadTimingInfo(load_timing_info))
+    load_timing_info->receive_headers_end = receive_headers_end_;
 }
 
 bool URLRequestHttpJob::GetResponseCookies(std::vector<std::string>* cookies) {
@@ -1132,6 +1140,7 @@ void URLRequestHttpJob::CancelAuth() {
 
   // These will be reset in OnStartCompleted.
   response_info_ = NULL;
+  receive_headers_end_ = base::TimeTicks::Now();
   response_cookies_.clear();
 
   ResetTimer();
@@ -1155,6 +1164,7 @@ void URLRequestHttpJob::ContinueWithCertificate(
   DCHECK(transaction_.get());
 
   DCHECK(!response_info_) << "should not have a response yet";
+  receive_headers_end_ = base::TimeTicks();
 
   ResetTimer();
 
@@ -1180,6 +1190,7 @@ void URLRequestHttpJob::ContinueDespiteLastError() {
     return;
 
   DCHECK(!response_info_) << "should not have a response yet";
+  receive_headers_end_ = base::TimeTicks();
 
   ResetTimer();
 
