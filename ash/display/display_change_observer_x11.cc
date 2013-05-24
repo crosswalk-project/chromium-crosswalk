c64058ae054011a5db906717c7c250803ba38aee
mukai@chromium.org <mukai@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2013-05-24 23:30:00 +0000

    Manual Merge 201756 "Changes how to compute the id of a display." (2nd)
    > Due to the discussion in crbug.com/240341, we prefer to use
    > the name field rather than the product_code field.
    >
    > BUG=240341
    > R=oshima@chromium.org, marcheu@chromium.org
    > TEST=compilation passed
    
    The first attempt of the merge was reverted due to a compile error.
    This 2nd manual merge includes the fix for the error (adding a
    missing include).
    
    TBR=oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/15830003
    
    git-svn-id: http://src.chromium.org/svn/branches/1500/src@202225 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- ash/display/display_change_observer_x11.cc
+++ ash/display/display_change_observer_x11.cc
@@ -15,6 +15,7 @@
 #include "ash/display/display_info.h"
 #include "ash/display/display_manager.h"
 #include "ash/shell.h"
+#include "base/hash.h"
 #include "base/message_pump_aurax11.h"
 #include "grit/ash_strings.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -84,13 +85,20 @@ std::string GetDisplayName(XID output_id) {
 
 int64 GetDisplayId(XID output_id, int output_index) {
   uint16 manufacturer_id = 0;
-  uint16 product_code = 0;
-  if (ui::GetOutputDeviceData(
-          output_id, &manufacturer_id, &product_code, NULL) &&
-      manufacturer_id != 0) {
+  std::string product_name;
+
+  // ui::GetOutputDeviceData fails if it doesn't have product_name.
+  ui::GetOutputDeviceData(output_id, &manufacturer_id, NULL, &product_name);
+
+  // Generates product specific value from product_name instead of product code.
+  // See crbug.com/240341
+  uint32 product_code_hash = product_name.empty() ?
+      0 : base::Hash(product_name);
+  if (manufacturer_id != 0) {
     // An ID based on display's index will be assigned later if this call
     // fails.
-    return gfx::Display::GetID(manufacturer_id, product_code, output_index);
+    return gfx::Display::GetID(
+        manufacturer_id, product_code_hash, output_index);
   }
   return gfx::Display::kInvalidDisplayID;
 }
