crosswalk-project/chromium-crosswalk

commit 42b3625e24bc4c2981e0e566b47d25e520fc17d0

Remove unused blink accessibility code.
The only functional change is to clickPoint, where we're now returning
the center of the element rect rather than the selection rect. There's
no difference 99% of the time. If that 1% is needed, we could rewrite
that logic in far less code.

Everything else was literally unused. Much is already implemented in
Chromium's browser process; the rest couldn't be used without refactoring
anyway.

BUG=247082

Review URL: https://chromiumcodereview.appspot.com/16489004

git-svn-id: svn://svn.chromium.org/blink/trunk@152036 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/accessibility/AXObjectCache.h
@@ -83,8 +83,6 @@ public:
 
     // Returns the root object for the entire document.
     AccessibilityObject* rootObject();
-    // Returns the root object for a specific frame.
-    AccessibilityObject* rootObjectForFrame(Frame*);
     
     // For AX objects with elements that back them.
     AccessibilityObject* getOrCreate(RenderObject*);

third_party/WebKit/Source/core/accessibility/AXObjectCache.cpp
@@ -396,16 +396,6 @@ AccessibilityObject* AXObjectCache::rootObject()
     return getOrCreate(m_document->view());
 }
 
-AccessibilityObject* AXObjectCache::rootObjectForFrame(Frame* frame)
-{
-    if (!gAccessibilityEnabled)
-        return 0;
-
-    if (!frame)
-        return 0;
-    return getOrCreate(frame->view());
-}    
-    
 AccessibilityObject* AXObjectCache::getOrCreate(AccessibilityRole role)
 {
     RefPtr<AccessibilityObject> obj = 0;

third_party/WebKit/Source/core/accessibility/AccessibilityListBox.cpp
@@ -123,20 +123,6 @@ void AccessibilityListBox::selectedChildren(AccessibilityChildrenVector& result)
     }    
 }
 
-void AccessibilityListBox::visibleChildren(AccessibilityChildrenVector& result)
-{
-    ASSERT(result.isEmpty());
-    
-    if (!hasChildren())
-        addChildren();
-    
-    unsigned length = m_children.size();
-    for (unsigned i = 0; i < length; i++) {
-        if (toRenderListBox(m_renderer)->listIndexIsVisible(i))
-            result.append(m_children[i]);
-    }
-}
-
 AccessibilityObject* AccessibilityListBox::listBoxOptionAccessibilityObject(HTMLElement* element) const
 {
     // skip hr elements

third_party/WebKit/Source/core/accessibility/AccessibilityListBox.h
@@ -48,7 +48,6 @@ public:
     virtual AccessibilityRole roleValue() const { return ListBoxRole; }
         
     virtual void selectedChildren(AccessibilityChildrenVector&);
-    virtual void visibleChildren(AccessibilityChildrenVector&);
     
     virtual void addChildren();
 

third_party/WebKit/Source/core/accessibility/AccessibilityListBoxOption.h
@@ -62,7 +62,6 @@ public:
 
     virtual LayoutRect elementRect() const;
     virtual AccessibilityObject* parentObject() const;
-    bool isListBoxOption() const { return true; }
     
 private:
     HTMLElement* m_optionElement;
@@ -70,7 +69,6 @@ private:
     virtual bool canHaveChildren() const { return false; }
     HTMLSelectElement* listBoxOptionParentNode() const;
     int listBoxOptionIndex() const;
-    IntRect listBoxOptionRect() const;
     AccessibilityObject* listBoxOptionAccessibilityObject(HTMLElement*) const;
     virtual bool computeAccessibilityIsIgnored() const;
 };

third_party/WebKit/Source/core/accessibility/AccessibilityMediaControls.h
@@ -62,8 +62,6 @@ public:
     static PassRefPtr<AccessibilityObject> create(RenderObject*);
     virtual ~AccessibilityMediaTimeline() { }
 
-    virtual bool isMediaTimeline() const { return true; }
-
     virtual String helpText() const;
     virtual String valueDescription() const;
     const AtomicString& getAttribute(const QualifiedName& attribute) const;
@@ -105,7 +103,6 @@ public:
 
 private:
     explicit AccessibilityMediaTimeDisplay(RenderObject*);
-    virtual bool isMediaControlLabel() const { return true; }
     virtual bool computeAccessibilityIsIgnored() const;
 };
 

third_party/WebKit/Source/core/accessibility/AccessibilityNodeObject.cpp
@@ -390,14 +390,6 @@ static Element* siblingWithAriaRole(String role, Node* node)
     return 0;
 }
 
-Element* AccessibilityNodeObject::menuElementForMenuButton() const
-{
-    if (ariaRoleAttribute() != MenuButtonRole)
-        return 0;
-
-    return siblingWithAriaRole("menu", node());
-}
-
 Element* AccessibilityNodeObject::menuItemElementForMenu() const
 {
     if (ariaRoleAttribute() != MenuRole)
@@ -496,11 +488,6 @@ bool AccessibilityNodeObject::isFieldset() const
     return node->hasTagName(fieldsetTag);
 }
 
-bool AccessibilityNodeObject::isGroup() const
-{
-    return roleValue() == GroupRole;
-}
-
 bool AccessibilityNodeObject::isHeading() const
 {
     return roleValue() == HeadingRole;
@@ -547,19 +534,9 @@ bool AccessibilityNodeObject::isMenu() const
     return roleValue() == MenuRole;
 }
 
-bool AccessibilityNodeObject::isMenuBar() const
-{
-    return roleValue() == MenuBarRole;
-}
-
 bool AccessibilityNodeObject::isMenuButton() const
 {
     return roleValue() == MenuButtonRole;
-}
-
-bool AccessibilityNodeObject::isMenuItem() const
-{
-    return roleValue() == MenuItemRole;
 }
 
 bool AccessibilityNodeObject::isMenuRelated() const
@@ -990,42 +967,6 @@ float AccessibilityNodeObject::minValueForRange() const
         return 0.0f;
 
     return getAttribute(aria_valueminAttr).toFloat();
-}
-
-AccessibilityObject* AccessibilityNodeObject::selectedRadioButton()
-{
-    if (!isRadioGroup())
-        return 0;
-
-    AccessibilityObject::AccessibilityChildrenVector children = this->children();
-
-    // Find the child radio button that is selected (ie. the intValue == 1).
-    size_t size = children.size();
-    for (size_t i = 0; i < size; ++i) {
-        AccessibilityObject* object = children[i].get();
-        if (object->roleValue() == RadioButtonRole && object->checkboxOrRadioValue() == ButtonStateOn)
-            return object;
-    }
-    return 0;
-}
-
-AccessibilityObject* AccessibilityNodeObject::selectedTabItem()
-{
-    if (!isTabList())
-        return 0;
-
-    // Find the child tab item that is selected (ie. the intValue == 1).
-    AccessibilityObject::AccessibilityChildrenVector tabs;
-    tabChildren(tabs);
-
-    AccessibilityObject::AccessibilityChildrenVector children = this->children();
-    size_t size = tabs.size();
-    for (size_t i = 0; i < size; ++i) {
-        AccessibilityObject* object = children[i].get();
-        if (object->isTabItem() && object->isChecked())
-            return object;
-    }
-    return 0;
 }
 
 float AccessibilityNodeObject::stepValueForRange() const

third_party/WebKit/Source/core/accessibility/AccessibilityNodeObject.h
@@ -88,7 +88,6 @@ protected:
     bool isGenericFocusableElement() const;
     HTMLLabelElement* labelForElement(Element*) const;
     AccessibilityObject* menuButtonForMenu() const;
-    Element* menuElementForMenuButton() const;
     Element* menuItemElementForMenu() const;
     Element* mouseButtonListener() const;
     AccessibilityRole remapAriaRoleDueToParent(AccessibilityRole) const;
@@ -106,7 +105,6 @@ protected:
     virtual bool isAnchor() const;
     virtual bool isControl() const;
     virtual bool isFieldset() const;
-    virtual bool isGroup() const;
     virtual bool isHeading() const;
     virtual bool isHovered() const;
     virtual bool isImage() const;
@@ -114,9 +112,7 @@ protected:
     virtual bool isInputImage() const;
     virtual bool isLink() const;
     virtual bool isMenu() const;
-    virtual bool isMenuBar() const;
     virtual bool isMenuButton() const;
-    virtual bool isMenuItem() const;
     virtual bool isMenuRelated() const;
     virtual bool isMultiSelectable() const;
     virtual bool isNativeCheckboxOrRadio() const;
@@ -151,8 +147,6 @@ protected:
     virtual float valueForRange() const;
     virtual float maxValueForRange() const;
     virtual float minValueForRange() const;
-    virtual AccessibilityObject* selectedRadioButton();
-    virtual AccessibilityObject* selectedTabItem();
     virtual float stepValueForRange() const;
     virtual String stringValue() const;
 

commit a1ee373b0d40f213a3380b883a6aa07c94c69581

Android compile fix
(Broken by r152036)

TBR=aboxhall

Review URL: https://codereview.chromium.org/16659002

git-svn-id: svn://svn.chromium.org/blink/trunk@152038 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/accessibility/AXObjectCache.h
@@ -249,7 +249,6 @@ inline AccessibilityObject* AXObjectCache::getOrCreate(RenderObject*) { return 0
 inline AccessibilityObject* AXObjectCache::getOrCreate(Node*) { return 0; }
 inline AccessibilityObject* AXObjectCache::getOrCreate(Widget*) { return 0; }
 inline AccessibilityObject* AXObjectCache::rootObject() { return 0; }
-inline AccessibilityObject* AXObjectCache::rootObjectForFrame(Frame*) { return 0; }
 inline Element* AXObjectCache::rootAXEditableElement(Node*) { return 0; }
 inline bool nodeHasRole(Node*, const String&) { return false; }
 inline void AXObjectCache::startCachingComputedObjectAttributesUntilTreeMutates() { }