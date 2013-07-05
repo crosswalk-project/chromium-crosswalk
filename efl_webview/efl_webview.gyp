dfffa10cf58b19cb5a6b40900ea2bd1c2fb42606
Dongseong Hwang <dongseong.hwang@intel.com>
2013-07-05 14:32:19 +0300

    XWalk must set ContentClient for user agent.
    This patch is base on Alex's finding.
    
    content_client_xwalk is copied for xwalk project.

--- efl_webview/efl_webview.gyp
+++ efl_webview/efl_webview.gyp
@@ -53,6 +53,8 @@
       'sources': [
         'lib/content_browser_client_xwalk.cc',
         'lib/content_browser_client_xwalk.h',
+        'lib/content_client_xwalk.cc',
+        'lib/content_client_xwalk.h',
         'lib/browser_context_xwalk.h',
         'lib/message_pump_xwalk.cc',
         'lib/message_pump_xwalk.h',
---
+++ efl_webview/lib/content_client_xwalk.cc
@@ -0,0 +1,52 @@
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "efl_webview/lib/content_client_xwalk.h"
+
+#include "base/command_line.h"
+#include "base/string_piece.h"
+#include "base/utf_string_conversions.h"
+#include "content/public/common/content_switches.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "webkit/user_agent/user_agent_util.h"
+
+namespace xwalk {
+
+ContentClientXWalk::ContentClientXWalk() {
+}
+
+ContentClientXWalk::~ContentClientXWalk() {
+}
+
+std::string ContentClientXWalk::GetUserAgent() const {
+  // TODO(hmin): Define user agent for xwalk.
+  std::string product = "Chrome/0.0.1";
+  CommandLine* command_line = CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kUseMobileUserAgent))
+    product += " Mobile";
+  return webkit_glue::BuildUserAgentFromProduct(product);
+}
+
+string16 ContentClientXWalk::GetLocalizedString(int message_id) const {
+  return l10n_util::GetStringUTF16(message_id);
+}
+
+base::StringPiece ContentClientXWalk::GetDataResource(
+    int resource_id,
+    ui::ScaleFactor scale_factor) const {
+  return ResourceBundle::GetSharedInstance().GetRawDataResourceForScale(
+      resource_id, scale_factor);
+}
+
+base::RefCountedStaticMemory* ContentClientXWalk::GetDataResourceBytes(
+    int resource_id) const {
+  return ResourceBundle::GetSharedInstance().LoadDataResourceBytes(resource_id);
+}
+
+gfx::Image& ContentClientXWalk::GetNativeImageNamed(int resource_id) const {
+  return ResourceBundle::GetSharedInstance().GetNativeImageNamed(resource_id);
+}
+
+}  // namespace xwalk
