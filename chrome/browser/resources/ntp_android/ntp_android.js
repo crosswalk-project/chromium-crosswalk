f15471747f73d9f331edf1c00a5d513ffb472a43
newt@chromium.org <newt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-11 20:32:39 +0000

    Merge 209843 "[Android] NTP most visited page fixes for ldpi dev..."
    > [Android] NTP most visited page fixes for ldpi devices.
    > 
    > NTP most visited page was visually broken on ldpi device a couple of ways:
    > 
    > 1. On portrait, thumbnails and texts are overlapped vertically.
    > 2. On landscape, it shows 2 columns, not 3 columns due to size miscalculation.
    > 3. Highlighting on thumbnail select overflowed to right hand side.
    > 
    > This CL fixes the issues listed above.
    > 
    > BUG=238928
    > 
    > Review URL: https://chromiumcodereview.appspot.com/17611002
    
    TBR=kkimlabs@chromium.org
    
    Review URL: https://codereview.chromium.org/5767282085068800
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1547/src@211204 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp_android/ntp_android.js
+++ chrome/browser/resources/ntp_android/ntp_android.js
@@ -2203,7 +2203,7 @@ cr.define('ntp', function() {
     // 'natural' height and width of the thumbnail
     var thumbHeight = 72;
     var thumbWidth = 108;
-    var labelHeight = 20;
+    var labelHeight = 25;
     var labelWidth = thumbWidth + 20;
     var labelLeft = (thumbWidth - labelWidth) / 2;
     var itemHeight = thumbHeight + labelHeight;
@@ -2215,9 +2215,6 @@ cr.define('ntp', function() {
     var itemMarginRight = 20;
 
     var listHeight = 0;
-    // set it to the unscaled size so centerGrid works correctly
-    modifyCssRule('body[device="phone"] .thumbnail-cell',
-        'width', thumbWidth + 'px');
 
     var screenHeight =
         document.documentElement.offsetHeight -
@@ -2226,7 +2223,9 @@ cr.define('ntp', function() {
     if (isPortrait()) {
       mostVisitedList.setAttribute(GRID_COLUMNS, '2');
       listHeight = screenHeight * .85;
-      listHeight = listHeight >= 420 ? 420 : listHeight;
+      // Ensure that listHeight is not too small and not too big.
+      listHeight = Math.max(listHeight, (itemHeight * 3) + 20);
+      listHeight = Math.min(listHeight, 420);
       // Size for 3 rows (4 gutters)
       itemMarginTop = (listHeight - (itemHeight * 3)) / 4;
     } else {
@@ -2242,8 +2241,8 @@ cr.define('ntp', function() {
         var scale = (screenHeight - 2 * labelHeight -
             targetRemainder) / (2 * thumbHeight);
         // update values based on scale
-        thumbWidth *= scale;
-        thumbHeight *= scale;
+        thumbWidth = Math.round(thumbWidth * scale);
+        thumbHeight = Math.round(thumbHeight * scale);
         labelWidth = thumbWidth + 20;
         itemHeight = thumbHeight + labelHeight;
       }
