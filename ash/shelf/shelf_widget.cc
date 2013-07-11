a78346a061049e4defc9d80ab01fc44240424c42
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-11 18:13:25 +0000

    Merge 209576 "cros: Fix launcher size not properly initialized."
    > cros: Fix launcher size not properly initialized.
    >
    > On Chrome OS, launcher is created after user login and could be created
    > after its container Window and status widget has finished sizing. An explicit
    > LayoutShelf call is needed to ensure its size is properly initialized.
    >
    > BUG=252533
    >
    > Review URL: https://chromiumcodereview.appspot.com/18416003
    
    TBR=xiyuan@chromium.org
    
    Review URL: https://codereview.chromium.org/18145018
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1547/src@211161 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shelf/shelf_widget.cc
+++ ash/shelf/shelf_widget.cc
@@ -517,6 +517,8 @@ void ShelfWidget::CreateLauncher() {
   launcher_->SetVisible(
       shell->session_state_delegate()->IsActiveUserSessionStarted());
 
+  shelf_layout_manager_->LayoutShelf();
+
   Show();
 }
 
