4da691526dccd273d54a27516152438a75789ab3
Hongbo Min <hongbo.min@intel.com>
2013-07-16 14:29:01 +0800

    Make the close button can close the native app window
    In case of using WM border, the DesktopRootWindowHostX11 should
    act as a RootWindow observer which closes itself when receiving
    WM_DELETE_WINDOW message from X server.

--- ui/views/widget/desktop_aura/desktop_root_window_host_x11.h
+++ ui/views/widget/desktop_aura/desktop_root_window_host_x11.h
@@ -14,6 +14,7 @@
 #include "base/memory/weak_ptr.h"
 #include "ui/aura/client/cursor_client.h"
 #include "ui/aura/root_window_host.h"
+#include "ui/aura/root_window_observer.h"
 #include "ui/base/cursor/cursor_loader_x11.h"
 #include "ui/base/dragdrop/desktop_selection_provider_aurax11.h"
 #include "ui/base/x/x11_atom_cache.h"
@@ -22,6 +23,7 @@
 #include "ui/views/widget/desktop_aura/desktop_root_window_host.h"
 
 namespace aura {
+class RootWindow;
 namespace client {
 class FocusClient;
 class ScreenPositionClient;
@@ -43,6 +45,7 @@ class CursorManager;
 class VIEWS_EXPORT DesktopRootWindowHostX11 :
     public DesktopRootWindowHost,
     public aura::RootWindowHost,
+    public aura::RootWindowObserver,
     public ui::DesktopSelectionProviderAuraX11,
     public base::MessageLoop::Dispatcher {
  public:
@@ -178,6 +181,9 @@ class VIEWS_EXPORT DesktopRootWindowHostX11 :
 
   // Overridden from Dispatcher:
   virtual bool Dispatch(const base::NativeEvent& event) OVERRIDE;
+
+  // Overriden from aura::RootWindowObserver:
+  virtual void OnRootWindowHostCloseRequested(const aura::RootWindow* root) OVERRIDE;
 
   base::WeakPtrFactory<DesktopRootWindowHostX11> close_widget_factory_;
 
