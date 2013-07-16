ec813055d4c7c34731a83b798d97af2e3ca8ddfe
Hongbo Min <hongbo.min@intel.com>
2013-07-16 13:16:41 +0800

    Add option to choose if the window border is showed
    The original DesktopRootWindowHostX11 impl is harded-code to
    not use window border. We turn it on by reusing the member
    |remove_standard_frame| in Widget::InitParams struct since
    it is originall desigend for Windows.

--- ui/views/widget/desktop_aura/desktop_root_window_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_root_window_host_x11.cc
@@ -289,7 +289,12 @@ aura::RootWindow* DesktopRootWindowHostX11::InitRootWindow(
   // TODO(erg): Unify this code once the other consumer goes away.
   x11_window_event_filter_.reset(
       new X11WindowEventFilter(root_window_, activation_client_.get()));
-  x11_window_event_filter_->SetUseHostWindowBorders(false);
+  
+  // We reuse the |remove_standard_frame| in to tell if the window border is
+  // used. Note the |remove_standard_frame| is originally designed for
+  // Windows, see comments in ui/views/widget.h.
+  bool use_os_border = params.remove_standard_frame ? false : true;
+  x11_window_event_filter_->SetUseHostWindowBorders(use_os_border);
   desktop_native_widget_aura_->root_window_event_filter()->AddHandler(
       x11_window_event_filter_.get());
 
