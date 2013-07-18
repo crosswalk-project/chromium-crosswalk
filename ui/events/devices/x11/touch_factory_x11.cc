15f6cdcc89a6b9040194ea5341ccc2289f32d2e1
Ningxin Hu <ningxin.hu@intel.com>
2013-07-18 17:11:21 +0800

    Aura: fix the touch event doesn't work on Tizen 3.0 issue.
    Chromium uses the sourceid of a XIDeviceEvent to decide if it is orignated
    from a touch device. See events_x.cc:717. However, Chromium uses deviceid to
    check touch device. See touch_factory_x11.cc:158.
    
    As X11 spec, only slave device has same sourceid and device id. So Chromium
    should setup touch device list with XISlavePointer other than XIMasterPointer
    device.
    
    TESTS=https://github.com/borismus/MagicTouch/blob/master/samples/tracker.html

--- ui/events/devices/x11/touch_factory_x11.cc
+++ ui/events/devices/x11/touch_factory_x11.cc
@@ -103,7 +103,7 @@ void TouchFactory::UpdateDeviceList(Display* display) {
       DeviceListCacheX11::GetInstance()->GetXI2DeviceList(display);
   for (int i = 0; i < xi_dev_list.count; i++) {
     const XIDeviceInfo& devinfo = xi_dev_list[i];
-    if (devinfo.use == XIFloatingSlave || devinfo.use == XIMasterPointer) {
+    if (devinfo.use == XIFloatingSlave || devinfo.use == XISlavePointer) {
       for (int k = 0; k < devinfo.num_classes; ++k) {
         XIAnyClassInfo* xiclassinfo = devinfo.classes[k];
         if (xiclassinfo->type == XITouchClass) {
