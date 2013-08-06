bda7bf15b9e6cb12b882eb9c285cff7e312d6aac
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 14:12:50 +0000

    Merge 213289 "Warn users about potentially unwanted downloads."
    > Warn users about potentially unwanted downloads.
    > 
    > Exposes POTENTIALLY_UNWANTED ClientDownloadResponse verdict from the
    > SafeBrowsing service to ChromeDownloadManagerDelegate. Downloads for
    > which this verdict is seen will be marked with the danger type
    > DOWNLOAD_DANGER_TYPE_POTENTIALLY_UNWANTED.
    > 
    > A dangerous download prompt will be displayed on the download shelf
    > indicating that the download may make changes to the browser or
    > system.
    > 
    > Changes are for Windows only since the POTENTIALLY_UNWANTED signal is
    > only implemented for Windows.
    > 
    > BUG=262983
    > 
    > Review URL: https://chromiumcodereview.appspot.com/19863005
    
    TBR=asanka@chromium.org
    
    Review URL: https://codereview.chromium.org/22370005
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1547/src@215890 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/downloads/downloads_api.cc
+++ chrome/browser/extensions/api/downloads/downloads_api.cc
@@ -107,6 +107,7 @@ const char kDangerFile[] = "file";
 const char kDangerKey[] = "danger";
 const char kDangerSafe[] = "safe";
 const char kDangerUncommon[] = "uncommon";
+const char kDangerUnwanted[] = "unwanted";
 const char kDangerAccepted[] = "accepted";
 const char kDangerHost[] = "host";
 const char kDangerUrl[] = "url";
@@ -147,6 +148,7 @@ const char* kDangerStrings[] = {
   kDangerUncommon,
   kDangerAccepted,
   kDangerHost,
+  kDangerUnwanted
 };
 COMPILE_ASSERT(arraysize(kDangerStrings) == content::DOWNLOAD_DANGER_TYPE_MAX,
                download_danger_type_enum_changed);
