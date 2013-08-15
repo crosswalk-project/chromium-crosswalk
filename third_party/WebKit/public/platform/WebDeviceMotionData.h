crosswalk-project/chromium-crosswalk

commit b27c398dffc62ed6f1f8adf4ea110cbf16aa2a1b

Get rid of the tryFast*alloc API.
Also removes WTF_MALLOC_VALIDATION, which we do not use and therefore the complexity it adds is unwelcome.

There are various benefits:
1) The standard ones from having less code and less complexity.
2) The API doesn't actually work, so every new caller becomes a possible bug. On (at least) Linux and Windows, our underlying malloc is tcmalloc which is hard-wired to crash on allocation failure. So tryFast*alloc can never return a fail for most of our users.
3) We've removed an inconsistency between platforms: I think tryFast*alloc might actually work as advertised on Mac, because of the built-in copy of tcmalloc.

This also removes 500KB of guff from the Linux x64 chrome debug binary. A negligible effect on the release binary, sadly. 


BUG=none
TEST=compile, webkit_tests pass

Review URL: https://chromiumcodereview.appspot.com/22038002

git-svn-id: svn://svn.chromium.org/blink/trunk@155495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/public/platform/WebDeviceMotionData.h
@@ -72,6 +72,8 @@ public:
 };
 
 #if WEBKIT_IMPLEMENTATION
+#include "wtf/Assertions.h"
+
 COMPILE_ASSERT(sizeof(WebDeviceMotionData) == (10 * sizeof(double) + 2 * sizeof(char)), WebDeviceMotionData_has_wrong_size);
 #endif
 

third_party/WebKit/public/platform/WebDeviceOrientationData.h
@@ -57,6 +57,8 @@ public:
 };
 
 #if WEBKIT_IMPLEMENTATION
+#include "wtf/Assertions.h"
+
 COMPILE_ASSERT(sizeof(WebDeviceOrientationData) == (3 * sizeof(double) + 1 * sizeof(char)), WebDeviceOrientationData_has_wrong_size);
 #endif
 

third_party/WebKit/Source/bindings/v8/ArrayValue.h
@@ -26,6 +26,7 @@
 #ifndef ArrayValue_h
 #define ArrayValue_h
 
+#include "wtf/Assertions.h"
 #include <v8.h>
 
 namespace WebCore {

third_party/WebKit/Source/bindings/v8/ScriptScope.cpp
@@ -32,6 +32,7 @@
 #include "bindings/v8/ScriptScope.h"
 
 #include "bindings/v8/ScriptState.h"
+#include "wtf/Assertions.h"
 
 #include <v8.h>
 

third_party/WebKit/Source/bindings/v8/V8Binding.cpp
@@ -106,9 +106,8 @@ class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
     virtual void* Allocate(size_t size) OVERRIDE
     {
         void* data;
-        if (WTF::ArrayBufferContents::allocateMemory(size, WTF::ArrayBufferContents::ZeroInitialize, data))
-            return data;
-        return 0;
+        WTF::ArrayBufferContents::allocateMemory(size, WTF::ArrayBufferContents::ZeroInitialize, data);
+        return data;
     }
 
     virtual void Free(void* data)

third_party/WebKit/Source/bindings/v8/WrapperTypeInfo.h
@@ -31,6 +31,7 @@
 #ifndef WrapperTypeInfo_h
 #define WrapperTypeInfo_h
 
+#include "wtf/Assertions.h"
 #include <v8.h>
 
 namespace WebCore {

third_party/WebKit/Source/bindings/v8/custom/V8WebGLRenderingContextCustom.cpp
@@ -80,15 +80,15 @@
 
 namespace WebCore {
 
-// Allocates new storage via tryFastMalloc.
+// Allocates new storage via fastMalloc.
 // Returns NULL if array failed to convert for any reason.
 static float* jsArrayToFloatArray(v8::Handle<v8::Array> array, uint32_t len)
 {
     // Convert the data element-by-element.
-    float* data = 0;
-    if (len > std::numeric_limits<uint32_t>::max() / sizeof(float)
-        || !tryFastMalloc(len * sizeof(float)).getValue(data))
+    if (len > std::numeric_limits<uint32_t>::max() / sizeof(float))
         return 0;
+    float* data = static_cast<float*>(fastMalloc(len * sizeof(float)));
+
     for (uint32_t i = 0; i < len; i++) {
         v8::Local<v8::Value> val = array->Get(i);
         if (!val->IsNumber()) {
@@ -100,15 +100,15 @@ static float* jsArrayToFloatArray(v8::Handle<v8::Array> array, uint32_t len)
     return data;
 }
 
-// Allocates new storage via tryFastMalloc.
+// Allocates new storage via fastMalloc.
 // Returns NULL if array failed to convert for any reason.
 static int* jsArrayToIntArray(v8::Handle<v8::Array> array, uint32_t len)
 {
     // Convert the data element-by-element.
-    int* data = 0;
-    if (len > std::numeric_limits<uint32_t>::max() / sizeof(int)
-        || !tryFastMalloc(len * sizeof(int)).getValue(data))
+    if (len > std::numeric_limits<uint32_t>::max() / sizeof(int))
         return 0;
+    int* data = static_cast<int*>(fastMalloc(len * sizeof(int)));
+
     for (uint32_t i = 0; i < len; i++) {
         v8::Local<v8::Value> val = array->Get(i);
         bool ok;

third_party/WebKit/Source/core/loader/NavigationPolicy.cpp
@@ -31,6 +31,8 @@
 #include "config.h"
 #include "NavigationPolicy.h"
 
+#include "wtf/Assertions.h"
+
 namespace WebCore {
 
 bool navigationPolicyFromMouseEvent(unsigned short button, bool ctrl, bool shift, bool alt, bool meta, NavigationPolicy* policy)

commit 10798a27883e21098755679a1bea490059a7d68c

Do not include Assertions.h within namespace WebKit.
Including Assertions.h leads to __darwin_ssize_t being
defined inside namespace WebKit but not outside that namespace.
This is because certain headers will not be pulled in again
later on in the same compilation unit.

This bug is caused by: r155495

Review URL: https://chromiumcodereview.appspot.com/23052005

git-svn-id: svn://svn.chromium.org/blink/trunk@156169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/public/platform/WebDeviceMotionData.h
@@ -33,6 +33,10 @@
 
 #include "WebCommon.h"
 
+#if WEBKIT_IMPLEMENTATION
+#include "wtf/Assertions.h"
+#endif
+
 namespace WebKit {
 
 #pragma pack(push, 1)
@@ -72,8 +76,6 @@ public:
 };
 
 #if WEBKIT_IMPLEMENTATION
-#include "wtf/Assertions.h"
-
 COMPILE_ASSERT(sizeof(WebDeviceMotionData) == (10 * sizeof(double) + 2 * sizeof(char)), WebDeviceMotionData_has_wrong_size);
 #endif
 

third_party/WebKit/public/platform/WebDeviceOrientationData.h
@@ -33,6 +33,10 @@
 
 #include "WebCommon.h"
 
+#if WEBKIT_IMPLEMENTATION
+#include "wtf/Assertions.h"
+#endif
+
 namespace WebKit {
 
 #pragma pack(push, 1)
@@ -57,8 +61,6 @@ public:
 };
 
 #if WEBKIT_IMPLEMENTATION
-#include "wtf/Assertions.h"
-
 COMPILE_ASSERT(sizeof(WebDeviceOrientationData) == (3 * sizeof(double) + 1 * sizeof(char)), WebDeviceOrientationData_has_wrong_size);
 #endif
 