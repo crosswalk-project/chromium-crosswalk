crosswalk-project/chromium-crosswalk

commit f8b88c403be482c6f1a49eddfba9ac7ce46c9a43

Paint flex items atomically, like inline-blocks
As required by http://dev.w3.org/csswg/css-flexbox/#painting

R=ojan@chromium.org
BUG=

Review URL: https://chromiumcodereview.appspot.com/23737004

git-svn-id: svn://svn.chromium.org/blink/trunk@157004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/rendering/InlineBox.cpp
@@ -197,30 +197,14 @@ void InlineBox::adjustPosition(float dx, float dy)
 
 void InlineBox::paint(PaintInfo& paintInfo, const LayoutPoint& paintOffset, LayoutUnit /* lineTop */, LayoutUnit /*lineBottom*/)
 {
-    if (!paintInfo.shouldPaintWithinRoot(renderer()) || (paintInfo.phase != PaintPhaseForeground && paintInfo.phase != PaintPhaseSelection))
+    if (!paintInfo.shouldPaintWithinRoot(renderer()))
         return;
 
     LayoutPoint childPoint = paintOffset;
     if (parent()->renderer()->style()->isFlippedBlocksWritingMode()) // Faster than calling containingBlock().
         childPoint = renderer()->containingBlock()->flipForWritingModeForChild(toRenderBox(renderer()), childPoint);
 
-    // Paint all phases of replaced elements atomically, as though the replaced element established its
-    // own stacking context.  (See Appendix E.2, section 6.4 on inline block/table elements in the CSS2.1
-    // specification.)
-    bool preservePhase = paintInfo.phase == PaintPhaseSelection || paintInfo.phase == PaintPhaseTextClip;
-    PaintInfo info(paintInfo);
-    info.phase = preservePhase ? paintInfo.phase : PaintPhaseBlockBackground;
-    renderer()->paint(info, childPoint);
-    if (!preservePhase) {
-        info.phase = PaintPhaseChildBlockBackgrounds;
-        renderer()->paint(info, childPoint);
-        info.phase = PaintPhaseFloat;
-        renderer()->paint(info, childPoint);
-        info.phase = PaintPhaseForeground;
-        renderer()->paint(info, childPoint);
-        info.phase = PaintPhaseOutline;
-        renderer()->paint(info, childPoint);
-    }
+    RenderBlock::paintAsInlineBlock(renderer(), paintInfo, childPoint);
 }
 
 bool InlineBox::nodeAtPoint(const HitTestRequest& request, HitTestResult& result, const HitTestLocation& locationInContainer, const LayoutPoint& accumulatedOffset, LayoutUnit /* lineTop */, LayoutUnit /*lineBottom*/)

third_party/WebKit/LayoutTests/css3/flexbox/mozilla/flexbox-items-as-stacking-contexts-2-expected.html
@@ -1,0 +1,64 @@
+<!DOCTYPE html>
+<!--
+     Any copyright is dedicated to the Public Domain.
+     http://creativecommons.org/publicdomain/zero/1.0/
+-->
+<html>
+<head>
+  <style>
+    body { font: 10px sans-serif }
+    .flexContainer {
+      background: orange;
+      width: 70px;
+      padding: 2px;
+      margin-bottom: 2px;
+    }
+
+    .flexContainer > div:first-child {
+      margin-right: 10px; /* the space between the flex items, in testcase */
+    }
+
+    .item1 {
+      display: inline-block;
+      background: lightblue;
+      width: 30px;
+    }
+    .item2 {
+      display: inline-block;
+      background: yellow;
+      width: 30px;
+    }
+  </style>
+</head>
+<body>
+  <div class="flexContainer"
+    ><div class="item1">ThisIsALongUnbrokenString</div
+    ><div class="item2">HereIsSomeMoreLongText</div
+  ></div>
+
+  <div class="flexContainer"
+    ><div class="item1" style="position:relative">ThisIsALongUnbrokenString</div
+    ><div class="item2">HereIsSomeMoreLongText</div
+  ></div>
+
+  <div class="flexContainer"
+    ><div class="item1" style="position:relative">ThisIsALongUnbrokenString</div
+    ><div class="item2">HereIsSomeMoreLongText</div
+  ></div>
+
+  <div class="flexContainer"
+    ><div class="item2">HereIsSomeMoreLongText</div
+    ><div class="item1">ThisIsALongUnbrokenString</div
+  ></div>
+
+  <div class="flexContainer"
+    ><div class="item2">HereIsSomeMoreLongText</div
+    ><div class="item1">ThisIsALongUnbrokenString</div
+  ></div>
+
+  <div class="flexContainer"
+    ><div class="item2" style="position:relative">HereIsSomeMoreLongText</div
+    ><div class="item1">ThisIsALongUnbrokenString</div
+  ></div>
+</body>
+</html>

third_party/WebKit/LayoutTests/css3/flexbox/mozilla/flexbox-items-as-stacking-contexts-2.html
@@ -1,0 +1,82 @@
+<!DOCTYPE html>
+<!--
+     Any copyright is dedicated to the Public Domain.
+     http://creativecommons.org/publicdomain/zero/1.0/
+-->
+<!-- This testcase checks flex items are painted atomically. In particular,
+     if one item has content that overflows into the region of another item,
+     then one item is painted "behind" the other; there shouldn't normally
+     any interleaving of backgrounds and content between the two items.
+
+     This testcase also tests some special cases that will change the paint
+     ordering - specifically, the properties "position", "z-index", and
+     "order" on flex items.
+ -->
+<html>
+<head>
+  <style>
+    body { font: 10px sans-serif }
+    .flexContainer {
+      background: orange;
+      display: flex;
+      justify-content: space-between;
+      width: 70px;
+      padding: 2px;
+      margin-bottom: 2px;
+    }
+    .item1 {
+      background: lightblue;
+      width: 30px;
+    }
+    .item2 {
+      background: yellow;
+      width: 30px;
+    }
+  </style>
+</head>
+<body>
+  <!-- This container has two flex items, the first of which has content
+       sticking out & overlapping the second.  If they're painting atomically
+       (and in the right order), the second item's background should cover the
+       first item's overflowing content. -->
+  <div class="flexContainer"
+    ><div class="item1">ThisIsALongUnbrokenString</div
+    ><div class="item2">HereIsSomeMoreLongText</div
+  ></div>
+
+  <!-- Now, the first item is relatively positioned, which should make it paint
+       on top of everything. -->
+  <div class="flexContainer"
+    ><div class="item1" style="position:relative">ThisIsALongUnbrokenString</div
+    ><div class="item2">HereIsSomeMoreLongText</div
+  ></div>
+
+  <!-- Now, the first item is has "z-index" set, which should make it paint on
+       top of everything. -->
+  <div class="flexContainer"
+    ><div class="item1" style="z-index: 1">ThisIsALongUnbrokenString</div
+    ><div class="item2">HereIsSomeMoreLongText</div
+  ></div>
+
+  <!-- Now, the first item has "order" set to a higher value than default,
+       which should make it paint on top (and at the far right) -->
+  <div class="flexContainer"
+    ><div class="item1" style="order: 1">ThisIsALongUnbrokenString</div
+    ><div class="item2">HereIsSomeMoreLongText</div
+  ></div>
+
+  <!-- And for thoroughness, let's set "order" to a lower value than default,
+       on the second item. (Should render the same as previous example.)  -->
+  <div class="flexContainer"
+    ><div class="item1">ThisIsALongUnbrokenString</div
+    ><div class="item2" style="order: -1">HereIsSomeMoreLongText</div
+  ></div>
+
+  <!-- ...but if we relatively position that second item, it should paint
+       on top again, despite its low "order" value. -->
+  <div class="flexContainer"
+    ><div class="item1">ThisIsALongUnbrokenString</div
+    ><div class="item2" style="order: -1; position: relative">HereIsSomeMoreLongText</div
+  ></div>
+</body>
+</html>

third_party/WebKit/Source/core/rendering/RenderBlock.cpp
@@ -3109,6 +3109,38 @@ void RenderBlock::paintChild(RenderBox* child, PaintInfo& paintInfo, const Layou
         child->paint(paintInfo, childPoint);
 }
 
+void RenderBlock::paintChildAsInlineBlock(RenderBox* child, PaintInfo& paintInfo, const LayoutPoint& paintOffset)
+{
+    LayoutPoint childPoint = flipForWritingModeForChild(child, paintOffset);
+    if (!child->hasSelfPaintingLayer() && !child->isFloating())
+        paintAsInlineBlock(child, paintInfo, childPoint);
+}
+
+void RenderBlock::paintAsInlineBlock(RenderObject* renderer, PaintInfo& paintInfo, const LayoutPoint& childPoint)
+{
+    if (paintInfo.phase != PaintPhaseForeground && paintInfo.phase != PaintPhaseSelection)
+        return;
+
+    // Paint all phases atomically, as though the element established its own
+    // stacking context.  (See Appendix E.2, section 7.2.1.4 on
+    // inline block/table/replaced elements in the CSS2.1 specification.)
+    // This is also used by other elements (e.g. flex items and grid items).
+    bool preservePhase = paintInfo.phase == PaintPhaseSelection || paintInfo.phase == PaintPhaseTextClip;
+    PaintInfo info(paintInfo);
+    info.phase = preservePhase ? paintInfo.phase : PaintPhaseBlockBackground;
+    renderer->paint(info, childPoint);
+    if (!preservePhase) {
+        info.phase = PaintPhaseChildBlockBackgrounds;
+        renderer->paint(info, childPoint);
+        info.phase = PaintPhaseFloat;
+        renderer->paint(info, childPoint);
+        info.phase = PaintPhaseForeground;
+        renderer->paint(info, childPoint);
+        info.phase = PaintPhaseOutline;
+        renderer->paint(info, childPoint);
+    }
+}
+
 bool RenderBlock::hasCaret(CaretType type) const
 {
     // Paint the caret if the FrameSelection says so or if caret browsing is enabled

third_party/WebKit/Source/core/rendering/RenderBlock.h
@@ -454,6 +454,10 @@ public:
     bool allowsShapeInsideInfoSharing() const { return !isInline() && !isFloating(); }
     virtual void imageChanged(WrappedImagePtr, const IntRect* = 0) OVERRIDE;
 
+    // inline-block elements paint all phases atomically. This function ensures that. Certain other elements
+    // (grid items, flex items) require this behavior as well, and this function exists as a helper for them.
+    // It is expected that the caller will call this function independent of the value of paintInfo.phase.
+    static void paintAsInlineBlock(RenderObject*, PaintInfo&, const LayoutPoint&);
 protected:
     virtual void willBeDestroyed();
 
@@ -498,6 +502,7 @@ protected:
     virtual void paintObject(PaintInfo&, const LayoutPoint&);
     virtual void paintChildren(PaintInfo&, const LayoutPoint&);
     void paintChild(RenderBox*, PaintInfo&, const LayoutPoint&);
+    void paintChildAsInlineBlock(RenderBox*, PaintInfo&, const LayoutPoint&);
 
     LayoutUnit logicalRightOffsetForLine(LayoutUnit logicalTop, LayoutUnit fixedOffset, bool applyTextIndent, LayoutUnit* heightRemaining = 0, LayoutUnit logicalHeight = 0) const
     {

third_party/WebKit/Source/core/rendering/RenderFlexibleBox.cpp
@@ -325,7 +325,7 @@ void RenderFlexibleBox::repaintChildrenDuringLayoutIfMoved(const ChildFrameRects
 void RenderFlexibleBox::paintChildren(PaintInfo& paintInfo, const LayoutPoint& paintOffset)
 {
     for (RenderBox* child = m_orderIterator.first(); child; child = m_orderIterator.next())
-        paintChild(child, paintInfo, paintOffset);
+        paintChildAsInlineBlock(child, paintInfo, paintOffset);
 }
 
 void RenderFlexibleBox::repositionLogicalHeightDependentFlexItems(Vector<LineContext>& lineContexts)

commit 6103ad8de9fd747eaf417050afee2354874feb81

Try to fix a performance regression
See bug report for details - try putting the early return back where it
was to fix the regression.

Regression was caused by r157004 aka https://chromiumcodereview.appspot.com/23737004

BUG=283893
R=ojan@chromium.org

Review URL: https://chromiumcodereview.appspot.com/23636009

git-svn-id: svn://svn.chromium.org/blink/trunk@157326 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/rendering/InlineBox.cpp
@@ -197,7 +197,7 @@ void InlineBox::adjustPosition(float dx, float dy)
 
 void InlineBox::paint(PaintInfo& paintInfo, const LayoutPoint& paintOffset, LayoutUnit /* lineTop */, LayoutUnit /*lineBottom*/)
 {
-    if (!paintInfo.shouldPaintWithinRoot(renderer()))
+    if (!paintInfo.shouldPaintWithinRoot(renderer()) || (paintInfo.phase != PaintPhaseForeground && paintInfo.phase != PaintPhaseSelection))
         return;
 
     LayoutPoint childPoint = paintOffset;