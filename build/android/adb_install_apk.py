900f499e750c05f0aa49812d2e763da9109b635a
Shiliu Wang <shiliu.wang@intel.com>
2013-09-11 16:44:27 +0800

    [Android] When running android test, try killing adb if no devices found
    It randomly happens that adb devices return empty list even device
    is actually connected, killing adb and restart it can fix this.
    
    CL=https://codereview.chromium.org/24194002
    
    Reworked for 37.0.2062.3

--- build/android/adb_install_apk.py
+++ build/android/adb_install_apk.py
@@ -9,6 +9,7 @@
 import optparse
 import os
 import sys
+import time
 
 from pylib import android_commands
 from pylib import constants
@@ -71,13 +72,25 @@ def main(argv):
   constants.SetBuildType(options.build_type)
   ValidateInstallAPKOption(parser, options, args)
 
-  devices = android_commands.GetAttachedDevices()
+  retry_times = 5
+  retry_interval = 15
+  while retry_times > 0:
+    devices = android_commands.GetAttachedDevices()
+    if options.device:
+      if options.device not in devices:
+        raise Exception('Error: %s not in attached devices %s' % \
+                        (options.device, ','.join(devices)))
+      devices = [options.device]
 
-  if options.device:
-    if options.device not in devices:
-      raise Exception('Error: %s not in attached devices %s' % (options.device,
-                      ','.join(devices)))
-    devices = [options.device]
+    if not devices:
+      print 'No connected devices found, '\
+            'kill adb server and retry in %d seconds...' % retry_interval
+      android_commands.AndroidCommands().KillAdbServer()
+      time.sleep(retry_interval)
+      retry_interval *= 2
+      retry_times -= 1
+    else:
+      break
 
   if not devices:
     raise Exception('Error: no connected devices')
