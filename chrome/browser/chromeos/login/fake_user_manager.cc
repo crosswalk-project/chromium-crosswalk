3026ccbca0e733859212fb2006e4cc3b3b7f3101
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-02 20:27:03 +0000

    Merge 226225 "Handle managed users in a specific way"
    > Handle managed users in a specific way
    > 
    > BUG=295164
    > R=nkostylev@chromium.org
    > NOTRY=true
    > 
    > Review URL: https://codereview.chromium.org/25080002
    
    TBR=antrim@chromium.org
    
    Review URL: https://codereview.chromium.org/25763003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1650/src@226549 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/fake_user_manager.cc
+++ chrome/browser/chromeos/login/fake_user_manager.cc
@@ -134,6 +134,11 @@ const User* FakeUserManager::FindLocallyManagedUser(
   return NULL;
 }
 
+const User* FakeUserManager::FindLocallyManagedUserBySyncId(
+    const std::string& sync_id) const {
+  return NULL;
+}
+
 const User* FakeUserManager::GetLoggedInUser() const {
   return NULL;
 }
