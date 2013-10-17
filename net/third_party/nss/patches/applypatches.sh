ae033029741b53033ca6bc4154aa8719d9fa80c1
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-17 00:40:07 +0000

    Merge 226126 "Change SSL_PeerCertificateChain to return a CERTCe..."
    > Change SSL_PeerCertificateChain to return a CERTCertList.
    > 
    > R=agl@chromium.org,rsleevi@chromium.org
    > BUG=none
    > TEST=none
    > 
    > Review URL: https://codereview.chromium.org/25107004
    
    TBR=wtc@chromium.org
    BUG=308270
    
    Review URL: https://codereview.chromium.org/26688005
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1650/src@229011 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/patches/applypatches.sh
+++ net/third_party/nss/patches/applypatches.sh
@@ -60,4 +60,6 @@ patch -p4 < $patches_dir/cachelocks.patch
 
 patch -p4 < $patches_dir/ciphersuiteversion.patch
 
+patch -p4 < $patches_dir/peercertchain2.patch
+
 patch -p4 < $patches_dir/canfalsestart.patch
