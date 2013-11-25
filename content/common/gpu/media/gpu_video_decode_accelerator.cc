b4cf54e2824094bc5e371a005c77632a520a7bcf
qing.zhang <qing.zhang@intel.com>
2013-11-24 23:36:24 -0500

    [Tizen] Add the EGL backend in VAVDA for Tizen Mobile.
    Why we need to maintain it in our side:
    ===========================================
    1) Upstream confirm VAVDA will continue to be restricted to
       CrOS/X86 for dev & testing only and not for chromium road map.
    2) CrOS/X86 no plan to expend EGL backend which finalize in
       June 2012 and be addressed to the CrOS graphics team.
    
    Refer to "https://codereview.chromium.org/43283002/" for more
     details. So, the upstream no plan to lerage VAVDA with EGL
     graphic backend for any X86.
    
    3) The tizen-mobile's driver only support EGL as texture
       backend. The video hw acceleration of xwalk have to
       rely on EGL not GLX to bind decoded pixmap.
    ===========================================
    That's why we enable specific EGL for VAVDA in tizen port.

--- content/common/gpu/media/gpu_video_decode_accelerator.cc
+++ content/common/gpu/media/gpu_video_decode_accelerator.cc
@@ -27,6 +27,8 @@
 #include "content/common/gpu/media/dxva_video_decode_accelerator.h"
 #elif defined(OS_CHROMEOS) && defined(ARCH_CPU_ARMEL) && defined(USE_X11)
 #include "content/common/gpu/media/exynos_video_decode_accelerator.h"
+#elif defined(OS_TIZEN_MOBILE) && defined(ARCH_CPU_X86_FAMILY)
+#include "content/common/gpu/media/vaapi_video_decode_accelerator_tizen.h"
 #elif defined(OS_LINUX) && defined(ARCH_CPU_X86_FAMILY) && defined(USE_X11)
 #include "ui/gl/gl_context_glx.h"
 #include "content/common/gpu/media/vaapi_video_decode_accelerator.h"
@@ -277,6 +279,12 @@ void GpuVideoDecodeAccelerator::Initialize(
       weak_factory_for_io_.GetWeakPtr(),
       make_context_current_,
       io_message_loop_));
+#elif defined(OS_TIZEN_MOBILE) && defined(ARCH_CPU_X86_FAMILY)
+  video_decode_accelerator_.reset(new VaapiVideoDecodeAccelerator(
+      gfx::GLSurfaceEGL::GetHardwareDisplay(),
+      stub_->decoder()->GetGLContext()->GetHandle(),
+      this,
+      make_context_current_));
 #elif defined(OS_LINUX) && defined(ARCH_CPU_X86_FAMILY) && defined(USE_X11)
   gfx::GLContextGLX* glx_context =
       static_cast<gfx::GLContextGLX*>(stub_->decoder()->GetGLContext());
