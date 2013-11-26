11ea5bc65af1a80d463c00fc9e885ac6b50aad1e
posciak@chromium.org <posciak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 01:01:42 +0000

    Merge 235559 "EVDA: Preserve resolution change event across reset."
    > EVDA: Preserve resolution change event across reset.
    > 
    > The codec maintains current resolution as a part of its state and carries it
    > over across the streamoff/streamon (i.e. Reset) sequence. The resolution change
    > event should thus not be discarded if it arrives before Reset and is not
    > addressed there, as it will be valid after Reset as well and should be
    > addressed then.
    > 
    > EVDA::DecodeBufferInitial() does not handle potential resolution changes (i.e.
    > it discards GetFormat() results) after Reset(), which is fine, because we
    > rely on the event to be informed about the resolution change instead.
    > 
    > Also the DCHECK, which verifies that we don't dequeue events in other states
    > than kDecoding, is not correct, because we might have to dequeue one in kAfterReset.
    > 
    > BUG=316074
    > TEST=vdatest, video playback, MSE, seek torture tests
    > 
    > Review URL: https://codereview.chromium.org/70183003
    
    TBR=posciak@chromium.org
    
    Review URL: https://codereview.chromium.org/86653003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1700/src@237210 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/media/exynos_video_decode_accelerator.cc
+++ content/common/gpu/media/exynos_video_decode_accelerator.cc
@@ -1210,7 +1210,7 @@ void ExynosVideoDecodeAccelerator::EnqueueMfc() {
 
 void ExynosVideoDecodeAccelerator::DequeueMfcEvents() {
   DCHECK_EQ(decoder_thread_.message_loop(), base::MessageLoop::current());
-  DCHECK_EQ(decoder_state_, kDecoding);
+  DCHECK_NE(decoder_state_, kUninitialized);
   DVLOG(3) << "DequeueMfcEvents()";
 
   struct v4l2_event ev;
@@ -1726,9 +1726,6 @@ void ExynosVideoDecodeAccelerator::ResetTask() {
   if (!StopDevicePoll(false))
     return;
 
-  DequeueMfcEvents();
-
-  resolution_change_pending_ = false;
   decoder_current_bitstream_buffer_.reset();
   decoder_input_queue_.clear();
 
@@ -1755,6 +1752,12 @@ void ExynosVideoDecodeAccelerator::ResetDoneTask() {
     DVLOG(2) << "ResetDoneTask(): early out: kError state";
     return;
   }
+
+  // We might have received a resolution change event while we were waiting
+  // for the reset to finish. The codec will not post another event if the
+  // resolution after reset remains the same as the one to which were just
+  // about to switch, so preserve the event across reset so we can address
+  // it after resuming.
 
   // Reset format-specific bits.
   if (video_profile_ >= media::H264PROFILE_MIN &&
