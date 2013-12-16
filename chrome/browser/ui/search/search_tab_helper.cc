77a8e3bcf45a970a2ad76f5f56b2b35dd0153325
jered@chromium.org <jered@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-16 21:38:51 +0000

    Merge 240314 "Don't redirect to local ntp if the navigation has ..."
    > Don't redirect to local ntp if the navigation has been canceled.
    > 
    > BUG=325908
    > 
    > Review URL: https://codereview.chromium.org/104893008
    
    TBR=jfweitz@chromium.org
    
    Review URL: https://codereview.chromium.org/102863003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1700/src@241033 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/search/search_tab_helper.cc
+++ chrome/browser/ui/search/search_tab_helper.cc
@@ -46,6 +46,7 @@
 #include "content/public/common/page_transition_types.h"
 #include "content/public/common/referrer.h"
 #include "grit/generated_resources.h"
+#include "net/base/net_errors.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
@@ -317,15 +318,16 @@ void SearchTabHelper::DidFailProvisionalLoad(
     const string16& frame_unique_name,
     bool is_main_frame,
     const GURL& validated_url,
-    int /* error_code */,
+    int error_code,
     const string16& /* error_description */,
     content::RenderViewHost* /* render_view_host */) {
-  Profile* profile =
-      Profile::FromBrowserContext(web_contents_->GetBrowserContext());
+  // If error_code is ERR_ABORTED means that the user has canceled this
+  // navigation so it shouldn't be redirected.
   if (is_main_frame &&
+      error_code != net::ERR_ABORTED &&
       chrome::ShouldUseCacheableNTP() &&
       validated_url != GURL(chrome::kChromeSearchLocalNtpUrl) &&
-      chrome::IsNTPURL(validated_url, profile)) {
+      chrome::IsNTPURL(validated_url, profile())) {
     RedirectToLocalNTP();
     RecordCacheableNTPLoadHistogram(false);
   }
