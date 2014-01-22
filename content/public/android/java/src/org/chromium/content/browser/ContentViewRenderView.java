919fb43121a9af57fb1abd1f921202e20195e2ee
guangzhen <guangzhen.li@intel.com>
2014-01-22 16:42:30 +0800

    [Android] Add a new listener to listen the first rendered frame
    To support the feature of launch screen. Added a new listener FirstRenderedFrameListener.
    This listener will be triggered when the first visible content has been updated to the screen.
    
    SPEC: https://docs.google.com/a/intel.com/document/d/17PuNuHRTQuREUpaCvj-eEx7uYi2avd-VW-oaMXMpvwo/edit?pli=1#
    Design Doc: https://docs.google.com/a/intel.com/document/d/1YLajlZC7CkBOtEvzD6p6yeBr0HrPsaIGX8Eqhr3oTF4/edit#heading=h.mfsu3bk9566e
    Bug: XWALK-637
    
    Reworked for 36.0.1985.18

--- content/public/android/java/src/org/chromium/content/browser/ContentViewRenderView.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewRenderView.java
@@ -44,6 +44,13 @@ public class ContentViewRenderView extends FrameLayout implements WindowAndroid.
     protected ContentViewCore mContentViewCore;
 
     private ContentReadbackHandler mContentReadbackHandler;
+    // The listener which will be triggered when below two conditions become valid.
+    // 1. The view has been initialized and ready to draw content to the screen.
+    // 2. The compositor finished compositing and the OpenGL buffers has been swapped.
+    //    Which means the view has been updated with visually non-empty content.
+    // This listener will be triggered only once after registered.
+    private FirstRenderedFrameListener mFirstRenderedFrameListener;
+    private boolean mFirstFrameReceived;
 
     private final Runnable mRenderRunnable = new Runnable() {
         @Override
@@ -51,6 +58,10 @@ public class ContentViewRenderView extends FrameLayout implements WindowAndroid.
             render();
         }
     };
+
+    public interface FirstRenderedFrameListener{
+        public void onFirstFrameReceived();
+    }
 
     /**
      * Constructs a new ContentViewRenderView that should be can to a view hierarchy.
@@ -172,6 +183,13 @@ public class ContentViewRenderView extends FrameLayout implements WindowAndroid.
         }
     }
 
+    public void registerFirstRenderedFrameListener(FirstRenderedFrameListener listener) {
+        mFirstRenderedFrameListener = listener;
+        if (mFirstFrameReceived && mFirstRenderedFrameListener != null) {
+            mFirstRenderedFrameListener.onFirstFrameReceived();
+        }
+    }
+
     /**
      * This method should be subclassed to provide actions to be performed once the view is ready to
      * render.
@@ -244,6 +262,14 @@ public class ContentViewRenderView extends FrameLayout implements WindowAndroid.
     @CalledByNative
     private void onSwapBuffersCompleted() {
         TraceEvent.instant("onSwapBuffersCompleted");
+
+        if (!mFirstFrameReceived && mCurrentContentView != null &&
+                mCurrentContentView.getContentViewCore().isReady()) {
+            mFirstFrameReceived = true;
+            if (mFirstRenderedFrameListener != null) {
+                mFirstRenderedFrameListener.onFirstFrameReceived();
+            }
+        }
 
         if (mPendingSwapBuffers == MAX_SWAP_BUFFER_COUNT && mNeedToRender) requestRender();
         if (mPendingSwapBuffers > 0) mPendingSwapBuffers--;
