070f556d4ee0928f384d1c9eb168b41774071083
xingnan <xingnan.wang@intel.com>
2014-02-07 21:45:37 +0800

    [Android] Add Resource loading filter for MediaPlayer
    Why: There are requirements to customize the MediaPlayer resource
    loading on Android which is implemented in Chromium code base.
    Till now we have two points:
        1) Support the file:///android_asset in MediaPlayer.
        2) Support app scheme in the resource loading of MediaPlayer.
    Unfortunately, the resource loading of MediaPlayer on Andoid goes
    a different way from other ports and no interface to handle this.
    
    How: Add a new interface ResourceLoadingFilter to filter the resource
    loading(setDataSource) in MediaPlayerBridge, which could be used in
    xwalk to do the customization by overriding the function of
    shouldOverrideResourceLoading() in xwalk.
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-880

--- media/base/android/java/src/org/chromium/media/MediaPlayerBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaPlayerBridge.java
@@ -35,6 +35,19 @@ import java.util.HashMap;
 @JNINamespace("media")
 public class MediaPlayerBridge {
 
+    public static class ResourceLoadingFilter {
+        public boolean shouldOverrideResourceLoading(
+                MediaPlayer mediaPlayer, Context context, Uri uri) {
+            return false;
+        }
+    }
+
+    private static ResourceLoadingFilter sResourceLoadFilter = null;
+
+    public static void setResourceLoadingFilter(ResourceLoadingFilter filter) {
+        sResourceLoadFilter = filter;
+    }
+
     private static final String TAG = "MediaPlayerBridge";
 
     // Local player to forward this to. We don't initialize it here since the subclass might not
@@ -152,6 +165,11 @@ public class MediaPlayerBridge {
             headersMap.put("allow-cross-domain-redirect", "false");
         }
         try {
+            if (sResourceLoadFilter != null &&
+                    sResourceLoadFilter.shouldOverrideResourceLoading(
+                            getLocalPlayer(), context, uri)) {
+                return true;
+            }
             getLocalPlayer().setDataSource(context, uri, headersMap);
             return true;
         } catch (Exception e) {
