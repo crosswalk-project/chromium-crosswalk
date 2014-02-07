e6c1681bed97cb1707a49f3bf1abbc2d1ffaa43b
xingnan <xingnan.wang@intel.com>
2014-02-07 21:45:37 +0800

    [Android] Add Resource loading filter for MediaPlayer
    Why: There are requirements to customize the MediaPlayer resource
    loading on Android which is implemented in Chromium code base.
    Till now we have two points:
        1) Support the file:///android_asset in MediaPlayer.
        2) Support app scheme in the resource loading of MediaPlayer.
    Unfortunately, the resource loading of MediaPlayer on Andoid goes
    a different way from other ports and no interface to handle this.
    
    How: Add a new interface ResourceLoadingFilter to filter the resource
    loading(setDataSource) in MediaPlayerBridge, which could be used in
    xwalk to do the customization by overriding the function of
    shouldOverrideResourceLoading() in xwalk.
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-880

--- media/base/android/media_player_bridge.cc
+++ media/base/android/media_player_bridge.cc
@@ -72,7 +72,7 @@ MediaPlayerBridge::~MediaPlayerBridge() {
 
 void MediaPlayerBridge::Initialize() {
   cookies_.clear();
-  if (url_.SchemeIsFile()) {
+  if (url_.SchemeIsFile() || url_.SchemeIs("app")) {
     ExtractMediaMetadata(url_.spec());
     return;
   }
