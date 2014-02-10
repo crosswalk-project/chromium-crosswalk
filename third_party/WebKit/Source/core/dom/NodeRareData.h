crosswalk-project/chromium-crosswalk

commit 194ca486da2f30c12e51e9e94f2e5406f1c6a23f

Have getElementsByClassName() / getElementsByTagName*() return an HTMLCollection
Have getElementsByClassName() / getElementsByTagName*() return an HTMLCollection
instead of a NodeList, as per the specification:
http://dom.spec.whatwg.org/#document
http://dom.spec.whatwg.org/#element

This behavior is consistent with Firefox 26 and IE 11.

R=arv, haraken, tkent
BUG=235008
BUG=339442

Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=166088

Review URL: https://codereview.chromium.org/143453010

git-svn-id: svn://svn.chromium.org/blink/trunk@166263 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/dom/NodeRareData.h
@@ -27,7 +27,7 @@
 #include "core/dom/LiveNodeList.h"
 #include "core/dom/MutationObserverRegistration.h"
 #include "core/dom/QualifiedName.h"
-#include "core/dom/TagNodeList.h"
+#include "core/dom/TagCollection.h"
 #include "core/page/Page.h"
 #include "wtf/HashSet.h"
 #include "wtf/OwnPtr.h"
@@ -96,7 +96,7 @@ public:
 
     typedef HashMap<std::pair<unsigned char, AtomicString>, LiveNodeListBase*, NodeListCacheMapEntryHash<AtomicString> > NodeListAtomicNameCacheMap;
     typedef HashMap<std::pair<unsigned char, String>, LiveNodeListBase*, NodeListCacheMapEntryHash<String> > NodeListNameCacheMap;
-    typedef HashMap<QualifiedName, TagNodeList*> TagNodeListCacheNS;
+    typedef HashMap<QualifiedName, TagCollection*> TagCollectionCacheNS;
 
     template<typename T>
     PassRefPtr<T> addCacheWithAtomicName(ContainerNode* node, CollectionType collectionType, const AtomicString& name)
@@ -141,14 +141,14 @@ public:
         return list.release();
     }
 
-    PassRefPtr<TagNodeList> addCacheWithQualifiedName(ContainerNode* node, const AtomicString& namespaceURI, const AtomicString& localName)
+    PassRefPtr<TagCollection> addCacheWithQualifiedName(ContainerNode* node, const AtomicString& namespaceURI, const AtomicString& localName)
     {
         QualifiedName name(nullAtom, localName, namespaceURI);
-        TagNodeListCacheNS::AddResult result = m_tagNodeListCacheNS.add(name, 0);
+        TagCollectionCacheNS::AddResult result = m_tagCollectionCacheNS.add(name, 0);
         if (!result.isNewEntry)
             return result.iterator->value;
 
-        RefPtr<TagNodeList> list = TagNodeList::create(node, namespaceURI, localName);
+        RefPtr<TagCollection> list = TagCollection::create(node, namespaceURI, localName);
         result.iterator->value = list.get();
         return list.release();
     }
@@ -169,13 +169,13 @@ public:
         m_nameCaches.remove(namedNodeListKey(collectionType, name));
     }
 
-    void removeCacheWithQualifiedName(LiveNodeList* list, const AtomicString& namespaceURI, const AtomicString& localName)
+    void removeCacheWithQualifiedName(LiveNodeListBase* list, const AtomicString& namespaceURI, const AtomicString& localName)
     {
         QualifiedName name(nullAtom, localName, namespaceURI);
-        ASSERT(list == m_tagNodeListCacheNS.get(name));
+        ASSERT(list == m_tagCollectionCacheNS.get(name));
         if (deleteThisAndUpdateNodeRareDataIfAboutToRemoveLastList(list->ownerNode()))
             return;
-        m_tagNodeListCacheNS.remove(name);
+        m_tagCollectionCacheNS.remove(name);
     }
 
     static PassOwnPtr<NodeListsNodeData> create()
@@ -186,7 +186,7 @@ public:
     void invalidateCaches(const QualifiedName* attrName = 0);
     bool isEmpty() const
     {
-        return m_atomicNameCaches.isEmpty() && m_nameCaches.isEmpty() && m_tagNodeListCacheNS.isEmpty();
+        return m_atomicNameCaches.isEmpty() && m_nameCaches.isEmpty() && m_tagCollectionCacheNS.isEmpty();
     }
 
     void adoptTreeScope()
@@ -213,8 +213,8 @@ public:
                 newDocument.registerNodeList(list);
             }
 
-            TagNodeListCacheNS::const_iterator tagEnd = m_tagNodeListCacheNS.end();
-            for (TagNodeListCacheNS::const_iterator it = m_tagNodeListCacheNS.begin(); it != tagEnd; ++it) {
+            TagCollectionCacheNS::const_iterator tagEnd = m_tagCollectionCacheNS.end();
+            for (TagCollectionCacheNS::const_iterator it = m_tagCollectionCacheNS.begin(); it != tagEnd; ++it) {
                 LiveNodeListBase* list = it->value;
                 ASSERT(!list->isRootedAtDocument());
                 oldDocument.unregisterNodeList(list);
@@ -244,7 +244,7 @@ private:
     NodeList* m_childNodeList;
     NodeListAtomicNameCacheMap m_atomicNameCaches;
     NodeListNameCacheMap m_nameCaches;
-    TagNodeListCacheNS m_tagNodeListCacheNS;
+    TagCollectionCacheNS m_tagCollectionCacheNS;
 };
 
 class NodeMutationObserverData {
@@ -310,7 +310,7 @@ inline bool NodeListsNodeData::deleteThisAndUpdateNodeRareDataIfAboutToRemoveLas
 {
     ASSERT(ownerNode);
     ASSERT(ownerNode->nodeLists() == this);
-    if ((m_childNodeList ? 1 : 0) + m_atomicNameCaches.size() + m_nameCaches.size() + m_tagNodeListCacheNS.size() != 1)
+    if ((m_childNodeList ? 1 : 0) + m_atomicNameCaches.size() + m_nameCaches.size() + m_tagCollectionCacheNS.size() != 1)
         return false;
     ownerNode->clearNodeLists();
     return true;

third_party/WebKit/LayoutTests/fast/dom/getElementsByClassName/dumpNodeList.html
@@ -14,7 +14,7 @@
         document.getElementById("console").appendChild(item);
     }
     
-    NodeList.prototype.dump = function()
+    HTMLCollection.prototype.dump = function()
     {
         var result = "";
         var i = 0;

third_party/WebKit/LayoutTests/fast/dom/non-numeric-values-numeric-parameters-expected.txt
@@ -51,7 +51,7 @@ PASS nonNumericPolicy('document.createEvent("MouseEvent").initMouseEvent("a", fa
 PASS nonNumericPolicy('document.createEvent("MouseEvent").initMouseEvent("a", false, false, null, 0, 0, 0, 0, 0, false, false, false, false, x, null)') is 'any type allowed'
 PASS nonNumericPolicy('document.body.attributes.item(x)') is 'any type allowed'
 PASS nonNumericPolicy('document.createNodeIterator(document, x, null, false)') is 'any type allowed'
-PASS nonNumericPolicy('document.getElementsByTagName("div").item(x)') is 'any type allowed (but not omitted)'
+PASS nonNumericPolicy('document.getElementsByTagName("div").item(x)') is 'any type allowed'
 PASS nonNumericPolicy('document.createRange().setStart(document, x)') is 'any type allowed (but not omitted)'
 PASS nonNumericPolicy('document.createRange().setEnd(document, x)') is 'any type allowed (but not omitted)'
 PASS nonNumericPolicy('document.createRange().comparePoint(document, x)') is 'any type allowed (but not omitted)'

third_party/WebKit/LayoutTests/fast/dom/script-tests/domListEnumeration.js
@@ -94,7 +94,7 @@ setup();
 var resultArray = new Array();
 
 // NodeList
-var nodeList = document.getElementsByTagName('ol');
+var nodeList = document.querySelectorAll('ol');
 resultArray = iterateList(nodeList);
 
 shouldBe("resultArray.length", "5");

third_party/WebKit/LayoutTests/fast/dom/script-tests/non-numeric-values-numeric-parameters.js
@@ -294,7 +294,7 @@ shouldBe("nonNumericPolicy('document.createNodeIterator(document, x, null, false
 
 // NodeList
 
-shouldBe("nonNumericPolicy('document.getElementsByTagName(\"div\").item(x)')", "'any type allowed (but not omitted)'");
+shouldBe("nonNumericPolicy('document.getElementsByTagName(\"div\").item(x)')", "'any type allowed'");
 
 // Range
 

third_party/WebKit/LayoutTests/fast/dom/wrapper-classes-expected.txt
@@ -32,10 +32,10 @@ PASS classString(document.createNodeIterator(root, 0, null, false)) is "NodeIter
 FAIL classString(document.createNodeIterator(root, 0, null, false).__proto__) should be NodeIteratorPrototype. Was Object.
 PASS classString(document.createNodeIterator(root, 0, null, false).constructor) is "Function"
 PASS document.createNodeIterator(root, 0, null, false).constructor.name is "NodeIterator"
-PASS classString(document.getElementsByTagName('div')) is "NodeList"
-FAIL classString(document.getElementsByTagName('div').__proto__) should be NodeListPrototype. Was Object.
+PASS classString(document.getElementsByTagName('div')) is "HTMLCollection"
+FAIL classString(document.getElementsByTagName('div').__proto__) should be HTMLCollectionPrototype. Was Object.
 PASS classString(document.getElementsByTagName('div').constructor) is "Function"
-PASS document.getElementsByTagName('div').constructor.name is "NodeList"
+PASS document.getElementsByTagName('div').constructor.name is "HTMLCollection"
 PASS classString(document.createRange()) is "Range"
 FAIL classString(document.createRange().__proto__) should be RangePrototype. Was Object.
 PASS classString(document.createRange().constructor) is "Function"

third_party/WebKit/LayoutTests/fast/dom/wrapper-classes.html
@@ -84,7 +84,7 @@ function runTest()
     test("document.implementation", "DOMImplementation");
     test("root.attributes", "NamedNodeMap");
     test("document.createNodeIterator(root, 0, null, false)", "NodeIterator");
-    test("document.getElementsByTagName('div')", "NodeList");
+    test("document.getElementsByTagName('div')", "HTMLCollection");
     test("document.createRange()", "Range");
     test("document.createTextNode('')", "Text");
     test("document.createTreeWalker(root, 0, null, false)", "TreeWalker");

third_party/WebKit/LayoutTests/fast/js/constructor-expected.txt
@@ -12,8 +12,8 @@ PASS window.document.body.constructor === window.HTMLBodyElement is true
 PASS window.document.body.constructor.prototype === window.HTMLBodyElement.prototype is true
 PASS window.document.body.constructor.prototype.__proto__ === window.HTMLBodyElement.prototype.__proto__ is true
 PASS window.document.body.constructor.prototype.__proto__ === window.HTMLElement.prototype is true
-PASS nodeList.constructor === window.NodeList is true
-PASS nodeList.constructor.prototype === window.NodeList.prototype is true
+PASS htmlCollection.constructor === window.HTMLCollection is true
+PASS htmlCollection.constructor.prototype === window.HTMLCollection.prototype is true
 PASS mutationEvent.constructor === window.MutationEvent is true
 PASS mutationEvent.constructor.prototype.__proto__ === window.Event.prototype is true
 PASS successfullyParsed is true

commit 4df05ef768fe57e8a2ae0e2ef40869ff3ef0c09f

Use a StringImpl* instead of an AtomicString for NodeListAtomicNameCacheMap's key
Use a StringImpl* instead of an AtomicString for NodeListAtomicNameCacheMap's key.
This results in slightly better performance, mainly by avoiding refcounting
churn on the StringImpl.

This improves Dromaeo's dom-attr test score by ~5.5% on my Linux desktop.
When profiling Dromaeo's dom-attr, I noticed that
NodeListsNodeData::invalidateCaches() was hot (6.7% of time spent), this patch
makes it go down to 5.0%.

This patch minimizes the performance regression caused by r166263, which made
getElementsByTagName() return an HTMLCollection instead of a NodeList. According
to the profiler, the performance impact is mainly due to node lists invalidation,
which is now occuring more frequently because HTMLCollection has an id/name cache
and therefore needs to be invalidated on id/name attribute change.

R=abarth, adamk
BUG=340325

Review URL: https://codereview.chromium.org/141743010

git-svn-id: svn://svn.chromium.org/blink/trunk@166796 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/dom/NodeRareData.h
@@ -94,7 +94,16 @@ public:
         static const bool safeToCompareToEmptyOrDeleted = DefaultHash<StringType>::Hash::safeToCompareToEmptyOrDeleted;
     };
 
-    typedef HashMap<std::pair<unsigned char, AtomicString>, LiveNodeListBase*, NodeListCacheMapEntryHash<AtomicString> > NodeListAtomicNameCacheMap;
+    struct NodeListAtomicCacheMapEntryHash {
+        static unsigned hash(const std::pair<unsigned char, StringImpl*>& entry)
+        {
+            return DefaultHash<StringImpl*>::Hash::hash(entry.second) + entry.first;
+        }
+        static bool equal(const std::pair<unsigned char, StringImpl*>& a, const std::pair<unsigned char, StringImpl*>& b) { return a == b; }
+        static const bool safeToCompareToEmptyOrDeleted = DefaultHash<StringImpl*>::Hash::safeToCompareToEmptyOrDeleted;
+    };
+
+    typedef HashMap<std::pair<unsigned char, StringImpl*>, LiveNodeListBase*, NodeListAtomicCacheMapEntryHash> NodeListAtomicNameCacheMap;
     typedef HashMap<std::pair<unsigned char, String>, LiveNodeListBase*, NodeListCacheMapEntryHash<String> > NodeListNameCacheMap;
     typedef HashMap<QualifiedName, TagCollection*> TagCollectionCacheNS;
 
@@ -228,9 +237,11 @@ private:
         : m_childNodeList(0)
     { }
 
-    std::pair<unsigned char, AtomicString> namedNodeListKey(CollectionType type, const AtomicString& name)
+    std::pair<unsigned char, StringImpl*> namedNodeListKey(CollectionType type, const AtomicString& name)
     {
-        return std::pair<unsigned char, AtomicString>(type, name);
+        // Holding the raw StringImpl is safe because |name| is retained by the NodeList and the NodeList
+        // is reponsible for removing itself from the cache on deletion.
+        return std::pair<unsigned char, StringImpl*>(type, name.impl());
     }
 
     std::pair<unsigned char, String> namedNodeListKey(CollectionType type, const String& name)