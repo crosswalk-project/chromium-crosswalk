df31cfbffd3cb9e2e0ec964b5e223e76b64848e9
Halton Huo <halton.huo@intel.com>
2014-02-10 16:39:09 +0800

    [Views] Fix compiler initialization order errors.

--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -127,8 +127,8 @@ Widget::InitParams::InitParams()
       top_level(false),
       layer_type(aura::WINDOW_LAYER_TEXTURED),
       context(NULL),
-      net_wm_pid(0),
-      force_show_in_taskbar(false) {
+      force_show_in_taskbar(false),
+      net_wm_pid(0) {
 }
 
 Widget::InitParams::InitParams(Type type)
@@ -154,8 +154,8 @@ Widget::InitParams::InitParams(Type type)
       top_level(false),
       layer_type(aura::WINDOW_LAYER_TEXTURED),
       context(NULL),
-      net_wm_pid(0),
-      force_show_in_taskbar(false) {
+      force_show_in_taskbar(false),
+      net_wm_pid(0) {
 }
 
 Widget::InitParams::~InitParams() {
