432b37dc3de1b4ee2a3c615cc8ff04b7572fc679
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 20:07:56 +0000

    Merge 253787 "[GCM] Make sure GCM checkout logic is invoked when..."
    > [GCM] Make sure GCM checkout logic is invoked when the profile is signed out
    > 
    > We now always create GCMProfileService that listens to profile
    > notifications such that GCM checkout logic could be invoked when the
    > profile is signed out.
    > 
    > We now move the GCM check-in logic from GCMClient::Initialize to GCMClient::CheckIn.
    > 
    > BUG=344031
    > TEST=new tests added and existing tests updated
    > 
    > Review URL: https://codereview.chromium.org/165993005
    
    TBR=jianli@chromium.org
    
    Review URL: https://codereview.chromium.org/187573007
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1847/src@255130 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gcm/gcm_client.h
+++ google_apis/gcm/gcm_client.h
@@ -132,7 +132,8 @@ class GCM_EXPORT GCMClient {
   GCMClient();
   virtual ~GCMClient();
 
-  // Begins initialization of the GCM Client.
+  // Begins initialization of the GCM Client. This will not trigger a
+  // connection.
   // |chrome_build_proto|: chrome info, i.e., version, channel and etc.
   // |store_path|: path to the GCM store.
   // |blocking_task_runner|: for running blocking file tasks.
@@ -146,6 +147,10 @@ class GCM_EXPORT GCMClient {
       const scoped_refptr<net::URLRequestContextGetter>&
           url_request_context_getter,
       Delegate* delegate) = 0;
+
+  // Loads the data from the persistent store. This will automatically kick off
+  // the check-in if the check-in info is not found in the store.
+  virtual void Load() = 0;
 
   // Checks out of the GCM service. This will erase all the cached and persisted
   // data.
@@ -176,9 +181,6 @@ class GCM_EXPORT GCMClient {
   virtual void Send(const std::string& app_id,
                     const std::string& receiver_id,
                     const OutgoingMessage& message) = 0;
-
-  // Returns true if GCM becomes ready.
-  virtual bool IsReady() const = 0;
 };
 
 }  // namespace gcm
