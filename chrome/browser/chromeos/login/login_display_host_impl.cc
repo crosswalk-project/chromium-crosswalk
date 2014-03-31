02976457ef2a4ce75f498162cdad3ba8fa8cd09d
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 08:01:55 +0000

    Merge 260094 "Startup sound is put behind the ChromeVox."
    > Startup sound is put behind the ChromeVox.
    > 
    > BUG=357509
    > TEST=none
    > R=nkostylev@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/216173005
    
    TBR=ygorshenin@chromium.org
    
    Review URL: https://codereview.chromium.org/218493004
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1847/src@260502 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_display_host_impl.cc
+++ chrome/browser/chromeos/login/login_display_host_impl.cc
@@ -487,7 +487,7 @@ void LoginDisplayHostImpl::GetAutoEnrollmentCheckResult(
 void LoginDisplayHostImpl::StartWizard(
     const std::string& first_screen_name,
     scoped_ptr<base::DictionaryValue> screen_parameters) {
-  startup_sound_honors_spoken_feedback_ = false;
+  startup_sound_honors_spoken_feedback_ = true;
   TryToPlayStartupSound();
 
   // Keep parameters to restore if renderer crashes.
