crosswalk-project/chromium-crosswalk

commit 17f2f2ad575bf8e7c3d6cdb4d63b9b56ffffdc8e

Remove the --force-compositing-mode flag except for LayoutTests.
Stop honoring the flag and remove IsForceCompositingEnabled
from content/. Now only WebkitTestRunner checks for the
presence of the flag.

R=piman@chromium.org, miu@chromium.org, darin@chromium.org
TBR=darin
BUG=363772

Review URL: https://codereview.chromium.org/240183005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264646 0039d316-1c4b-4281-b951-d872f2087c98

webkit/common/webpreferences.cc
@@ -62,7 +62,6 @@ WebPreferences::WebPreferences()
       asynchronous_spell_checking_enabled(true),
       unified_textchecker_enabled(false),
       accelerated_compositing_enabled(false),
-      force_compositing_mode(false),
       accelerated_compositing_for_3d_transforms_enabled(false),
       accelerated_compositing_for_animation_enabled(false),
       accelerated_compositing_for_video_enabled(false),

chrome/browser/extensions/api/cast_streaming/performance_test.cc
@@ -373,11 +373,8 @@ class CastV2PerformanceTest
       command_line->AppendSwitchASCII(switches::kWindowSize, "2000,1500");
     }
 
-    if (!HasFlag(kUseGpu)) {
+    if (!HasFlag(kUseGpu))
       command_line->AppendSwitch(switches::kDisableGpu);
-    } else {
-      command_line->AppendSwitch(switches::kForceCompositingMode);
-    }
 
     if (HasFlag(kDisableVsync))
       command_line->AppendSwitch(switches::kDisableGpuVsync);

chrome/browser/extensions/api/tab_capture/tab_capture_performancetest.cc
@@ -125,11 +125,8 @@ class TabCapturePerformanceTest
       command_line->AppendSwitchASCII(switches::kWindowSize, "2000,1500");
     }
 
-    if (!HasFlag(kUseGpu)) {
+    if (!HasFlag(kUseGpu))
       command_line->AppendSwitch(switches::kDisableGpu);
-    } else {
-      command_line->AppendSwitch(switches::kForceCompositingMode);
-    }
 
     if (HasFlag(kDisableVsync))
       command_line->AppendSwitch(switches::kDisableGpuVsync);

content/browser/android/content_startup_flags.cc
@@ -56,7 +56,6 @@ void SetContentCommandLineFlags(int max_render_process_count,
       RenderProcessHost::SetMaxRendererProcessCount(max_render_process_count);
   }
 
-  parsed_command_line->AppendSwitch(switches::kForceCompositingMode);
   parsed_command_line->AppendSwitch(switches::kEnableThreadedCompositing);
   parsed_command_line->AppendSwitch(
       switches::kEnableCompositingForFixedPosition);

content/browser/browser_main_loop.cc
@@ -924,7 +924,7 @@ int BrowserMainLoop::BrowserThreadsStarted() {
   // otherwise we'll trigger the assertion about doing IO on the UI thread.
   GpuDataManagerImpl::GetInstance()->Initialize();
 
-  bool always_uses_gpu = IsForceCompositingModeEnabled();
+  bool always_uses_gpu = true;
   bool established_gpu_channel = false;
 #if defined(USE_AURA) || defined(OS_MACOSX)
   if (ShouldInitializeBrowserGpuChannelAndTransportSurface()) {

content/browser/gpu/compositor_util.cc
@@ -140,7 +140,7 @@ const GpuFeatureInfo GetGpuFeatureInfo(size_t index, bool* eof) {
       {
           "force_compositing_mode",
           false,
-          !IsForceCompositingModeEnabled(),
+          false,
           "Force compositing mode is off, either disabled at the command"
           " line or not supported by the current system.",
           false
@@ -163,21 +163,6 @@ const GpuFeatureInfo GetGpuFeatureInfo(size_t index, bool* eof) {
   return kGpuFeatureInfo[index];
 }
 
-bool CanDoAcceleratedCompositing() {
-  const GpuDataManagerImpl* manager = GpuDataManagerImpl::GetInstance();
-
-  // Don't use force compositing mode if gpu access has been blocked or
-  // accelerated compositing is blacklisted.
-  if (!manager->GpuAccessAllowed(NULL))
-    return false;
-
-  // Check for SwiftShader.
-  if (manager->ShouldUseSwiftShader())
-    return false;
-
-  return true;
-}
-
 }  // namespace
 
 bool IsThreadedCompositingEnabled() {
@@ -198,30 +183,6 @@ bool IsThreadedCompositingEnabled() {
 #endif
 }
 
-bool IsForceCompositingModeEnabled() {
-  // Force compositing mode is a subset of threaded compositing mode.
-  if (IsThreadedCompositingEnabled())
-    return true;
-
-  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
-
-  // Command line switches take precedence over blacklisting.
-  if (command_line.HasSwitch(switches::kForceCompositingMode))
-    return true;
-
-  if (!CanDoAcceleratedCompositing())
-    return false;
-
-#if defined(OS_MACOSX) || defined(OS_WIN)
-  // Windows Vista+ has been shipping with TCM enabled at 100% since M24 and
-  // Mac OSX 10.8+ since M28. The blacklist check above takes care of returning
-  // false before this hits on unsupported Win/Mac versions.
-  return true;
-#else
-  return false;
-#endif
-}
-
 bool IsDelegatedRendererEnabled() {
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
   bool enabled = false;
@@ -236,8 +197,7 @@ bool IsDelegatedRendererEnabled() {
   enabled &= !command_line.HasSwitch(switches::kDisableDelegatedRenderer);
 
   // Needs compositing, and thread.
-  if (enabled &&
-      (!IsForceCompositingModeEnabled() || !IsThreadedCompositingEnabled())) {
+  if (enabled && !IsThreadedCompositingEnabled()) {
     enabled = false;
     LOG(ERROR) << "Disabling delegated-rendering because it needs "
                << "force-compositing-mode and threaded-compositing.";
@@ -342,9 +302,6 @@ base::Value* GetFeatureStatus() {
         status += "_readback";
       bool has_thread = IsThreadedCompositingEnabled();
       if (gpu_feature_info.name == "compositing") {
-        bool force_compositing = IsForceCompositingModeEnabled();
-        if (force_compositing)
-          status += "_force";
         if (has_thread)
           status += "_threaded";
       } else if (gpu_feature_info.name == "css_animation") {

content/browser/gpu/compositor_util.h
@@ -16,9 +16,6 @@ namespace content {
 // Returns true if the threaded compositor is on (via flags or field trial).
 CONTENT_EXPORT bool IsThreadedCompositingEnabled();
 
-// Returns true if force-compositing-mode is on (via flags or field trial).
-CONTENT_EXPORT bool IsForceCompositingModeEnabled();
-
 // Returns true if delegated-renderer is on (via flags, or platform default).
 CONTENT_EXPORT bool IsDelegatedRendererEnabled();
 

content/browser/gpu/compositor_util_browsertest.cc
@@ -41,10 +41,6 @@ IN_PROC_BROWSER_TEST_F(CompositorUtilTest, CompositingModeAsExpected) {
     expected_mode = THREADED;
 #endif
 
-  EXPECT_EQ(expected_mode == ENABLED ||
-            expected_mode == THREADED ||
-            expected_mode == DELEGATED,
-            IsForceCompositingModeEnabled());
   EXPECT_EQ(expected_mode == THREADED ||
             expected_mode == DELEGATED,
             IsThreadedCompositingEnabled());

commit 1857573f3587a4f5cff30b9ef4be23f80690c736

Initialize force_compositing_mode to true in WebPreferences constructor.
Fixes uninit errors introduced by r264646.

BUG=364829
R=piman@chromium.org
TBR=darin@chromium.org

Review URL: https://codereview.chromium.org/243243002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264858 0039d316-1c4b-4281-b951-d872f2087c98

webkit/common/webpreferences.cc
@@ -62,6 +62,7 @@ WebPreferences::WebPreferences()
       asynchronous_spell_checking_enabled(true),
       unified_textchecker_enabled(false),
       accelerated_compositing_enabled(false),
+      force_compositing_mode(true),
       accelerated_compositing_for_3d_transforms_enabled(false),
       accelerated_compositing_for_animation_enabled(false),
       accelerated_compositing_for_video_enabled(false),