a9c02ce49bf71551dd611b9841cee99310822bf5
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 00:07:59 +0000

    Merge 264859 "Check if the target window for focus is a transien..."
    > Check if the target window for focus is a transient child of the AppListView window when the mouse is pressed.
    > 
    > BUG=364337
    > TBR=xiyuan
    > 
    > Review URL: https://codereview.chromium.org/241643002
    
    TBR=jennyz@chromium.org
    
    Review URL: https://codereview.chromium.org/248333003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1916/src@265431 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/app_list_controller.cc
+++ ash/wm/app_list_controller.cc
@@ -306,7 +306,7 @@ void AppListController::ProcessLocatedEvent(ui::LocatedEvent* event) {
     }
   }
 
-  aura::Window* window = view_->GetWidget()->GetNativeView();
+  aura::Window* window = view_->GetWidget()->GetNativeView()->parent();
   if (!window->Contains(target))
     SetVisible(false, window);
 }
