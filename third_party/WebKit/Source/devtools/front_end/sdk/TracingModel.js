crosswalk-project/chromium-crosswalk

commit 9176c97dd429320f76bacc67339bb1747f58528e

DevTools: add Tracing agent on back-end
We used to have tracing agent as a browser target only, this adds handling
of start command to the back-end and moves the session id logic that was
initialliy implemented in https://codereview.chromium.org/247513002/ to
the tracing agent.

BUG=361045
R=yurys@chromium.org

Review URL: https://codereview.chromium.org/254613002

git-svn-id: svn://svn.chromium.org/blink/trunk@172623 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/devtools/front_end/sdk/TracingModel.js
@@ -65,18 +65,36 @@ WebInspector.TracingModel.MetadataEvent = {
     ThreadName: "thread_name"
 }
 
+WebInspector.TracingModel.DevToolsMetadataEventCategory = "disabled-by-default-devtools.timeline";
+
+WebInspector.TracingModel.DevToolsMetadataEvent = {
+    TracingStartedInPage: "TracingStartedInPage",
+    SetLayerTreeId: "SetLayerTreeId"
+};
+
 WebInspector.TracingModel.prototype = {
     /**
-     * @param {string} categoryPatterns
+     * @param {string} categoryFilter
      * @param {string} options
      * @param {function(?string)=} callback
      */
-    start: function(categoryPatterns, options, callback)
+    start: function(categoryFilter, options, callback)
     {
         this.reset();
         var bufferUsageReportingIntervalMs = 500;
-        TracingAgent.start(categoryPatterns, options, bufferUsageReportingIntervalMs, callback);
+        /**
+         * @param {?string} error
+         * @param {string} sessionId
+         * @this {WebInspector.TracingModel}
+         */
+        function callbackWrapper(error, sessionId)
+        {
+            this._sessionId = sessionId;
+            callback(error);
+        }
+        TracingAgent.start(categoryFilter, options, bufferUsageReportingIntervalMs, callbackWrapper.bind(this));
         this._active = true;
+        this._sessionId = null;
     },
 
     /**
@@ -90,6 +108,14 @@ WebInspector.TracingModel.prototype = {
         }
         this._pendingStopCallback = callback;
         TracingAgent.end();
+    },
+
+    /**
+     * @return {?string}
+     */
+    sessionId: function()
+    {
+        return this._sessionId;
     },
 
     /**
@@ -433,11 +459,17 @@ WebInspector.TracingDispatcher = function(tracingModel)
 }
 
 WebInspector.TracingDispatcher.prototype = {
+    /**
+     * @param {number} usage
+     */
     bufferUsage: function(usage)
     {
         this._tracingModel._bufferUsage(usage);
     },
 
+    /**
+     * @param {!Array.<!WebInspector.TracingModel.EventPayload>} data
+     */
     dataCollected: function(data)
     {
         this._tracingModel._eventsCollected(data);

third_party/WebKit/Source/core/core.gypi
@@ -1274,6 +1274,8 @@
             'inspector/InspectorTimelineAgent.h',
             'inspector/InspectorTraceEvents.cpp',
             'inspector/InspectorTraceEvents.h',
+            'inspector/InspectorTracingAgent.cpp',
+            'inspector/InspectorTracingAgent.h',
             'inspector/InspectorWorkerAgent.cpp',
             'inspector/InspectorWorkerAgent.h',
             'inspector/InstrumentingAgents.h',

third_party/WebKit/Source/core/inspector/InspectorController.cpp
@@ -58,6 +58,7 @@
 #include "core/inspector/InspectorResourceAgent.h"
 #include "core/inspector/InspectorState.h"
 #include "core/inspector/InspectorTimelineAgent.h"
+#include "core/inspector/InspectorTracingAgent.h"
 #include "core/inspector/InspectorWorkerAgent.h"
 #include "core/inspector/InstrumentingAgents.h"
 #include "core/inspector/PageConsoleAgent.h"
@@ -98,6 +99,10 @@ InspectorController::InspectorController(Page* page, InspectorClient* inspectorC
     OwnPtr<InspectorLayerTreeAgent> layerTreeAgentPtr(InspectorLayerTreeAgent::create(m_domAgent, m_page));
     m_layerTreeAgent = layerTreeAgentPtr.get();
     m_agents.append(layerTreeAgentPtr.release());
+
+    OwnPtr<InspectorTracingAgent> tracingAgentPtr = InspectorTracingAgent::create();
+    m_tracingAgent = tracingAgentPtr.get();
+    m_agents.append(tracingAgentPtr.release());
 
     OwnPtr<InspectorTimelineAgent> timelineAgentPtr(InspectorTimelineAgent::create(m_pageAgent, m_domAgent, m_layerTreeAgent,
         overlay, InspectorTimelineAgent::PageInspector, inspectorClient));
@@ -269,6 +274,7 @@ void InspectorController::setProcessId(long processId)
 void InspectorController::setLayerTreeId(int id)
 {
     m_timelineAgent->setLayerTreeId(id);
+    m_tracingAgent->setLayerTreeId(id);
 }
 
 void InspectorController::webViewResized(const IntSize& size)

third_party/WebKit/Source/core/inspector/InspectorController.h
@@ -55,6 +55,7 @@ class InspectorFrontendClient;
 class InspectorLayerTreeAgent;
 class InspectorPageAgent;
 class InspectorTimelineAgent;
+class InspectorTracingAgent;
 class InspectorOverlay;
 class InspectorState;
 class InstrumentingAgents;
@@ -152,6 +153,7 @@ private:
     InspectorPageAgent* m_pageAgent;
     InspectorTimelineAgent* m_timelineAgent;
     InspectorLayerTreeAgent* m_layerTreeAgent;
+    InspectorTracingAgent* m_tracingAgent;
 
     RefPtr<InspectorBackendDispatcher> m_inspectorBackendDispatcher;
     OwnPtr<InspectorFrontendClient> m_inspectorFrontendClient;

third_party/WebKit/Source/core/inspector/InspectorTimelineAgent.cpp
@@ -73,7 +73,6 @@ static const char includeCounters[] = "includeCounters";
 static const char includeGPUEvents[] = "includeGPUEvents";
 static const char bufferEvents[] = "bufferEvents";
 static const char liveEvents[] = "liveEvents";
-static const char traceEventCategoryFilter[] = "traceEventCategoryFilter";
 }
 
 // Must be kept in sync with WebInspector.TimelineModel.RecordType in TimelineModel.js
@@ -300,10 +299,9 @@ void InspectorTimelineAgent::restore()
     }
 }
 
-void InspectorTimelineAgent::enable(ErrorString*, const String* traceEventCategoryFilter)
+void InspectorTimelineAgent::enable(ErrorString*)
 {
     m_state->setBoolean(TimelineAgentState::enabled, true);
-    m_state->setString(TimelineAgentState::traceEventCategoryFilter, traceEventCategoryFilter ? *traceEventCategoryFilter : "");
 }
 
 void InspectorTimelineAgent::disable(ErrorString*)
@@ -357,13 +355,6 @@ void InspectorTimelineAgent::innerStart()
     if (m_overlay)
         m_overlay->startedRecordingProfile();
     m_state->setBoolean(TimelineAgentState::started, true);
-    String traceEventCategoryFilter = m_state->getString(TimelineAgentState::traceEventCategoryFilter);
-    if (!traceEventCategoryFilter.isEmpty()) {
-        m_client->enableTracing(traceEventCategoryFilter);
-        m_disableTracingOnStop = true;
-    } else {
-        m_disableTracingOnStop = false;
-    }
     m_instrumentingAgents->setInspectorTimelineAgent(this);
     ScriptGCEvent::addEventListener(this);
     if (m_client) {
@@ -417,11 +408,6 @@ void InspectorTimelineAgent::innerStop(bool fromConsole)
         TraceEventDispatcher::instance()->removeAllListeners(this, m_client);
         if (m_state->getBoolean(TimelineAgentState::includeGPUEvents))
             m_client->stopGPUEventsRecording();
-    }
-    // If we have enabled tracing, disable it now.
-    if (m_disableTracingOnStop) {
-        m_client->disableTracing();
-        m_disableTracingOnStop = false;
     }
     m_instrumentingAgents->setInspectorTimelineAgent(0);
     ScriptGCEvent::removeEventListener(this);
@@ -1214,7 +1200,6 @@ InspectorTimelineAgent::InspectorTimelineAgent(InspectorPageAgent* pageAgent, In
     , m_styleRecalcElementCounter(0)
     , m_mayEmitFirstPaint(false)
     , m_lastProgressTimestamp(0)
-    , m_disableTracingOnStop(false)
 {
 }
 

third_party/WebKit/Source/core/inspector/InspectorTimelineAgent.h
@@ -127,7 +127,7 @@ public:
     virtual void clearFrontend() OVERRIDE;
     virtual void restore() OVERRIDE;
 
-    virtual void enable(ErrorString*, const String* traceEventCategoryFilter) OVERRIDE;
+    virtual void enable(ErrorString*) OVERRIDE;
     virtual void disable(ErrorString*) OVERRIDE;
     virtual void start(ErrorString*, const int* maxCallStackDepth, const bool* bufferEvents, const String* liveEvents, const bool* includeCounters, const bool* includeGPUEvents) OVERRIDE;
     virtual void stop(ErrorString*, RefPtr<TypeBuilder::Array<TypeBuilder::Timeline::TimelineEvent> >& events) OVERRIDE;
@@ -327,7 +327,6 @@ private:
     bool m_mayEmitFirstPaint;
     HashSet<String> m_liveEvents;
     double m_lastProgressTimestamp;
-    bool m_disableTracingOnStop;
 };
 
 } // namespace WebCore

third_party/WebKit/Source/core/inspector/InspectorTracingAgent.cpp
@@ -1,0 +1,68 @@
+//
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#include "config.h"
+
+#include "core/inspector/InspectorTracingAgent.h"
+
+#include "core/inspector/IdentifiersFactory.h"
+#include "core/inspector/InspectorClient.h"
+#include "core/inspector/InspectorState.h"
+#include "platform/TraceEvent.h"
+
+namespace WebCore {
+
+namespace TracingAgentState {
+const char sessionId[] = "sessionId";
+}
+
+namespace {
+const char devtoolsMetadataEventCategory[] = TRACE_DISABLED_BY_DEFAULT("devtools.timeline");
+}
+
+InspectorTracingAgent::InspectorTracingAgent()
+    : InspectorBaseAgent<InspectorTracingAgent>("Tracing")
+    , m_layerTreeId(0)
+{
+}
+
+void InspectorTracingAgent::restore()
+{
+    emitMetadataEvents();
+}
+
+void InspectorTracingAgent::start(ErrorString*, const String&, const String&, const double*, String* outSessionId)
+{
+    innerStart();
+    *outSessionId = sessionId();
+}
+
+void InspectorTracingAgent::innerStart()
+{
+    String sessionId = IdentifiersFactory::createIdentifier();
+    m_state->setString(TracingAgentState::sessionId, sessionId);
+    emitMetadataEvents();
+}
+
+String InspectorTracingAgent::sessionId()
+{
+    return m_state->getString(TracingAgentState::sessionId);
+}
+
+void InspectorTracingAgent::emitMetadataEvents()
+{
+    TRACE_EVENT_INSTANT1(devtoolsMetadataEventCategory, "TracingStartedInPage", "sessionId", sessionId().utf8());
+    if (m_layerTreeId)
+        setLayerTreeId(m_layerTreeId);
+}
+
+void InspectorTracingAgent::setLayerTreeId(int layerTreeId)
+{
+    m_layerTreeId = layerTreeId;
+    TRACE_EVENT_INSTANT2(devtoolsMetadataEventCategory, "SetLayerTreeId", "sessionId", sessionId().utf8(), "layerTreeId", m_layerTreeId);
+}
+
+}

third_party/WebKit/Source/core/inspector/InspectorTracingAgent.h
@@ -1,0 +1,50 @@
+/*
+ * Copyright 2014 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef InspectorTracingAgent_h
+#define InspectorTracingAgent_h
+
+#include "InspectorFrontend.h"
+#include "core/inspector/InspectorBaseAgent.h"
+#include "wtf/PassOwnPtr.h"
+#include "wtf/text/WTFString.h"
+
+namespace WebCore {
+
+class InspectorClient;
+
+class InspectorTracingAgent FINAL
+    : public InspectorBaseAgent<InspectorTracingAgent>
+    , public InspectorBackendDispatcher::TracingCommandHandler {
+    WTF_MAKE_NONCOPYABLE(InspectorTracingAgent);
+public:
+    static PassOwnPtr<InspectorTracingAgent> create()
+    {
+        return adoptPtr(new InspectorTracingAgent());
+    }
+
+    // Base agent methods.
+    virtual void restore() OVERRIDE;
+
+    // Protocol method implementations.
+    virtual void start(ErrorString*, const String&, const String&, const double*, String* sessionId) OVERRIDE;
+
+    // Methods for other agents to use.
+    void setLayerTreeId(int);
+
+private:
+    InspectorTracingAgent();
+
+    void innerStart();
+    void emitMetadataEvents();
+    String sessionId();
+
+    int m_layerTreeId;
+};
+
+}
+
+#endif // InspectorTracingAgent_h

commit 01f6e42d80e8b226f316272df118c6ebac863c67

Timeline: fix "undefined is not a function" when tracing experiment is on
This was broken by r172623.
TBR=yurys
BUG=

Review URL: https://codereview.chromium.org/253143002

git-svn-id: svn://svn.chromium.org/blink/trunk@172908 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/devtools/front_end/sdk/TracingModel.js
@@ -90,7 +90,8 @@ WebInspector.TracingModel.prototype = {
         function callbackWrapper(error, sessionId)
         {
             this._sessionId = sessionId;
-            callback(error);
+            if (callback)
+                callback(error);
         }
         TracingAgent.start(categoryFilter, options, bufferUsageReportingIntervalMs, callbackWrapper.bind(this));
         this._active = true;