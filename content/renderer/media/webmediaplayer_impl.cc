40d520e31d1f983c77499f4ad308226005e5743a
karen@chromium.org <karen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 16:55:50 +0000

    Revert 240017 "Enable HW video decode acceleration for MSE on Wi..."
    > Enable HW video decode acceleration for MSE on Windows.
    > 
    > Mid-stream resolution-change was added to DXVAVDA in bug 177422 but apparently
    > we forgot to take advantage of it.  D'oh!
    > 
    > BUG=151045
    > 
    > Review URL: https://codereview.chromium.org/86173002
    
    TBR=fischman@chromium.org
    
    Review URL: https://codereview.chromium.org/263823006
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1916/src@267820 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webmediaplayer_impl.cc
+++ content/renderer/media/webmediaplayer_impl.cc
@@ -1152,6 +1152,13 @@ void WebMediaPlayerImpl::StartPipeline() {
         base::Bind(&LogMediaSourceError, media_log_),
         false);
     demuxer_.reset(chunk_demuxer_);
+
+#if !defined(OS_CHROMEOS)
+    // Disable GpuVideoDecoder creation on platforms other than CrOS until
+    // they support codec config changes.
+    // TODO(acolwell): Remove this once http://crbug.com/151045 is fixed.
+    gpu_factories_ = NULL;
+#endif
   }
 
   scoped_ptr<media::FilterCollection> filter_collection(
