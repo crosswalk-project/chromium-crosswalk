crosswalk-project/chromium-crosswalk

commit fc2b46bc8a826bbce32c0a1cfef6678aa7b0bb2b

Hide WebContentsView from Chrome. This also removes WebContentsViewPort.
TestWebContentsView goes away since it's not needed anymore

BUG=304341
R=avi@chromium.org, thestig@chromium.org

Review URL: https://codereview.chromium.org/262823006

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268072 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/frame_host/render_frame_host_manager.cc
@@ -1070,7 +1070,7 @@ void RenderFrameHostManager::CommitPending() {
     delegate_->SetFocusToLocationBar(false);
   } else if (focus_render_view &&
              render_frame_host_->render_view_host()->GetView()) {
-    render_frame_host_->render_view_host()->GetView()->Focus();
+    render_frame_host_->render_view_host()->Focus();
   }
 
   // Notify that we've swapped RenderFrameHosts. We do this before shutting down

commit 2a1ea0f85521daeb7b7a43c41918409a5fb9941b

Merge 268387 "Fix initial focus not working in sign-in page. Thi..."
> Fix initial focus not working in sign-in page. This was a regression from r268072.
> 
> BUG=369921
> R=avi@chromium.org
> 
> Review URL: https://codereview.chromium.org/263773006

TBR=jam@chromium.org

Review URL: https://codereview.chromium.org/264243002

git-svn-id: svn://svn.chromium.org/chrome/branches/1976/src@268404 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/frame_host/render_frame_host_manager.cc
@@ -1070,7 +1070,7 @@ void RenderFrameHostManager::CommitPending() {
     delegate_->SetFocusToLocationBar(false);
   } else if (focus_render_view &&
              render_frame_host_->render_view_host()->GetView()) {
-    render_frame_host_->render_view_host()->Focus();
+    render_frame_host_->render_view_host()->GetView()->Focus();
   }
 
   // Notify that we've swapped RenderFrameHosts. We do this before shutting down