5c7bab994de0d359ceba5d092fff729d867ec499
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-12 19:59:18 +0000

    Merge 269667 "Revert of Android-side of insertion/selection hand..."
    > Revert of Android-side of insertion/selection handles visibility. (https://codereview.chromium.org/188023002/)
    > 
    > Reason for revert:
    > There are a number of cases where this incorrectly hides the selection/insertion
    > handles in an editable node.
    > 
    > Original issue's description:
    > > Android-side of insertion/selection handles visibility.
    > > 
    > > This patch relies on other two patches:
    > > https://codereview.chromium.org/177903010/
    > > https://codereview.chromium.org/186753002/
    > > to correctly compute the visibility of selection and
    > > insertion handles when editing text input, text area
    > > and contenteditable elements.
    > > 
    > > BUG=236033
    > > 
    > > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=258839
    > 
    > BUG=372056
    > TBR=tedchoc@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/278923003
    
    TBR=jdduke@chromium.org
    
    Review URL: https://codereview.chromium.org/285573002
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1916/src@269853 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -610,9 +610,6 @@ void RenderWidgetHostViewAndroid::SelectionBoundsChanged(
 
 void RenderWidgetHostViewAndroid::SelectionRootBoundsChanged(
     const gfx::Rect& bounds) {
-  if (content_view_core_) {
-    content_view_core_->OnSelectionRootBoundsChanged(bounds);
-  }
 }
 
 void RenderWidgetHostViewAndroid::ScrollOffsetChanged() {
