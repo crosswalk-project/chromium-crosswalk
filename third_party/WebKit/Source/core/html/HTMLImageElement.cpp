crosswalk-project/chromium-crosswalk

commit 0dd8269f29b94e6f49bbf703b0411ee1c03cbd95

Add HTMLPictureElement-based source selection to HTMLImageElement
This CL adds HTMLPictureElement-based source selection algorithm to 
HTMLImageElement and makes sure that the right resource is picked when
HTMLImageElement has an HTMLPictureElement parent.

BUG=368830

Review URL: https://codereview.chromium.org/288033018

git-svn-id: svn://svn.chromium.org/blink/trunk@174354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/html/HTMLImageElement.cpp
@@ -25,8 +25,10 @@
 
 #include "CSSPropertyNames.h"
 #include "HTMLNames.h"
+#include "MediaTypeNames.h"
 #include "RuntimeEnabledFeatures.h"
 #include "bindings/v8/ScriptEventListener.h"
+#include "core/css/MediaQueryMatcher.h"
 #include "core/css/MediaValuesCached.h"
 #include "core/css/parser/SizesAttributeParser.h"
 #include "core/dom/Attribute.h"
@@ -34,10 +36,12 @@
 #include "core/html/HTMLAnchorElement.h"
 #include "core/html/HTMLCanvasElement.h"
 #include "core/html/HTMLFormElement.h"
+#include "core/html/HTMLSourceElement.h"
 #include "core/html/canvas/CanvasRenderingContext.h"
 #include "core/html/parser/HTMLParserIdioms.h"
 #include "core/html/parser/HTMLSrcsetParser.h"
 #include "core/rendering/RenderImage.h"
+#include "platform/MIMETypeRegistry.h"
 
 using namespace std;
 
@@ -150,23 +154,28 @@ void HTMLImageElement::resetFormOwner()
     }
 }
 
+void HTMLImageElement::setBestFitURLAndDPRFromImageCandidate(const ImageCandidate& candidate)
+{
+    m_bestFitImageURL = candidate.url();
+    float candidateScaleFactor = candidate.scaleFactor();
+    // FIXME: Make this ">0" part match the spec, once it settles.
+    if (candidateScaleFactor > 0)
+        m_imageDevicePixelRatio = 1 / candidateScaleFactor;
+    if (renderer() && renderer()->isImage())
+        toRenderImage(renderer())->setImageDevicePixelRatio(m_imageDevicePixelRatio);
+}
+
 void HTMLImageElement::parseAttribute(const QualifiedName& name, const AtomicString& value)
 {
     if (name == altAttr) {
         if (renderer() && renderer()->isImage())
             toRenderImage(renderer())->updateAltText();
     } else if (name == srcAttr || name == srcsetAttr || name == sizesAttr) {
-        int effectiveSize = 0;
+        unsigned effectiveSize = 0;
         if (RuntimeEnabledFeatures::pictureSizesEnabled())
             effectiveSize = SizesAttributeParser::findEffectiveSize(fastGetAttribute(sizesAttr), MediaValuesCached::create(document()));
         ImageCandidate candidate = bestFitSourceForImageAttributes(document().devicePixelRatio(), effectiveSize, fastGetAttribute(srcAttr), fastGetAttribute(srcsetAttr));
-        m_bestFitImageURL = candidate.toAtomicString();
-        float candidateScaleFactor = candidate.scaleFactor();
-        // FIXME: Make this ">0" part match the spec, once it settles.
-        if (candidateScaleFactor > 0)
-            m_imageDevicePixelRatio = 1 / candidateScaleFactor;
-        if (renderer() && renderer()->isImage())
-            toRenderImage(renderer())->setImageDevicePixelRatio(m_imageDevicePixelRatio);
+        setBestFitURLAndDPRFromImageCandidate(candidate);
         m_imageLoader.updateFromElementIgnoringPreviousError();
     } else if (name == usemapAttr) {
         setIsLink(!value.isNull());
@@ -190,6 +199,49 @@ const AtomicString& HTMLImageElement::altText() const
         return alt;
     // fall back to title attribute
     return fastGetAttribute(titleAttr);
+}
+
+static bool supportedImageType(const String& type)
+{
+    return MIMETypeRegistry::isSupportedImageResourceMIMEType(type);
+}
+
+// http://picture.responsiveimages.org/#update-source-set
+ImageCandidate HTMLImageElement::findBestFitImageFromPictureParent()
+{
+    ASSERT(isMainThread());
+    Node* parent = parentNode();
+    if (!parent || !isHTMLPictureElement(*parent))
+        return ImageCandidate();
+    for (Node* child = parent->firstChild(); child; child = child->nextSibling()) {
+        if (child == this)
+            return ImageCandidate();
+
+        if (!isHTMLSourceElement(*child))
+            continue;
+
+        HTMLSourceElement* source = toHTMLSourceElement(child);
+        String srcset = source->fastGetAttribute(srcsetAttr);
+        if (srcset.isEmpty())
+            continue;
+        String type = source->fastGetAttribute(typeAttr);
+        if (!type.isEmpty() && !supportedImageType(type))
+            continue;
+
+        String media = source->fastGetAttribute(mediaAttr);
+        if (!media.isEmpty()) {
+            RefPtr<MediaQuerySet> mediaQueries = MediaQuerySet::create(media);
+            if (!document().mediaQueryMatcher().evaluate(mediaQueries.get()))
+                continue;
+        }
+
+        unsigned effectiveSize = SizesAttributeParser::findEffectiveSize(source->fastGetAttribute(sizesAttr), MediaValuesCached::create(document()));
+        ImageCandidate candidate = bestFitSourceForSrcsetAttribute(document().devicePixelRatio(), effectiveSize, source->fastGetAttribute(srcsetAttr));
+        if (candidate.isEmpty())
+            continue;
+        return candidate;
+    }
+    return ImageCandidate();
 }
 
 RenderObject* HTMLImageElement::createRenderer(RenderStyle* style)
@@ -236,9 +288,18 @@ Node::InsertionNotificationRequest HTMLImageElement::insertedInto(ContainerNode*
     if (!m_formWasSetByParser || insertionPoint->highestAncestorOrSelf() != m_form->highestAncestorOrSelf())
         resetFormOwner();
 
+    bool imageWasModified = false;
+    if (RuntimeEnabledFeatures::pictureEnabled()) {
+        ImageCandidate candidate = findBestFitImageFromPictureParent();
+        if (!candidate.isEmpty()) {
+            setBestFitURLAndDPRFromImageCandidate(candidate);
+            imageWasModified = true;
+        }
+    }
+
     // If we have been inserted from a renderer-less document,
     // our loader may have not fetched the image, so do it now.
-    if (insertionPoint->inDocument() && !m_imageLoader.image())
+    if ((insertionPoint->inDocument() && !m_imageLoader.image()) || imageWasModified)
         m_imageLoader.updateFromElement();
 
     return HTMLElement::insertedInto(insertionPoint);
@@ -315,6 +376,7 @@ int HTMLImageElement::naturalHeight() const
 
 const AtomicString& HTMLImageElement::currentSrc() const
 {
+    // FIXME: Need to absolutize the returned value.
     return m_bestFitImageURL;
 }
 

third_party/WebKit/LayoutTests/fast/dom/HTMLImageElement/image-picture-1x-expected.txt
@@ -1,0 +1,22 @@
+PASS successfullyParsed is true
+
+TEST COMPLETE
+PASS document.getElementById("canary").clientWidth is 75
+PASS document.getElementById("canary").currentSrc is "resources/blue_rect.jpg?0"
+PASS document.getElementById("simple").clientWidth is 800
+PASS document.getElementById("simple").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("multiple").clientWidth is 800
+PASS document.getElementById("multiple").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("other_children").clientWidth is 800
+PASS document.getElementById("other_children").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("img_before_source").clientWidth is 75
+PASS document.getElementById("img_before_source").currentSrc is "resources/blue_rect.jpg?1"
+PASS document.getElementById("unknown_type").clientWidth is 1600
+PASS document.getElementById("unknown_type").currentSrc is "resources/image-set-4x.png?3"
+PASS document.getElementById("empty_srcset").clientWidth is 1600
+PASS document.getElementById("empty_srcset").currentSrc is "resources/image-set-4x.png?3"
+PASS document.getElementById("no_srcset").clientWidth is 1600
+PASS document.getElementById("no_srcset").currentSrc is "resources/image-set-4x.png?3"
+   
+some text
+     

third_party/WebKit/LayoutTests/fast/dom/HTMLImageElement/image-picture-1x.html
@@ -1,0 +1,98 @@
+<!DOCTYPE html>
+<script src="../../../resources/js-test.js"></script>
+<script>
+    if (window.testRunner)
+        testRunner.dumpAsText();
+
+    addEventListener("load", function() {
+        shouldBe('document.getElementById("canary").clientWidth', '75');
+        shouldBe('document.getElementById("canary").currentSrc', '"resources/blue_rect.jpg?0"');
+        shouldBe('document.getElementById("simple").clientWidth', '800');
+        shouldBe('document.getElementById("simple").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("multiple").clientWidth', '800');
+        shouldBe('document.getElementById("multiple").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("other_children").clientWidth', '800');
+        shouldBe('document.getElementById("other_children").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("img_before_source").clientWidth', '75');
+        shouldBe('document.getElementById("img_before_source").currentSrc', '"resources/blue_rect.jpg?1"');
+        shouldBe('document.getElementById("unknown_type").clientWidth', '1600');
+        shouldBe('document.getElementById("unknown_type").currentSrc', '"resources/image-set-4x.png?3"');
+        shouldBe('document.getElementById("empty_srcset").clientWidth', '1600');
+        shouldBe('document.getElementById("empty_srcset").currentSrc', '"resources/image-set-4x.png?3"');
+        shouldBe('document.getElementById("no_srcset").clientWidth', '1600');
+        shouldBe('document.getElementById("no_srcset").currentSrc', '"resources/image-set-4x.png?3"');
+    }, false);
+</script>
+<!-- A canary image to see that the test is actually working -->
+<img id="canary" src="resources/blue_rect.jpg?0">
+<!-- simple picture construct -->
+<picture>
+    <source media="(max-width: 400px)" sizes="400px" srcset="../../hidpi/resources/image-set-1x.png?1 400w, ../../hidpi/resources/image-set-2x.png?1 800w,
+        resources/image-set-4x.png?1 1600w">
+    <source media="(max-width: 800px)" sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+        resources/image-set-4x.png?2 1600w">
+    <source media="(max-width: 1600px)" sizes="1600px" srcset="../../hidpi/resources/image-set-1x.png?3 400w, ../../hidpi/resources/image-set-2x.png?3 800w,
+        resources/image-set-4x.png?3 1600w">
+    <img id="simple" src="resources/blue_rect.jpg?1">
+</picture>
+<!-- Multiple img elements -->
+<picture>
+    <source media="(max-width: 400px)" sizes="400px" srcset="../../hidpi/resources/image-set-1x.png?1 400w, ../../hidpi/resources/image-set-2x.png?1 800w,
+        resources/image-set-4x.png?1 1600w">
+    <source media="(max-width: 800px)" sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+        resources/image-set-4x.png?2 1600w">
+    <source media="(max-width: 1600px)" sizes="1600px" srcset="../../hidpi/resources/image-set-1x.png?3 400w, ../../hidpi/resources/image-set-2x.png?3 800w,
+        resources/image-set-4x.png?3 1600w">
+    <img src="resources/blue_rect.jpg?1">
+    <img id="multiple" src="resources/blue_rect.jpg?1">
+</picture>
+<!-- non-source children - should be ignored-->
+<picture>
+    <div>some text</div>
+    <source media="(max-width: 400px)" sizes="400px" srcset="../../hidpi/resources/image-set-1x.png?1 400w, ../../hidpi/resources/image-set-2x.png?1 800w,
+        resources/image-set-4x.png?1 1600w">
+    <source media="(max-width: 800px)" sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+        resources/image-set-4x.png?2 1600w">
+    <source media="(max-width: 1600px)" sizes="1600px" srcset="../../hidpi/resources/image-set-1x.png?3 400w, ../../hidpi/resources/image-set-2x.png?3 800w,
+        resources/image-set-4x.png?3 1600w">
+    <img id="other_children" src="resources/blue_rect.jpg?1">
+</picture>
+<!-- img before its source siblings - should ignore its siblings -->
+<picture>
+    <img id="img_before_source" src="resources/blue_rect.jpg?1">
+    <source media="(max-width: 400px)" sizes="400px" srcset="../../hidpi/resources/image-set-1x.png?1 400w, ../../hidpi/resources/image-set-2x.png?1 800w,
+        resources/image-set-4x.png?1 1600w">
+    <source media="(max-width: 800px)" sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+        resources/image-set-4x.png?2 1600w">
+    <source media="(max-width: 1600px)" sizes="1600px" srcset="../../hidpi/resources/image-set-1x.png?3 400w, ../../hidpi/resources/image-set-2x.png?3 800w,
+        resources/image-set-4x.png?3 1600w">
+</picture>
+<!-- source with unknown type - should be ignored -->
+<picture>
+    <source media="(max-width: 400px)" sizes="400px" srcset="../../hidpi/resources/image-set-1x.png?1 400w, ../../hidpi/resources/image-set-2x.png?1 800w,
+        resources/image-set-4x.png?1 1600w">
+    <source media="(max-width: 800px)" type="image/jp2" sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+        resources/image-set-4x.png?2 1600w">
+    <source media="(max-width: 1600px)" sizes="1600px" srcset="../../hidpi/resources/image-set-1x.png?3 400w, ../../hidpi/resources/image-set-2x.png?3 800w,
+        resources/image-set-4x.png?3 1600w">
+    <img id="unknown_type" src="resources/blue_rect.jpg?1">
+</picture>
+<!-- source with empty srcset - should be ignored -->
+<picture>
+    <source media="(max-width: 400px)" sizes="400px" srcset="../../hidpi/resources/image-set-1x.png?1 400w, ../../hidpi/resources/image-set-2x.png?1 800w,
+        resources/image-set-4x.png?1 1600w">
+    <source media="(max-width: 800px)" sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400r, ../../hidpi/resources/image-set-2x.png?2 800r,
+        resources/image-set-4x.png?2 1600r">
+    <source media="(max-width: 1600px)" sizes="1600px" srcset="../../hidpi/resources/image-set-1x.png?3 400w, ../../hidpi/resources/image-set-2x.png?3 800w,
+        resources/image-set-4x.png?3 1600w">
+    <img id="empty_srcset" src="resources/blue_rect.jpg?1">
+</picture>
+<!-- source with no srcset - should be ignored -->
+<picture>
+    <source media="(max-width: 400px)" sizes="400px" srcset="../../hidpi/resources/image-set-1x.png?1 400w, ../../hidpi/resources/image-set-2x.png?1 800w,
+        resources/image-set-4x.png?1 1600w">
+    <source media="(max-width: 800px)" sizes="800px">
+    <source media="(max-width: 1600px)" sizes="1600px" srcset="../../hidpi/resources/image-set-1x.png?3 400w, ../../hidpi/resources/image-set-2x.png?3 800w,
+        resources/image-set-4x.png?3 1600w">
+    <img id="no_srcset" src="resources/blue_rect.jpg?1">
+</picture>

third_party/WebKit/LayoutTests/fast/dom/HTMLImageElement/image-picture-invalid-expected.txt
@@ -1,0 +1,19 @@
+PASS successfullyParsed is true
+
+TEST COMPLETE
+PASS document.getElementById("canary").clientWidth is 75
+PASS document.getElementById("canary").currentSrc is "resources/blue_rect.jpg?0"
+PASS document.getElementById("div_end_picture").clientWidth is 800
+PASS document.getElementById("div_end_picture").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("div_multiple_end").clientWidth is 800
+PASS document.getElementById("div_multiple_end").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("source_in_div").clientWidth is 75
+PASS document.getElementById("source_in_div").currentSrc is "resources/blue_rect.jpg"
+PASS document.getElementById("not_in_picture").clientWidth is 75
+PASS document.getElementById("not_in_picture").currentSrc is "resources/blue_rect.jpg"
+
+
+ 
+ 
+ 
+

third_party/WebKit/LayoutTests/fast/dom/HTMLImageElement/image-picture-invalid.html
@@ -1,0 +1,64 @@
+<!DOCTYPE html>
+<script src="../../../resources/js-test.js"></script>
+<script>
+    if (window.testRunner)
+        testRunner.dumpAsText();
+
+    addEventListener("load", function() {
+        shouldBe('document.getElementById("canary").clientWidth', '75');
+        shouldBe('document.getElementById("canary").currentSrc', '"resources/blue_rect.jpg?0"');
+        shouldBe('document.getElementById("div_end_picture").clientWidth', '800');
+        shouldBe('document.getElementById("div_end_picture").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("div_multiple_end").clientWidth', '800');
+        shouldBe('document.getElementById("div_multiple_end").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("source_in_div").clientWidth', '75');
+        shouldBe('document.getElementById("source_in_div").currentSrc', '"resources/blue_rect.jpg"');
+        shouldBe('document.getElementById("not_in_picture").clientWidth', '75');
+        shouldBe('document.getElementById("not_in_picture").currentSrc', '"resources/blue_rect.jpg"');
+    }, false);
+</script>
+<!-- A canary image to see that the test is actually working -->
+<img id="canary" src="resources/blue_rect.jpg?0">
+<!-- Invalid nesting tests -->
+<!-- In the following tests, nesting shouldn't mess up the selection algorithm -->
+<picture>
+    <picture>
+        <source sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+            resources/image-set-4x.png?2 1600w">
+        <div>
+            </picture>
+        </div>
+        <img id="div_end_picture" src="resources/blue_rect.jpg">
+    </picture>
+</picture>
+<picture>
+    <div>
+        </picture>
+        </picture>
+        </picture>
+    </div>
+    <source sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+        resources/image-set-4x.png?2 1600w">
+    <img id="div_multiple_end" src="resources/blue_rect.jpg">
+</picture>
+<!-- In the following tests, nesting messes up the selection algorithm -->
+<picture>
+    <div>
+        <picture>
+            <div>
+                <source sizes="400px" srcset="resources/base-image1.png?4 200w, resources/base-image3.png?4 400w, resources/base-image2.png?4 800w">
+            </div>
+            <img id="source_in_div" src="resources/blue_rect.jpg">
+        </picture>
+    </div>
+</picture>
+<picture>
+    <div>
+        <picture>
+            <div>
+                <source sizes="400px" srcset="resources/base-image1.png?5 200w, resources/base-image3.png?5 400w, resources/base-image2.png?5 800w">
+                <img id="not_in_picture" src="resources/blue_rect.jpg">
+            </div>
+        </picture>
+    </div>
+</picture>

third_party/WebKit/LayoutTests/fast/dom/HTMLImageElement/image-picture-nested-expected.txt
@@ -1,0 +1,25 @@
+PASS successfullyParsed is true
+
+TEST COMPLETE
+PASS document.getElementById("canary").clientWidth is 75
+PASS document.getElementById("canary").currentSrc is "resources/blue_rect.jpg?0"
+PASS document.getElementById("picture_in_picture").clientWidth is 800
+PASS document.getElementById("picture_in_picture").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("picture_in_div_in_picture").clientWidth is 800
+PASS document.getElementById("picture_in_div_in_picture").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("picture_in_div_with_source_above").clientWidth is 800
+PASS document.getElementById("picture_in_div_with_source_above").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("picture_in_the_way").clientWidth is 800
+PASS document.getElementById("picture_in_the_way").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("picture_in_picture_1").clientWidth is 800
+PASS document.getElementById("picture_in_picture_1").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("picture_in_picture_2").clientWidth is 800
+PASS document.getElementById("picture_in_picture_2").currentSrc is "../../hidpi/resources/image-set-2x.png?2"
+PASS document.getElementById("picture_at_end").clientWidth is 75
+PASS document.getElementById("picture_at_end").currentSrc is "resources/blue_rect.jpg?52"
+PASS document.getElementById("picture_no_source").clientWidth is 75
+PASS document.getElementById("picture_no_source").currentSrc is "resources/blue_rect.jpg?6"
+ 
+ 
+ 
+     

third_party/WebKit/LayoutTests/fast/dom/HTMLImageElement/image-picture-nested.html
@@ -1,0 +1,70 @@
+<!DOCTYPE html>
+<script src="../../../resources/js-test.js"></script>
+<script>
+    if (window.testRunner)
+        testRunner.dumpAsText();
+
+    addEventListener("load", function() {
+        shouldBe('document.getElementById("canary").clientWidth', '75');
+        shouldBe('document.getElementById("canary").currentSrc', '"resources/blue_rect.jpg?0"');
+        shouldBe('document.getElementById("picture_in_picture").clientWidth', '800');
+        shouldBe('document.getElementById("picture_in_picture").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("picture_in_div_in_picture").clientWidth', '800');
+        shouldBe('document.getElementById("picture_in_div_in_picture").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("picture_in_div_with_source_above").clientWidth', '800');
+        shouldBe('document.getElementById("picture_in_div_with_source_above").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("picture_in_the_way").clientWidth', '800');
+        shouldBe('document.getElementById("picture_in_the_way").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("picture_in_picture_1").clientWidth', '800');
+        shouldBe('document.getElementById("picture_in_picture_1").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("picture_in_picture_2").clientWidth', '800');
+        shouldBe('document.getElementById("picture_in_picture_2").currentSrc', '"../../hidpi/resources/image-set-2x.png?2"');
+        shouldBe('document.getElementById("picture_at_end").clientWidth', '75');
+        shouldBe('document.getElementById("picture_at_end").currentSrc', '"resources/blue_rect.jpg?52"');
+        shouldBe('document.getElementById("picture_no_source").clientWidth', '75');
+        shouldBe('document.getElementById("picture_no_source").currentSrc', '"resources/blue_rect.jpg?6"');
+    }, false);
+</script>
+<!-- A canary image to see that the test is actually working -->
+<img id="canary" src="resources/blue_rect.jpg?0">
+<!-- nested element tests -->
+<picture>
+    <picture>
+        <source sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+            resources/image-set-4x.png?2 1600w">
+        <img id="picture_in_picture" src="resources/blue_rect.jpg?1">
+    </picture>
+</picture>
+<picture>
+    <div>
+        <picture>
+            <source sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+                resources/image-set-4x.png?2 1600w">
+            <img id="picture_in_div_in_picture" src="resources/blue_rect.jpg?2">
+        </picture>
+    </div>
+</picture>
+<picture>
+    <source sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+        resources/image-set-4x.png?2 1600w">
+    <div>
+        <picture>
+            <source sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+                resources/image-set-4x.png?2 1600w">
+            <img id="picture_in_div_with_source_above" src="resources/blue_rect.jpg?4">
+        </picture>
+    </div>
+    <img id="picture_in_the_way" src="resources/blue_rect.jpg?4">
+</picture>
+<picture>
+    <picture>
+        <source sizes="800px" srcset="../../hidpi/resources/image-set-1x.png?2 400w, ../../hidpi/resources/image-set-2x.png?2 800w,
+            resources/image-set-4x.png?2 1600w">
+        <img id="picture_in_picture_1" src="resources/blue_rect.jpg?5">
+        <img id="picture_in_picture_2" src="resources/blue_rect.jpg?51">
+    </picture>
+    <img id="picture_at_end" src="resources/blue_rect.jpg?52">
+</picture>
+<picture>
+    <img id="picture_no_source" src="resources/blue_rect.jpg?6">
+</picture>

third_party/WebKit/LayoutTests/fast/dom/HTMLImageElement/image-sizes-1x.html
@@ -39,7 +39,7 @@
 <body id="body">
     <img id="simple" src="" sizes="(max-width: 300px) 400px, 800px" srcset="../../hidpi/resources/image-set-1x.png 400w, ../../hidpi/resources/image-set-2x.png 800w">
     <img id="small_sizes" src="" sizes="300px" srcset="../../hidpi/resources/image-set-1x.png 400w, ../../hidpi/resources/image-set-2x.png 800w">
-    <!-- 'sizes' has no imapct on 'x' or 'src' resources -->
+    <!-- 'sizes' has no impact on 'x' or 'src' resources -->
     <img id="sizes_x_descriptors" src="" sizes="50vw" srcset="../../hidpi/resources/image-set-1x.png 1x, ../../hidpi/resources/image-set-2x.png 2x">
     <img id="sizes_src" src="../../hidpi/resources/image-set-1x.png" sizes="50vw">
     <!-- Missing sizes or an invalid one, give a source size of 100vw -->

commit 09333d2e3bbc869a35224b842dcc4ef261172b1c

Oilpan: Fix build broken by r174354.
MediaQuerySet obejct should be holded by RefPtrWillBeRawPtr<>.

TBR=yoav@yoav.ws, oilpan-reviews@chromium.org
BUG=

Review URL: https://codereview.chromium.org/295993003

git-svn-id: svn://svn.chromium.org/blink/trunk@174364 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/html/HTMLImageElement.cpp
@@ -230,7 +230,7 @@ ImageCandidate HTMLImageElement::findBestFitImageFromPictureParent()
 
         String media = source->fastGetAttribute(mediaAttr);
         if (!media.isEmpty()) {
-            RefPtr<MediaQuerySet> mediaQueries = MediaQuerySet::create(media);
+            RefPtrWillBeRawPtr<MediaQuerySet> mediaQueries = MediaQuerySet::create(media);
             if (!document().mediaQueryMatcher().evaluate(mediaQueries.get()))
                 continue;
         }