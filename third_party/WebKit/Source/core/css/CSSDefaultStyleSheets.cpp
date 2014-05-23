crosswalk-project/chromium-crosswalk

commit 138013320d5360b762cb2df454a4a745982387c7

Navigation transitions: added default transition stylesheet that applies in navigation transitions.
This adds a new user agent stylesheet which specifies default properties that apply to navigation transitions.

Navigation Transitions:

Design doc: https://docs.google.com/a/chromium.org/document/d/17jg1RRL3RI969cLwbKBIcoGDsPwqaEdBxafGNYGwiY4/edit#
Implementation details: https://docs.google.com/a/chromium.org/document/d/1kREPtFJaeLoDKwrfmrYTD7DHCdxX1RzFBga2gNY8lyE/edit#heading=h.bng2kpmyvxq5

BUG=370696

Review URL: https://codereview.chromium.org/273193005

git-svn-id: svn://svn.chromium.org/blink/trunk@174637 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/css/CSSDefaultStyleSheets.cpp
@@ -89,6 +89,7 @@ CSSDefaultStyleSheets::CSSDefaultStyleSheets()
     , m_defaultPrintStyle(nullptr)
     , m_defaultViewSourceStyle(nullptr)
     , m_defaultXHTMLMobileProfileStyle(nullptr)
+    , m_defaultTransitionStyle(nullptr)
     , m_defaultStyleSheet(nullptr)
     , m_viewportStyleSheet(nullptr)
     , m_quirksStyleSheet(nullptr)
@@ -130,6 +131,17 @@ RuleSet* CSSDefaultStyleSheets::defaultViewSourceStyle()
         m_defaultViewSourceStyle->addRulesFromSheet(stylesheet.release().leakRef(), screenEval());
     }
     return m_defaultViewSourceStyle.get();
+}
+
+RuleSet* CSSDefaultStyleSheets::defaultTransitionStyle()
+{
+    if (!m_defaultTransitionStyle) {
+        m_defaultTransitionStyle = RuleSet::create();
+        // Loaded stylesheet is leaked on purpose.
+        RefPtrWillBeRawPtr<StyleSheetContents> stylesheet = parseUASheet(navigationTransitionsUserAgentStyleSheet, sizeof(navigationTransitionsUserAgentStyleSheet));
+        m_defaultTransitionStyle->addRulesFromSheet(stylesheet.release().leakRef(), screenEval());
+    }
+    return m_defaultTransitionStyle.get();
 }
 
 RuleSet* CSSDefaultStyleSheets::defaultXHTMLMobileProfileStyle()

third_party/WebKit/Source/core/core_generated.gyp
@@ -538,6 +538,7 @@
               'css/themeWin.css',
               'css/themeWinQuirks.css',
               'css/svg.css',
+              'css/navigationTransitions.css',
               'css/mathml.css',
               'css/mediaControls.css',
               'css/mediaControlsAndroid.css',

third_party/WebKit/Source/core/css/CSSDefaultStyleSheets.h
@@ -42,6 +42,7 @@ public:
     RuleSet* defaultQuirksStyle() { return m_defaultQuirksStyle.get(); }
     RuleSet* defaultPrintStyle() { return m_defaultPrintStyle.get(); }
     RuleSet* defaultViewSourceStyle();
+    RuleSet* defaultTransitionStyle();
 
     // FIXME: Remove WAP support.
     RuleSet* defaultXHTMLMobileProfileStyle();
@@ -65,6 +66,7 @@ private:
     OwnPtrWillBeMember<RuleSet> m_defaultPrintStyle;
     OwnPtrWillBeMember<RuleSet> m_defaultViewSourceStyle;
     OwnPtrWillBeMember<RuleSet> m_defaultXHTMLMobileProfileStyle;
+    OwnPtrWillBeMember<RuleSet> m_defaultTransitionStyle;
 
     RefPtrWillBeMember<StyleSheetContents> m_defaultStyleSheet;
     RefPtrWillBeMember<StyleSheetContents> m_viewportStyleSheet;

third_party/WebKit/Source/core/css/navigationTransitions.css
@@ -1,0 +1,13 @@
+/*
+ * Copyright 2014 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+body {
+  animation-duration: 300ms;
+  transition-duration: 300ms;
+
+  /* Prevent user interaction. */
+  pointer-events: none !important;
+}

third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -295,6 +295,9 @@ void StyleResolver::collectFeatures()
     if (document().isViewSource())
         m_features.add(defaultStyleSheets.defaultViewSourceStyle()->features());
 
+    if (document().isTransitionDocument())
+        m_features.add(defaultStyleSheets.defaultTransitionStyle()->features());
+
     if (m_watchedSelectorsRules)
         m_features.add(m_watchedSelectorsRules->features());
 
@@ -489,6 +492,9 @@ void StyleResolver::matchUARules(ElementRuleCollector& collector)
     // If document uses view source styles (in view source mode or in xml viewer mode), then we match rules from the view source style sheet.
     if (document().isViewSource())
         matchUARules(collector, defaultStyleSheets.defaultViewSourceStyle());
+
+    if (document().isTransitionDocument())
+        matchUARules(collector, defaultStyleSheets.defaultTransitionStyle());
 
     collector.setMatchingUARules(false);
 

third_party/WebKit/Source/core/dom/Document.cpp
@@ -456,6 +456,7 @@ Document::Document(const DocumentInit& initializer, DocumentClassFlags documentC
     , m_sawElementsInKnownNamespaces(false)
     , m_isSrcdocDocument(false)
     , m_isMobileDocument(false)
+    , m_isTransitionDocument(false)
     , m_renderView(0)
     , m_weakFactory(this)
     , m_contextDocument(initializer.contextDocument())

third_party/WebKit/Source/core/dom/Document.h
@@ -416,6 +416,9 @@ public:
     bool isSrcdocDocument() const { return m_isSrcdocDocument; }
     bool isMobileDocument() const { return m_isMobileDocument; }
 
+    bool isTransitionDocument() const { return m_isTransitionDocument; }
+    void setIsTransitionDocument() { m_isTransitionDocument = true; }
+
     StyleResolver* styleResolver() const;
     StyleResolver& ensureStyleResolver() const;
 
@@ -1325,6 +1328,7 @@ private:
     bool m_sawElementsInKnownNamespaces;
     bool m_isSrcdocDocument;
     bool m_isMobileDocument;
+    bool m_isTransitionDocument;
 
     RenderView* m_renderView;
 

commit 0a9dcb40ffc4077b33b1f53e8e71347ab3bfcbbd

Oilpan: Fix a build broken by r174637.
TBR=simonhatch@chromium.org, oilpan-reviews@chromium.org
BUG=

Review URL: https://codereview.chromium.org/292663016

git-svn-id: svn://svn.chromium.org/blink/trunk@174645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/css/CSSDefaultStyleSheets.cpp
@@ -206,6 +206,7 @@ void CSSDefaultStyleSheets::trace(Visitor* visitor)
     visitor->trace(m_defaultPrintStyle);
     visitor->trace(m_defaultViewSourceStyle);
     visitor->trace(m_defaultXHTMLMobileProfileStyle);
+    visitor->trace(m_defaultTransitionStyle);
     visitor->trace(m_defaultStyleSheet);
     visitor->trace(m_viewportStyleSheet);
     visitor->trace(m_quirksStyleSheet);