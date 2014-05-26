7fe929ba4ebf071d1d005145e19ace66a1aa2e82
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-26 12:56:37 +0000

    Merge 272322 "Reland "Only dispatch menu events if they have a v..."
    > Reland "Only dispatch menu events if they have a valid target. (https://codereview.chromium.org/284903002/)"
    >
    > Clean up MenuController at end of tests and on subsequent RunMenu calls.
    >
    > BUG=370162
    > TEST=MenuControllerTest.EventTargeter
    > TEST=In touchview, open wrench menu and press a key (i.e. 'T'), nothing should
    > happen.
    >
    > Review URL: https://codereview.chromium.org/286933007
    
    TBR=flackr@chromium.org
    
    Review URL: https://codereview.chromium.org/290013008
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1985/src@272812 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_event_dispatcher_linux.cc
+++ ui/views/controls/menu/menu_event_dispatcher_linux.cc
@@ -4,10 +4,13 @@
 
 #include "ui/views/controls/menu/menu_event_dispatcher_linux.h"
 
+#include "base/memory/scoped_ptr.h"
+#include "ui/aura/window.h"
 #include "ui/events/event_utils.h"
 #include "ui/events/keycodes/keyboard_code_conversion.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/views/controls/menu/menu_controller.h"
+#include "ui/views/widget/widget.h"
 
 namespace views {
 namespace internal {
@@ -22,29 +25,48 @@ bool MenuEventDispatcher::CanDispatchEvent(const ui::PlatformEvent& event) {
 }
 
 uint32_t MenuEventDispatcher::DispatchEvent(const ui::PlatformEvent& event) {
+  bool should_process_event = true;
+
+  // Check if the event should be handled.
+  scoped_ptr<ui::Event> ui_event(ui::EventFromNative(event));
+  if (ui_event && menu_controller_->owner()) {
+    aura::Window* menu_window = menu_controller_->owner()->GetNativeWindow();
+    aura::Window* target_window = static_cast<aura::Window*>(
+        static_cast<ui::EventTarget*>(menu_window->GetRootWindow())->
+            GetEventTargeter()->FindTargetForEvent(menu_window,
+                                                   ui_event.get()));
+    // TODO(flackr): The event shouldn't be handled if target_window is not
+    // menu_window, however the event targeter does not properly target the
+    // open menu. For now, we allow targeters to prevent handling by the menu.
+    if (!target_window)
+      should_process_event = false;
+  }
+
   if (menu_controller_->exit_type() == MenuController::EXIT_ALL ||
       menu_controller_->exit_type() == MenuController::EXIT_DESTROYED)
     return (ui::POST_DISPATCH_QUIT_LOOP | ui::POST_DISPATCH_PERFORM_DEFAULT);
 
-  switch (ui::EventTypeFromNative(event)) {
-    case ui::ET_KEY_PRESSED: {
-      if (!menu_controller_->OnKeyDown(ui::KeyboardCodeFromNative(event)))
-        return ui::POST_DISPATCH_QUIT_LOOP;
-
-      // Do not check mnemonics if the Alt or Ctrl modifiers are pressed.
-      int flags = ui::EventFlagsFromNative(event);
-      if ((flags & (ui::EF_CONTROL_DOWN | ui::EF_ALT_DOWN)) == 0) {
-        char c = ui::GetCharacterFromKeyCode(ui::KeyboardCodeFromNative(event),
-                                             flags);
-        if (menu_controller_->SelectByChar(c))
+  if (should_process_event) {
+    switch (ui::EventTypeFromNative(event)) {
+      case ui::ET_KEY_PRESSED: {
+        if (!menu_controller_->OnKeyDown(ui::KeyboardCodeFromNative(event)))
           return ui::POST_DISPATCH_QUIT_LOOP;
+
+        // Do not check mnemonics if the Alt or Ctrl modifiers are pressed.
+        int flags = ui::EventFlagsFromNative(event);
+        if ((flags & (ui::EF_CONTROL_DOWN | ui::EF_ALT_DOWN)) == 0) {
+          char c = ui::GetCharacterFromKeyCode(
+              ui::KeyboardCodeFromNative(event), flags);
+          if (menu_controller_->SelectByChar(c))
+            return ui::POST_DISPATCH_QUIT_LOOP;
+        }
+        return ui::POST_DISPATCH_NONE;
       }
-      return ui::POST_DISPATCH_NONE;
+      case ui::ET_KEY_RELEASED:
+        return ui::POST_DISPATCH_NONE;
+      default:
+        break;
     }
-    case ui::ET_KEY_RELEASED:
-      return ui::POST_DISPATCH_NONE;
-    default:
-      break;
   }
 
   return ui::POST_DISPATCH_PERFORM_DEFAULT |
