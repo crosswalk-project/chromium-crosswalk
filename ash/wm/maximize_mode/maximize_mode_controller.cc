c72856b29f85e6fbb273d32d7ea3977b821093b0
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-27 14:16:06 +0000

    Merge 271808 "Re-landing "Suppressed screen rotation notificatio..."
    > Re-landing "Suppressed screen rotation notifications triggeres by the accelerometer."
    > Original review: https://codereview.chromium.org/267743010/
    >
    > BUG=364949
    > Test=MaximizeModeControllerTest.BlockRotationNotifications
    > TBR=flackr
    > TBR=sadrul
    >
    > Review URL: https://codereview.chromium.org/291233002
    
    R=bruthig@chromium.org
    TBR=bruthig@chromium.org
    
    Review URL: https://codereview.chromium.org/302543003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1985/src@272968 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/maximize_mode/maximize_mode_controller.cc
+++ ash/wm/maximize_mode/maximize_mode_controller.cc
@@ -9,6 +9,7 @@
 #include "ash/display/display_manager.h"
 #include "ash/shell.h"
 #include "ash/wm/maximize_mode/maximize_mode_event_blocker.h"
+#include "base/auto_reset.h"
 #include "base/command_line.h"
 #include "ui/gfx/vector3d_f.h"
 
@@ -85,7 +86,8 @@ float ClockwiseAngleBetweenVectorsInDegrees(const gfx::Vector3dF& base,
 
 MaximizeModeController::MaximizeModeController()
     : rotation_locked_(false),
-      have_seen_accelerometer_data_(false) {
+      have_seen_accelerometer_data_(false),
+      in_set_screen_rotation_(false) {
   Shell::GetInstance()->accelerometer_controller()->AddObserver(this);
 }
 
@@ -184,8 +186,8 @@ void MaximizeModeController::HandleScreenRotation(const gfx::Vector3dF& lid) {
       // Also, SetDisplayRotation will save the setting to the local store,
       // this should be stored in a way that we can distinguish what the
       // rotation was set by.
-      display_manager->SetDisplayRotation(gfx::Display::InternalDisplayId(),
-                                          gfx::Display::ROTATE_0);
+      SetDisplayRotation(display_manager,
+                         gfx::Display::ROTATE_0);
     }
     rotation_locked_ = false;
     return;
@@ -243,9 +245,18 @@ void MaximizeModeController::HandleScreenRotation(const gfx::Vector3dF& lid) {
   // match screen orientation.
   if (new_rotation == gfx::Display::ROTATE_0 ||
       maximize_mode_engaged) {
-    display_manager->SetDisplayRotation(gfx::Display::InternalDisplayId(),
-                                        new_rotation);
+    SetDisplayRotation(display_manager,
+                       new_rotation);
   }
+}
+
+void MaximizeModeController::SetDisplayRotation(
+    DisplayManager* display_manager,
+    gfx::Display::Rotation rotation) {
+  base::AutoReset<bool> auto_in_set_screen_rotation(
+      &in_set_screen_rotation_, true);
+  display_manager->SetDisplayRotation(gfx::Display::InternalDisplayId(),
+                                      rotation);
 }
 
 }  // namespace ash
