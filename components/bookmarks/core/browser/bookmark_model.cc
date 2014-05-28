e7c0157df07ac3f4f5d8650abe7aebba3e185d6d
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 19:15:12 +0000

    Fixed missing include fixes from http://crrev.com/273301.
    BUG=367952
    
    Review URL: https://codereview.chromium.org/303873002
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1985/src@273314 0039d316-1c4b-4281-b951-d872f2087c98

--- components/bookmarks/core/browser/bookmark_model.cc
+++ components/bookmarks/core/browser/bookmark_model.cc
@@ -20,7 +20,7 @@
 #include "components/bookmarks/core/browser/bookmark_storage.h"
 #include "components/bookmarks/core/browser/bookmark_utils.h"
 #include "components/favicon_base/favicon_types.h"
-#include "grit/component_strings.h"
+#include "grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/favicon_size.h"
 
