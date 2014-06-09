55a1ba41d8fe8d70943252281e56e3d714f221ac
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-09 22:50:43 +0000

    Merge 275719 "Always round up coded size to avoid VideoFrame res..."
    > Always round up coded size to avoid VideoFrame restrictions.
    > 
    > Prior to http://crrev.com/268831 we never considered the actual
    > coded size when constructing buffers for FFmpeg.  We do now, but
    > were not aligning the extents properly.
    > 
    > Sadly we had no test coverage for odd sized videos :(  I've fixed
    > the extents and added a test using one of the bug clips (which
    > will be landed separately).
    > 
    > BUG=379127
    > TEST=new pipeline test
    > NOTRY=true
    > 
    > Review URL: https://codereview.chromium.org/318133002
    
    TBR=dalecurtis@chromium.org
    
    Review URL: https://codereview.chromium.org/325903004
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1985/src@275868 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_video_decoder.cc
+++ media/filters/ffmpeg_video_decoder.cc
@@ -54,6 +54,12 @@ static int GetThreadCount(AVCodecID codec_id) {
   return decode_threads;
 }
 
+static size_t RoundUp(size_t value, size_t alignment) {
+  // Check that |alignment| is a power of 2.
+  DCHECK((alignment + (alignment - 1)) == (alignment | (alignment - 1)));
+  return ((value + (alignment - 1)) & ~(alignment - 1));
+}
+
 FFmpegVideoDecoder::FFmpegVideoDecoder(
     const scoped_refptr<base::SingleThreadTaskRunner>& task_runner)
     : task_runner_(task_runner), state_(kUninitialized) {}
@@ -91,9 +97,13 @@ int FFmpegVideoDecoder::GetVideoBuffer(AVCodecContext* codec_context,
   //
   // When lowres is non-zero, dimensions should be divided by 2^(lowres), but
   // since we don't use this, just DCHECK that it's zero.
+  //
+  // Always round up to a multiple of two to match VideoFrame restrictions on
+  // frame alignment.
   DCHECK_EQ(codec_context->lowres, 0);
-  gfx::Size coded_size(std::max(size.width(), codec_context->coded_width),
-                       std::max(size.height(), codec_context->coded_height));
+  gfx::Size coded_size(
+      RoundUp(std::max(size.width(), codec_context->coded_width), 2),
+      RoundUp(std::max(size.height(), codec_context->coded_height), 2));
 
   if (!VideoFrame::IsValidConfig(
           format, coded_size, gfx::Rect(size), natural_size))
