41a87a6b8aa097c2f6bee94dae89656a31fbb822
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-10 18:03:23 +0000

    Merge 270621 "Fixes the crash when the transient parent is NULL."
    > Fixes the crash when the transient parent is NULL.
    > 
    > I couldn't find the actual repro steps but anyways the method
    > signature of ui/shell_dialogs/select_file_dialog.h says that
    > the parent could be NULL.
    > 
    > BUG=373522
    > R=erg@chromium.org
    > TEST=None
    > 
    > Review URL: https://codereview.chromium.org/283303002
    
    TBR=mukai@chromium.org
    
    Review URL: https://codereview.chromium.org/328683004
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1985/src@276073 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/libgtk2ui/print_dialog_gtk2.cc
+++ chrome/browser/ui/libgtk2ui/print_dialog_gtk2.cc
@@ -242,7 +242,8 @@ void PrintDialogGtk2::ShowDialog(
   // TODO(mukai): take care of parent as select_file_dialog_impl_gtk2.
   dialog_ = gtk_print_unix_dialog_new(NULL, NULL);
   libgtk2ui::SetGtkTransientForAura(dialog_, parent_view);
-  parent_view->AddObserver(this);
+  if (parent_view)
+    parent_view->AddObserver(this);
   g_signal_connect(dialog_, "delete-event",
                    G_CALLBACK(gtk_widget_hide_on_delete), NULL);
 
