799ea5c2b64553182b9824f768239ad5cf7f48d8
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2014-06-17 13:18:20 +0200

    Enable Blink Screen Orientation [Lock] for Android

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -50,6 +50,7 @@ static void SetRuntimeFeatureDefaultsForPlatform() {
   // Android does not yet support NavigatorContentUtils.
   WebRuntimeFeatures::enableNavigatorContentUtils(false);
   WebRuntimeFeatures::enableTouchIconLoading(true);
+  WebRuntimeFeatures::enableScreenOrientation(true);
   WebRuntimeFeatures::enableOrientationEvent(true);
   WebRuntimeFeatures::enableFastMobileScrolling(true);
 #else
