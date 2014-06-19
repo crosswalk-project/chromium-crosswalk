crosswalk-project/chromium-crosswalk

commit b6af1a2209cca61eb19ce4ece6ca22687eb3ed7e

Fix WebMStreamParser to continue parsing after a cluster end
Previously the parser would stop after cluster end even if there is more
data to be parsed.

BUG=382807,335676
R=acolwell@chromium.org, wolenetz@chromium.org

Review URL: https://codereview.chromium.org/328653002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277633 0039d316-1c4b-4281-b951-d872f2087c98

media/formats/webm/webm_stream_parser.cc
@@ -143,6 +143,7 @@ int WebMStreamParser::ParseInfoAndTracks(const uint8* data, int size) {
     case kWebMIdCRC32:
     case kWebMIdCues:
     case kWebMIdChapters:
+      // TODO(matthewjheaney): Implement support for chapters.
       if (cur_size < (result + element_size)) {
         // We don't have the whole element yet. Signal we need more data.
         return 0;
@@ -242,61 +243,70 @@ int WebMStreamParser::ParseCluster(const uint8* data, int size) {
   if (!cluster_parser_)
     return -1;
 
-  int id;
-  int64 element_size;
-  int result = WebMParseElementHeader(data, size, &id, &element_size);
+  int result = 0;
+  int bytes_parsed;
+  bool cluster_ended;
+  do {
+    cluster_ended = false;
 
-  if (result <= 0)
-    return result;
+    // If we are not parsing a cluster then handle the case when the next
+    // element is not a cluster.
+    if (!parsing_cluster_) {
+      int id;
+      int64 element_size;
+      bytes_parsed = WebMParseElementHeader(data, size, &id, &element_size);
 
-  // TODO(matthewjheaney): implement support for chapters
-  if (id == kWebMIdCues || id == kWebMIdChapters) {
-    // TODO(wolenetz): Handle unknown-sized cluster parse completion correctly.
-    // See http://crbug.com/335676.
-    if (size < (result + element_size)) {
-      // We don't have the whole element yet. Signal we need more data.
-      return 0;
+      if (bytes_parsed < 0)
+        return bytes_parsed;
+
+      if (bytes_parsed == 0)
+        return result;
+
+      if (id != kWebMIdCluster) {
+        ChangeState(kParsingHeaders);
+        return result;
+      }
     }
-    // Skip the element.
-    return result + element_size;
-  }
 
-  if (id == kWebMIdEBMLHeader) {
-    // TODO(wolenetz): Handle unknown-sized cluster parse completion correctly.
-    // See http://crbug.com/335676.
-    ChangeState(kParsingHeaders);
-    return 0;
-  }
+    bytes_parsed = cluster_parser_->Parse(data, size);
 
-  int bytes_parsed = cluster_parser_->Parse(data, size);
+    if (bytes_parsed < 0)
+      return bytes_parsed;
 
-  if (bytes_parsed <= 0)
-    return bytes_parsed;
+    // If cluster detected, immediately notify new segment if we have not
+    // already done this.
+    if (!parsing_cluster_ && bytes_parsed > 0) {
+      parsing_cluster_ = true;
+      new_segment_cb_.Run();
+    }
 
-  // If cluster detected, immediately notify new segment if we have not already
-  // done this.
-  if (id == kWebMIdCluster && !parsing_cluster_) {
-    parsing_cluster_ = true;
-    new_segment_cb_.Run();
-  }
+    const BufferQueue& audio_buffers = cluster_parser_->GetAudioBuffers();
+    const BufferQueue& video_buffers = cluster_parser_->GetVideoBuffers();
+    const TextBufferQueueMap& text_map = cluster_parser_->GetTextBuffers();
 
-  const BufferQueue& audio_buffers = cluster_parser_->GetAudioBuffers();
-  const BufferQueue& video_buffers = cluster_parser_->GetVideoBuffers();
-  const TextBufferQueueMap& text_map = cluster_parser_->GetTextBuffers();
+    cluster_ended = cluster_parser_->cluster_ended();
 
-  bool cluster_ended = cluster_parser_->cluster_ended();
+    if ((!audio_buffers.empty() || !video_buffers.empty() ||
+         !text_map.empty()) &&
+        !new_buffers_cb_.Run(audio_buffers, video_buffers, text_map)) {
+      return -1;
+    }
 
-  if ((!audio_buffers.empty() || !video_buffers.empty() || !text_map.empty()) &&
-      !new_buffers_cb_.Run(audio_buffers, video_buffers, text_map)) {
-    return -1;
-  }
+    if (cluster_ended) {
+      parsing_cluster_ = false;
+      end_of_segment_cb_.Run();
+    }
 
-  if (cluster_ended) {
-    parsing_cluster_ = false;
-    end_of_segment_cb_.Run();
-  }
+    result += bytes_parsed;
+    data += bytes_parsed;
+    size -= bytes_parsed;
 
-  return bytes_parsed;
+    // WebMClusterParser returns 0 and |cluster_ended| is true if previously
+    // parsing an unknown-size cluster and |data| does not continue that
+    // cluster. Try parsing again in that case.
+  } while (size > 0 && (bytes_parsed > 0 || cluster_ended));
+
+  return result;
 }
 
 void WebMStreamParser::FireNeedKey(const std::string& key_id) {

media/filters/chunk_demuxer_unittest.cc
@@ -673,6 +673,13 @@ class ChunkDemuxerTest : public ::testing::TestWithParam<bool> {
   scoped_ptr<Cluster> GenerateCluster(int first_audio_timecode,
                                       int first_video_timecode,
                                       int block_count) {
+    return GenerateCluster(first_audio_timecode, first_video_timecode,
+                           block_count, false);
+  }
+  scoped_ptr<Cluster> GenerateCluster(int first_audio_timecode,
+                                      int first_video_timecode,
+                                      int block_count,
+                                      bool unknown_size) {
     CHECK_GT(block_count, 0);
 
     int size = 10;
@@ -722,7 +729,7 @@ class ChunkDemuxerTest : public ::testing::TestWithParam<bool> {
                        kWebMFlagKeyframe, data.get(), size);
     }
 
-    return cb.Finish();
+    return unknown_size ? cb.FinishWithUnknownSize() : cb.Finish();
   }
 
   scoped_ptr<Cluster> GenerateSingleStreamCluster(int timecode,
@@ -2893,11 +2900,14 @@ TEST_P(ChunkDemuxerTest, EmitBuffersDuringAbort) {
 #endif
 
 TEST_P(ChunkDemuxerTest, WebMIsParsingMediaSegmentDetection) {
-  // TODO(wolenetz): Also test 'unknown' sized clusters.
-  // See http://crbug.com/335676.
   const uint8 kBuffer[] = {
     0x1F, 0x43, 0xB6, 0x75, 0x83,  // CLUSTER (size = 3)
     0xE7, 0x81, 0x01,                // Cluster TIMECODE (value = 1)
+
+    0x1F, 0x43, 0xB6, 0x75, 0xFF,  // CLUSTER (size = unknown; really 3 due to:)
+    0xE7, 0x81, 0x02,                // Cluster TIMECODE (value = 2)
+    /* e.g. put some blocks here... */
+    0x1A, 0x45, 0xDF, 0xA3, 0x8A,  // EBMLHEADER (size = 10, not fully appended)
   };
 
   // This array indicates expected return value of IsParsingMediaSegment()
@@ -2905,6 +2915,11 @@ TEST_P(ChunkDemuxerTest, WebMIsParsingMediaSegmentDetection) {
   const bool kExpectedReturnValues[] = {
     false, false, false, false, true,
     true, true, false,
+
+    false, false, false, false, true,
+    true, true, true,
+
+    true, true, true, true, false,
   };
 
   COMPILE_ASSERT(arraysize(kBuffer) == arraysize(kExpectedReturnValues),
@@ -3452,6 +3467,17 @@ TEST_P(ChunkDemuxerTest, SeekCompletesWithoutTextCues) {
   // Verify that audio & video streams continue to return expected values.
   CheckExpectedBuffers(audio_stream, "160 180");
   CheckExpectedBuffers(video_stream, "180 210");
+}
+
+TEST_P(ChunkDemuxerTest, ClusterWithUnknownSize) {
+  ASSERT_TRUE(InitDemuxer(HAS_AUDIO | HAS_VIDEO));
+
+  AppendCluster(GenerateCluster(0, 0, 4, true));
+  CheckExpectedRanges(kSourceId, "{ [0,46) }");
+
+  // A new cluster indicates end of the previous cluster with unknown size.
+  AppendCluster(GenerateCluster(46, 66, 5, true));
+  CheckExpectedRanges(kSourceId, "{ [0,115) }");
 }
 
 // Generate two sets of tests: one using FrameProcessor, and one using

media/formats/webm/cluster_builder.cc
@@ -6,6 +6,7 @@
 
 #include "base/logging.h"
 #include "media/base/data_buffer.h"
+#include "media/formats/webm/webm_constants.h"
 
 namespace media {
 
@@ -174,6 +175,16 @@ scoped_ptr<Cluster> ClusterBuilder::Finish() {
   DCHECK_NE(cluster_timecode_, -1);
 
   UpdateUInt64(kClusterSizeOffset, bytes_used_ - (kClusterSizeOffset + 8));
+
+  scoped_ptr<Cluster> ret(new Cluster(buffer_.Pass(), bytes_used_));
+  Reset();
+  return ret.Pass();
+}
+
+scoped_ptr<Cluster> ClusterBuilder::FinishWithUnknownSize() {
+  DCHECK_NE(cluster_timecode_, -1);
+
+  UpdateUInt64(kClusterSizeOffset, kWebMUnknownSize);
 
   scoped_ptr<Cluster> ret(new Cluster(buffer_.Pass(), bytes_used_));
   Reset();

media/formats/webm/cluster_builder.h
@@ -40,6 +40,7 @@ class ClusterBuilder {
                      int flags, const uint8* data, int size);
 
   scoped_ptr<Cluster> Finish();
+  scoped_ptr<Cluster> FinishWithUnknownSize();
 
  private:
   void AddBlockGroupInternal(int track_num, int64 timecode,

commit 03b76471cf0df67caf7761cfc10c39bdc1fe2acc

Fix WebMStreamParser to handle Cues between Clusters correctly.
Previously webm parser was correctly handling Cues element between
clusters. It was broken in r277633. Also added tests to cover this
case.

BUG=386122
R=acolwell@chromium.org, wolenetz@chromium.org

Review URL: https://codereview.chromium.org/346613003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278452 0039d316-1c4b-4281-b951-d872f2087c98

media/formats/webm/webm_stream_parser.cc
@@ -20,8 +20,7 @@ namespace media {
 
 WebMStreamParser::WebMStreamParser()
     : state_(kWaitingForInit),
-      unknown_segment_size_(false),
-      parsing_cluster_(false) {
+      unknown_segment_size_(false) {
 }
 
 WebMStreamParser::~WebMStreamParser() {
@@ -59,12 +58,12 @@ void WebMStreamParser::Flush() {
   DCHECK_NE(state_, kWaitingForInit);
 
   byte_queue_.Reset();
-  parsing_cluster_ = false;
-
-  if (state_ != kParsingClusters)
-    return;
-
-  cluster_parser_->Reset();
+  if (cluster_parser_)
+    cluster_parser_->Reset();
+  if (state_ == kParsingClusters) {
+    ChangeState(kParsingHeaders);
+    end_of_segment_cb_.Run();
+  }
 }
 
 bool WebMStreamParser::Parse(const uint8* buf, int size) {
@@ -151,6 +150,15 @@ int WebMStreamParser::ParseInfoAndTracks(const uint8* data, int size) {
       // Skip the element.
       return result + element_size;
       break;
+    case kWebMIdCluster:
+      if (!cluster_parser_) {
+        MEDIA_LOG(log_cb_) << "Found Cluster element before Info.";
+        return -1;
+      }
+      ChangeState(kParsingClusters);
+      new_segment_cb_.Run();
+      return 0;
+      break;
     case kWebMIdSegment:
       // Segment of unknown size indicates live stream.
       if (element_size == kWebMUnknownSize)
@@ -231,8 +239,6 @@ int WebMStreamParser::ParseInfoAndTracks(const uint8* data, int size) {
       tracks_parser.video_encryption_key_id(),
       log_cb_));
 
-  ChangeState(kParsingClusters);
-
   if (!init_cb_.is_null())
     base::ResetAndReturn(&init_cb_).Run(true, params);
 
@@ -243,70 +249,28 @@ int WebMStreamParser::ParseCluster(const uint8* data, int size) {
   if (!cluster_parser_)
     return -1;
 
-  int result = 0;
-  int bytes_parsed;
-  bool cluster_ended;
-  do {
-    cluster_ended = false;
+  int bytes_parsed = cluster_parser_->Parse(data, size);
+  if (bytes_parsed < 0)
+    return bytes_parsed;
 
-    // If we are not parsing a cluster then handle the case when the next
-    // element is not a cluster.
-    if (!parsing_cluster_) {
-      int id;
-      int64 element_size;
-      bytes_parsed = WebMParseElementHeader(data, size, &id, &element_size);
+  const BufferQueue& audio_buffers = cluster_parser_->GetAudioBuffers();
+  const BufferQueue& video_buffers = cluster_parser_->GetVideoBuffers();
+  const TextBufferQueueMap& text_map = cluster_parser_->GetTextBuffers();
 
-      if (bytes_parsed < 0)
-        return bytes_parsed;
+  bool cluster_ended = cluster_parser_->cluster_ended();
 
-      if (bytes_parsed == 0)
-        return result;
+  if ((!audio_buffers.empty() || !video_buffers.empty() ||
+       !text_map.empty()) &&
+      !new_buffers_cb_.Run(audio_buffers, video_buffers, text_map)) {
+    return -1;
+  }
 
-      if (id != kWebMIdCluster) {
-        ChangeState(kParsingHeaders);
-        return result;
-      }
-    }
+  if (cluster_ended) {
+    ChangeState(kParsingHeaders);
+    end_of_segment_cb_.Run();
+  }
 
-    bytes_parsed = cluster_parser_->Parse(data, size);
-
-    if (bytes_parsed < 0)
-      return bytes_parsed;
-
-    // If cluster detected, immediately notify new segment if we have not
-    // already done this.
-    if (!parsing_cluster_ && bytes_parsed > 0) {
-      parsing_cluster_ = true;
-      new_segment_cb_.Run();
-    }
-
-    const BufferQueue& audio_buffers = cluster_parser_->GetAudioBuffers();
-    const BufferQueue& video_buffers = cluster_parser_->GetVideoBuffers();
-    const TextBufferQueueMap& text_map = cluster_parser_->GetTextBuffers();
-
-    cluster_ended = cluster_parser_->cluster_ended();
-
-    if ((!audio_buffers.empty() || !video_buffers.empty() ||
-         !text_map.empty()) &&
-        !new_buffers_cb_.Run(audio_buffers, video_buffers, text_map)) {
-      return -1;
-    }
-
-    if (cluster_ended) {
-      parsing_cluster_ = false;
-      end_of_segment_cb_.Run();
-    }
-
-    result += bytes_parsed;
-    data += bytes_parsed;
-    size -= bytes_parsed;
-
-    // WebMClusterParser returns 0 and |cluster_ended| is true if previously
-    // parsing an unknown-size cluster and |data| does not continue that
-    // cluster. Try parsing again in that case.
-  } while (size > 0 && (bytes_parsed > 0 || cluster_ended));
-
-  return result;
+  return bytes_parsed;
 }
 
 void WebMStreamParser::FireNeedKey(const std::string& key_id) {

media/formats/webm/webm_stream_parser.h
@@ -78,9 +78,6 @@ class WebMStreamParser : public StreamParser {
 
   bool unknown_segment_size_;
 
-  // True if a new cluster id has been seen and its end has not yet been parsed.
-  bool parsing_cluster_;
-
   scoped_ptr<WebMClusterParser> cluster_parser_;
   ByteQueue byte_queue_;
 

media/filters/chunk_demuxer_unittest.cc
@@ -45,6 +45,11 @@ const uint8 kVP8Keyframe[] = {
 // WebM Block bytes that represent a VP8 interframe.
 const uint8 kVP8Interframe[] = { 0x11, 0x00, 0x00 };
 
+static const uint8 kCuesHeader[] = {
+  0x1C, 0x53, 0xBB, 0x6B,  // Cues ID
+  0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // cues(size = 0)
+};
+
 const int kTracksHeaderSize = sizeof(kTracksHeader);
 const int kTracksSizeOffset = 4;
 
@@ -3450,6 +3455,29 @@ TEST_F(ChunkDemuxerTest, ClusterWithUnknownSize) {
 
   // A new cluster indicates end of the previous cluster with unknown size.
   AppendCluster(GenerateCluster(46, 66, 5, true));
+  CheckExpectedRanges(kSourceId, "{ [0,115) }");
+}
+
+TEST_F(ChunkDemuxerTest, CuesBetweenClustersWithUnknownSize) {
+  ASSERT_TRUE(InitDemuxer(HAS_AUDIO | HAS_VIDEO));
+
+  // Add two clusters separated by Cues in a single Append() call.
+  scoped_ptr<Cluster> cluster = GenerateCluster(0, 0, 4, true);
+  std::vector<uint8> data(cluster->data(), cluster->data() + cluster->size());
+  data.insert(data.end(), kCuesHeader, kCuesHeader + sizeof(kCuesHeader));
+  cluster = GenerateCluster(46, 66, 5, true);
+  data.insert(data.end(), cluster->data(), cluster->data() + cluster->size());
+  AppendData(&*data.begin(), data.size());
+
+  CheckExpectedRanges(kSourceId, "{ [0,115) }");
+}
+
+TEST_F(ChunkDemuxerTest, CuesBetweenClusters) {
+  ASSERT_TRUE(InitDemuxer(HAS_AUDIO | HAS_VIDEO));
+
+  AppendCluster(GenerateCluster(0, 0, 4));
+  AppendData(kCuesHeader, sizeof(kCuesHeader));
+  AppendCluster(GenerateCluster(46, 66, 5));
   CheckExpectedRanges(kSourceId, "{ [0,115) }");
 }
 