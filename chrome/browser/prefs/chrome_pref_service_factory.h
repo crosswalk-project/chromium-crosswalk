3ae27adc553b017f0ffdeff11b6f301932d3e1b5
caitkp@google.com <caitkp@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 23:03:04 +0000

    Revert 278412 "Merge 277790 "Flip Finch Configs so we can harden..."
    > Merge 277790 "Flip Finch Configs so we can harden DSE w/o extens..."
    > 
    > > Flip Finch Configs so we can harden DSE w/o extensions
    > > 
    > > BUG=385318
    > > TEST=test manually that new finch groups behave as expected.
    > > 
    > > Review URL: https://codereview.chromium.org/338093007
    > 
    > TBR=caitkp@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/331303003
    
    TBR=caitkp@google.com
    
    Review URL: https://codereview.chromium.org/336943006
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1985/src@278522 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/chrome_pref_service_factory.h
+++ chrome/browser/prefs/chrome_pref_service_factory.h
@@ -40,7 +40,7 @@ extern const char kSettingsEnforcementTrialName[];
 extern const char kSettingsEnforcementGroupNoEnforcement[];
 extern const char kSettingsEnforcementGroupEnforceOnload[];
 extern const char kSettingsEnforcementGroupEnforceAlways[];
-extern const char kSettingsEnforcementGroupEnforceAlwaysWithDSE[];
+extern const char kSettingsEnforcementGroupEnforceAlwaysWithExtensions[];
 extern const char kSettingsEnforcementGroupEnforceAlwaysWithExtensionsAndDSE[];
 
 }  // namespace internals
