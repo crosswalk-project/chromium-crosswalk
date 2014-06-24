crosswalk-project/chromium-crosswalk

commit 7bcb80190b39d23a2ed6909861fb21cb0254edfd

Ack swap buffers after a timeout
In r272469, a timer that would automatically ack swaps after a
quarter-second passed was removed. The purpose of the timer
was to manage the transition between async and sync displays,
but it had an unintentional side-effect of un-blocking the renderer
in the event that the CALayer's display function was never called.

Re-add a timer explicitly for this purpose.

BUG=378445

Review URL: https://codereview.chromium.org/310183002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274802 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/renderer_host/compositing_iosurface_layer_mac.mm
@@ -17,6 +17,29 @@
 #include "ui/gfx/size_conversions.h"
 #include "ui/gl/gpu_switching_manager.h"
 
+@interface CompositingIOSurfaceLayer(Private)
+- (void)immediatelyForceDisplayAndAck;
+- (void)ackPendingFrame:(bool)success;
+- (void)timerFired;
+@end
+
+namespace content {
+
+// The base::DelayTimer needs a C++ class to operate on, rather than Objective C
+// class. This helper class provides a bridge between the two.
+class CompositingIOSurfaceLayerHelper {
+ public:
+  CompositingIOSurfaceLayerHelper(CompositingIOSurfaceLayer* layer)
+      : layer_(layer) {}
+  void TimerFired() {
+    [layer_ timerFired];
+  }
+ private:
+  CompositingIOSurfaceLayer* layer_;
+};
+
+}  // namespace content
+
 @implementation CompositingIOSurfaceLayer
 
 - (content::CompositingIOSurfaceMac*)iosurface {
@@ -33,11 +56,18 @@
   if (self = [super init]) {
     iosurface_ = iosurface;
     client_ = client;
+    helper_.reset(new content::CompositingIOSurfaceLayerHelper(self));
+    timer_.reset(new base::DelayTimer<content::CompositingIOSurfaceLayerHelper>(
+        FROM_HERE,
+        base::TimeDelta::FromSeconds(1) / 6,
+        helper_.get(),
+        &content::CompositingIOSurfaceLayerHelper::TimerFired));
 
     context_ = content::CompositingIOSurfaceContext::Get(
         content::CompositingIOSurfaceContext::kCALayerContextWindowNumber);
     DCHECK(context_);
     needs_display_ = NO;
+    has_pending_frame_ = NO;
     did_not_draw_counter_ = 0;
 
     [self setBackgroundColor:CGColorGetConstantColor(kCGColorWhite)];
@@ -57,23 +87,50 @@
 }
 
 - (void)gotNewFrame {
+  has_pending_frame_ = YES;
+  timer_->Reset();
+
+  // A trace value of 2 indicates that there is a pending swap ack. See
+  // canDrawInCGLContext for other value meanings.
+  TRACE_COUNTER_ID1("browser", "PendingSwapAck", self, 2);
+
   if (context_ && context_->is_vsync_disabled()) {
     // If vsync is disabled, draw immediately and don't bother trying to use
     // the isAsynchronous property to ensure smooth animation.
-    [self setNeedsDisplay];
-    [self displayIfNeeded];
-
-    // Calls to setNeedsDisplay can sometimes be ignored, especially if issued
-    // rapidly (e.g, with vsync off). This is unacceptable because the failure
-    // to ack a single frame will hang the renderer. Ensure that the renderer
-    // not be blocked by lying and claiming that we drew the frame.
-    if (needs_display_ && client_)
-      client_->AcceleratedLayerDidDrawFrame(true);
+    [self immediatelyForceDisplayAndAck];
   } else {
     needs_display_ = YES;
     if (![self isAsynchronous])
       [self setAsynchronous:YES];
   }
+}
+
+// Private methods:
+
+- (void)immediatelyForceDisplayAndAck {
+  [self setNeedsDisplay];
+  [self displayIfNeeded];
+
+  // Calls to setNeedsDisplay can sometimes be ignored, especially if issued
+  // rapidly (e.g, with vsync off). This is unacceptable because the failure
+  // to ack a single frame will hang the renderer. Ensure that the renderer
+  // not be blocked by lying and claiming that we drew the frame.
+  [self ackPendingFrame:true];
+}
+
+- (void)ackPendingFrame:(bool)success {
+  if (!has_pending_frame_)
+    return;
+
+  TRACE_COUNTER_ID1("browser", "PendingSwapAck", self, 0);
+  has_pending_frame_ = NO;
+  if (client_)
+    client_->AcceleratedLayerDidDrawFrame(success);
+}
+
+- (void)timerFired {
+  if (has_pending_frame_)
+    [self immediatelyForceDisplayAndAck];
 }
 
 // The remaining methods implement the CAOpenGLLayer interface.
@@ -106,12 +163,9 @@
   // ack) indicates that we did not request a draw. This would be more natural
   // to do with a tracing pseudo-thread
   // http://crbug.com/366300
-  if (client_) {
-    TRACE_COUNTER_ID1("browser", "PendingSwapAck", self,
-        needs_display_ ? 3 : 1);
-    TRACE_COUNTER_ID1("browser", "PendingSwapAck", self,
-        client_->AcceleratedLayerHasNotAckedPendingFrame() ? 2 : 0);
-  }
+  TRACE_COUNTER_ID1("browser", "PendingSwapAck", self, needs_display_ ? 3 : 1);
+  TRACE_COUNTER_ID1("browser", "PendingSwapAck", self,
+                    has_pending_frame_ ? 2 : 0);
 
   // If we return NO 30 times in a row, switch to being synchronous to avoid
   // burning CPU cycles on this callback.
@@ -153,9 +207,8 @@
   bool draw_succeeded = iosurface_->DrawIOSurface(
       context_, window_rect, window_scale_factor, false);
 
+  [self ackPendingFrame:draw_succeeded];
   needs_display_ = NO;
-  if (client_)
-    client_->AcceleratedLayerDidDrawFrame(draw_succeeded);
 
   [super drawInCGLContext:glContext
               pixelFormat:pixelFormat

content/browser/renderer_host/compositing_iosurface_layer_mac.h
@@ -9,15 +9,16 @@
 
 #include "base/mac/scoped_cftyperef.h"
 #include "base/memory/ref_counted.h"
+#include "base/timer/timer.h"
 
 namespace content {
 class CompositingIOSurfaceMac;
 class CompositingIOSurfaceContext;
+class CompositingIOSurfaceLayerHelper;
 
 class CompositingIOSurfaceLayerClient {
  public:
   virtual void AcceleratedLayerDidDrawFrame(bool succeeded) = 0;
-  virtual bool AcceleratedLayerHasNotAckedPendingFrame() const = 0;
 };
 
 }
@@ -29,10 +30,20 @@ class CompositingIOSurfaceLayerClient {
   scoped_refptr<content::CompositingIOSurfaceMac> iosurface_;
   scoped_refptr<content::CompositingIOSurfaceContext> context_;
 
+  // The browser places back-pressure on the GPU by not acknowledging swap
+  // calls until they appear on the screen. This can lead to hangs if the
+  // view is moved offscreen (among other things). Prevent hangs by always
+  // acknowledging the frame after timeout of 1/6th of a second  has passed.
+  scoped_ptr<content::CompositingIOSurfaceLayerHelper> helper_;
+  scoped_ptr<base::DelayTimer<content::CompositingIOSurfaceLayerHelper>> timer_;
+
   // Used to track when canDrawInCGLContext should return YES. This can be
   // in response to receiving a new compositor frame, or from any of the events
   // that cause setNeedsDisplay to be called on the layer.
   BOOL needs_display_;
+
+  // This is set when a frame is received, and un-set when the frame is drawn.
+  BOOL has_pending_frame_;
 
   // Incremented every time that this layer is asked to draw but does not have
   // new content to draw.

content/browser/renderer_host/render_widget_host_view_mac.h
@@ -345,7 +345,6 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
 
   // CompositingIOSurfaceLayerClient implementation.
   virtual void AcceleratedLayerDidDrawFrame(bool succeeded) OVERRIDE;
-  virtual bool AcceleratedLayerHasNotAckedPendingFrame() const OVERRIDE;
 
   // Forwards the mouse event to the renderer.
   void ForwardMouseEvent(const blink::WebMouseEvent& event);

content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -2357,11 +2357,6 @@ void RenderWidgetHostViewMac::AddPendingSwapAck(
   // loss. Drop the old acks.
   pending_swap_ack_.reset(new PendingSwapAck(
       route_id, gpu_host_id, renderer_id));
-
-  // A trace value of 2 indicates that there is a pending swap ack. See
-  // CompositingIOSurfaceLayer's canDrawInCGLContext for other value meanings.
-  TRACE_COUNTER_ID1("browser", "PendingSwapAck",
-                    compositing_iosurface_layer_.get(), 2);
 }
 
 void RenderWidgetHostViewMac::SendPendingSwapAck() {
@@ -2375,7 +2370,6 @@ void RenderWidgetHostViewMac::SendPendingSwapAck() {
                                                  pending_swap_ack_->gpu_host_id,
                                                  ack_params);
   pending_swap_ack_.reset();
-  TRACE_COUNTER_ID1("browser", "PendingSwapAck", this, 0);
 }
 
 void RenderWidgetHostViewMac::PauseForPendingResizeOrRepaintsAndDraw() {
@@ -2483,10 +2477,6 @@ void RenderWidgetHostViewMac::AcceleratedLayerDidDrawFrame(bool succeeded) {
   SendPendingSwapAck();
   if (!succeeded)
     GotAcceleratedCompositingError();
-}
-
-bool RenderWidgetHostViewMac::AcceleratedLayerHasNotAckedPendingFrame() const {
-  return pending_swap_ack_;
 }
 
 }  // namespace content

commit 2e9530b1092322cfd5c1f342cc201ddd6601b852

Merge 278967 "Fix hang on Mac when switching CALayers"
> Fix hang on Mac when switching CALayers
> 
> We switch the underlying CALayer used to display content when the DPI of
> the screen changes. In the CALayer we track whether or not we have a pending
> swap ack that needs to be sent when the frame is displayed. When we switch
> layers, we would discard the bit that indicated that there was a pending swap
> ack to send on display. Ensure that this bit is not lost, by making sure that it
> is sent to the CALayer's client (the RWHVMac) when the CALayer is
> disconnected from its client.
> 
> This bug was introduced in r274802.
> 
> BUG=385188
> 
> Review URL: https://codereview.chromium.org/347763004

TBR=ccameron@chromium.org

Review URL: https://codereview.chromium.org/354653005

git-svn-id: svn://svn.chromium.org/chrome/branches/2062/src@279484 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/renderer_host/compositing_iosurface_layer_mac.mm
@@ -84,6 +84,9 @@ class CompositingIOSurfaceLayerHelper {
 }
 
 - (void)resetClient {
+  // Any acks that were waiting on this layer to draw will not occur, so ack
+  // them now to prevent blocking the renderer.
+  [self ackPendingFrame:true];
   client_ = NULL;
 }
 