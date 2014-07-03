crosswalk-project/chromium-crosswalk

commit e618a783a3e81f991c930e212ff587b56f24440b

Oilpan: Clear out memory when the memory is added to a free list
This CL changes a place where memory is cleared out.
Before this CL, memory was cleared out in Heap::allocate().
After this CL, memory is cleared out when added to a free list.

This is good for:

- preventing freed memory from being exploited.
- improving performance of Heap::allocate().

BUG=

Review URL: https://codereview.chromium.org/353343002

git-svn-id: svn://svn.chromium.org/blink/trunk@177304 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -464,16 +464,20 @@ void HeapObjectHeader::finalize(const GCInfo* gcInfo, Address object, size_t obj
     if (gcInfo->hasFinalizer()) {
         gcInfo->m_finalize(object);
     }
+
 #if !defined(NDEBUG) || defined(LEAK_SANITIZER)
-    // Zap freed memory with a recognizable zap value in debug mode.
-    // Also zap when using leak sanitizer because the heap is used as
-    // a root region for lsan and therefore pointers in unreachable
-    // memory could hide leaks.
+    // In Debug builds, memory is zapped when it's freed, and the zapped memory is
+    // zeroed out when the memory is reused. Memory is also zapped when using Leak
+    // Sanitizer because the heap is used as a root region for LSan and therefore
+    // pointers in unreachable memory could hide leaks.
     for (size_t i = 0; i < objectSize; i++)
         object[i] = finalizedZapValue;
-#endif
-    // Zap the primary vTable entry (secondary vTable entries are not zapped)
+
+    // Zap the primary vTable entry (secondary vTable entries are not zapped).
     *(reinterpret_cast<uintptr_t*>(object)) = zappedVTable;
+#endif
+    // In Release builds, the entire object is zeroed out when it is added to the free list.
+    // This happens right after sweeping the page and before the thread commences execution.
 }
 
 NO_SANITIZE_ADDRESS
@@ -658,6 +662,9 @@ void ThreadHeap<Header>::addToFreeList(Address address, size_t size)
     // from them we are 8 byte aligned due to the header size).
     ASSERT(!((reinterpret_cast<uintptr_t>(address) + sizeof(Header)) & allocationMask));
     ASSERT(!(size & allocationMask));
+#if defined(NDEBUG) && !defined(LEAK_SANITIZER)
+    memset(address, 0, size);
+#endif
     ASAN_POISON_MEMORY_REGION(address, size);
     FreeListEntry* entry;
     if (size < sizeof(*entry)) {

third_party/WebKit/Source/platform/heap/Heap.h
@@ -1373,7 +1373,9 @@ Address ThreadHeap<Header>::allocate(size_t size, const GCInfo* gcInfo)
     ASSERT(!(reinterpret_cast<uintptr_t>(result) & allocationMask));
     // Unpoison the memory used for the object (payload).
     ASAN_UNPOISON_MEMORY_REGION(result, payloadSize);
+#if !defined(NDEBUG) || defined(LEAK_SANITIZER)
     memset(result, 0, payloadSize);
+#endif
     ASSERT(heapPageFromAddress(headerAddress + allocationSize - 1));
     return result;
 }

commit 3bafd3be00f59c2d63c5ee0e43c3585131ad67c8

Don't zero out memory added to a free list if ASan is enabled
This CL fixes a regression caused by r177304.

BUG=390936

Review URL: https://codereview.chromium.org/363173002

git-svn-id: svn://svn.chromium.org/blink/trunk@177444 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -465,7 +465,7 @@ void HeapObjectHeader::finalize(const GCInfo* gcInfo, Address object, size_t obj
         gcInfo->m_finalize(object);
     }
 
-#if !defined(NDEBUG) || defined(LEAK_SANITIZER)
+#if !defined(NDEBUG) || defined(LEAK_SANITIZER) || defined(ADDRESS_SANITIZER)
     // In Debug builds, memory is zapped when it's freed, and the zapped memory is
     // zeroed out when the memory is reused. Memory is also zapped when using Leak
     // Sanitizer because the heap is used as a root region for LSan and therefore
@@ -662,7 +662,7 @@ void ThreadHeap<Header>::addToFreeList(Address address, size_t size)
     // from them we are 8 byte aligned due to the header size).
     ASSERT(!((reinterpret_cast<uintptr_t>(address) + sizeof(Header)) & allocationMask));
     ASSERT(!(size & allocationMask));
-#if defined(NDEBUG) && !defined(LEAK_SANITIZER)
+#if defined(NDEBUG) && !defined(LEAK_SANITIZER) && !defined(ADDRESS_SANITIZER)
     memset(address, 0, size);
 #endif
     ASAN_POISON_MEMORY_REGION(address, size);
@@ -677,7 +677,7 @@ void ThreadHeap<Header>::addToFreeList(Address address, size_t size)
     }
     entry = new (NotNull, address) FreeListEntry(size);
 #if defined(ADDRESS_SANITIZER)
-    // For ASAN we don't add the entry to the free lists until the asanDeferMemoryReuseCount
+    // For ASan we don't add the entry to the free lists until the asanDeferMemoryReuseCount
     // reaches zero. However we always add entire pages to ensure that adding a new page will
     // increase the allocation space.
     if (HeapPage<Header>::payloadSize() != size && !entry->shouldAddToFreeList())
@@ -702,7 +702,7 @@ Address ThreadHeap<Header>::allocateLargeObject(size_t size, const GCInfo* gcInf
     // headerPadding<Header> bytes to ensure it 8 byte aligned.
     allocationSize += headerPadding<Header>();
 
-    // If ASAN is supported we add allocationGranularity bytes to the allocated space and
+    // If ASan is supported we add allocationGranularity bytes to the allocated space and
     // poison that to detect overflows
 #if defined(ADDRESS_SANITIZER)
     allocationSize += allocationGranularity;
@@ -856,7 +856,7 @@ void ThreadHeap<Header>::sweep()
 {
     ASSERT(isConsistentForGC());
 #if defined(ADDRESS_SANITIZER) && STRICT_ASAN_FINALIZATION_CHECKING
-    // When using ASAN do a pre-sweep where all unmarked objects are poisoned before
+    // When using ASan do a pre-sweep where all unmarked objects are poisoned before
     // calling their finalizer methods. This can catch the cases where one objects
     // finalizer tries to modify another object as part of finalization.
     for (HeapPage<Header>* page = m_firstPage; page; page = page->next())
@@ -1074,7 +1074,7 @@ void HeapPage<Header>::sweep()
         Header* header = static_cast<Header*>(basicHeader);
 
         if (!header->isMarked()) {
-            // For ASAN we unpoison the specific object when calling the finalizer and
+            // For ASan we unpoison the specific object when calling the finalizer and
             // poison it again when done to allow the object's own finalizer to operate
             // on the object, but not have other finalizers be allowed to access it.
             ASAN_UNPOISON_MEMORY_REGION(header->payload(), header->payloadSize());

third_party/WebKit/Source/platform/heap/Heap.h
@@ -398,9 +398,9 @@ public:
         : HeapObjectHeader(freeListEncodedSize(size))
         , m_next(0)
     {
-#if !defined(NDEBUG) && !ASAN
+#if !defined(NDEBUG) && !defined(ADDRESS_SANITIZER)
         // Zap free area with asterisks, aka 0x2a2a2a2a.
-        // For ASAN don't zap since we keep accounting in the freelist entry.
+        // For ASan don't zap since we keep accounting in the freelist entry.
         for (size_t i = sizeof(*this); i < size; i++)
             reinterpret_cast<Address>(this)[i] = freelistZapValue;
         ASSERT(size >= objectHeaderSize);
@@ -1373,7 +1373,7 @@ Address ThreadHeap<Header>::allocate(size_t size, const GCInfo* gcInfo)
     ASSERT(!(reinterpret_cast<uintptr_t>(result) & allocationMask));
     // Unpoison the memory used for the object (payload).
     ASAN_UNPOISON_MEMORY_REGION(result, payloadSize);
-#if !defined(NDEBUG) || defined(LEAK_SANITIZER)
+#if !defined(NDEBUG) || defined(LEAK_SANITIZER) || defined(ADDRESS_SANITIZER)
     memset(result, 0, payloadSize);
 #endif
     ASSERT(heapPageFromAddress(headerAddress + allocationSize - 1));