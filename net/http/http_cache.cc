fa82719a18ea39f04cad74cddfea6f1a3205d5b5
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-14 16:14:28 +0000

    Merge 280379 "Remove HttpCache's assumption that it sits on an H..."
    > Remove HttpCache's assumption that it sits on an HttpNetworkSession.
    > 
    > This used to be a correct assumption, until revision 273584
    > (https://src.chromium.org/viewvc/chrome?revision=273584&view=revision).
    > There's no need for the assumption, anyways.
    > 
    > BUG=385691
    > 
    > Review URL: https://codereview.chromium.org/339683004
    
    TBR=mmenke@chromium.org
    
    Review URL: https://codereview.chromium.org/390833005
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/2062/src@282958 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_cache.cc
+++ net/http/http_cache.cc
@@ -416,17 +416,13 @@ void HttpCache::WriteMetadata(const GURL& url,
 }
 
 void HttpCache::CloseAllConnections() {
-  net::HttpNetworkLayer* network =
-      static_cast<net::HttpNetworkLayer*>(network_layer_.get());
-  HttpNetworkSession* session = network->GetSession();
+  HttpNetworkSession* session = GetSession();
   if (session)
     session->CloseAllConnections();
 }
 
 void HttpCache::CloseIdleConnections() {
-  net::HttpNetworkLayer* network =
-      static_cast<net::HttpNetworkLayer*>(network_layer_.get());
-  HttpNetworkSession* session = network->GetSession();
+  HttpNetworkSession* session = GetSession();
   if (session)
     session->CloseIdleConnections();
 }
@@ -466,9 +462,7 @@ HttpCache* HttpCache::GetCache() {
 }
 
 HttpNetworkSession* HttpCache::GetSession() {
-  net::HttpNetworkLayer* network =
-      static_cast<net::HttpNetworkLayer*>(network_layer_.get());
-  return network->GetSession();
+  return network_layer_->GetSession();
 }
 
 scoped_ptr<HttpTransactionFactory>
