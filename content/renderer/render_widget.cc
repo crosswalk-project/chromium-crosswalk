9d075e8097f475380f58501de057dadc6db72e1d
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-14 20:31:49 +0000

    Merge 280970 "Add switch to disable recording whole document"
    > Add switch to disable recording whole document
    > 
    > Add an android webview switch to disable the workaround to record the
    > whole document on in each commit. The setting is plumbed through
    > SynchronousCompositorFactory.
    > 
    > BUG=390702
    > 
    > Review URL: https://codereview.chromium.org/361073002
    
    TBR=boliu@chromium.org
    
    Review URL: https://codereview.chromium.org/389973005
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/2062/src@283008 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -504,14 +504,6 @@ void RenderWidget::SetSwappedOut(bool is_swapped_out) {
     RenderProcess::current()->AddRefProcess();
 }
 
-bool RenderWidget::UsingSynchronousRendererCompositor() const {
-#if defined(OS_ANDROID)
-  return SynchronousCompositorFactory::GetInstance() != NULL;
-#else
-  return false;
-#endif
-}
-
 void RenderWidget::EnableScreenMetricsEmulation(
     const WebDeviceEmulationParams& params) {
   if (!screen_metrics_emulator_)
