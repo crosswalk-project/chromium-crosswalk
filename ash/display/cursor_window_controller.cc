79d8bb960d1c6477f6106816bafbfe1c7342fdda
matthewyuan@chromium.org <matthewyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-15 03:51:29 +0000

    Temporary revert.
    Revert 282994 "Merge 277043 "Don't update the display in CursorW..."
    
    > Merge 277043 "Don't update the display in CursorWindowController..."
    > 
    > > Don't update the display in CursorWindowController::UpdateContainer in cursor compositing mode.
    > > 
    > > Also update the display only if the mirror window exists. This can be null when exitting software
    > > mirror mode.
    > > 
    > > BUG=379239
    > > TEST=I'll add CHECKS in separate CL. (https://codereview.chromium.org/323123002/. I want to merge this to 36)
    > > R=hshi@chromium.org
    > > 
    > > Review URL: https://codereview.chromium.org/335473005
    > 
    > TBR=oshima@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/395523002
    
    TBR=oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/395603005
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1985/src@283140 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/cursor_window_controller.cc
+++ ash/display/cursor_window_controller.cc
@@ -113,21 +113,14 @@ void CursorWindowController::SetCursorCompositingEnabled(bool enabled) {
 }
 
 void CursorWindowController::UpdateContainer() {
+  display_ = Shell::GetScreen()->GetPrimaryDisplay();
   if (is_cursor_compositing_enabled_) {
-    gfx::Screen* screen = Shell::GetScreen();
-    gfx::Display display = screen->GetDisplayNearestPoint(
-        screen->GetCursorScreenPoint());
-    DCHECK(display.is_valid());
-    if (display.is_valid())
-      SetDisplay(display);
+    SetDisplay(display_);
   } else {
-    aura::Window* mirror_window = Shell::GetInstance()->
-        display_controller()->
-        mirror_window_controller()->
-        GetWindow();
-    if (mirror_window)
-      display_ = Shell::GetScreen()->GetPrimaryDisplay();
-    SetContainer(mirror_window);
+    SetContainer(Shell::GetInstance()->
+                 display_controller()->
+                 mirror_window_controller()->
+                 GetWindow());
   }
 }
 
@@ -149,6 +142,7 @@ void CursorWindowController::SetDisplay(const gfx::Display& display) {
 void CursorWindowController::UpdateLocation() {
   if (!cursor_window_)
     return;
+
   gfx::Point point = aura::Env::GetInstance()->last_mouse_location();
   if (!is_cursor_compositing_enabled_) {
     Shell::GetPrimaryRootWindow()->GetHost()->ConvertPointToHost(&point);
@@ -187,20 +181,20 @@ void CursorWindowController::SetVisibility(bool visible) {
 void CursorWindowController::SetContainer(aura::Window* container) {
   if (container_ == container)
     return;
+
   container_ = container;
   if (!container) {
     cursor_window_.reset();
     return;
   }
 
-  // Reusing the window does not work when the display is disconnected.
-  // Just creates a new one instead. crbug.com/384218.
-  cursor_window_.reset(new aura::Window(delegate_.get()));
-  cursor_window_->SetTransparent(true);
-  cursor_window_->Init(aura::WINDOW_LAYER_TEXTURED);
-  cursor_window_->set_ignore_events(true);
-  cursor_window_->set_owned_by_parent(false);
-  UpdateCursorImage();
+  if (!cursor_window_) {
+    cursor_window_.reset(new aura::Window(delegate_.get()));
+    cursor_window_->SetTransparent(true);
+    cursor_window_->Init(aura::WINDOW_LAYER_TEXTURED);
+    cursor_window_->set_ignore_events(true);
+    cursor_window_->set_owned_by_parent(false);
+  }
 
   container->AddChild(cursor_window_.get());
   cursor_window_->Show();
