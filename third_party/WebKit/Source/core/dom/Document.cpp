crosswalk-project/chromium-crosswalk

commit 0f95b4ceb5cc647bbf84aaaeeb32718207a6dd42

Navigation transitions: Added createStyledMarkupForNavigationTransition
This will be used to serialize markup for transition elements, which is then sent to the transition layer.

BUG=370696

Review URL: https://codereview.chromium.org/281383006

git-svn-id: svn://svn.chromium.org/blink/trunk@178403 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/dom/Document.cpp
@@ -87,6 +87,7 @@
 #include "core/dom/ScriptRunner.h"
 #include "core/dom/ScriptedAnimationController.h"
 #include "core/dom/SelectorQuery.h"
+#include "core/dom/StaticNodeList.h"
 #include "core/dom/StyleEngine.h"
 #include "core/dom/TouchList.h"
 #include "core/dom/TransformSource.h"
@@ -100,6 +101,7 @@
 #include "core/editing/Editor.h"
 #include "core/editing/FrameSelection.h"
 #include "core/editing/SpellChecker.h"
+#include "core/editing/markup.h"
 #include "core/events/BeforeUnloadEvent.h"
 #include "core/events/Event.h"
 #include "core/events/EventFactory.h"
@@ -5698,6 +5700,44 @@ Element* Document::activeElement() const
     if (Element* element = treeScope().adjustedFocusedElement())
         return element;
     return body();
+}
+
+void Document::getTransitionElementData(Vector<TransitionElementData>& elementData)
+{
+    if (!head())
+        return;
+
+    for (HTMLMetaElement* metaElement = Traversal<HTMLMetaElement>::firstChild(*head()); metaElement; metaElement = Traversal<HTMLMetaElement>::nextSibling(*metaElement)) {
+        if (metaElement->name() != "transition-elements")
+            continue;
+
+        const String& metaElementContents = metaElement->content().string();
+        size_t firstSemicolon = metaElementContents.find(';');
+        if (firstSemicolon == kNotFound)
+            continue;
+
+        TrackExceptionState exceptionState;
+        AtomicString selector(metaElementContents.substring(0, firstSemicolon));
+        RefPtr<StaticNodeList> nodeList = querySelectorAll(selector, exceptionState);
+        if (!nodeList || exceptionState.hadException())
+            continue;
+
+        unsigned nodeListLength = nodeList->length();
+        if (!nodeListLength)
+            continue;
+
+        StringBuilder markup;
+        for (unsigned nodeIndex = 0; nodeIndex < nodeListLength; ++nodeIndex) {
+            Node* node = nodeList->item(nodeIndex);
+            markup.append(createStyledMarkupForNavigationTransition(node));
+        }
+
+        TransitionElementData newElements;
+        newElements.scope = metaElementContents.substring(firstSemicolon + 1).stripWhiteSpace();
+        newElements.selector = selector;
+        newElements.markup = markup.toString();
+        elementData.append(newElements);
+    }
 }
 
 bool Document::hasFocus() const

third_party/WebKit/LayoutTests/fast/html/navigation-transition-expected.txt
@@ -1,0 +1,15 @@
+Tests for the HTML markup serializer used for transition elements
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS sourceElementRect.top is destElement.offsetTop
+PASS sourceElementRect.left is destElement.offsetLeft
+PASS sourceElementRect.height is destElement.offsetHeight
+PASS sourceElementRect.width is destElement.offsetWidth
+PASS successfullyParsed is true
+
+TEST COMPLETE
+This text should appear twice.
+
+

third_party/WebKit/LayoutTests/fast/html/navigation-transition.html
@@ -1,0 +1,80 @@
+<!DOCTYPE html>
+
+<meta name="transition-elements" content="#transitionElement,#boxTransitionElement;*">
+<script src="../../resources/js-test.js"></script>
+<script>
+
+description('Tests for the HTML markup serializer used for transition elements');
+jsTestIsAsync = true;
+
+window.onload = function() {
+    if (window.internals) {
+        var serializedHTML = internals.serializeNavigationMarkup(document);
+        var testiframe = document.getElementById('testiframe');
+        testiframe.contentDocument.write(serializedHTML);
+
+        sourceElement = document.getElementById('boxTransitionElement');
+        destElement = testiframe.contentDocument.getElementById('boxTransitionElement');
+
+        sourceElementRect = sourceElement.getBoundingClientRect();
+        shouldBe('sourceElementRect.top', 'destElement.offsetTop');
+        shouldBe('sourceElementRect.left', 'destElement.offsetLeft');
+        shouldBe('sourceElementRect.height', 'destElement.offsetHeight');
+        shouldBe('sourceElementRect.width', 'destElement.offsetWidth');
+
+        finishJSTest();
+    }
+}
+</script>
+
+<style>
+
+body {
+    background: white;
+    margin-height: 0px;
+    margin-width: 0px;
+}
+
+#wrappingElement {
+    background: gray;
+    padding: 13px 20px 8px 30px;
+    border-style: solid;
+    border-width: 5px;
+    width: 400px;
+    margin: 44px 2px 13px 30px;
+}
+
+#transitionElement {
+    padding-bottom: 0px;
+}
+
+#boxTransitionElement {
+    background: red;
+    padding: 8px 50px 50px 18px;
+    border-style: solid;
+    border-width: 30px;
+    margin: 55px 15px 25px 135px;
+}
+
+#blueBox {
+    padding: 25px 75px 50px 100px;
+}
+
+#testiframe {
+    width: 100%;
+    height: 600px;
+    background: gray;
+    border-width: 0px;
+}
+
+</style>
+
+<div id="wrappingElement">
+    <div id="transitionElement">This text should appear twice.</div>
+
+    <div id="boxTransitionElement">
+        <img id="blueBox" src="resources/images/blue.png">
+    </div>
+</div>
+
+<iframe id="testiframe"></iframe>

third_party/WebKit/Source/core/dom/Document.h
@@ -393,6 +393,13 @@ public:
     bool isTransitionDocument() const { return m_isTransitionDocument; }
     void setIsTransitionDocument() { m_isTransitionDocument = true; }
 
+    struct TransitionElementData {
+        String scope;
+        String selector;
+        String markup;
+    };
+    void getTransitionElementData(Vector<TransitionElementData>&);
+
     StyleResolver* styleResolver() const;
     StyleResolver& ensureStyleResolver() const;
 

third_party/WebKit/Source/core/editing/EditingStyle.cpp
@@ -34,6 +34,7 @@
 #include "core/css/CSSRuleList.h"
 #include "core/css/CSSStyleRule.h"
 #include "core/css/CSSValueList.h"
+#include "core/css/CSSValuePool.h"
 #include "core/css/FontSize.h"
 #include "core/css/RuntimeCSSEnabled.h"
 #include "core/css/StylePropertySet.h"
@@ -52,6 +53,8 @@
 #include "core/editing/htmlediting.h"
 #include "core/frame/LocalFrame.h"
 #include "core/html/HTMLFontElement.h"
+#include "core/rendering/RenderBox.h"
+#include "core/rendering/RenderObject.h"
 #include "core/rendering/style/RenderStyle.h"
 
 namespace WebCore {
@@ -1221,6 +1224,31 @@ void EditingStyle::removePropertiesInElementDefaultStyle(Element* element)
     RefPtrWillBeRawPtr<StylePropertySet> defaultStyle = styleFromMatchedRulesForElement(element, StyleResolver::UAAndUserCSSRules);
 
     removePropertiesInStyle(m_mutableStyle.get(), defaultStyle.get());
+}
+
+void EditingStyle::addAbsolutePositioningFromElement(const Element& element)
+{
+    LayoutRect rect = element.boundingBox();
+    RenderObject* renderer = element.renderer();
+
+    LayoutUnit x = rect.x();
+    LayoutUnit y = rect.y();
+    LayoutUnit width = rect.width();
+    LayoutUnit height = rect.height();
+    if (renderer && renderer->isBox()) {
+        RenderBox* renderBox = toRenderBox(renderer);
+
+        x -= renderBox->marginLeft();
+        y -= renderBox->marginTop();
+
+        m_mutableStyle->setProperty(CSSPropertyBoxSizing, CSSValueBorderBox);
+    }
+
+    m_mutableStyle->setProperty(CSSPropertyPosition, CSSValueAbsolute);
+    m_mutableStyle->setProperty(CSSPropertyLeft, cssValuePool().createValue(x, CSSPrimitiveValue::CSS_PX));
+    m_mutableStyle->setProperty(CSSPropertyTop, cssValuePool().createValue(y, CSSPrimitiveValue::CSS_PX));
+    m_mutableStyle->setProperty(CSSPropertyWidth, cssValuePool().createValue(width, CSSPrimitiveValue::CSS_PX));
+    m_mutableStyle->setProperty(CSSPropertyHeight, cssValuePool().createValue(height, CSSPrimitiveValue::CSS_PX));
 }
 
 void EditingStyle::forceInline()

third_party/WebKit/Source/core/editing/EditingStyle.h
@@ -133,6 +133,7 @@ public:
     void mergeStyleFromRulesForSerialization(Element*);
     void removeStyleFromRulesAndContext(Element*, Node* context);
     void removePropertiesInElementDefaultStyle(Element*);
+    void addAbsolutePositioningFromElement(const Element&);
     void forceInline();
     int legacyFontSize(Document*) const;
 

third_party/WebKit/Source/core/editing/HTMLInterchange.h
@@ -38,7 +38,7 @@ class Text;
 #define AppleStyleSpanClass       "Apple-style-span"
 #define AppleTabSpanClass         "Apple-tab-span"
 
-enum EAnnotateForInterchange { DoNotAnnotateForInterchange, AnnotateForInterchange };
+enum EAnnotateForInterchange { DoNotAnnotateForInterchange, AnnotateForInterchange, AnnotateForNavigationTransition };
 
 String convertHTMLTextToInterchangeFormat(const String&, const Text&);
 

third_party/WebKit/Source/core/editing/markup.cpp
@@ -146,7 +146,7 @@ private:
     enum NodeTraversalMode { EmitString, DoNotEmitString };
     Node* traverseNodesForSerialization(Node* startNode, Node* pastEnd, NodeTraversalMode);
 
-    bool shouldAnnotate() { return m_shouldAnnotate == AnnotateForInterchange; }
+    bool shouldAnnotate() const { return m_shouldAnnotate == AnnotateForInterchange || m_shouldAnnotate == AnnotateForNavigationTransition; }
     bool shouldApplyWrappingStyle(const Node& node) const
     {
         return m_highestNodeToBeSerialized && m_highestNodeToBeSerialized->parentNode() == node.parentNode()
@@ -317,6 +317,9 @@ void StyledMarkupAccumulator::appendElement(StringBuilder& out, Element& element
             if (shouldAnnotate())
                 newInlineStyle->mergeStyleFromRulesForSerialization(&toHTMLElement(element));
 
+            if (&element == m_highestNodeToBeSerialized && m_shouldAnnotate == AnnotateForNavigationTransition)
+                newInlineStyle->addAbsolutePositioningFromElement(element);
+
             if (addDisplayInline)
                 newInlineStyle->forceInline();
 
@@ -343,8 +346,14 @@ Node* StyledMarkupAccumulator::serializeNodes(Node* startNode, Node* pastEnd)
         m_highestNodeToBeSerialized = lastClosed;
     }
 
-    if (m_highestNodeToBeSerialized && m_highestNodeToBeSerialized->parentNode())
+    if (m_highestNodeToBeSerialized && m_highestNodeToBeSerialized->parentNode()) {
         m_wrappingStyle = EditingStyle::wrappingStyleForSerialization(m_highestNodeToBeSerialized->parentNode(), shouldAnnotate());
+        if (m_shouldAnnotate == AnnotateForNavigationTransition) {
+            m_wrappingStyle->style()->removeProperty(CSSPropertyBackgroundColor);
+            m_wrappingStyle->style()->removeProperty(CSSPropertyBackgroundImage);
+        }
+    }
+
 
     return traverseNodesForSerialization(startNode, pastEnd, EmitString);
 }
@@ -371,7 +380,7 @@ Node* StyledMarkupAccumulator::traverseNodesForSerialization(Node* startNode, No
             // Don't write out empty block containers that aren't fully selected.
             continue;
 
-        if (!n->renderer() && !enclosingNodeWithTag(firstPositionInOrBeforeNode(n), selectTag)) {
+        if (!n->renderer() && !enclosingNodeWithTag(firstPositionInOrBeforeNode(n), selectTag) && m_shouldAnnotate != AnnotateForNavigationTransition) {
             next = NodeTraversal::nextSkippingChildren(*n);
             // Don't skip over pastEnd.
             if (pastEnd && pastEnd->isDescendantOf(n))
@@ -1093,6 +1102,17 @@ void mergeWithNextTextNode(PassRefPtrWillBeRawPtr<Node> node, ExceptionState& ex
     textNode->appendData(textNext->data());
     if (textNext->parentNode()) // Might have been removed by mutation event.
         textNext->remove(exceptionState);
+}
+
+String createStyledMarkupForNavigationTransition(Node* node)
+{
+    node->document().updateLayoutIgnorePendingStylesheets();
+
+    StyledMarkupAccumulator accumulator(0, ResolveAllURLs, AnnotateForNavigationTransition, nullptr, 0);
+    accumulator.serializeNodes(node, NodeTraversal::nextSkippingChildren(*node));
+
+    static const char* documentMarkup = "<!DOCTYPE html><meta name=\"viewport\" content=\"width=device-width, user-scalable=0\">";
+    return documentMarkup + accumulator.takeResults();
 }
 
 }

commit 120a2cb3147b35003fc2d9c87a1c91078c86d209

Fix Oilpan build broken by r178403
TBR=oysteine@chromium.org,oilpan-reviews@chromium.org
BUG=

Review URL: https://codereview.chromium.org/404563006

git-svn-id: svn://svn.chromium.org/blink/trunk@178412 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/dom/Document.cpp
@@ -5712,7 +5712,7 @@ void Document::getTransitionElementData(Vector<TransitionElementData>& elementDa
 
         TrackExceptionState exceptionState;
         AtomicString selector(metaElementContents.substring(0, firstSemicolon));
-        RefPtr<StaticNodeList> nodeList = querySelectorAll(selector, exceptionState);
+        RefPtrWillBeRawPtr<StaticNodeList> nodeList = querySelectorAll(selector, exceptionState);
         if (!nodeList || exceptionState.hadException())
             continue;
 