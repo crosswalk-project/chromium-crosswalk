fda0363d96d4d9d66f92fb51bd4b862a56ecafb6
Sudarsana Nagineni <sudarsana.nagineni@intel.com>
2014-09-11 17:35:26 +0300

    [Tizen] Add Tizen Murphy resource policy daemon integration
    This patch includes Chromium side changes that are needed for
    integrating WebMediaPlayer with the Murphy resource policy daemon.

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -72,6 +72,12 @@ class MediaPermission;
 class WebEncryptedMediaClientImpl;
 }
 
+#if defined(OS_TIZEN) && defined(ENABLE_MURPHY)
+namespace tizen {
+class RendererMediaPlayerManager;
+}
+#endif
+
 namespace content {
 
 class ChildFrameCompositingHelper;
@@ -752,6 +758,8 @@ class CONTENT_EXPORT RenderFrameImpl
       blink::WebContentDecryptionModule* initial_cdm);
 
   RendererMediaPlayerManager* GetMediaPlayerManager();
+#elif defined(OS_TIZEN) && defined(ENABLE_MURPHY)
+  tizen::RendererMediaPlayerManager* GetTizenMediaPlayerManager();
 #endif
 
   media::MediaPermission* GetMediaPermission();
@@ -864,6 +872,8 @@ class CONTENT_EXPORT RenderFrameImpl
   // real media player in the browser process. It's okay to use a raw pointer
   // since it's a RenderFrameObserver.
   RendererMediaPlayerManager* media_player_manager_;
+#elif defined(OS_TIZEN) && defined(ENABLE_MURPHY)
+  tizen::RendererMediaPlayerManager* media_player_manager_;
 #endif
 
 #if defined(ENABLE_BROWSER_CDMS)
