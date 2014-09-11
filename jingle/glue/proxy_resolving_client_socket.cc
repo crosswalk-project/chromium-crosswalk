858498ee9ed0e983a494dc6b360a6ab7091c5698
Ben Greenstein <bengr@chromium.org>
2014-09-10 18:28:19 -0700

    Keep track of network error in ProxyRetryInfo.
    Use it to log error type in BypassOneNetworkError UMA.
    This fixes DataReductionProxy.BypassOnNetworkError UMA recording bug.
    
    BUG=395769
    
    Review URL: https://codereview.chromium.org/473513002
    
    Cr-Commit-Position: refs/heads/master@{#290054}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290054 0039d316-1c4b-4281-b951-d872f2087c98
    (cherry picked from commit d0483b1909b7094854485c0e6ae9360e46e39929)
    
    Review URL: https://codereview.chromium.org/558413002
    
    Cr-Commit-Position: refs/branch-heads/2125@{#310}
    Cr-Branched-From: b68026d94bda36dd106a3d91a098719f952a9477-refs/heads/master@{#290040}

--- jingle/glue/proxy_resolving_client_socket.cc
+++ jingle/glue/proxy_resolving_client_socket.cc
@@ -320,7 +320,7 @@ int ProxyResolvingClientSocket::ReconsiderProxyAfterError(int error) {
 }
 
 void ProxyResolvingClientSocket::ReportSuccessfulProxyConnection() {
-  network_session_->proxy_service()->ReportSuccess(proxy_info_);
+  network_session_->proxy_service()->ReportSuccess(proxy_info_, NULL);
 }
 
 void ProxyResolvingClientSocket::Disconnect() {
