fec5bc663e64978bc11744c49d38e43645c74780
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2014-09-24 16:41:30 +0300

    [Tizen] Make content fit to the viewport
    This actually means that 'setShrinksViewportContentToFit'
    setting should be set. This is necessary for viewport
    meta tag enabling on Tizen.

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -1085,6 +1085,8 @@ void RenderView::ApplyWebPreferences(const WebPreferences& prefs,
 
   // Enable double tap to zoom when zoomable.
   settings->setDoubleTapToZoomEnabled(true);
+
+  settings->setShrinksViewportContentToFit(true);
 #endif
 
 #if defined(OS_ANDROID)
