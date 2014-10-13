001c46d0b577aba5fb1c2eb2277eb0ed71c9518b
Anton Vayvod <avayvod@google.com>
2014-10-13 21:45:36 +0100

    Merge to 39 - [Cast, Android] Don't hold the wake lock if the video is playing remotely.
    Since the video is actually playing on a remote device, there's no need to prevent
    the screen from fading and locking.
    
    BUG=420665
    TEST=manual
    TBR=aberent@chromium.org
    
    Review URL: https://codereview.chromium.org/644463003
    
    Cr-Commit-Position: refs/heads/master@{#299137}
    (cherry picked from commit 68017be636f8e3446174f9915503fb80345bf54f)
    
    Review URL: https://codereview.chromium.org/640423007
    
    Cr-Commit-Position: refs/branch-heads/2171@{#124}
    Cr-Branched-From: 267aeeb8d85c8503a7fd12bd14654b8ea78d3974-refs/heads/master@{#297060}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -3209,7 +3209,7 @@ void RenderFrameImpl::didChangeManifest(blink::WebLocalFrame* frame)
 void RenderFrameImpl::DidPlay(blink::WebMediaPlayer* player) {
   Send(new FrameHostMsg_MediaPlayingNotification(
       routing_id_, reinterpret_cast<int64>(player), player->hasVideo(),
-      player->hasAudio()));
+      player->hasAudio(), player->isRemote()));
 }
 
 void RenderFrameImpl::DidPause(blink::WebMediaPlayer* player) {
