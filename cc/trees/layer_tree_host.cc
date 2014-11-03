b6a5b52d529be7d3cef1b0fb5e6bee43c7640589
David Bokan <bokan@google.com>
2014-11-03 15:22:17 -0500

    Revert "Apply scroll and scale delta values to client even if root layer is NULL."
    This reverts commit ca150d8e81d07d04bffc143c55da232765279ec9.
    
    Cr-Commit-Position: refs/branch-heads/2171@{#337}
    Cr-Branched-From: 267aeeb8d85c8503a7fd12bd14654b8ea78d3974-refs/heads/master@{#297060}

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -1059,6 +1059,9 @@ void LayerTreeHost::PaintLayerContents(
 }
 
 void LayerTreeHost::ApplyScrollAndScale(ScrollAndScaleSet* info) {
+  if (!root_layer_.get())
+    return;
+
   ScopedPtrVector<SwapPromise>::iterator it = info->swap_promises.begin();
   for (; it != info->swap_promises.end(); ++it) {
     scoped_ptr<SwapPromise> swap_promise(info->swap_promises.take(it));
@@ -1072,20 +1075,18 @@ void LayerTreeHost::ApplyScrollAndScale(ScrollAndScaleSet* info) {
   gfx::Vector2d inner_viewport_scroll_delta;
   gfx::Vector2d outer_viewport_scroll_delta;
 
-  if (root_layer_.get()) {
-    for (size_t i = 0; i < info->scrolls.size(); ++i) {
-      Layer* layer = LayerTreeHostCommon::FindLayerInSubtree(
-          root_layer_.get(), info->scrolls[i].layer_id);
-      if (!layer)
-        continue;
-      if (layer == outer_viewport_scroll_layer_.get()) {
-        outer_viewport_scroll_delta += info->scrolls[i].scroll_delta;
-      } else if (layer == inner_viewport_scroll_layer_.get()) {
-        inner_viewport_scroll_delta += info->scrolls[i].scroll_delta;
-      } else {
-        layer->SetScrollOffsetFromImplSide(layer->scroll_offset() +
-                                           info->scrolls[i].scroll_delta);
-      }
+  for (size_t i = 0; i < info->scrolls.size(); ++i) {
+    Layer* layer = LayerTreeHostCommon::FindLayerInSubtree(
+        root_layer_.get(), info->scrolls[i].layer_id);
+    if (!layer)
+      continue;
+    if (layer == outer_viewport_scroll_layer_.get()) {
+      outer_viewport_scroll_delta += info->scrolls[i].scroll_delta;
+    } else if (layer == inner_viewport_scroll_layer_.get()) {
+      inner_viewport_scroll_delta += info->scrolls[i].scroll_delta;
+    } else {
+      layer->SetScrollOffsetFromImplSide(layer->scroll_offset() +
+                                         info->scrolls[i].scroll_delta);
     }
   }
 
@@ -1093,22 +1094,22 @@ void LayerTreeHost::ApplyScrollAndScale(ScrollAndScaleSet* info) {
       !outer_viewport_scroll_delta.IsZero() ||
       info->page_scale_delta != 1.f ||
       info->top_controls_delta) {
+    // SetScrollOffsetFromImplSide above could have destroyed the tree,
+    // so re-get this layer before doing anything to it.
+
     // Preemptively apply the scroll offset and scale delta here before sending
     // it to the client.  If the client comes back and sets it to the same
     // value, then the layer can early out without needing a full commit.
+    DCHECK(inner_viewport_scroll_layer_.get());  // We should always have this.
 
-    if (inner_viewport_scroll_layer_.get()) {
-      inner_viewport_scroll_layer_->SetScrollOffsetFromImplSide(
-          inner_viewport_scroll_layer_->scroll_offset() +
-          inner_viewport_scroll_delta);
-    }
-
+    inner_viewport_scroll_layer_->SetScrollOffsetFromImplSide(
+        inner_viewport_scroll_layer_->scroll_offset() +
+        inner_viewport_scroll_delta);
     if (outer_viewport_scroll_layer_.get()) {
       outer_viewport_scroll_layer_->SetScrollOffsetFromImplSide(
           outer_viewport_scroll_layer_->scroll_offset() +
           outer_viewport_scroll_delta);
     }
-
     ApplyPageScaleDeltaFromImplSide(info->page_scale_delta);
 
     client_->ApplyViewportDeltas(
