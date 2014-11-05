bc8fd6a0e7255c18fd5fb02f2816478600116105
Sudarsana Nagineni <sudarsana.nagineni@intel.com>
2014-11-05 11:33:54 +0200

    [Tizen] Set app ID and media class properties for the audio streams.
    Set an application ID and media class properties for the audio streams in
    pulseaudio and Murphy, so that the Murphy resource policy daemon can dictate
    when the audio will be paused and resumed in response to other audio playing.
    
    BUG=XWALK-2788

--- media/audio/audio_output_controller.h
+++ media/audio/audio_output_controller.h
@@ -5,6 +5,10 @@
 #ifndef MEDIA_AUDIO_AUDIO_OUTPUT_CONTROLLER_H_
 #define MEDIA_AUDIO_AUDIO_OUTPUT_CONTROLLER_H_
 
+#if defined(OS_TIZEN)
+#include <string>
+#endif
+
 #include "base/atomic_ref_count.h"
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
@@ -68,6 +72,11 @@ class MEDIA_EXPORT AudioOutputController
     virtual void OnPaused() = 0;
     virtual void OnError() = 0;
     virtual void OnDeviceChange(int new_buffer_size, int new_sample_rate) = 0;
+
+#if defined(OS_TIZEN)
+    virtual std::string app_id() const = 0;
+    virtual std::string app_class() const = 0;
+#endif
 
    protected:
     virtual ~EventHandler() {}
