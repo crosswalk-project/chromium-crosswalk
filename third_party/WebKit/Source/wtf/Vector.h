crosswalk-project/chromium-crosswalk

commit 49ba82d97a582847541d82ae04f0ee359e68cecf

Oilpan: Vector::reserveCapacity shouldn't reallocate backing storage always
In the current implementation, when Vector::reserveCapacity expands its backing storage,
we always reallocate the backing storage (i.e., allocate a new storage -> memmove the storage
-> deallocate the old storage).

In this CL, we reallocate the backing storage only when necessary. If we can expand the tail
of the backing storage to the new capacity, we just expand the storage instead of reallocating
the storage.

This optimization works well for the following code:

  HeapVector<X> vec;
  for (...) {
    X* x = new X();  // Note that X is allocated in a different heap than the collection backing heap.
    vec.append(x);  // If this calls Vector::reserveCapacity, we can expand the backing storage without reallocation.
  }

but doesn't work for the following code:

  HeapVector<X> vec;
  HeapVector<X> vec2;
  for (...) {
    X* x = new X();
    X* x2 = new X();
    vec.append(x);  // If this calls Vector::reserveCapacity, we have to reallocate the storage.
    vec2.append(x2);  // If this calls Vector::reserveCapacity, we have to reallocate the storage.
  }

This significantly improves a lot of benchmarks:

parser.query-selector-*: 20 - 50%
parser.html-parser: 5%
parser.xml-parser: 11%
shadow_dom.ShadowReprojection: 6%


BUG=420515

Review URL: https://codereview.chromium.org/720163002

git-svn-id: svn://svn.chromium.org/blink/trunk@185350 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/wtf/Vector.h
@@ -359,6 +359,16 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
             Allocator::backingFree(bufferToDeallocate);
         }
 
+        bool expandBuffer(size_t newCapacity)
+        {
+            size_t sizeToAllocate = allocationSize(newCapacity);
+            if (Allocator::backingExpand(m_buffer, sizeToAllocate)) {
+                m_capacity = sizeToAllocate / sizeof(T);
+                return true;
+            }
+            return false;
+        }
+
         void resetBufferPointer()
         {
             m_buffer = 0;
@@ -426,6 +436,20 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
         {
             if (UNLIKELY(bufferToDeallocate != inlineBuffer()))
                 reallyDeallocateBuffer(bufferToDeallocate);
+        }
+
+        bool expandBuffer(size_t newCapacity)
+        {
+            ASSERT(newCapacity > inlineCapacity);
+            if (m_buffer == inlineBuffer())
+                return false;
+
+            size_t sizeToAllocate = allocationSize(newCapacity);
+            if (Allocator::backingExpand(m_buffer, sizeToAllocate)) {
+                m_capacity = sizeToAllocate / sizeof(T);
+                return true;
+            }
+            return false;
         }
 
         void resetBufferPointer()
@@ -949,6 +973,8 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
             return;
         T* oldBuffer = begin();
         T* oldEnd = end();
+        if (Base::expandBuffer(newCapacity))
+            return;
         Base::allocateBuffer(newCapacity);
         TypeOperations::move(oldBuffer, oldEnd, begin());
         Base::deallocateBuffer(oldBuffer);

third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -863,6 +863,29 @@ void FreeList<Header>::addToFreeList(Address address, size_t size)
 }
 
 template<typename Header>
+bool ThreadHeap<Header>::expandObject(Header* header, size_t newSize)
+{
+    ASSERT(header->payloadSize() < newSize);
+    size_t allocationSize = allocationSizeFromSize(newSize);
+    ASSERT(allocationSize > header->size());
+    size_t expandSize = allocationSize - header->size();
+    if (header->payloadEnd() == m_currentAllocationPoint && expandSize <= m_remainingAllocationSize) {
+        m_currentAllocationPoint += expandSize;
+        m_remainingAllocationSize -= expandSize;
+
+        // Unpoison the memory used for the object (payload).
+        ASAN_UNPOISON_MEMORY_REGION(header->payloadEnd(), expandSize);
+#if ENABLE(ASSERT) || defined(LEAK_SANITIZER) || defined(ADDRESS_SANITIZER)
+        memset(header->payloadEnd(), 0, expandSize);
+#endif
+        header->setSize(allocationSize);
+        ASSERT(heapPageFromAddress(header->payloadEnd() - 1));
+        return true;
+    }
+    return false;
+}
+
+template<typename Header>
 void ThreadHeap<Header>::promptlyFreeObject(Header* header)
 {
     ASSERT(!m_threadState->isSweepInProgress());
@@ -2864,6 +2887,33 @@ void HeapAllocator::backingFree(void* address)
     int heapIndex = HeapTraits::index(gcInfo->hasFinalizer(), header->payloadSize());
     HeapType* heap = static_cast<HeapType*>(state->heap(heapIndex));
     heap->promptlyFreeObject(header);
+}
+
+bool HeapAllocator::backingExpand(void* address, size_t newSize)
+{
+    if (!address || ThreadState::isAnyThreadInGC())
+        return false;
+
+    ThreadState* state = ThreadState::current();
+    if (state->isSweepInProgress())
+        return false;
+    ASSERT(state->isAllocationAllowed());
+
+    BaseHeapPage* page = pageHeaderFromObject(address);
+    if (page->isLargeObject() || page->threadState() != state)
+        return false;
+
+    typedef HeapIndexTrait<CollectionBackingHeap> HeapTraits;
+    typedef HeapTraits::HeapType HeapType;
+    typedef HeapTraits::HeaderType HeaderType;
+
+    HeaderType* header = HeaderType::fromPayload(address);
+    header->checkHeader();
+
+    const GCInfo* gcInfo = header->gcInfo();
+    int heapIndex = HeapTraits::index(gcInfo->hasFinalizer(), header->payloadSize());
+    HeapType* heap = static_cast<HeapType*>(state->heap(heapIndex));
+    return heap->expandObject(header, newSize);
 }
 
 BaseHeapPage* Heap::lookup(Address address)

third_party/WebKit/Source/platform/heap/Heap.h
@@ -304,6 +304,9 @@ public:
     NO_SANITIZE_ADDRESS
     size_t size() const { return m_size & sizeMask; }
 
+    NO_SANITIZE_ADDRESS
+    void setSize(size_t size) { m_size = (size | (m_size & ~sizeMask)); }
+
 #if ENABLE(GC_PROFILE_HEAP)
     NO_SANITIZE_ADDRESS
     size_t encodedSize() const { return m_size; }
@@ -813,6 +816,7 @@ public:
     void removePageFromHeap(HeapPage<Header>*);
 
     PLATFORM_EXPORT void promptlyFreeObject(Header*);
+    PLATFORM_EXPORT bool expandObject(Header*, size_t);
 
 private:
     void addPageToHeap(const GCInfo*);
@@ -1491,6 +1495,7 @@ public:
         return reinterpret_cast<Return>(Heap::allocate<Metadata>(size));
     }
     PLATFORM_EXPORT static void backingFree(void* address);
+    PLATFORM_EXPORT static bool backingExpand(void*, size_t);
 
     static void free(void* address) { }
     template<typename T>

third_party/WebKit/Source/wtf/DefaultAllocator.h
@@ -88,6 +88,10 @@ public:
         return reinterpret_cast<Return>(fastMalloc(size));
     }
     WTF_EXPORT static void backingFree(void* address);
+    WTF_EXPORT static inline bool backingExpand(void*, size_t)
+    {
+        return false;
+    }
     static void free(void* address)
     {
         fastFree(address);

third_party/WebKit/Source/wtf/Deque.h
@@ -330,7 +330,10 @@ namespace WTF {
     {
         size_t oldCapacity = m_buffer.capacity();
         T* oldBuffer = m_buffer.buffer();
-        m_buffer.allocateBuffer(std::max(static_cast<size_t>(16), oldCapacity + oldCapacity / 4 + 1));
+        size_t newCapacity = std::max(static_cast<size_t>(16), oldCapacity + oldCapacity / 4 + 1);
+        if (m_buffer.expandBuffer(newCapacity))
+            return;
+        m_buffer.allocateBuffer(newCapacity);
         if (m_start <= m_end)
             TypeOperations::move(oldBuffer + m_start, oldBuffer + m_end, m_buffer.buffer() + m_start);
         else {

commit 0d2c5229e963b42e531a5fbbca4675756bbb363e

Oilpan: Speculative fix for performance regressions caused by r185350
It looks like r185350 caused performance regressions in blink_perf.css
only in Nexus10. I don't see why, but the only reason I can come up with
is that the compiler is not smart to eliminate the branch of if(Base::expandBuffer)
for off-heap vectors.

This CL changes the branch to if(Allocator::isGarbageCollected && Base::expandBuffer)
to help the compiler eliminate the branch statically.

BUG=433883

Review URL: https://codereview.chromium.org/745993002

git-svn-id: svn://svn.chromium.org/blink/trunk@185757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/wtf/Vector.h
@@ -972,7 +972,10 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
             return;
         T* oldBuffer = begin();
         T* oldEnd = end();
-        if (Base::expandBuffer(newCapacity))
+        // The Allocator::isGarbageCollected check is not needed.
+        // The check is just a static hint for a compiler to indicate that
+        // Base::expandBuffer returns false if Allocator is a DefaultAllocator.
+        if (Allocator::isGarbageCollected && Base::expandBuffer(newCapacity))
             return;
         Base::allocateBuffer(newCapacity);
         TypeOperations::move(oldBuffer, oldEnd, begin());