0fdf608e9339b795f23af3fd04fc7313008cd416
Luke Nihlen <luken@chromium.org>
2014-11-24 12:06:56 -0800

    We previously had a setting in chrome defaults but this deprecates that for a command line flag and utility function that always defers to the flag but otherwise returns a default based on operating system. The CL then removes some of the code that depended on the default for a more robust direct check by the render_view_impl. This should also help with some of the CrOS UI bugs that link disambiguation created with views that didn't necessarily initialize their RendererPreferences.
    BUG=433007,428163
    TBR=jar
    
    Review URL: https://codereview.chromium.org/725243002
    
    Cr-Commit-Position: refs/heads/master@{#305385}
    (cherry picked from commit e23094a2fb86eb15f76caf5b55927d0b7e9ba8f5)
    
    Conflicts:
    	chrome/app/generated_resources.grd
    	tools/metrics/histograms/histograms.xml
    
    Review URL: https://codereview.chromium.org/725293005
    
    Cr-Commit-Position: refs/branch-heads/2171_62@{#7}
    Cr-Branched-From: 6ac5b3b7a7861707e68ff88605ec7e2e1dd3f941-refs/branch-heads/2171@{#415}
    Cr-Branched-From: 267aeeb8d85c8503a7fd12bd14654b8ea78d3974-refs/heads/master@{#297060}

--- chrome/browser/ui/views/tab_contents/chrome_web_contents_view_delegate_views.cc
+++ chrome/browser/ui/views/tab_contents/chrome_web_contents_view_delegate_views.cc
@@ -178,9 +178,6 @@ void ChromeWebContentsViewDelegateViews::ShowDisambiguationPopup(
     const base::Callback<void(ui::GestureEvent*)>& gesture_cb,
     const base::Callback<void(ui::MouseEvent*)>& mouse_cb) {
 #if defined(USE_AURA)
-  if (!browser_defaults::kShowLinkDisambiguationPopup)
-    return;
-
   link_disambiguation_popup_.reset(new LinkDisambiguationPopup);
   link_disambiguation_popup_->Show(
       zoomed_bitmap, target_rect, content, gesture_cb, mouse_cb);
