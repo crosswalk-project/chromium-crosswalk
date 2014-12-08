6743f60427a1887b048dc84028b7a6409bff384f
Vaclav Brozek <vabr@chromium.org>
2014-12-08 11:57:42 +0100

    [Merge] LoginDatabase should allow retrieveing credentials with IP addresses
    This is the merge of https://codereview.chromium.org/779183003 to branch 2214 (current beta). Merge was approved in http://crbug.com/423327#c96. Original description follows:
    
    Currently, when a credential for an IP-specified host is saved in
    the LoginDatabase, it cannot be retrieved, because it is subjected
    to the PSL matching check, and that fails for IP addresses.
    
    The reason is that IP addresses cannot be PSL matched (there is no
    subdomain structure to meaningfully match against).
    
    This CL makes sure, that PSL matching for IP addresses reduces
    to identity checking. Also, it makes sute that the LoginDatabase
    does not even attempt PSL matching on non-HTML forms (PSL
    matching is not intended for those).
    
    BUG=423327
    TBR=matthewyuan@chromium.org
    
    Cr-Commit-Position: refs/heads/master@{#307107}
    (cherry picked from commit 71c5a60d9723bc4b8255177cf0dc60b9c750a908)
    
    Review URL: https://codereview.chromium.org/782223004
    
    Cr-Commit-Position: refs/branch-heads/2214@{#233}
    Cr-Branched-From: 03655fd3f6d72165dc3c9bd2c89807305316fe6c-refs/heads/master@{#303346}

--- components/password_manager/core/browser/psl_matching_helper.cc
+++ components/password_manager/core/browser/psl_matching_helper.cc
@@ -18,7 +18,8 @@ namespace password_manager {
 
 bool ShouldPSLDomainMatchingApply(
     const std::string& registry_controlled_domain) {
-  return registry_controlled_domain != "google.com";
+  return !registry_controlled_domain.empty() &&
+         registry_controlled_domain != "google.com";
 }
 
 bool IsPublicSuffixDomainMatch(const std::string& url1,
@@ -28,6 +29,9 @@ bool IsPublicSuffixDomainMatch(const std::string& url1,
 
   if (!gurl1.is_valid() || !gurl2.is_valid())
     return false;
+
+  if (gurl1 == gurl2)
+    return true;
 
   return gurl1.scheme() == gurl2.scheme() &&
          GetRegistryControlledDomain(gurl1) ==
