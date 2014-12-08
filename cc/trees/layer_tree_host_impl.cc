223b1e5245196f119888abb6c02f17ea4ec6921c
matthewyuan <matthewyuan@google.com>
2014-12-08 11:01:04 -0800

    Revert "Properly handle viewport-flings after a bubbling scroll" since it's breaking compile on branch.
    This reverts commit 880fc69df51a998bb49f77a47fb601ba17cdf3f6.
    
    BUG=440038
    
    Review URL: https://codereview.chromium.org/783243002
    
    Cr-Commit-Position: refs/branch-heads/2214@{#239}
    Cr-Branched-From: 03655fd3f6d72165dc3c9bd2c89807305316fe6c-refs/heads/master@{#303346}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -2738,16 +2738,10 @@ InputHandlerScrollResult LayerTreeHostImpl::ScrollBy(
     }
 
     did_lock_scrolling_layer_ = true;
-
-    // When scrolls are allowed to bubble, it's important that the original
-    // scrolling layer be preserved. This ensures that, after a scroll bubbles,
-    // the user can reverse scroll directions and immediately resume scrolling
-    // the original layer that scrolled.
-    if (!should_bubble_scrolls_)
+    if (!allow_bubbling_for_current_layer) {
       active_tree_->SetCurrentlyScrollingLayer(layer_impl);
-
-    if (!allow_bubbling_for_current_layer)
       break;
+    }
 
     if (allow_unrestricted_bubbling_for_current_layer) {
       pending_delta -= applied_delta;
