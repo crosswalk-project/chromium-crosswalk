crosswalk-project/chromium-crosswalk

commit 17b44a00e420f752fab01241d92f6522ba5252bb

Implement select listbox using shadow DOM
User visible behavior changes.
- Old listbox could only scroll in itemHeight increments but now we have smooth scroll.
- Mouse events need to be dispatched on the option element, not select element.
- Select element height is determined based on the height of the first item, not the font size.
- Spatial navigation now draws a black dashed outline around the active selection instead of a focus ring.

We can no longer let RenderListBox draw the focus ring for spatial navigation mode because the items are drawn on another RenderLayer.
I added a user agent stylesheet only :-internal-spatial-navigation-focus pseudo type so a style can be applied to the spatial navigation active element.
If we add the default focus ring on the option element, it gets clipped to the select element so I changed it to a black dashed outline.

Option and optgroups were represented as AXListBoxOptions but now only options are AXListBoxOptions and optgroups are represented by the optgroup-label AXNodeObject. We let AXRenderObject handle hit testing so we can remove the custom hit testing.

BUG=234871

Review URL: https://codereview.chromium.org/347773002

git-svn-id: svn://svn.chromium.org/blink/trunk@178354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/css/html.css
@@ -641,6 +641,11 @@ input[type="button"]:active:disabled, input[type="submit"]:active:disabled, inpu
     border-style: outset
 }
 
+option:-internal-spatial-navigation-focus {
+    outline: black dashed 1px;
+    outline-offset: -1px;
+}
+
 datalist {
     display: none
 }
@@ -759,24 +764,54 @@ select[size][multiple] {
     align-items: flex-start;
     border: 1px inset gray;
     border-radius: initial;
-    white-space: initial;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    vertical-align: text-bottom;
+    -webkit-user-select: none;
+    white-space: nowrap;
 }
 
 select[size="0"],
 select[size="1"] {
     -webkit-appearance: menulist;
     align-items: center;
-    border: 1px solid;
+    border-width: 1px;
+    border-style: solid;
+    border-color: initial;
     border-radius: 5px;
+    overflow-x: initial;
+    overflow-y: initial;
+    vertical-align: initial;
+    -webkit-user-select: initial;
     white-space: pre;
 }
 
 optgroup {
     font-weight: bolder;
+    display: block;
 }
 
 option {
     font-weight: normal;
+    display: block;
+    padding: 0 2px 1px 2px;
+    white-space: pre;
+    min-height: 1.2em;
+}
+
+option:checked {
+    background-color: -internal-inactive-list-box-selection;
+    color: -internal-inactive-list-box-selection-text;
+}
+
+select:focus option:checked {
+    background-color: -internal-active-list-box-selection;
+    color: -internal-active-list-box-selection-text;
+}
+
+select:disabled option:checked,
+option:checked:disabled {
+    color: gray;
 }
 
 output {

third_party/WebKit/Source/core/html/HTMLOptGroupElement.cpp
@@ -27,8 +27,14 @@
 
 #include "core/HTMLNames.h"
 #include "core/dom/NodeRenderStyle.h"
+#include "core/dom/Text.h"
+#include "core/editing/htmlediting.h"
+#include "core/html/HTMLContentElement.h"
+#include "core/html/HTMLDivElement.h"
 #include "core/html/HTMLSelectElement.h"
+#include "core/html/shadow/ShadowElementNames.h"
 #include "wtf/StdLibExtras.h"
+#include "wtf/unicode/CharacterNames.h"
 
 namespace WebCore {
 
@@ -37,21 +43,19 @@ using namespace HTMLNames;
 inline HTMLOptGroupElement::HTMLOptGroupElement(Document& document)
     : HTMLElement(optgroupTag, document)
 {
-    setHasCustomStyleCallbacks();
     ScriptWrappable::init(this);
 }
 
-DEFINE_NODE_FACTORY(HTMLOptGroupElement)
+PassRefPtrWillBeRawPtr<HTMLOptGroupElement> HTMLOptGroupElement::create(Document& document)
+{
+    RefPtrWillBeRawPtr<HTMLOptGroupElement> optGroupElement = adoptRefWillBeNoop(new HTMLOptGroupElement(document));
+    optGroupElement->ensureUserAgentShadowRoot();
+    return optGroupElement.release();
+}
 
 bool HTMLOptGroupElement::isDisabledFormControl() const
 {
     return fastHasAttribute(disabledAttr);
-}
-
-bool HTMLOptGroupElement::rendererIsFocusable() const
-{
-    // Optgroup elements do not have a renderer so we check the renderStyle instead.
-    return renderStyle() && renderStyle()->display() != NONE;
 }
 
 void HTMLOptGroupElement::childrenChanged(const ChildrenChange& change)
@@ -67,48 +71,14 @@ void HTMLOptGroupElement::parseAttribute(const QualifiedName& name, const Atomic
 
     if (name == disabledAttr)
         didAffectSelector(AffectedSelectorDisabled | AffectedSelectorEnabled);
+    else if (name == labelAttr)
+        updateGroupLabel();
 }
 
 void HTMLOptGroupElement::recalcSelectOptions()
 {
     if (HTMLSelectElement* select = Traversal<HTMLSelectElement>::firstAncestor(*this))
         select->setRecalcListItems();
-}
-
-void HTMLOptGroupElement::attach(const AttachContext& context)
-{
-    if (context.resolvedStyle) {
-        ASSERT(!m_style || m_style == context.resolvedStyle);
-        m_style = context.resolvedStyle;
-    }
-    HTMLElement::attach(context);
-}
-
-void HTMLOptGroupElement::detach(const AttachContext& context)
-{
-    m_style.clear();
-    HTMLElement::detach(context);
-}
-
-void HTMLOptGroupElement::updateNonRenderStyle()
-{
-    bool oldDisplayNoneStatus = isDisplayNone();
-    m_style = originalStyleForRenderer();
-    if (oldDisplayNoneStatus != isDisplayNone()) {
-        if (HTMLSelectElement* select = ownerSelectElement())
-            select->updateListOnRenderer();
-    }
-}
-
-RenderStyle* HTMLOptGroupElement::nonRendererStyle() const
-{
-    return m_style.get();
-}
-
-PassRefPtr<RenderStyle> HTMLOptGroupElement::customStyleForRenderer()
-{
-    updateNonRenderStyle();
-    return m_style;
 }
 
 String HTMLOptGroupElement::groupLabelText() const
@@ -136,10 +106,34 @@ void HTMLOptGroupElement::accessKeyAction(bool)
         select->accessKeyAction(false);
 }
 
-bool HTMLOptGroupElement::isDisplayNone() const
+void HTMLOptGroupElement::didAddUserAgentShadowRoot(ShadowRoot& root)
 {
-    RenderStyle* style = nonRendererStyle();
-    return style && style->display() == NONE;
+    DEFINE_STATIC_LOCAL(AtomicString, labelPadding, ("0 2px 1px 2px", AtomicString::ConstructFromLiteral));
+    DEFINE_STATIC_LOCAL(AtomicString, labelMinHeight, ("1.2em", AtomicString::ConstructFromLiteral));
+    RefPtrWillBeRawPtr<HTMLDivElement> label = HTMLDivElement::create(document());
+    label->setAttribute(roleAttr, AtomicString("group", AtomicString::ConstructFromLiteral));
+    label->setAttribute(aria_labelAttr, AtomicString());
+    label->setInlineStyleProperty(CSSPropertyPadding, labelPadding);
+    label->setInlineStyleProperty(CSSPropertyMinHeight, labelMinHeight);
+    label->setIdAttribute(ShadowElementNames::optGroupLabel());
+    root.appendChild(label);
+
+    RefPtrWillBeRawPtr<HTMLContentElement> content = HTMLContentElement::create(document());
+    content->setAttribute(selectAttr, "option,optgroup");
+    root.appendChild(content);
+}
+
+void HTMLOptGroupElement::updateGroupLabel()
+{
+    const String& labelText = groupLabelText();
+    HTMLDivElement& label = optGroupLabelElement();
+    label.setTextContent(labelText);
+    label.setAttribute(aria_labelAttr, AtomicString(labelText));
+}
+
+HTMLDivElement& HTMLOptGroupElement::optGroupLabelElement() const
+{
+    return *toHTMLDivElement(userAgentShadowRoot()->getElementById(ShadowElementNames::optGroupLabel()));
 }
 
 } // namespace

third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
@@ -39,16 +39,23 @@
 #include "core/events/GestureEvent.h"
 #include "core/events/KeyboardEvent.h"
 #include "core/events/MouseEvent.h"
+#include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
 #include "core/html/FormDataList.h"
 #include "core/html/HTMLFormElement.h"
+#include "core/html/HTMLOptGroupElement.h"
 #include "core/html/HTMLOptionElement.h"
 #include "core/html/forms/FormController.h"
+#include "core/page/AutoscrollController.h"
 #include "core/page/EventHandler.h"
+#include "core/page/Page.h"
 #include "core/page/SpatialNavigation.h"
+#include "core/rendering/HitTestRequest.h"
+#include "core/rendering/HitTestResult.h"
 #include "core/rendering/RenderListBox.h"
 #include "core/rendering/RenderMenuList.h"
 #include "core/rendering/RenderTheme.h"
+#include "core/rendering/RenderView.h"
 #include "platform/PlatformMouseEvent.h"
 #include "platform/text/PlatformLocale.h"
 
@@ -80,12 +87,16 @@ HTMLSelectElement::HTMLSelectElement(Document& document, HTMLFormElement* form)
 
 PassRefPtrWillBeRawPtr<HTMLSelectElement> HTMLSelectElement::create(Document& document)
 {
-    return adoptRefWillBeNoop(new HTMLSelectElement(document, 0));
+    RefPtrWillBeRawPtr<HTMLSelectElement> select = adoptRefWillBeNoop(new HTMLSelectElement(document, 0));
+    select->ensureUserAgentShadowRoot();
+    return select.release();
 }
 
 PassRefPtrWillBeRawPtr<HTMLSelectElement> HTMLSelectElement::create(Document& document, HTMLFormElement* form)
 {
-    return adoptRefWillBeNoop(new HTMLSelectElement(document, form));
+    RefPtrWillBeRawPtr<HTMLSelectElement> select = adoptRefWillBeNoop(new HTMLSelectElement(document, form));
+    select->ensureUserAgentShadowRoot();
+    return select.release();
 }
 
 const AtomicString& HTMLSelectElement::formControlType() const
@@ -523,7 +534,11 @@ int HTMLSelectElement::nextValidIndex(int listIndex, SkipDirection direction, in
     for (listIndex += direction; listIndex >= 0 && listIndex < size; listIndex += direction) {
         --skip;
         HTMLElement* element = listItems[listIndex];
-        if (!isHTMLOptionElement(*element) || toHTMLOptionElement(element)->isDisabledFormControl() || toHTMLOptionElement(element)->isDisplayNone())
+        if (!isHTMLOptionElement(*element))
+            continue;
+        if (element->isDisabledFormControl())
+            continue;
+        if (!usesMenuList() && !element->renderer())
             continue;
         lastGoodIndex = listIndex;
         if (skip <= 0)
@@ -626,7 +641,10 @@ void HTMLSelectElement::setActiveSelectionAnchorIndex(int index)
 
 void HTMLSelectElement::setActiveSelectionEndIndex(int index)
 {
+    if (index == m_activeSelectionEndIndex)
+        return;
     m_activeSelectionEndIndex = index;
+    setNeedsStyleRecalc(SubtreeStyleChange);
 }
 
 void HTMLSelectElement::updateListBoxSelection(bool deselectOtherOptions)
@@ -640,7 +658,7 @@ void HTMLSelectElement::updateListBoxSelection(bool deselectOtherOptions)
     const WillBeHeapVector<RawPtrWillBeMember<HTMLElement> >& items = listItems();
     for (unsigned i = 0; i < items.size(); ++i) {
         HTMLElement* element = items[i];
-        if (!isHTMLOptionElement(*element) || toHTMLOptionElement(element)->isDisabledFormControl() || toHTMLOptionElement(element)->isDisplayNone())
+        if (!isHTMLOptionElement(*element) || toHTMLOptionElement(element)->isDisabledFormControl() || !toHTMLOptionElement(element)->renderer())
             continue;
 
         if (i >= start && i <= end)
@@ -651,8 +669,8 @@ void HTMLSelectElement::updateListBoxSelection(bool deselectOtherOptions)
             toHTMLOptionElement(element)->setSelectedState(m_cachedStateForActiveSelection[i]);
     }
 
-    scrollToSelection();
     setNeedsValidityCheck();
+    scrollToSelection();
     notifyFormStateChanged();
 }
 
@@ -703,11 +721,13 @@ void HTMLSelectElement::dispatchInputAndChangeEventForMenuList(bool requiresUser
 
 void HTMLSelectElement::scrollToSelection()
 {
+    if (!isFinishedParsingChildren())
+        return;
     if (usesMenuList())
         return;
-
-    if (RenderObject* renderer = this->renderer())
-        toRenderListBox(renderer)->selectionChanged();
+    scrollTo(activeSelectionEndListIndex());
+    if (AXObjectCache* cache = document().existingAXObjectCache())
+        cache->selectedChildrenChanged(this);
 }
 
 void HTMLSelectElement::setOptionsChangedOnRenderer()
@@ -715,8 +735,6 @@ void HTMLSelectElement::setOptionsChangedOnRenderer()
     if (RenderObject* renderer = this->renderer()) {
         if (usesMenuList())
             toRenderMenuList(renderer)->setOptionsChanged(true);
-        else
-            toRenderListBox(renderer)->setOptionsChanged(true);
     }
 }
 
@@ -858,9 +876,19 @@ void HTMLSelectElement::setSuggestedIndex(int suggestedIndex)
 
     if (RenderObject* renderer = this->renderer())  {
         renderer->updateFromElement();
-        if (renderer->isListBox())
-            toRenderListBox(renderer)->scrollToRevealElementAtListIndex(suggestedIndex);
+        scrollTo(suggestedIndex);
     }
+}
+
+void HTMLSelectElement::scrollTo(int listIndex)
+{
+    if (listIndex < 0)
+        return;
+    const WillBeHeapVector<RawPtrWillBeMember<HTMLElement> >& items = listItems();
+    int listSize = static_cast<int>(items.size());
+    if (listIndex >= listSize)
+        return;
+    items[listIndex]->scrollIntoViewIfNeeded(false);
 }
 
 void HTMLSelectElement::optionSelectionStateChanged(HTMLOptionElement* option, bool optionIsSelected)
@@ -872,6 +900,17 @@ void HTMLSelectElement::optionSelectionStateChanged(HTMLOptionElement* option, b
         selectOption(-1);
     else
         selectOption(nextSelectableListIndex(-1));
+}
+
+void HTMLSelectElement::optionRemoved(const HTMLOptionElement& option)
+{
+    if (m_activeSelectionAnchorIndex < 0 && m_activeSelectionEndIndex < 0)
+        return;
+    int listIndex = optionToListIndex(option.index());
+    if (listIndex <= m_activeSelectionAnchorIndex)
+        m_activeSelectionAnchorIndex--;
+    if (listIndex <= m_activeSelectionEndIndex)
+        m_activeSelectionEndIndex--;
 }
 
 void HTMLSelectElement::selectOption(int optionIndex, SelectOptionFlags flags)
@@ -909,10 +948,12 @@ void HTMLSelectElement::selectOption(int optionIndex, SelectOptionFlags flags)
         if (flags & DispatchInputAndChangeEvent)
             dispatchInputAndChangeEventForMenuList();
         if (RenderObject* renderer = this->renderer()) {
-            if (usesMenuList())
+            if (usesMenuList()) {
                 toRenderMenuList(renderer)->didSetSelectedIndex(listIndex);
-            else if (renderer->isListBox())
-                toRenderListBox(renderer)->selectionChanged();
+            } else if (renderer->isListBox()) {
+                if (AXObjectCache* cache = document().existingAXObjectCache())
+                    cache->selectedChildrenChanged(this);
+            }
         }
     }
 
@@ -1053,11 +1094,10 @@ void HTMLSelectElement::restoreFormControlState(const FormControlState& state)
 
 void HTMLSelectElement::parseMultipleAttribute(const AtomicString& value)
 {
-    bool oldUsesMenuList = usesMenuList();
     m_multiple = !value.isNull();
     setNeedsValidityCheck();
-    if (oldUsesMenuList != usesMenuList())
-        lazyReattachIfAttached();
+
+    lazyReattachIfAttached();
 }
 
 bool HTMLSelectElement::appendFormData(FormDataList& list, bool)
@@ -1317,6 +1357,40 @@ void HTMLSelectElement::updateSelectedState(int listIndex, bool multi, bool shif
     updateListBoxSelection(!multiSelect);
 }
 
+int HTMLSelectElement::listIndexForEventTargetOption(const Event& event)
+{
+    Node* targetNode = event.target()->toNode();
+    if (!targetNode || !isHTMLOptionElement(*targetNode))
+        return -1;
+    return listIndexForOption(toHTMLOptionElement(*targetNode));
+}
+
+int HTMLSelectElement::listIndexForOption(const HTMLOptionElement& option)
+{
+    const WillBeHeapVector<RawPtrWillBeMember<HTMLElement> >& items = this->listItems();
+    size_t length = items.size();
+    for (size_t i = 0; i < length; ++i) {
+        if (items[i].get() == &option)
+            return i;
+    }
+    return -1;
+}
+
+AutoscrollController* HTMLSelectElement::autoscrollController() const
+{
+    if (Page* page = document().page())
+        return &page->autoscrollController();
+    return 0;
+}
+
+void HTMLSelectElement::handleMouseRelease()
+{
+    // We didn't start this click/drag on any options.
+    if (m_lastOnChangeSelection.isEmpty())
+        return;
+    listBoxOnChange();
+}
+
 void HTMLSelectElement::listBoxDefaultEventHandler(Event* event)
 {
     const WillBeHeapVector<RawPtrWillBeMember<HTMLElement> >& listItems = this->listItems();
@@ -1328,8 +1402,7 @@ void HTMLSelectElement::listBoxDefaultEventHandler(Event* event)
 
         // Convert to coords relative to the list box if needed.
         GestureEvent& gestureEvent = toGestureEvent(*event);
-        IntPoint localOffset = roundedIntPoint(renderer()->absoluteToLocal(gestureEvent.absoluteLocation(), UseTransforms));
-        int listIndex = toRenderListBox(renderer())->listIndexAtOffset(toIntSize(localOffset));
+        int listIndex = listIndexForEventTargetOption(gestureEvent);
         if (listIndex >= 0) {
             if (!isDisabledFormControl())
                 updateSelectedState(listIndex, true, gestureEvent.shiftKey());
@@ -1343,8 +1416,7 @@ void HTMLSelectElement::listBoxDefaultEventHandler(Event* event)
 
         // Convert to coords relative to the list box if needed.
         MouseEvent* mouseEvent = toMouseEvent(event);
-        IntPoint localOffset = roundedIntPoint(renderer()->absoluteToLocal(mouseEvent->absoluteLocation(), UseTransforms));
-        int listIndex = toRenderListBox(renderer())->listIndexAtOffset(toIntSize(localOffset));
+        int listIndex = listIndexForEventTargetOption(*mouseEvent);
         if (listIndex >= 0) {
             if (!isDisabledFormControl()) {
 #if OS(MACOSX)
@@ -1358,13 +1430,15 @@ void HTMLSelectElement::listBoxDefaultEventHandler(Event* event)
 
             event->setDefaultHandled();
         }
-    } else if (event->type() == EventTypeNames::mousemove && event->isMouseEvent() && !toRenderBox(renderer())->canBeScrolledAndHasScrollableArea()) {
+    } else if (event->type() == EventTypeNames::mousemove && event->isMouseEvent()) {
         MouseEvent* mouseEvent = toMouseEvent(event);
         if (mouseEvent->button() != LeftButton || !mouseEvent->buttonDown())
             return;
 
-        IntPoint localOffset = roundedIntPoint(renderer()->absoluteToLocal(mouseEvent->absoluteLocation(), UseTransforms));
-        int listIndex = toRenderListBox(renderer())->listIndexAtOffset(toIntSize(localOffset));
+        if (Page* page = document().page())
+            page->autoscrollController().startAutoscrollForSelection(renderer());
+
+        int listIndex = listIndexForEventTargetOption(*mouseEvent);
         if (listIndex >= 0) {
             if (!isDisabledFormControl()) {
                 if (m_multiple) {
@@ -1381,11 +1455,11 @@ void HTMLSelectElement::listBoxDefaultEventHandler(Event* event)
                 }
             }
         }
-    } else if (event->type() == EventTypeNames::mouseup && event->isMouseEvent() && toMouseEvent(event)->button() == LeftButton && renderer() && !toRenderBox(renderer())->autoscrollInProgress()) {
-        // We didn't start this click/drag on any options.
-        if (m_lastOnChangeSelection.isEmpty())
-            return;
-        listBoxOnChange();
+    } else if (event->type() == EventTypeNames::mouseup && event->isMouseEvent() && toMouseEvent(event)->button() == LeftButton && renderer()) {
+        if (document().page() && document().page()->autoscrollController().autoscrollInProgress(toRenderBox(renderer())))
+            document().page()->autoscrollController().stopAutoscroll();
+        else
+            handleMouseRelease();
     } else if (event->type() == EventTypeNames::keydown) {
         if (!event->isKeyboardEvent())
             return;
@@ -1460,7 +1534,7 @@ void HTMLSelectElement::listBoxDefaultEventHandler(Event* event)
                 setActiveSelectionAnchorIndex(m_activeSelectionEndIndex);
             }
 
-            toRenderListBox(renderer())->scrollToRevealElementAtListIndex(endIndex);
+            scrollTo(endIndex);
             if (selectNewItem) {
                 updateListBoxSelection(deselectOthers);
                 listBoxOnChange();
@@ -1611,6 +1685,7 @@ void HTMLSelectElement::finishParsingChildren()
 {
     HTMLFormControlElementWithState::finishParsingChildren();
     updateListItemSelectedStates();
+    scrollToSelection();
 }
 
 bool HTMLSelectElement::anonymousIndexedSetter(unsigned index, PassRefPtrWillBeRawPtr<HTMLOptionElement> value, ExceptionState& exceptionState)
@@ -1644,6 +1719,26 @@ void HTMLSelectElement::trace(Visitor* visitor)
     visitor->trace(m_listItems);
 #endif
     HTMLFormControlElementWithState::trace(visitor);
+}
+
+void HTMLSelectElement::didAddUserAgentShadowRoot(ShadowRoot& root)
+{
+    RefPtrWillBeRawPtr<HTMLContentElement> content = HTMLContentElement::create(document());
+    content->setAttribute(selectAttr, "option,optgroup");
+    root.appendChild(content);
+}
+
+HTMLOptionElement* HTMLSelectElement::spatialNavigationFocusedOption()
+{
+    if (!isSpatialNavigationEnabled(document().frame()))
+        return nullptr;
+    int focusedIndex = activeSelectionEndListIndex();
+    if (focusedIndex < 0)
+        focusedIndex = firstSelectableListIndex();
+    if (focusedIndex < 0)
+        return nullptr;
+    HTMLElement* focused = listItems()[focusedIndex];
+    return isHTMLOptionElement(focused) ? toHTMLOptionElement(focused) : nullptr;
 }
 
 } // namespace

commit 11a8c8f12fa5940eb72953b8f1e07038a63a1a9e

Regression: Add back support for hr elements in select listbox
Regression caused by r178354.

BUG=346582

Review URL: https://codereview.chromium.org/801283003

git-svn-id: svn://svn.chromium.org/blink/trunk@187221 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/css/html.css
@@ -815,6 +815,9 @@ select:-internal-list-box option:checked:disabled {
     color: gray !important;
 }
 
+select:-internal-list-box hr {
+    border-style: none;
+}
 
 output {
     display: inline;

third_party/WebKit/Source/core/html/HTMLOptGroupElement.cpp
@@ -156,7 +156,7 @@ void HTMLOptGroupElement::didAddUserAgentShadowRoot(ShadowRoot& root)
     root.appendChild(label);
 
     RefPtrWillBeRawPtr<HTMLContentElement> content = HTMLContentElement::create(document());
-    content->setAttribute(selectAttr, "option,optgroup");
+    content->setAttribute(selectAttr, "option,optgroup,hr");
     root.appendChild(content);
 }
 

third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
@@ -1774,7 +1774,7 @@ void HTMLSelectElement::trace(Visitor* visitor)
 void HTMLSelectElement::didAddUserAgentShadowRoot(ShadowRoot& root)
 {
     RefPtrWillBeRawPtr<HTMLContentElement> content = HTMLContentElement::create(document());
-    content->setAttribute(selectAttr, "option,optgroup");
+    content->setAttribute(selectAttr, "option,optgroup,hr");
     root.appendChild(content);
 }
 

third_party/WebKit/LayoutTests/TestExpectations
@@ -71,6 +71,8 @@ crbug.com/437743 virtual/slimmingpaint/fast/block/float/overhanging-tall-block.h
 crbug.com/437743 [ Mac ] virtual/slimmingpaint/fast/block/positioning/inline-block-relposition.html [ ImageOnlyFailure ]
 crbug.com/437743 [ Mac ] virtual/slimmingpaint/fast/block/margin-collapse/103.html [ Failure ImageOnlyFailure ]
 
+crbug.com/346582 fast/forms/select/listbox-appearance-separator.html [ NeedsRebaseline ]
+
 # Text::inDocument() returns false but should not.
 crbug.com/264138 dom/xhtml/level3/core/nodecomparedocumentposition38.xhtml [ Failure ]
 

third_party/WebKit/LayoutTests/fast/forms/select/listbox-appearance-separator.html
@@ -1,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+<script>
+window.enablePixelTesting = true;
+</script>
+<script src="../../../resources/js-test.js"></script>
+</head>
+<body>
+<script>
+if (window.testRunner)
+    testRunner.setUseMockTheme(false);
+</script>
+
+<p>There should be a separator between two selected options.</p>
+
+<select multiple id="listbox"></select>
+
+<script>
+var listbox = document.getElementById('listbox');
+listbox.appendChild(new Option('foo', 'foo', true, true));
+listbox.appendChild(document.createElement('hr'));
+listbox.appendChild(new Option('bar', 'bar', true, true));
+</script>
+
+</body>

third_party/WebKit/LayoutTests/fast/forms/suggested-value-expected.txt
@@ -79,7 +79,7 @@ instead of "initial value".
 |       "TX"
 |   <shadow:root>
 |     <content>
-|       select="option,optgroup"
+|       select="option,optgroup,hr"
 | "input.value: initial value"
 | "internals.suggestedValue(input): suggested value"
 | "input.selectionStart: 0"

third_party/WebKit/LayoutTests/platform/linux/fast/forms/suggested-value-expected.txt
@@ -79,7 +79,7 @@ instead of "initial value".
 |       "TX"
 |   <shadow:root>
 |     <content>
-|       select="option,optgroup"
+|       select="option,optgroup,hr"
 | "input.value: initial value"
 | "internals.suggestedValue(input): suggested value"
 | "input.selectionStart: 0"

third_party/WebKit/LayoutTests/platform/win/fast/forms/suggested-value-expected.txt
@@ -79,7 +79,7 @@ instead of "initial value".
 |       "TX"
 |   <shadow:root>
 |     <content>
-|       select="option,optgroup"
+|       select="option,optgroup,hr"
 | "input.value: initial value"
 | "internals.suggestedValue(input): suggested value"
 | "input.selectionStart: 0"