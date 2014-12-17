4ec0420b5835cfc425207814621e7760790dd764
huangs <huangs@google.com>
2014-12-17 13:42:27 -0500

    [Local NTP] Rerender all tiles on theme change.
    Main changes:
    - Extract renderAllTiles() from onMostVisitedChange() for semantic correctness.
    - Calling renderAllTiles() (instead of renderAndShowTiles()) on theme change.
    
    BUG=436666
    
    Review URL: https://codereview.chromium.org/756503003
    
    Cr-Commit-Position: refs/heads/master@{#306482}
    (cherry picked from commit b81089b926a576938d3798056c932b815f047056)
    
    Conflicts:
    	chrome/browser/resources/local_ntp/local_ntp.js
    
    R=mathp@chromium.org
    
    Review URL: https://codereview.chromium.org/808193002
    
    Cr-Commit-Position: refs/branch-heads/2214@{#315}
    Cr-Branched-From: 03655fd3f6d72165dc3c9bd2c89807305316fe6c-refs/heads/master@{#303346}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -176,8 +176,7 @@ var focusedIframe = null;
 
 /**
  * True if a page has been blacklisted and we're waiting on the
- * onmostvisitedchange callback. See onMostVisitedChange() for how this
- * is used.
+ * onmostvisitedchange callback. See renderAllTiles() for how this is used.
  * @type {boolean}
  */
 var isBlacklisting = false;
@@ -193,7 +192,7 @@ var numColumnsShown = 0;
 
 /**
  * A flag to indicate Most Visited changed caused by user action. If true, then
- * in onMostVisitedChange() tiles remain visible so no flickering occurs.
+ * in renderAllTiles() tiles remain visible so no flickering occurs.
  * @type {boolean}
  */
 var userInitiatedMostVisitedChange = false;
@@ -380,7 +379,7 @@ function renderTheme() {
 function onThemeChange() {
   renderTheme();
   tilesContainer.innerHTML = '';
-  renderAndShowTiles();
+  renderAllTiles();
 }
 
 
@@ -495,9 +494,17 @@ function convertToRGBAColor(color) {
 
 
 /**
- * Handles a new set of Most Visited page data.
+ * Called when page data change.
  */
 function onMostVisitedChange() {
+  renderAllTiles();
+}
+
+
+/**
+ * Rerenders all tiles based on Most Visited page data.
+ */
+function renderAllTiles() {
   if (isBlacklisting) {
     // Trigger the blacklist animation, which then triggers reloadAllTiles().
     var lastBlacklistedTileElem = lastBlacklistedTile.elem;
@@ -521,8 +528,8 @@ function blacklistAnimationDone() {
   lastBlacklistedTile.elem.removeEventListener(
       'webkitTransitionEnd', blacklistAnimationDone);
   // Need to call explicitly to re-render the tiles, since the initial
-  // onmostvisitedchange issued by the blacklist function only triggered
-  // the animation.
+  // renderAllTiles() issued by the blacklist function only triggered the
+  // animation.
   reloadAllTiles();
 }
 
@@ -1135,7 +1142,7 @@ function init() {
     onInputStart();
 
   renderTheme();
-  onMostVisitedChange();
+  renderAllTiles();
 
   searchboxApiHandle = topLevelHandle.searchBox;
 
