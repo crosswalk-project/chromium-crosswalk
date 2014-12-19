c7fc7fc7c09a084ac61b2d2620870279957ceef0
Kibeom Kim <kkimlabs@chromium.org>
2014-12-19 12:43:14 -0800

    M40 branch Header file inlucde compile error fix.
    base/metrics/histogram_macros.h didn't exist on M40 and it was
    base/metrics/histogram.h
    
    Review URL: https://codereview.chromium.org/817023002
    
    Cr-Commit-Position: refs/branch-heads/2214@{#344}
    Cr-Branched-From: 03655fd3f6d72165dc3c9bd2c89807305316fe6c-refs/heads/master@{#303346}

--- chrome/browser/android/appmenu/app_menu_drag_helper.cc
+++ chrome/browser/android/appmenu/app_menu_drag_helper.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram.h"
 #include "chrome/browser/android/appmenu/app_menu_drag_helper.h"
 #include "jni/AppMenuDragHelper_jni.h"
 
