79858cd2684d8c4e7b0c5cb4f6dafc89649012cf
Newton Allen <newt@chromium.org>
2015-01-08 13:42:06 -0800

    Wrap ContentView in a parent view to fix infobar accessibility.
    Previously, it was impossible to select infobars when using explore by
    touch and traversing through the views using forward and backward
    swipes. The problem was that ContentView contained both the
    InfoBarContainer (a real View) as well as virtual views for all the
    elements in the webpage -- but Android doesn't support ViewGroups that
    contain both real and virtual views.
    
    The fix is to add a FrameLayout, which is now a common parent of both
    the ContentView and the InfoBarContainer. Here's the new view hierarchy:
    
      CompositorViewHolder
               |
          FrameLayout
           |        \
      ContentView  InfoBarContainer
    
    Tab.getView() now returns the parent FrameLayout, rather than the
    ContentView itself.
    
    BUG=416663
    
    Review URL: https://codereview.chromium.org/821393004
    
    Cr-Commit-Position: refs/heads/master@{#310125}
    (cherry picked from commit 2f93e319a2c8b6a047d1ec6fd974b381681a916d)
    
    Review URL: https://codereview.chromium.org/842813005
    
    Cr-Commit-Position: refs/branch-heads/2214@{#409}
    Cr-Branched-From: 03655fd3f6d72165dc3c9bd2c89807305316fe6c-refs/heads/master@{#303346}

--- chrome/android/java/src/org/chromium/chrome/browser/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/Tab.java
@@ -10,6 +10,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.view.ContextMenu;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import org.chromium.base.CalledByNative;
 import org.chromium.base.ObserverList;
@@ -113,6 +114,9 @@ public class Tab {
      * The {@link ContentViewCore} showing the current page or {@code null} if the tab is frozen.
      */
     private ContentViewCore mContentViewCore;
+
+    /** The parent view of the ContentView and the InfoBarContainer. */
+    private FrameLayout mContentViewParent;
 
     /**
      * A list of Tab observers.  These are used to broadcast Tab events to listeners.
@@ -502,8 +506,7 @@ public class Tab {
      *         This can be {@code null}, if the tab is frozen or being initialized or destroyed.
      */
     public View getView() {
-        return mNativePage != null ? mNativePage.getView() :
-                (mContentViewCore != null ? mContentViewCore.getContainerView() : null);
+        return mNativePage != null ? mNativePage.getView() : mContentViewParent;
     }
 
     /**
@@ -835,6 +838,18 @@ public class Tab {
 
         mContentViewCore = cvc;
 
+        // Wrap the ContentView in a FrameLayout, which will contain both the ContentView and the
+        // InfoBarContainer. The alternative -- placing the InfoBarContainer inside the ContentView
+        // -- causes problems since then the ContentView would contain both real views (the
+        // infobars) and virtual views (the web page elements), which breaks Android accessibility.
+        // http://crbug.com/416663
+        if (mContentViewParent != null) {
+            assert false;
+            mContentViewParent.removeAllViews();
+        }
+        mContentViewParent = new FrameLayout(mContext);
+        mContentViewParent.addView(cvc.getContainerView());
+
         mWebContentsDelegate = createWebContentsDelegate();
         mWebContentsObserver = new TabWebContentsObserver(mContentViewCore.getWebContents());
         mVoiceSearchTabHelper = new VoiceSearchTabHelper(mContentViewCore.getWebContents());
@@ -854,9 +869,9 @@ public class Tab {
             WebContents webContents = mContentViewCore.getWebContents();
             mInfoBarContainer = new InfoBarContainer(
                     (Activity) mContext, createAutoLoginProcessor(), getId(),
-                    mContentViewCore.getContainerView(), webContents);
+                    mContentViewParent, webContents);
         } else {
-            mInfoBarContainer.onParentViewChanged(getId(), mContentViewCore.getContainerView());
+            mInfoBarContainer.onParentViewChanged(getId(), mContentViewParent);
         }
 
         if (AppBannerManager.isEnabled() && mAppBannerManager == null) {
@@ -1009,9 +1024,8 @@ public class Tab {
 
         destroyContentViewCoreInternal(mContentViewCore);
 
-        if (mInfoBarContainer != null && mInfoBarContainer.getParent() != null) {
-            mInfoBarContainer.removeFromParentView();
-        }
+        mContentViewParent.removeAllViews();
+        mContentViewParent = null;
         mContentViewCore.destroy();
 
         mContentViewCore = null;
