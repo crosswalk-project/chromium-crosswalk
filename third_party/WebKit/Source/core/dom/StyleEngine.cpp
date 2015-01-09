crosswalk-project/chromium-crosswalk

commit 8252b1778a734627b753dccac084d46556ea8cab

Improve performance of processStyleSheet.
Beause of StyleEngine::insertTreeScopeInDocumentOrder, processStyleSheet is slow.
Changed the way to insert a given treescope into m_activeTreeScopes.
The old code used linear search to find where the treescope is inserted into. Now, binary search.

BUG=433225

Review URL: https://codereview.chromium.org/763163004

git-svn-id: svn://svn.chromium.org/blink/trunk@188121 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/dom/StyleEngine.cpp
@@ -117,31 +117,56 @@ inline Document* StyleEngine::master()
     return import->master();
 }
 
-void StyleEngine::insertTreeScopeInDocumentOrder(TreeScopeSet& treeScopes, TreeScope* treeScope)
+void StyleEngine::OrderedTreeScopeSet::insert(TreeScope* treeScope)
 {
-    if (treeScopes.isEmpty()) {
-        treeScopes.add(treeScope);
+    if (m_treeScopes.isEmpty()) {
+        m_treeScopes.append(treeScope);
+        m_hash.add(treeScope);
         return;
     }
-    if (treeScopes.contains(treeScope))
+    if (m_hash.contains(treeScope))
         return;
 
-    TreeScopeSet::iterator begin = treeScopes.begin();
-    TreeScopeSet::iterator end = treeScopes.end();
-    TreeScopeSet::iterator it = end;
-    TreeScope* followingTreeScope = 0;
-    do {
-        --it;
-        TreeScope* n = *it;
-        unsigned short position = n->comparePosition(*treeScope);
-        if (position & Node::DOCUMENT_POSITION_FOLLOWING) {
-            treeScopes.insertBefore(followingTreeScope, treeScope);
-            return;
-        }
-        followingTreeScope = n;
-    } while (it != begin);
+    int end = m_treeScopes.size() - 1;
+    int start = 0;
+    int position = 0;
+    unsigned result = 0;
 
-    treeScopes.insertBefore(followingTreeScope, treeScope);
+    while (start <= end) {
+        position = (start + end) / 2;
+        result = m_treeScopes[position]->comparePosition(*treeScope);
+
+        if (result & Node::DOCUMENT_POSITION_PRECEDING) {
+            end = position - 1;
+        } else {
+            ASSERT(result & Node::DOCUMENT_POSITION_FOLLOWING);
+            start = position + 1;
+        }
+    }
+
+    if (result & Node::DOCUMENT_POSITION_FOLLOWING) {
+        ++position;
+        ASSERT(static_cast<size_t>(position) == m_treeScopes.size() || (m_treeScopes[position]->comparePosition(*treeScope) & Node::DOCUMENT_POSITION_PRECEDING));
+    }
+    m_treeScopes.insert(position, treeScope);
+    m_hash.add(treeScope);
+
+#if ENABLE(ASSERT)
+    // Check whether m_treeScopes is sorted in document order or not.
+    for (unsigned i = 0; i < m_treeScopes.size() - 1; ++i) {
+        unsigned result = m_treeScopes[i]->comparePosition(*m_treeScopes[i + 1]);
+        ASSERT(result & Node::DOCUMENT_POSITION_FOLLOWING);
+    }
+#endif
+}
+
+void StyleEngine::OrderedTreeScopeSet::remove(TreeScope* treeScope)
+{
+    if (!m_hash.contains(treeScope))
+        return;
+    size_t position = m_treeScopes.find(treeScope);
+    m_treeScopes.remove(position);
+    m_hash.remove(treeScope);
 }
 
 TreeScopeStyleSheetCollection* StyleEngine::ensureStyleSheetCollectionFor(TreeScope& treeScope)
@@ -299,7 +324,7 @@ void StyleEngine::addStyleSheetCandidateNode(Node* node, bool createdByParser)
 
     markTreeScopeDirty(treeScope);
     if (treeScope != m_document)
-        insertTreeScopeInDocumentOrder(m_activeTreeScopes, &treeScope);
+        m_activeTreeScopes.insert(&treeScope);
 }
 
 void StyleEngine::removeStyleSheetCandidateNode(Node* node)
@@ -345,9 +370,9 @@ bool StyleEngine::shouldUpdateShadowTreeStyleSheetCollection(StyleResolverUpdate
     return !m_dirtyTreeScopes.isEmpty() || updateMode == FullStyleUpdate;
 }
 
-void StyleEngine::clearMediaQueryRuleSetOnTreeScopeStyleSheets(TreeScopeSet treeScopes)
+void StyleEngine::clearMediaQueryRuleSetOnTreeScopeStyleSheets(UnorderedTreeScopeSet::iterator begin, UnorderedTreeScopeSet::iterator end)
 {
-    for (TreeScopeSet::iterator it = treeScopes.begin(); it != treeScopes.end(); ++it) {
+    for (UnorderedTreeScopeSet::iterator it = begin; it != end; ++it) {
         TreeScope& treeScope = **it;
         ASSERT(treeScope != m_document);
         ShadowTreeStyleSheetCollection* collection = static_cast<ShadowTreeStyleSheetCollection*>(styleSheetCollectionFor(treeScope));
@@ -359,8 +384,8 @@ void StyleEngine::clearMediaQueryRuleSetOnTreeScopeStyleSheets(TreeScopeSet tree
 void StyleEngine::clearMediaQueryRuleSetStyleSheets()
 {
     documentStyleSheetCollection()->clearMediaQueryRuleSetStyleSheets();
-    clearMediaQueryRuleSetOnTreeScopeStyleSheets(m_activeTreeScopes);
-    clearMediaQueryRuleSetOnTreeScopeStyleSheets(m_dirtyTreeScopes);
+    clearMediaQueryRuleSetOnTreeScopeStyleSheets(m_activeTreeScopes.beginUnordered(), m_activeTreeScopes.endUnordered());
+    clearMediaQueryRuleSetOnTreeScopeStyleSheets(m_dirtyTreeScopes.begin(), m_dirtyTreeScopes.end());
 }
 
 void StyleEngine::updateStyleSheetsInImport(DocumentStyleSheetCollector& parentCollector)
@@ -370,6 +395,20 @@ void StyleEngine::updateStyleSheetsInImport(DocumentStyleSheetCollector& parentC
     ImportedDocumentStyleSheetCollector subcollector(parentCollector, sheetsForList);
     documentStyleSheetCollection()->collectStyleSheets(this, subcollector);
     documentStyleSheetCollection()->swapSheetsForSheetList(sheetsForList);
+}
+
+void StyleEngine::updateActiveStyleSheetsInShadow(StyleResolverUpdateMode updateMode, TreeScope* treeScope, UnorderedTreeScopeSet& treeScopesRemoved)
+{
+    ASSERT(treeScope != m_document);
+    ShadowTreeStyleSheetCollection* collection = static_cast<ShadowTreeStyleSheetCollection*>(styleSheetCollectionFor(*treeScope));
+    ASSERT(collection);
+    collection->updateActiveStyleSheets(this, updateMode);
+    if (!collection->hasStyleSheetCandidateNodes()) {
+        treeScopesRemoved.add(treeScope);
+        // When removing TreeScope from ActiveTreeScopes,
+        // its resolver should be destroyed by invoking resetAuthorStyle.
+        ASSERT(!treeScope->scopedStyleResolver());
+    }
 }
 
 void StyleEngine::updateActiveStyleSheets(StyleResolverUpdateMode updateMode)
@@ -384,23 +423,18 @@ void StyleEngine::updateActiveStyleSheets(StyleResolverUpdateMode updateMode)
         documentStyleSheetCollection()->updateActiveStyleSheets(this, updateMode);
 
     if (shouldUpdateShadowTreeStyleSheetCollection(updateMode)) {
-        TreeScopeSet treeScopes = updateMode == FullStyleUpdate ? m_activeTreeScopes : m_dirtyTreeScopes;
-        HashSet<TreeScope*> treeScopesRemoved;
+        UnorderedTreeScopeSet treeScopesRemoved;
 
-        for (TreeScopeSet::iterator it = treeScopes.begin(); it != treeScopes.end(); ++it) {
-            TreeScope* treeScope = *it;
-            ASSERT(treeScope != m_document);
-            ShadowTreeStyleSheetCollection* collection = static_cast<ShadowTreeStyleSheetCollection*>(styleSheetCollectionFor(*treeScope));
-            ASSERT(collection);
-            collection->updateActiveStyleSheets(this, updateMode);
-            if (!collection->hasStyleSheetCandidateNodes()) {
-                treeScopesRemoved.add(treeScope);
-                // When removing TreeScope from ActiveTreeScopes,
-                // its resolver should be destroyed by invoking resetAuthorStyle.
-                ASSERT(!treeScope->scopedStyleResolver());
+        if (updateMode == FullStyleUpdate) {
+            for (unsigned i = 0; i < m_activeTreeScopes.size(); ++i)
+                updateActiveStyleSheetsInShadow(updateMode, m_activeTreeScopes[i], treeScopesRemoved);
+        } else {
+            for (UnorderedTreeScopeSet::iterator it = m_dirtyTreeScopes.begin(); it != m_dirtyTreeScopes.end(); ++it) {
+                updateActiveStyleSheetsInShadow(updateMode, *it, treeScopesRemoved);
             }
         }
-        m_activeTreeScopes.removeAll(treeScopesRemoved);
+        for (UnorderedTreeScopeSet::iterator it = treeScopesRemoved.begin(); it != treeScopesRemoved.end(); ++it)
+            m_activeTreeScopes.remove(*it);
     }
 
     InspectorInstrumentation::activeStyleSheetsUpdated(m_document);
@@ -418,11 +452,9 @@ const WillBeHeapVector<RefPtrWillBeMember<CSSStyleSheet> > StyleEngine::activeSt
     WillBeHeapVector<RefPtrWillBeMember<CSSStyleSheet> > activeStyleSheets;
 
     activeStyleSheets.appendVector(documentStyleSheetCollection()->activeAuthorStyleSheets());
-
-    TreeScopeSet::const_iterator begin = m_activeTreeScopes.begin();
-    TreeScopeSet::const_iterator end = m_activeTreeScopes.end();
-    for (TreeScopeSet::const_iterator it = begin; it != end; ++it) {
-        if (TreeScopeStyleSheetCollection* collection = m_styleSheetCollectionMap.get(*it))
+    for (unsigned i = 0; i < m_activeTreeScopes.size(); ++i) {
+        TreeScope* treeScope = const_cast<TreeScope*>(m_activeTreeScopes[i]);
+        if (TreeScopeStyleSheetCollection* collection = m_styleSheetCollectionMap.get(treeScope))
             activeStyleSheets.appendVector(collection->activeAuthorStyleSheets());
     }
 
@@ -451,11 +483,8 @@ void StyleEngine::appendActiveAuthorStyleSheets()
     ASSERT(isMaster());
 
     m_resolver->appendAuthorStyleSheets(documentStyleSheetCollection()->activeAuthorStyleSheets());
-
-    TreeScopeSet::iterator begin = m_activeTreeScopes.begin();
-    TreeScopeSet::iterator end = m_activeTreeScopes.end();
-    for (TreeScopeSet::iterator it = begin; it != end; ++it) {
-        if (TreeScopeStyleSheetCollection* collection = m_styleSheetCollectionMap.get(*it))
+    for (unsigned i = 0; i < m_activeTreeScopes.size(); ++i) {
+        if (TreeScopeStyleSheetCollection* collection = m_styleSheetCollectionMap.get(m_activeTreeScopes[i]))
             m_resolver->appendAuthorStyleSheets(collection->activeAuthorStyleSheets());
     }
     m_resolver->finishAppendAuthorStyleSheets();
@@ -489,8 +518,8 @@ void StyleEngine::clearResolver()
     // in removeStyleSheetCandidateNode. So we should not invoke
     // treeScope->clearScopedStyleResolver when document is not active.
     if (document().isActive()) {
-        for (auto& treeScope: m_activeTreeScopes)
-            treeScope->clearScopedStyleResolver();
+        for (UnorderedTreeScopeSet::iterator it = m_activeTreeScopes.beginUnordered(); it != m_activeTreeScopes.endUnordered(); ++it)
+            (*it)->clearScopedStyleResolver();
     }
 
     if (m_resolver)
@@ -660,7 +689,8 @@ void StyleEngine::collectScopedStyleFeaturesTo(RuleFeatureSet& features) const
     HashSet<const StyleSheetContents*> visitedSharedStyleSheetContents;
     if (document().scopedStyleResolver())
         document().scopedStyleResolver()->collectFeaturesTo(features, visitedSharedStyleSheetContents);
-    for (auto& treeScope: m_activeTreeScopes) {
+    for (unsigned i = 0; i < m_activeTreeScopes.size(); ++i) {
+        TreeScope* treeScope = const_cast<TreeScope*>(m_activeTreeScopes[i]);
         // When creating StyleResolver, dirty treescopes might not be processed.
         // So some active treescopes might not have a scoped style resolver.
         // In this case, we should skip collectFeatures for the treescopes without

third_party/WebKit/Source/core/dom/StyleEngine.h
@@ -193,9 +193,48 @@ private:
     Document* master();
     Document& document() const { return *m_document; }
 
-    typedef ListHashSet<TreeScope*, 16> TreeScopeSet;
-    static void insertTreeScopeInDocumentOrder(TreeScopeSet&, TreeScope*);
-    void clearMediaQueryRuleSetOnTreeScopeStyleSheets(TreeScopeSet treeScopes);
+    typedef WillBeHeapHashSet<TreeScope*> UnorderedTreeScopeSet;
+
+    // A class which holds document-ordered treescopes which have stylesheets.
+    // ListHashSet allows only sequential access, not random access.
+    // So it gets slow when the size of treescopes gets larger when finding
+    // the best place to insert a treescope into the document-ordered
+    // treescopes (requires linear search).
+    // To solve this, use a vector for the document-ordered treescopes and
+    // use a hashset for quickly checking whether a given treescope is
+    // in the document-ordered treescopes or not.
+    class OrderedTreeScopeSet {
+        WTF_MAKE_NONCOPYABLE(OrderedTreeScopeSet);
+    public:
+        OrderedTreeScopeSet() { }
+
+        void insert(TreeScope*);
+        void remove(TreeScope*);
+
+        // When we don't need to consider document-order, use this iterator.
+        // Otherwise, use [] operator.
+        UnorderedTreeScopeSet::iterator beginUnordered() { return m_hash.begin(); }
+        UnorderedTreeScopeSet::iterator endUnordered() { return m_hash.end(); }
+
+        bool isEmpty() const { return m_treeScopes.isEmpty(); }
+        void clear()
+        {
+            m_treeScopes.clear();
+            m_hash.clear();
+        }
+
+        size_t size() const { return m_treeScopes.size(); }
+
+        TreeScope* operator[](size_t i) { return m_treeScopes[i]; }
+        const TreeScope* operator[](size_t i) const { return m_treeScopes[i]; }
+
+    private:
+        WillBeHeapVector<TreeScope*, 16> m_treeScopes;
+        UnorderedTreeScopeSet m_hash;
+    };
+
+    static void insertTreeScopeInDocumentOrder(OrderedTreeScopeSet&, TreeScope*);
+    void clearMediaQueryRuleSetOnTreeScopeStyleSheets(UnorderedTreeScopeSet::iterator begin, UnorderedTreeScopeSet::iterator end);
 
     void createResolver();
 
@@ -210,6 +249,8 @@ private:
     {
         return m_documentStyleSheetCollection.get();
     }
+
+    void updateActiveStyleSheetsInShadow(StyleResolverUpdateMode, TreeScope*, UnorderedTreeScopeSet& treeScopesRemoved);
 
     RawPtrWillBeMember<Document> m_document;
     bool m_isMaster;
@@ -231,8 +272,8 @@ private:
     StyleSheetCollectionMap m_styleSheetCollectionMap;
 
     bool m_documentScopeDirty;
-    TreeScopeSet m_dirtyTreeScopes;
-    TreeScopeSet m_activeTreeScopes;
+    UnorderedTreeScopeSet m_dirtyTreeScopes;
+    OrderedTreeScopeSet m_activeTreeScopes;
 
     String m_preferredStylesheetSetName;
     String m_selectedStylesheetSetName;

commit 1df74b70951566aef06b487992a4cca7fddc3255

Fix oilpan build error caused by r188121
OrderedTreeScopeSet is not correctly implemented for oilpan build.

c.f. https://src.chromium.org/viewvc/blink?view=rev&revision=188121

BUG=
NOTRY=true

Review URL: https://codereview.chromium.org/830423009

git-svn-id: svn://svn.chromium.org/blink/trunk@188125 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/dom/StyleEngine.cpp
@@ -169,6 +169,14 @@ void StyleEngine::OrderedTreeScopeSet::remove(TreeScope* treeScope)
     m_hash.remove(treeScope);
 }
 
+void StyleEngine::OrderedTreeScopeSet::trace(Visitor* visitor)
+{
+#if ENABLE(OILPAN)
+    visitor->trace(m_treeScopes);
+    visitor->trace(m_hash);
+#endif
+}
+
 TreeScopeStyleSheetCollection* StyleEngine::ensureStyleSheetCollectionFor(TreeScope& treeScope)
 {
     if (treeScope == m_document)
@@ -727,6 +735,8 @@ void StyleEngine::trace(Visitor* visitor)
     visitor->trace(m_documentStyleSheetCollection);
     visitor->trace(m_styleSheetCollectionMap);
     visitor->trace(m_resolver);
+    visitor->trace(m_dirtyTreeScopes);
+    visitor->trace(m_activeTreeScopes);
     visitor->trace(m_fontSelector);
     visitor->trace(m_textToSheetCache);
     visitor->trace(m_sheetToTextCache);

third_party/WebKit/Source/core/dom/StyleEngine.h
@@ -193,7 +193,7 @@ private:
     Document* master();
     Document& document() const { return *m_document; }
 
-    typedef WillBeHeapHashSet<TreeScope*> UnorderedTreeScopeSet;
+    typedef WillBeHeapHashSet<RawPtrWillBeMember<TreeScope>> UnorderedTreeScopeSet;
 
     // A class which holds document-ordered treescopes which have stylesheets.
     // ListHashSet allows only sequential access, not random access.
@@ -203,7 +203,8 @@ private:
     // To solve this, use a vector for the document-ordered treescopes and
     // use a hashset for quickly checking whether a given treescope is
     // in the document-ordered treescopes or not.
-    class OrderedTreeScopeSet {
+    class OrderedTreeScopeSet final {
+        DISALLOW_ALLOCATION();
         WTF_MAKE_NONCOPYABLE(OrderedTreeScopeSet);
     public:
         OrderedTreeScopeSet() { }
@@ -228,8 +229,10 @@ private:
         TreeScope* operator[](size_t i) { return m_treeScopes[i]; }
         const TreeScope* operator[](size_t i) const { return m_treeScopes[i]; }
 
+        void trace(Visitor*);
+
     private:
-        WillBeHeapVector<TreeScope*, 16> m_treeScopes;
+        WillBeHeapVector<RawPtrWillBeMember<TreeScope>, 16> m_treeScopes;
         UnorderedTreeScopeSet m_hash;
     };
 