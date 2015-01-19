819885c80d7f243d6b7337daed09ae254d83229f
Miguel Garcia <miguelg@chromium.org>
2015-01-19 14:57:11 +0000

    Cherry pick into M41 commit e10a825e60bf64c494acc37ebf66868309d86062
    "Original CL description:
    Remove thread jumping from the geolocation permission context.
    
    This fixes a crash in the browser when the context is freed before from the
    callback.
    
    We used to jump to the blocking pool to avoid strict mode violation on startup
    but the code has been refactored since then and we no longer hit such strict mode violation.
    "
    
    BUG=447282
    
    Review URL: https://codereview.chromium.org/836963004
    
    Cr-Commit-Position: refs/branch-heads/2272@{#51}
    Cr-Branched-From: 827a380cfdb31aa54c8d56e63ce2c3fd8c3ba4d4-refs/heads/master@{#310958}

--- chrome/browser/geolocation/geolocation_permission_context_android.h
+++ chrome/browser/geolocation/geolocation_permission_context_android.h
@@ -8,9 +8,9 @@
 // The flow for geolocation permissions on Android needs to take into account
 // the global geolocation settings so it differs from the desktop one. It
 // works as follows.
-// GeolocationPermissionContextAndroid::DecidePermission intercepts the flow in
-// the UI thread, and posts a task to the blocking pool to CheckSystemLocation.
-// CheckSystemLocation will in fact check several possible settings
+// GeolocationPermissionContextAndroid::RequestPermission intercepts the flow
+// and proceeds to check the system location.
+// This will in fact check several possible settings
 //     - The global system geolocation setting
 //     - The Google location settings on pre KK devices
 //     - An old internal Chrome setting on pre-JB MR1 devices
@@ -20,67 +20,34 @@
 // infobars, etc.).
 //
 // Otherwise the permission is already decided.
-
-// There is a bit of thread jumping since some of the permissions (like the
-// per site settings) are queried on the UI thread while the system level
-// permissions are considered I/O and thus checked in the blocking thread pool.
-
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "chrome/browser/geolocation/geolocation_permission_context.h"
-#include "components/content_settings/core/common/permission_request_id.h"
-#include "url/gurl.h"
 
 namespace content {
 class WebContents;
 }
 
 class GoogleLocationSettingsHelper;
+class GURL;
+class PermissionRequestID;
 
 class GeolocationPermissionContextAndroid
     : public GeolocationPermissionContext {
  public:
   explicit GeolocationPermissionContextAndroid(Profile* profile);
-  virtual ~GeolocationPermissionContextAndroid();
+  ~GeolocationPermissionContextAndroid() override;
 
  private:
-  struct PermissionRequestInfo {
-    PermissionRequestInfo();
-
-    PermissionRequestID id;
-    GURL requesting_origin;
-    GURL embedder_origin;
-    bool user_gesture;
-  };
-
-  // PermissionContextBase:
-  virtual void RequestPermission(
+  // GeolocationPermissionContext:
+  void RequestPermission(
       content::WebContents* web_contents,
        const PermissionRequestID& id,
        const GURL& requesting_frame_origin,
        bool user_gesture,
        const BrowserPermissionCallback& callback) override;
 
-  void CheckMasterLocation(content::WebContents* web_contents,
-                           const PermissionRequestInfo& info,
-                           const BrowserPermissionCallback& callback);
-
-  void ProceedDecidePermission(content::WebContents* web_contents,
-                               const PermissionRequestInfo& info,
-                               base::Callback<void(bool)> callback);
-
-  // Will perform a final check on the system location settings before
-  // granting the permission.
-  void InterceptPermissionCheck(const BrowserPermissionCallback& callback,
-                                bool granted);
 
   scoped_ptr<GoogleLocationSettingsHelper> google_location_settings_helper_;
-  base::WeakPtrFactory<GeolocationPermissionContextAndroid> weak_factory_;
-
- private:
-  void CheckSystemLocation(content::WebContents* web_contents,
-                           const PermissionRequestInfo& info,
-                           base::Callback<void(bool)> callback);
 
   DISALLOW_COPY_AND_ASSIGN(GeolocationPermissionContextAndroid);
 };
