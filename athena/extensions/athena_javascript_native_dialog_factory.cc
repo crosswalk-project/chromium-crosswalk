cf9cfd4d1bbc7636bb1a5885333ab676229ff5da
Jochen Eisinger <jochen@chromium.org>
2015-01-27 10:19:52 +0100

    Fix athena compilation on 2272
    Port https://codereview.chromium.org/874943002 to the meanwhile delete
    athena/
    
    BUG=452396,450547
    R=marja@chromium.org
    
    Review URL: https://codereview.chromium.org/882623002
    
    Cr-Commit-Position: refs/branch-heads/2272@{#127}
    Cr-Branched-From: 827a380cfdb31aa54c8d56e63ce2c3fd8c3ba4d4-refs/heads/master@{#310958}

--- athena/extensions/athena_javascript_native_dialog_factory.cc
+++ athena/extensions/athena_javascript_native_dialog_factory.cc
@@ -9,6 +9,12 @@
 #include "components/app_modal/javascript_native_dialog_factory.h"
 #include "components/app_modal/views/javascript_app_modal_dialog_views.h"
 #include "components/constrained_window/constrained_window_views.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_delegate.h"
+
+#if defined(USE_AURA)
+#include "ui/aura/window.h"
+#endif
 
 class JavaScriptAppModalDialog;
 class NativeAppModalDialog;
@@ -25,10 +31,21 @@ class AthenaJavaScriptNativeDialogFactory
  private:
   // app_modal::JavScriptNativeDialogFactory:
   app_modal::NativeAppModalDialog* CreateNativeJavaScriptDialog(
-      app_modal::JavaScriptAppModalDialog* dialog,
-      gfx::NativeWindow parent_window) override{
+      app_modal::JavaScriptAppModalDialog* dialog) override {
     app_modal::JavaScriptAppModalDialogViews* d =
         new app_modal::JavaScriptAppModalDialogViews(dialog);
+
+    dialog->web_contents()->GetDelegate()->ActivateContents(
+        dialog->web_contents());
+    gfx::NativeWindow parent_window =
+        dialog->web_contents()->GetTopLevelNativeWindow();
+#if defined(USE_AURA)
+    if (!parent_window->GetRootWindow()) {
+      // When we are part of a WebContents that isn't actually being displayed
+      // on the screen, we can't actually attach to it.
+      parent_window = NULL;
+    }
+#endif
     constrained_window::CreateBrowserModalDialogViews(d, parent_window);
     return d;
   }
