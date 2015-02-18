fab61d207c816514770113fa23095f64b27949ad
Devlin Cronin <rdevlin.cronin@chromium.org>
2015-02-18 14:05:20 -0800

    [Extensions Toolbar Mac] Properly adjust width after icon removal
    The max width for the toolbar is set when an action is removed, but the action
    was still counted in the calculations (since it was still in the underlying
    vector). Fix this.
    
    BUG=454338
    
    Review URL: https://codereview.chromium.org/901303004
    
    Cr-Commit-Position: refs/heads/master@{#315587}
    (cherry picked from commit 2b26eb3ff127a64bcd1c8cf9afb2898fac0616e3)
    
    TBR=avi@chromium.org
    
    Review URL: https://codereview.chromium.org/936863002
    
    Cr-Commit-Position: refs/branch-heads/2272@{#328}
    Cr-Branched-From: 827a380cfdb31aa54c8d56e63ce2c3fd8c3ba4d4-refs/heads/master@{#310958}

--- chrome/browser/ui/cocoa/extensions/browser_actions_controller.mm
+++ chrome/browser/ui/cocoa/extensions/browser_actions_controller.mm
@@ -465,8 +465,7 @@ void ToolbarActionsBarBridge::OnOverflowedActionWantsToRunChanged(
              name:kBrowserActionButtonDraggingNotification
            object:newButton];
 
-  [containerView_ setMaxWidth:
-      toolbarActionsBar_->IconCountToWidth([self buttonCount])];
+  [containerView_ setMaxWidth:toolbarActionsBar_->GetMaximumWidth()];
 }
 
 - (void)redraw {
@@ -521,8 +520,7 @@ void ToolbarActionsBarBridge::OnOverflowedActionWantsToRunChanged(
   [button onRemoved];
   [buttons_ removeObject:button];
 
-  [containerView_ setMaxWidth:
-      toolbarActionsBar_->IconCountToWidth([self buttonCount])];
+  [containerView_ setMaxWidth:toolbarActionsBar_->GetMaximumWidth()];
 }
 
 - (void)removeAllViews {
