a310715cfacd8a71a8f072f801fce144073c8ac3
Raman Tenneti <rtenneti@chromium.org>
2015-03-04 14:25:28 -0800

    QUIC - Add a field trial not to load QUIC server information from the disk cache.
    TBR=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/948143004
    
    Cr-Commit-Position: refs/heads/master@{#317916}
    (cherry picked from commit 34dffe756274e6ddf7c8b6b0d2edbebe69c837f7)
    
    BUG=463992
    
    Review URL: https://codereview.chromium.org/982563005
    
    Cr-Commit-Position: refs/branch-heads/2311@{#135}
    Cr-Branched-From: 09b7de5dd7254947cd4306de907274fa63373d48-refs/heads/master@{#317474}

--- chrome/browser/io_thread.cc
+++ chrome/browser/io_thread.cc
@@ -1053,6 +1053,8 @@ void IOThread::InitializeNetworkSessionParamsFromGlobals(
       &params->quic_enable_truncated_connection_ids);
   globals.quic_enable_connection_racing.CopyToIfSet(
       &params->quic_enable_connection_racing);
+  globals.quic_disable_disk_cache.CopyToIfSet(
+      &params->quic_disable_disk_cache);
   globals.enable_quic_port_selection.CopyToIfSet(
       &params->enable_quic_port_selection);
   globals.quic_max_packet_length.CopyToIfSet(&params->quic_max_packet_length);
@@ -1186,14 +1188,16 @@ void IOThread::ConfigureQuicGlobals(
     }
     float load_server_info_timeout_srtt_multiplier =
         GetQuicLoadServerInfoTimeoutSrttMultiplier(quic_trial_params);
-    globals->quic_enable_truncated_connection_ids.set(
-        ShouldQuicEnableTruncatedConnectionIds(quic_trial_params));
-    globals->quic_enable_connection_racing.set(
-        ShouldQuicEnableConnectionRacing(quic_trial_params));
     if (load_server_info_timeout_srtt_multiplier != 0) {
       globals->quic_load_server_info_timeout_srtt_multiplier.set(
           load_server_info_timeout_srtt_multiplier);
     }
+    globals->quic_enable_truncated_connection_ids.set(
+        ShouldQuicEnableTruncatedConnectionIds(quic_trial_params));
+    globals->quic_enable_connection_racing.set(
+        ShouldQuicEnableConnectionRacing(quic_trial_params));
+    globals->quic_disable_disk_cache.set(
+        ShouldQuicDisableDiskCache(quic_trial_params));
     globals->enable_quic_port_selection.set(
         ShouldEnableQuicPortSelection(command_line));
     globals->quic_connection_options =
@@ -1396,6 +1400,13 @@ bool IOThread::ShouldQuicEnableConnectionRacing(
   return LowerCaseEqualsASCII(
       GetVariationParam(quic_trial_params, "enable_connection_racing"),
       "true");
+}
+
+// static
+bool IOThread::ShouldQuicDisableDiskCache(
+    const VariationParameters& quic_trial_params) {
+  return LowerCaseEqualsASCII(
+      GetVariationParam(quic_trial_params, "disable_disk_cache"), "true");
 }
 
 // static
