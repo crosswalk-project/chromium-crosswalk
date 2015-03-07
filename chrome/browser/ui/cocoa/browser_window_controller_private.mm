121a25804e0a56b25b0b0617a8a3e43cda3afb7f
Andre Santoso <andresantoso@chromium.org>
2015-03-06 16:43:36 -0800

    Mac: Fix crashes related to LocationBarViewMac::GetPageInfoBubblePoint
    There are 2 different crash stacks, both ends up with a null LocationBarViewMac.
    The first one involves showing a permission bubble while in immersive
    fullscreen. Fix it by telling PermissionBubbleCocoa when the parent window
    should change.
    The second one involves animating an infobar while dragging a tab. I was not
    able to reproduce this one, but I think it can be fixed by removing the need
    for InfoBarContainerController to find the LocationBarMac through the window
    controller. It is not a good idea anyway for InfobarContainerController to
    have a direct dependency on BrowserWindowController.
    
    BUG=461541
    TBR=rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/978723003
    
    Cr-Commit-Position: refs/heads/master@{#319368}
    (cherry picked from commit bfd680f92427ceb16c4deaa355a4a590f6d77227)
    
    Review URL: https://codereview.chromium.org/986623005
    
    Cr-Commit-Position: refs/branch-heads/2311@{#171}
    Cr-Branched-From: 09b7de5dd7254947cd4306de907274fa63373d48-refs/heads/master@{#317474}

--- chrome/browser/ui/cocoa/browser_window_controller_private.mm
+++ chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -388,6 +388,8 @@ willPositionSheet:(NSWindow*)sheet
   if (statusBubble_)
     statusBubble_->SwitchParentWindow(destWindow);
 
+  permissionBubbleCocoa_->SwitchParentWindow(destWindow);
+
   // Move the title over.
   [destWindow setTitle:[sourceWindow title]];
 
@@ -920,6 +922,8 @@ willPositionSheet:(NSWindow*)sheet
   [[infoBarContainerController_ view] setFrame:output.infoBarFrame];
   [infoBarContainerController_
       setMaxTopArrowHeight:output.infoBarMaxTopArrowHeight];
+  [infoBarContainerController_
+      setInfobarArrowX:[self locationBarBridge]->GetPageInfoBubblePoint().x];
 
   if (!NSIsEmptyRect(output.downloadShelfFrame))
     [[downloadShelfController_ view] setFrame:output.downloadShelfFrame];
