35956e001311ab7844aa4c6c9c67e8d354b37a19
Steve McKay <smckay@chromium.org>
2015-03-24 13:46:33 -0700

    Add missing line break in details panel scanning message. Use placeholders for all uses of line-break tags.
    BUG=468957
    TEST=manual
    TBR=cpu
    
    Review URL: https://codereview.chromium.org/997173004
    
    Cr-Commit-Position: refs/heads/master@{#321463}
    (cherry picked from commit abbb88e74dcc639c2420cca735905f4f16f0e3f5)
    
    Don't show import buttons when import is disabled, and use correct icons for insufficient space state (were inverted).
    
    BUG=468971
    TEST=manual
    
    Review URL: https://codereview.chromium.org/1020083002
    
    Cr-Commit-Position: refs/heads/master@{#321473}
    (cherry picked from commit 7547b20c9a1a39f5cdf9f44dd0a60b18abd00e94)
    
    TBR=kenobi
    
    Review URL: https://codereview.chromium.org/1022483004
    
    Cr-Commit-Position: refs/branch-heads/2311@{#341}
    Cr-Branched-From: 09b7de5dd7254947cd4306de907274fa63373d48-refs/heads/master@{#317474}

--- ui/file_manager/file_manager/foreground/js/import_controller.js
+++ ui/file_manager/file_manager/foreground/js/import_controller.js
@@ -636,7 +636,7 @@ importer.RuntimeCommandWidget.prototype.update =
 
       this.mainButton_.hidden = false;
       this.sideButton_.hidden = false;
-      this.panelButton_.hidden = false;
+      this.panelButton_.hidden = true;
 
       this.toolbarIcon_.setAttribute('icon', 'autorenew');
       this.statusIcon_.setAttribute('icon', 'autorenew');
@@ -656,10 +656,10 @@ importer.RuntimeCommandWidget.prototype.update =
 
       this.mainButton_.hidden = false;
       this.sideButton_.hidden = false;
-      this.panelButton_.hidden = false;
+      this.panelButton_.hidden = true;
 
-      this.toolbarIcon_.setAttribute('icon', 'image:photo');
-      this.statusIcon_.setAttribute('icon', 'cloud-off');
+      this.toolbarIcon_.setAttribute('icon', 'cloud-off');
+      this.statusIcon_.setAttribute('icon', 'image:photo');
       break;
 
     case importer.ActivityState.NO_MEDIA:
