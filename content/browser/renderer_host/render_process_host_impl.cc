9e86a2cbcf6b8da2ba0832b7bda062ceb54471ea
Shao Changbin <changbin.shao@intel.com>
2015-04-02 16:51:02 +0800

    Fix issue of build error for media shared library.
    The implementation fixed issue of mutual dependency of 'media' and 'content'
    module.

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1371,6 +1371,9 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
 #if defined(OS_CHROMEOS)
     switches::kDisableVaapiAcceleratedVideoEncode,
 #endif
+#if defined(OS_LINUX)
+    switches::kProprietaryCodecLibPath,
+#endif
   };
   renderer_cmd->CopySwitchesFrom(browser_cmd, kSwitchNames,
                                  arraysize(kSwitchNames));
