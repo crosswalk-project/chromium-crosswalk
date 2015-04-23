crosswalk-project/chromium-crosswalk

commit 7ad5d850fd1f14ceb84a853335dae312e59a0078

Oilpan: Consider PartitionAlloc's memory usage when triggering a GC
Oilpan builds sometimes increase peak memory usage because it does not understand the amount of memory used in embedders (e.g., PartitionAlloc) and thus fails in triggering a GC.

This CL fixes the issue by taking into account PartitionAlloc's memory usage when triggering an Oilpan's GC. See this design document (https://docs.google.com/document/d/1QMDr1YDEXdvZXoOC7RuHH6VRU7ot3SpArPlHokoBVPw/edit) for more details.

BUG=474470

Review URL: https://codereview.chromium.org/1063083002

git-svn-id: svn://svn.chromium.org/blink/trunk@194282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -42,6 +42,7 @@
 #include "public/platform/WebScheduler.h"
 #include "public/platform/WebThread.h"
 #include "public/platform/WebTraceLocation.h"
+#include "wtf/Partitions.h"
 #include "wtf/ThreadingPrimitives.h"
 #if ENABLE(GC_PROFILING)
 #include "platform/TracedValue.h"
@@ -500,7 +501,7 @@ Mutex& ThreadState::globalRootsMutex()
     return mutex;
 }
 
-// FIXME: We should improve the GC heuristics.
+// TODO(haraken): We should improve the GC heuristics.
 // These heuristics affect performance significantly.
 bool ThreadState::shouldScheduleIdleGC()
 {
@@ -511,17 +512,19 @@ bool ThreadState::shouldScheduleIdleGC()
     // sweeping. If this thread reaches here before the main thread finishes
     // lazy sweeping, the thread will use the estimated size of the last GC.
     size_t estimatedLiveObjectSize = Heap::estimatedLiveObjectSize();
-    // Schedule an idle GC if more than 512 KB has been allocated since
-    // the last GC and the current memory usage (=allocated + estimated)
-    // is >50% larger than the estimated live memory usage.
     size_t allocatedObjectSize = Heap::allocatedObjectSize();
-    return allocatedObjectSize >= 512 * 1024 && allocatedObjectSize > estimatedLiveObjectSize / 2;
+    // Heap::markedObjectSize() may be underestimated if any thread has not
+    // finished completeSweep().
+    size_t currentObjectSize = allocatedObjectSize + Heap::markedObjectSize() + WTF::Partitions::totalSizeOfCommittedPages();
+    // Schedule an idle GC if the current memory usage is >1MB
+    // and is >50% larger than the estimated live memory usage.
+    return currentObjectSize >= 1024 * 1024 && currentObjectSize > estimatedLiveObjectSize * 3 / 2;
 #else
     return false;
 #endif
 }
 
-// FIXME: We should improve the GC heuristics.
+// TODO(haraken): We should improve the GC heuristics.
 // These heuristics affect performance significantly.
 bool ThreadState::shouldSchedulePreciseGC()
 {
@@ -534,57 +537,53 @@ bool ThreadState::shouldSchedulePreciseGC()
     // sweeping. If this thread reaches here before the main thread finishes
     // lazy sweeping, the thread will use the estimated size of the last GC.
     size_t estimatedLiveObjectSize = Heap::estimatedLiveObjectSize();
-    // Schedule a precise GC if more than 512 KB has been allocated since
-    // the last GC and the current memory usage (=allocated + estimated)
-    // is >50% larger than the estimated live memory usage.
     size_t allocatedObjectSize = Heap::allocatedObjectSize();
-    return allocatedObjectSize >= 512 * 1024 && allocatedObjectSize > estimatedLiveObjectSize / 2;
+    // Heap::markedObjectSize() may be underestimated if any thread has not
+    // finished completeSweep().
+    size_t currentObjectSize = allocatedObjectSize + Heap::markedObjectSize() + WTF::Partitions::totalSizeOfCommittedPages();
+    // Schedule a precise GC if the current memory usage is >1MB
+    // and is >50% larger than the estimated live memory usage.
+    return currentObjectSize >= 1024 * 1024 && currentObjectSize > estimatedLiveObjectSize * 3 / 2;
 #endif
 }
 
-// FIXME: We should improve the GC heuristics.
+// TODO(haraken): We should improve the GC heuristics.
 // These heuristics affect performance significantly.
 bool ThreadState::shouldForceConservativeGC()
 {
     if (UNLIKELY(m_gcForbiddenCount))
         return false;
 
-    if (Heap::isUrgentGCRequested())
-        return true;
-
     // The estimated size is updated when the main thread finishes lazy
     // sweeping. If this thread reaches here before the main thread finishes
     // lazy sweeping, the thread will use the estimated size of the last GC.
     size_t estimatedLiveObjectSize = Heap::estimatedLiveObjectSize();
     size_t allocatedObjectSize = Heap::allocatedObjectSize();
-    if (Heap::markedObjectSize() + allocatedObjectSize >= 300 * 1024 * 1024) {
+    // Heap::markedObjectSize() may be underestimated if any thread has not
+    // finished completeSweep().
+    size_t currentObjectSize = allocatedObjectSize + Heap::markedObjectSize() + WTF::Partitions::totalSizeOfCommittedPages();
+    if (currentObjectSize >= 300 * 1024 * 1024) {
         // If we're consuming too much memory, trigger a conservative GC
         // aggressively. This is a safe guard to avoid OOM.
-        return allocatedObjectSize > estimatedLiveObjectSize / 2;
+        return currentObjectSize > estimatedLiveObjectSize * 3 / 2;
     }
-    // Schedule a conservative GC if more than 32 MB has been allocated since
-    // the last GC and the current memory usage (=allocated + estimated)
-    // is >500% larger than the estimated live memory usage.
-    return allocatedObjectSize >= 32 * 1024 * 1024 && allocatedObjectSize > 4 * estimatedLiveObjectSize;
+    // Schedule a conservative GC if the current memory usage is >32MB
+    // and is >400% larger than the estimated live memory usage.
+    // TODO(haraken): 400% is too large. Lower the heap growing factor.
+    return currentObjectSize >= 32 * 1024 * 1024 && currentObjectSize > 5 * estimatedLiveObjectSize;
 }
 
 void ThreadState::scheduleGCIfNeeded()
 {
     checkThread();
     // Allocation is allowed during sweeping, but those allocations should not
-    // trigger nested GCs. Does not apply if an urgent GC has been requested.
-    if (isSweepingInProgress() && UNLIKELY(!Heap::isUrgentGCRequested()))
+    // trigger nested GCs.
+    if (isSweepingInProgress())
         return;
     ASSERT(!sweepForbidden());
 
     if (shouldForceConservativeGC()) {
-        if (Heap::isUrgentGCRequested()) {
-            // If GC is deemed urgent, eagerly sweep and finalize any external allocations right away.
-            Heap::collectGarbage(HeapPointersOnStack, GCWithSweep, Heap::ConservativeGC);
-        } else {
-            // Otherwise, schedule a lazy sweeping in an idle task.
-            Heap::collectGarbage(HeapPointersOnStack, GCWithoutSweep, Heap::ConservativeGC);
-        }
+        Heap::collectGarbage(HeapPointersOnStack, GCWithoutSweep, Heap::ConservativeGC);
         return;
     }
     if (shouldSchedulePreciseGC())
@@ -846,8 +845,13 @@ void ThreadState::completeSweep()
 
 void ThreadState::postSweep()
 {
-    if (isMainThread())
-        Heap::setEstimatedLiveObjectSize(Heap::markedObjectSize());
+    if (isMainThread()) {
+        // At the point where the main thread finishes lazy sweeping,
+        // we estimate the live object size. Heap::markedObjectSize()
+        // may be underestimated if any other thread has not finished
+        // lazy sweeping.
+        Heap::setEstimatedLiveObjectSize(Heap::markedObjectSize() + Heap::externalObjectSizeAtLastGC());
+    }
 
     switch (gcState()) {
     case Sweeping:

third_party/WebKit/Source/core/dom/Text.cpp
@@ -42,48 +42,13 @@
 
 namespace blink {
 
-#if ENABLE(OILPAN)
-namespace {
-// If the external string kept by a Text node exceed this threshold length,
-// Oilpan is informed. External allocation amounts owned by heap objects are
-// taken into account when scheduling urgent Oilpan GCs.
-//
-// FIXME: having only Text nodes with strings above an ad-hoc local threshold
-// influence Oilpan's GC behavior isn't a satisfactory long-term solution.
-// But code that allocates a lot of Text nodes in tight loops, and not much more,
-// we do have to trigger Oilpan GCs to avoid PartitionAlloc OOMs. The accounting
-// does add overhead on the allocation of every Text node however, so for now, only
-// register those above the given threshold. TBC.
-const size_t stringLengthThreshold = 256;
-
-void increaseExternallyAllocatedBytesIfNeeded(size_t length)
-{
-    if (length > stringLengthThreshold)
-        Heap::increaseExternallyAllocatedBytes(length);
-}
-
-void increaseExternallyAllocatedBytesAliveIfNeeded(size_t length)
-{
-    if (length > stringLengthThreshold)
-        Heap::increaseExternallyAllocatedBytesAlive(length);
-}
-
-} // namespace
-#endif
-
 PassRefPtrWillBeRawPtr<Text> Text::create(Document& document, const String& data)
 {
-#if ENABLE(OILPAN)
-    increaseExternallyAllocatedBytesIfNeeded(data.length());
-#endif
     return adoptRefWillBeNoop(new Text(document, data, CreateText));
 }
 
 PassRefPtrWillBeRawPtr<Text> Text::createEditingText(Document& document, const String& data)
 {
-#if ENABLE(OILPAN)
-    increaseExternallyAllocatedBytesIfNeeded(data.length());
-#endif
     return adoptRefWillBeNoop(new Text(document, data, CreateEditingText));
 }
 
@@ -445,9 +410,6 @@ PassRefPtrWillBeRawPtr<Text> Text::cloneWithData(const String& data)
 
 DEFINE_TRACE(Text)
 {
-#if ENABLE(OILPAN)
-    increaseExternallyAllocatedBytesAliveIfNeeded(m_data.length());
-#endif
     CharacterData::trace(visitor);
 }
 

third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -2663,24 +2663,7 @@ void Heap::resetHeapCounters()
 
     s_allocatedObjectSize = 0;
     s_markedObjectSize = 0;
-
-    // Similarly, reset the amount of externally allocated memory.
-    s_externallyAllocatedBytes = 0;
-    s_externallyAllocatedBytesAlive = 0;
-
-    s_requestedUrgentGC = false;
-}
-
-void Heap::requestUrgentGC()
-{
-    // The urgent-gc flag will be considered the next time an out-of-line
-    // allocation is made. Bump allocations from the current block will
-    // go ahead until it can no longer service an allocation request.
-    //
-    // FIXME: if that delays urgently needed GCs for too long, consider
-    // flushing out per-heap "allocation points" to trigger the GC
-    // right away.
-    releaseStore(&s_requestedUrgentGC, 1);
+    s_externalObjectSizeAtLastGC = WTF::Partitions::totalSizeOfCommittedPages();
 }
 
 Visitor* Heap::s_markingVisitor;
@@ -2700,10 +2683,7 @@ size_t Heap::s_markedObjectSize = 0;
 // We don't want to use 0 KB for the initial value because it may end up
 // triggering the first GC of some thread too prematurely.
 size_t Heap::s_estimatedLiveObjectSize = 512 * 1024;
-
-size_t Heap::s_externallyAllocatedBytes = 0;
-size_t Heap::s_externallyAllocatedBytesAlive = 0;
-unsigned Heap::s_requestedUrgentGC = false;
+size_t Heap::s_externalObjectSizeAtLastGC = 0;
 double Heap::s_estimatedMarkingTimePerByte = 0.0;
 
 } // namespace blink

third_party/WebKit/Source/platform/heap/Heap.h
@@ -1021,25 +1021,10 @@ public:
     static size_t allocatedSpace() { return acquireLoad(&s_allocatedSpace); }
     static size_t estimatedLiveObjectSize() { return acquireLoad(&s_estimatedLiveObjectSize); }
     static void setEstimatedLiveObjectSize(size_t size) { releaseStore(&s_estimatedLiveObjectSize, size); }
+    static size_t externalObjectSizeAtLastGC() { return acquireLoad(&s_externalObjectSizeAtLastGC); }
 
     static double estimatedMarkingTime();
     static void reportMemoryUsageHistogram();
-
-    // On object allocation, register the object's externally allocated memory.
-    static void increaseExternallyAllocatedBytes(size_t);
-    static size_t externallyAllocatedBytes() { return acquireLoad(&s_externallyAllocatedBytes); }
-
-    // On object tracing, register the object's externally allocated memory (as still live.)
-    static void increaseExternallyAllocatedBytesAlive(size_t delta)
-    {
-        ASSERT(ThreadState::current()->isInGC());
-        s_externallyAllocatedBytesAlive += delta;
-    }
-    static size_t externallyAllocatedBytesAlive() { return s_externallyAllocatedBytesAlive; }
-
-    static void requestUrgentGC();
-    static void clearUrgentGC() { releaseStore(&s_requestedUrgentGC, 0); }
-    static bool isUrgentGCRequested() { return acquireLoad(&s_requestedUrgentGC); }
 
 private:
     // A RegionTree is a simple binary search tree of PageMemoryRegions sorted
@@ -1079,9 +1064,7 @@ private:
     static size_t s_allocatedObjectSize;
     static size_t s_markedObjectSize;
     static size_t s_estimatedLiveObjectSize;
-    static size_t s_externallyAllocatedBytes;
-    static size_t s_externallyAllocatedBytesAlive;
-    static unsigned s_requestedUrgentGC;
+    static size_t s_externalObjectSizeAtLastGC;
     static double s_estimatedMarkingTimePerByte;
 
     friend class ThreadState;
@@ -1476,28 +1459,6 @@ Address Heap::reallocate(void* previous, size_t size)
         copySize = size;
     memcpy(address, previous, copySize);
     return address;
-}
-
-inline void Heap::increaseExternallyAllocatedBytes(size_t delta)
-{
-    // Flag GC urgency on a 50% increase in external allocation
-    // since the last GC, but not for less than 100M.
-    //
-    // FIXME: consider other, 'better' policies (e.g., have the count of
-    // heap objects with external allocations be taken into
-    // account, ...) The overall goal here is to trigger a
-    // GC such that it considerably lessens memory pressure
-    // for a renderer process, when absolutely needed.
-    size_t externalBytesAllocatedSinceLastGC = atomicAdd(&s_externallyAllocatedBytes, static_cast<long>(delta));
-    if (LIKELY(externalBytesAllocatedSinceLastGC < 100 * 1024 * 1024))
-        return;
-
-    if (UNLIKELY(isUrgentGCRequested()))
-        return;
-
-    size_t externalBytesAliveAtLastGC = externallyAllocatedBytesAlive();
-    if (UNLIKELY(externalBytesAllocatedSinceLastGC > externalBytesAliveAtLastGC / 2))
-        Heap::requestUrgentGC();
 }
 
 template<bool needsTracing, WTF::WeakHandlingFlag weakHandlingFlag, WTF::ShouldWeakPointersBeMarkedStrongly strongify, typename T, typename Traits> struct CollectionBackingTraceTrait;

commit 375e2bbd7676bdbf9e581799c59a6ef795f9c7a0

Oilpan: Oilpan's GC should not be triggered without having allocated any object
This is a fix for a regression caused by r194282.
With the current threshold, Oilpan's GC can be triggered without having allocated
any object on Oilpan's heap. That is wrong. This CL changes the threshold
so that Oilpan's GC is not triggered until Oilpan has allocated a certain amount
of objects.

BUG=474470

Review URL: https://codereview.chromium.org/1100283003

git-svn-id: svn://svn.chromium.org/blink/trunk@194320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -516,9 +516,10 @@ bool ThreadState::shouldScheduleIdleGC()
     // Heap::markedObjectSize() may be underestimated if any thread has not
     // finished completeSweep().
     size_t currentObjectSize = allocatedObjectSize + Heap::markedObjectSize() + WTF::Partitions::totalSizeOfCommittedPages();
-    // Schedule an idle GC if the current memory usage is >1MB
-    // and is >50% larger than the estimated live memory usage.
-    return currentObjectSize >= 1024 * 1024 && currentObjectSize > estimatedLiveObjectSize * 3 / 2;
+    // Schedule an idle GC if Oilpan has allocated more than 1 MB since
+    // the last GC and the current memory usage is >50% larger than
+    // the estimated live memory usage.
+    return allocatedObjectSize >= 1024 * 1024 && currentObjectSize > estimatedLiveObjectSize * 3 / 2;
 #else
     return false;
 #endif
@@ -541,9 +542,10 @@ bool ThreadState::shouldSchedulePreciseGC()
     // Heap::markedObjectSize() may be underestimated if any thread has not
     // finished completeSweep().
     size_t currentObjectSize = allocatedObjectSize + Heap::markedObjectSize() + WTF::Partitions::totalSizeOfCommittedPages();
-    // Schedule a precise GC if the current memory usage is >1MB
-    // and is >50% larger than the estimated live memory usage.
-    return currentObjectSize >= 1024 * 1024 && currentObjectSize > estimatedLiveObjectSize * 3 / 2;
+    // Schedule a precise GC if Oilpan has allocated more than 1 MB since
+    // the last GC and the current memory usage is >50% larger than
+    // the estimated live memory usage.
+    return allocatedObjectSize >= 1024 * 1024 && currentObjectSize > estimatedLiveObjectSize * 3 / 2;
 #endif
 }
 
@@ -567,10 +569,11 @@ bool ThreadState::shouldForceConservativeGC()
         // aggressively. This is a safe guard to avoid OOM.
         return currentObjectSize > estimatedLiveObjectSize * 3 / 2;
     }
-    // Schedule a conservative GC if the current memory usage is >32MB
-    // and is >400% larger than the estimated live memory usage.
+    // Schedule a conservative GC if Oilpan has allocated more than 32 MB since
+    // the last GC and the current memory usage is >400% larger than
+    // the estimated live memory usage.
     // TODO(haraken): 400% is too large. Lower the heap growing factor.
-    return currentObjectSize >= 32 * 1024 * 1024 && currentObjectSize > 5 * estimatedLiveObjectSize;
+    return allocatedObjectSize >= 32 * 1024 * 1024 && currentObjectSize > 5 * estimatedLiveObjectSize;
 }
 
 void ThreadState::scheduleGCIfNeeded()