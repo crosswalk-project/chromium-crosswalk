crosswalk-project/chromium-crosswalk

commit abfb433a3995ac20128bedd92d95a01e45057579

[CSS Grid Layout] Implement min-width and min-height auto
From the spec (http://dev.w3.org/csswg/css-grid/#grid-items):
"The auto value of min-width and min-height behaves on grid items in the
relevant axis analogously to its behavior on flex items in the main
axis."

By default grid items shouldn't shrink to fit if the grid area is
smaller than their contents.

BUG=426898
TEST=fast/css-grid-layout/min-width-height-auto.html

Review URL: https://codereview.chromium.org/1098013002

git-svn-id: svn://svn.chromium.org/blink/trunk@194408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -2006,6 +2006,14 @@ void LayoutBox::computeLogicalWidth(LogicalExtentComputedValues& computedValues)
     LayoutUnit containerLogicalWidth = std::max(LayoutUnit(), containingBlockLogicalWidthForContent());
     bool hasPerpendicularContainingBlock = cb->isHorizontalWritingMode() != isHorizontalWritingMode();
 
+    if (parent()->isLayoutGrid() && style()->logicalWidth().isAuto() && style()->minWidth().isAuto()) {
+        LayoutUnit minLogicalWidth = minPreferredLogicalWidth();
+        if (containerLogicalWidth < minLogicalWidth) {
+            computedValues.m_extent = constrainLogicalWidthByMinMax(minLogicalWidth, containerLogicalWidth, cb);
+            return;
+        }
+    }
+
     if (isInline() && !isInlineBlockOrInlineTable()) {
         // just calculate margins
         computedValues.m_margins.m_start = minimumValueForLength(styleToUse.marginStart(), containerLogicalWidth);
@@ -2331,7 +2339,13 @@ void LayoutBox::computeLogicalHeight(LayoutUnit logicalHeight, LayoutUnit logica
         // FIXME: Account for writing-mode in flexible boxes.
         // https://bugs.webkit.org/show_bug.cgi?id=46418
         if (hasOverrideLogicalContentHeight() && (parent()->isFlexibleBoxIncludingDeprecated() || parent()->isLayoutGrid())) {
-            h = Length(overrideLogicalContentHeight(), Fixed);
+            LayoutUnit contentHeight = overrideLogicalContentHeight();
+            if (parent()->isLayoutGrid() && style()->minHeight().isAuto()) {
+                ASSERT(style()->logicalHeight().isAuto());
+                LayoutUnit minContentHeight = computeContentLogicalHeight(MinSize, Length(MinContent), computedValues.m_extent - borderAndPaddingLogicalHeight());
+                contentHeight = std::max(contentHeight, constrainLogicalHeightByMinMax(minContentHeight, computedValues.m_extent - borderAndPaddingLogicalHeight()));
+            }
+            h = Length(contentHeight, Fixed);
         } else if (treatAsReplaced) {
             h = Length(computeReplacedLogicalHeight(), Fixed);
         } else {

third_party/WebKit/LayoutTests/fast/css-grid-layout/grid-item-stretch-with-margins-borders-padding-expected.txt
@@ -2,49 +2,49 @@ This test checks that the 'stretch' value is applied correctly for 'align' and '
 
 direction: LTR | margin: 4px 8px 12px 16px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: LTR | border: 5px 10px 15px 20px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: LTR | padding: 4px 6px 8px 10px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: LTR | margin: 4px 8px 12px 16px | border: 5px 10px 15px 20px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: LTR | margin: 4px 8px 12px 16px | padding: 4px 6px 8px 10px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: LTR | margin: 4px 8px 12px 16px | border: 5px 10px 15px 20px | padding: 4px 6px 8px 10px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: RTL | margin: 4px 8px 12px 16px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: RTL | border: 5px 10px 15px 20px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: RTL | padding: 4px 6px 8px 10px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: RTL | margin: 4px 8px 12px 16px | border: 5px 10px 15px 20px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: RTL | margin: 4px 8px 12px 16px | padding: 4px 6px 8px 10px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS
 direction: RTL | margin: 4px 8px 12px 16px | border: 5px 10px 15px 20px | padding: 4px 6px 8px 10px
 
-X X X X X X X X X X X X X X
+X X X X X X X X X X
 PASS

third_party/WebKit/LayoutTests/fast/css-grid-layout/grid-item-stretch-with-margins-borders-padding.html
@@ -39,7 +39,7 @@ body {
 <div style="position: relative">
     <p>direction: LTR | margin: 4px 8px 12px 16px</p>
     <div class="grid" data-expected-width="200" data-expected-height="400">
-        <div class="margins firstRowFirstColumn" data-offset-x="16" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184">X X X X X X X X X X X X X X</div>
+        <div class="margins firstRowFirstColumn" data-offset-x="16" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="100" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="0" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="margins secondRowSecondColumn" data-offset-x="116" data-offset-y="204" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184"></div>
@@ -49,7 +49,7 @@ body {
 <div style="position: relative">
     <p>direction: LTR | border: 5px 10px 15px 20px</p>
     <div class="grid" data-expected-width="200" data-expected-height="400">
-        <div class="borders firstRowFirstColumn" data-offset-x="0" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="70" data-expected-client-height="180">X X X X X X X X X X X X X X</div>
+        <div class="borders firstRowFirstColumn" data-offset-x="0" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="70" data-expected-client-height="180">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="100" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="0" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="borders secondRowSecondColumn" data-offset-x="100" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="70" data-expected-client-height="180"></div>
@@ -59,7 +59,7 @@ body {
 <div style="position: relative">
     <p>direction: LTR | padding: 4px 6px 8px 10px</p>
     <div class="grid" data-expected-width="200" data-expected-height="400">
-        <div class="paddings firstRowFirstColumn" data-total-x="0" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200">X X X X X X X X X X X X X X</div>
+        <div class="paddings firstRowFirstColumn" data-total-x="0" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="100" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="0" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="paddings secondRowSecondColumn" data-offset-x="100" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
@@ -69,7 +69,7 @@ body {
 <div style="position: relative">
     <p>direction: LTR | margin: 4px 8px 12px 16px | border: 5px 10px 15px 20px</p>
     <div class="grid" data-expected-width="200" data-expected-height="400">
-        <div class="margins borders firstRowFirstColumn" data-offset-x="16" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="46" data-expected-client-height="164">X X X X X X X X X X X X X X</div>
+        <div class="margins borders firstRowFirstColumn" data-offset-x="16" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="46" data-expected-client-height="164">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="100" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="0" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="margins borders secondRowSecondColumn" data-offset-x="116" data-offset-y="204" data-expected-width="76" data-expected-height="184" data-expected-client-width="46" data-expected-client-height="164"></div>
@@ -79,7 +79,7 @@ body {
 <div style="position: relative">
     <p>direction: LTR | margin: 4px 8px 12px 16px | padding: 4px 6px 8px 10px</p>
     <div class="grid" data-expected-width="200" data-expected-height="400">
-        <div class="margins paddings firstRowFirstColumn" data-offset-x="16" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184">X X X X X X X X X X X X X X</div>
+        <div class="margins paddings firstRowFirstColumn" data-offset-x="16" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="100" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="0" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="margins paddings secondRowSecondColumn" data-offset-x="116" data-offset-y="204" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184"></div>
@@ -89,7 +89,7 @@ body {
 <div style="position: relative">
     <p>direction: LTR | margin: 4px 8px 12px 16px | border: 5px 10px 15px 20px | padding: 4px 6px 8px 10px</p>
     <div class="grid" data-expected-width="200" data-expected-height="400">
-        <div class="margins borders paddings firstRowFirstColumn" data-offset-x="16" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="46" data-expected-client-height="164">X X X X X X X X X X X X X X</div>
+        <div class="margins borders paddings firstRowFirstColumn" data-offset-x="16" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="46" data-expected-client-height="164">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="100" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="0" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="margins borders paddings secondRowSecondColumn" data-offset-x="116" data-offset-y="204" data-expected-width="76" data-expected-height="184"></div>
@@ -100,7 +100,7 @@ body {
 <div style="position: relative">
     <p>direction: RTL | margin: 4px 8px 12px 16px</p>
     <div class="grid directionRTL" data-expected-width="200" data-expected-height="400">
-        <div class="margins firstRowFirstColumn" data-offset-x="116" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184">X X X X X X X X X X X X X X</div>
+        <div class="margins firstRowFirstColumn" data-offset-x="116" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="0" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="100" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="margins secondRowSecondColumn" data-offset-x="16" data-offset-y="204" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184"></div>
@@ -110,7 +110,7 @@ body {
 <div style="position: relative">
     <p>direction: RTL | border: 5px 10px 15px 20px</p>
     <div class="grid directionRTL" data-expected-width="200" data-expected-height="400">
-        <div class="borders firstRowFirstColumn" data-offset-x="100" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="70" data-expected-client-height="180">X X X X X X X X X X X X X X</div>
+        <div class="borders firstRowFirstColumn" data-offset-x="100" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="70" data-expected-client-height="180">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="0" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="100" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="borders secondRowSecondColumn" data-offset-x="0" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="70" data-expected-client-height="180"></div>
@@ -120,7 +120,7 @@ body {
 <div style="position: relative">
     <p>direction: RTL | padding: 4px 6px 8px 10px</p>
     <div class="grid directionRTL" data-expected-width="200" data-expected-height="400">
-        <div class="paddings firstRowFirstColumn" data-total-x="100" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200">X X X X X X X X X X X X X X</div>
+        <div class="paddings firstRowFirstColumn" data-total-x="100" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="0" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="100" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="paddings secondRowSecondColumn" data-offset-x="0" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
@@ -130,7 +130,7 @@ body {
 <div style="position: relative">
     <p>direction: RTL | margin: 4px 8px 12px 16px | border: 5px 10px 15px 20px</p>
     <div class="grid directionRTL" data-expected-width="200" data-expected-height="400">
-        <div class="margins borders firstRowFirstColumn" data-offset-x="116" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="46" data-expected-client-height="164">X X X X X X X X X X X X X X</div>
+        <div class="margins borders firstRowFirstColumn" data-offset-x="116" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="46" data-expected-client-height="164">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="0" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="100" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="margins borders secondRowSecondColumn" data-offset-x="16" data-offset-y="204" data-expected-width="76" data-expected-height="184" data-expected-client-width="46" data-expected-client-height="164"></div>
@@ -140,7 +140,7 @@ body {
 <div style="position: relative">
     <p>direction: RTL | margin: 4px 8px 12px 16px | padding: 4px 6px 8px 10px</p>
     <div class="grid directionRTL" data-expected-width="200" data-expected-height="400">
-        <div class="margins paddings firstRowFirstColumn" data-offset-x="116" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184">X X X X X X X X X X X X X X</div>
+        <div class="margins paddings firstRowFirstColumn" data-offset-x="116" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="0" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="100" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="margins paddings secondRowSecondColumn" data-offset-x="16" data-offset-y="204" data-expected-width="76" data-expected-height="184" data-expected-client-width="76" data-expected-client-height="184"></div>
@@ -150,7 +150,7 @@ body {
 <div style="position: relative">
     <p>direction: RTL | margin: 4px 8px 12px 16px | border: 5px 10px 15px 20px | padding: 4px 6px 8px 10px</p>
     <div class="grid  directionRTL" data-expected-width="200" data-expected-height="400">
-        <div class="margins borders paddings firstRowFirstColumn" data-offset-x="116" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="46" data-expected-client-height="164">X X X X X X X X X X X X X X</div>
+        <div class="margins borders paddings firstRowFirstColumn" data-offset-x="116" data-offset-y="4" data-expected-width="76" data-expected-height="184" data-expected-client-width="46" data-expected-client-height="164">X X X X X X X X X X</div>
         <div class="firstRowSecondColumn" data-offset-x="0" data-offset-y="0" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="secondRowFirstColumn" data-offset-x="100" data-offset-y="200" data-expected-width="100" data-expected-height="200" data-expected-client-width="100" data-expected-client-height="200"></div>
         <div class="margins borders paddings secondRowSecondColumn" data-offset-x="16" data-offset-y="204" data-expected-width="76" data-expected-height="184"></div>

third_party/WebKit/LayoutTests/fast/css-grid-layout/min-width-height-auto-expected.txt
@@ -1,0 +1,12 @@
+This test checks min-width|height auto behavior for grids
+
+XXXX
+PASS
+XXXX
+PASS
+XXXX
+PASS
+XXXX
+PASS
+XXXX
+PASS

third_party/WebKit/LayoutTests/fast/css-grid-layout/min-width-height-auto.html
@@ -1,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<link href="resources/grid.css" rel="stylesheet">
+<style>
+.grid {
+    grid-template-columns: 5px;
+    grid-template-rows: 5px;
+}
+
+.container {
+    width: 200px;
+    height: 100px;
+}
+
+.ahem {
+    font: 25px/1 Ahem;
+}
+
+.minSmaller {
+    min-width: 10px;
+    min-height: 10px;
+}
+
+.minBigger {
+    min-width: 150px;
+    min-height: 75px;
+}
+
+.maxSmaller {
+    max-width: 10px;
+    max-height: 10px;
+}
+
+.maxBigger {
+    max-width: 150px;
+    max-height: 75px;
+}
+</style>
+<script src="../../resources/check-layout.js"></script>
+<body onload="checkLayout('.grid')">
+
+<p>This test checks min-width|height auto behavior for grids</p>
+
+<div class="container">
+    <div class="grid">
+        <div class="ahem" data-expected-width="100" data-expected-height="25">XXXX</div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="grid">
+        <div class="ahem minSmaller" data-expected-width="10" data-expected-height="10">XXXX</div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="grid">
+        <div class="ahem minBigger" data-expected-width="150" data-expected-height="75">XXXX</div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="grid">
+        <div class="ahem maxSmaller" data-expected-width="10" data-expected-height="10">XXXX</div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="grid">
+        <div class="ahem maxBigger" data-expected-width="100" data-expected-height="25">XXXX</div>
+    </div>
+</div>
+
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/repaint/justify-self-change-expected.txt
@@ -11,9 +11,9 @@
         [125, 53, 50, 100],
         [124, 52, 52, 150],
         [25, 53, 50, 150],
-        [24, 52, 52, 151],
+        [24, 52, 52, 152],
         [1, 53, 50, 150],
-        [0, 52, 52, 151]
+        [0, 52, 52, 152]
       ]
     }
   ]

third_party/WebKit/LayoutTests/fast/repaint/justify-self-overflow-change-expected.txt
@@ -8,12 +8,12 @@
       "repaintRects": [
         [1, 53, 150, 150],
         [0, 52, 200, 300],
-        [0, 52, 152, 151],
+        [0, 52, 152, 152],
         [0, 16, 800, 336],
         [0, 0, 800, 352],
         [-51, 53, 150, 150],
         [-52, 52, 252, 300],
-        [-52, 52, 152, 151],
+        [-52, 52, 152, 152],
         [-52, 16, 852, 336],
         [-52, 0, 852, 352]
       ]

third_party/WebKit/LayoutTests/virtual/slimmingpaint/fast/repaint/justify-self-change-expected.txt
@@ -11,9 +11,9 @@
         [125, 53, 50, 100],
         [124, 52, 52, 150],
         [25, 53, 50, 150],
-        [24, 52, 52, 151],
+        [24, 52, 52, 152],
         [1, 53, 50, 150],
-        [0, 52, 52, 151]
+        [0, 52, 52, 152]
       ],
       "paintInvalidationClients": [
         "LayoutBlockFlow DIV class='item1'",

commit 0b3187b4c98c183ddd0621aae0c1b97d7764d7d5

[CSS Grid Layout] Fix grid items overflow
This is a regression introduced in r194408:
https://codereview.chromium.org/1098013002

Per spec, min-width|height auto should only apply if overflow is
visible:
http://dev.w3.org/csswg/css-flexbox-1/#valdef-min-width-auto

This patch adds the missing condition to the if statements.

BUG=426898,483473
TEST=fast/css-grid-layout/min-width-height-auto-overflow.html

Review URL: https://codereview.chromium.org/1122763002

git-svn-id: svn://svn.chromium.org/blink/trunk@194863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -2006,7 +2006,7 @@ void LayoutBox::computeLogicalWidth(LogicalExtentComputedValues& computedValues)
     LayoutUnit containerLogicalWidth = std::max(LayoutUnit(), containingBlockLogicalWidthForContent());
     bool hasPerpendicularContainingBlock = cb->isHorizontalWritingMode() != isHorizontalWritingMode();
 
-    if (parent()->isLayoutGrid() && style()->logicalWidth().isAuto() && style()->minWidth().isAuto()) {
+    if (parent()->isLayoutGrid() && style()->logicalWidth().isAuto() && style()->minWidth().isAuto() && style()->overflowX() == OVISIBLE) {
         LayoutUnit minLogicalWidth = minPreferredLogicalWidth();
         if (containerLogicalWidth < minLogicalWidth) {
             computedValues.m_extent = constrainLogicalWidthByMinMax(minLogicalWidth, containerLogicalWidth, cb);
@@ -2340,7 +2340,7 @@ void LayoutBox::computeLogicalHeight(LayoutUnit logicalHeight, LayoutUnit logica
         // https://bugs.webkit.org/show_bug.cgi?id=46418
         if (hasOverrideLogicalContentHeight() && (parent()->isFlexibleBoxIncludingDeprecated() || parent()->isLayoutGrid())) {
             LayoutUnit contentHeight = overrideLogicalContentHeight();
-            if (parent()->isLayoutGrid() && style()->minHeight().isAuto()) {
+            if (parent()->isLayoutGrid() && style()->minHeight().isAuto() && style()->overflowY() == OVISIBLE) {
                 ASSERT(style()->logicalHeight().isAuto());
                 LayoutUnit minContentHeight = computeContentLogicalHeight(MinSize, Length(MinContent), computedValues.m_extent - borderAndPaddingLogicalHeight());
                 contentHeight = std::max(contentHeight, constrainLogicalHeightByMinMax(minContentHeight, computedValues.m_extent - borderAndPaddingLogicalHeight()));

third_party/WebKit/LayoutTests/fast/css-grid-layout/min-width-height-auto-overflow-expected.html
@@ -1,0 +1,36 @@
+<!DOCTYPE html>
+<style>
+.item {
+    height: 50px;
+    width: 50px;
+    margin: 50px;
+    font: 50px/1 Ahem;
+}
+
+.overflowVisible {
+    overflow: visible;
+}
+
+.overflowHidden {
+    overflow: hidden;
+}
+
+.overflowScroll {
+    overflow-y: scroll;
+}
+
+.overflowAuto {
+    overflow: auto;
+}
+</style>
+
+<p>This test checks that min-width|height auto behavior for grid items only applies if overflow is visible.</p>
+<p>The test passes if you see three 50x50 squares and one 100x100 square.</p>
+
+<div class="item overflowHidden">XX<br>XX</div>
+
+<div class="item overflowScroll">XX<br>XX</div>
+
+<div class="item overflowAuto">XX<br>XX</div>
+
+<div class="item overflowVisible">XX<br>XX</div>

third_party/WebKit/LayoutTests/fast/css-grid-layout/min-width-height-auto-overflow.html
@@ -1,0 +1,50 @@
+<!DOCTYPE html>
+<style>
+.grid {
+    display: grid;
+    grid-template-columns: 50px;
+    grid-template-rows: 50px;
+    height: 50px;
+    width: 50px;
+    margin: 50px;
+}
+
+.item {
+    font: 50px/1 Ahem;
+}
+
+.overflowVisible {
+    overflow: visible;
+}
+
+.overflowHidden {
+    overflow: hidden;
+}
+
+.overflowScroll {
+    overflow: scroll;
+}
+
+.overflowAuto {
+    overflow: auto;
+}
+</style>
+
+<p>This test checks that min-width|height auto behavior for grid items only applies if overflow is visible.</p>
+<p>The test passes if you see three 50x50 squares and one 100x100 square.</p>
+
+<div class="grid">
+    <div class="item overflowHidden">XX<br>XX</div>
+</div>
+
+<div class="grid">
+    <div class="item overflowScroll">XX<br>XX</div>
+</div>
+
+<div class="grid">
+    <div class="item overflowAuto">XX<br>XX</div>
+</div>
+
+<div class="grid">
+    <div class="item overflowVisible">XX<br>XX</div>
+</div>