db0b64a9be67cc1a581ef7b95e8511521e969683
hujiajie <jiajie.hu@intel.com>
2015-05-24 20:39:27 +0800

    [Blink] WebCL support for Crosswalk.
    WebCL 1.0 defines a JavaScript binding to the Khronos OpenCL standard
    for heterogeneous parallel computing. Its spec is available at:
    http://www.khronos.org/registry/webcl/specs/latest/1.0/
    
    This patch includes WebCL basic APIs support, dynamic loading mechanism
    etc. WebCL support is controlled by "ENABLE_WEBCL" flag, and it's enabled
    by default on Android. If you want to disable with WebCL,
    please add "-Denable_webcl=0" when running the "xwalk/gyp_xwalk" before building.
    
    Currently, Intel CPU/GPU, Qualcomm CPU/GPU and Power VR GPU are supported.
    But OpenCL library isn't always shipped with device by default, even it supports OpenCL.
    For example: Google Nexus series. It requires to install OpenCL library manually.
    
    TEST=WebCL conformances test on Asus MemoPad and Xiaomi3.
    
    Known issue:
    1. miss oilpan support.
    2. miss cl_khr_gl_sharing extension support.
    3. load library in render process.
    
    R=dongseong.hwang@intel.com, halton.huo@intel.com, kenneth.r.christiansen@intel.com
    
    BUG=XWALK-4254

---
+++ third_party/WebKit/Source/modules/webcl/WebCLExtension.cpp
@@ -0,0 +1,78 @@
+// Copyright (C) 2013 Samsung Electronics Corporation. All rights reserved.
+// Copyright (C) 2015 Intel Corporation All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "config.h"
+
+#if ENABLE(WEBCL)
+
+#include "modules/webcl/WebCLExtension.h"
+
+namespace blink {
+
+bool WebCLExtension::enableExtension(const String& name)
+{
+    if (equalIgnoringCase(name, "WEBCL_html_image")) {
+        m_enabledExtensions.append("WEBCL_html_image");
+        return true;
+    }
+
+    if (equalIgnoringCase(name, "WEBCL_html_video")) {
+        m_enabledExtensions.append("WEBCL_html_video");
+        return true;
+    }
+
+    if (equalIgnoringCase(name, "KHR_fp64")) {
+        bool khrFP64 = m_supportedCLExtensions.contains("cl_khr_fp64");
+        if (khrFP64)
+            m_enabledExtensions.append("KHR_fp64");
+        return khrFP64;
+    }
+
+    if (equalIgnoringCase(name, "KHR_fp16")) {
+        bool khrFP16 = m_supportedCLExtensions.contains("cl_khr_fp16");
+        if (khrFP16)
+            m_enabledExtensions.append("KHR_fp16");
+        return khrFP16;
+    }
+
+    return false;
+}
+
+Vector<String> WebCLExtension::getSupportedExtensions()
+{
+    // Filter the opencl supported extension.
+    Vector<String> result;
+    result.append("WEBCL_html_image");
+    result.append("WEBCL_html_video");
+    if (m_supportedCLExtensions.contains("cl_khr_fp64"))
+        result.append("KHR_fp64");
+
+    if (m_supportedCLExtensions.contains("cl_khr_fp16"))
+        result.append("KHR_fp16");
+
+    return result;
+}
+
+bool WebCLExtension::isEnabledExtension(const String& name) const
+{
+    return m_enabledExtensions.contains(name);
+}
+
+void WebCLExtension::getEnabledExtensions(HashSet<String>& extensions)
+{
+    for (auto enabledExtension : m_enabledExtensions) {
+        const String& extensionString = enabledExtension;
+        if (!extensionString.isEmpty())
+            extensions.add(extensionString);
+    }
+}
+
+void WebCLExtension::addSupportedCLExtension(const String& name)
+{
+    m_supportedCLExtensions.append(name);
+}
+
+} // blink
+
+#endif // ENABLE(WEBCL)
