d93f6f3ce85263cf4cf84ed310e63136dd67898e
hujiajie <jiajie.hu@intel.com>
2015-05-24 20:39:27 +0800

    [Blink] WebCL support for Crosswalk.
    WebCL 1.0 defines a JavaScript binding to the Khronos OpenCL standard
    for heterogeneous parallel computing. Its spec is available at:
    http://www.khronos.org/registry/webcl/specs/latest/1.0/
    
    This patch includes WebCL basic APIs support, dynamic loading mechanism
    etc. WebCL support is controlled by "ENABLE_WEBCL" flag, and it's enabled
    by default on Android. If you want to disable with WebCL,
    please add "-Denable_webcl=0" when running the "xwalk/gyp_xwalk" before building.
    
    Currently, Intel CPU/GPU, Qualcomm CPU/GPU and Power VR GPU are supported.
    But OpenCL library isn't always shipped with device by default, even it supports OpenCL.
    For example: Google Nexus series. It requires to install OpenCL library manually.
    
    TEST=WebCL conformances test on Asus MemoPad and Xiaomi3.
    
    Known issue:
    1. miss oilpan support.
    2. miss cl_khr_gl_sharing extension support.
    3. load library in render process.
    
    R=dongseong.hwang@intel.com, halton.huo@intel.com, kenneth.r.christiansen@intel.com
    
    BUG=XWALK-4254
    
    [M48: adapted to the following upstream changes:
     - https://codereview.chromium.org/1303153005
     - https://codereview.chromium.org/1362103002
     - https://codereview.chromium.org/1372463002
     - https://codereview.chromium.org/1441573002]

---
+++ third_party/WebKit/Source/modules/webcl/WebCLKernel.h
@@ -0,0 +1,64 @@
+// Copyright (C) 2011 Samsung Electronics Corporation. All rights reserved.
+// Copyright (C) 2015 Intel Corporation All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebCLKernel_h
+#define WebCLKernel_h
+
+#if ENABLE(WEBCL)
+#include "modules/webcl/WebCLKernelArgInfoProvider.h"
+#include "modules/webcl/WebCLProgram.h"
+
+#include <wtf/PassRefPtr.h>
+#include <wtf/RefCounted.h>
+
+namespace blink {
+
+class DOMArrayBufferView;
+class ExceptionState;
+class WebCL;
+class WebCLCommandQueue;
+class WebCLDevice;
+class WebCLKernelArgInfo;
+class WebCLMemoryObject;
+class WebCLPlatform;
+class WebCLSampler;
+
+class WebCLKernel : public WebCLObject, public ScriptWrappable {
+    DEFINE_WRAPPERTYPEINFO();
+public:
+    ~WebCLKernel() override;
+    static PassRefPtr<WebCLKernel> create(cl_kernel, PassRefPtr<WebCLContext>, WebCLProgram*, const String&);
+
+    ScriptValue getInfo(ScriptState*, int, ExceptionState&);
+    ScriptValue getWorkGroupInfo(ScriptState*, WebCLDevice*, int, ExceptionState&);
+    WebCLKernelArgInfo* getArgInfo(unsigned index, ExceptionState&);
+    void setArg(unsigned, const ScriptValue&, ExceptionState&);
+    void setArg(unsigned, WebCLMemoryObject*, ExceptionState&);
+    void setArg(unsigned, WebCLSampler*, ExceptionState&);
+    void setArg(unsigned, DOMArrayBufferView*, ExceptionState&);
+    void setArg(unsigned, size_t, const void*, ExceptionState&);
+    void release() override;
+
+    unsigned numberOfArguments();
+    unsigned associatedArguments();
+    WebCLProgram* program() const { return m_program; }
+    const String& kernelName() const { return m_kernelName; }
+    const Vector<unsigned>& requiredArguments() { return m_argumentInfoProvider.requiredArguments(); }
+    cl_kernel getKernel() const { return m_clKernel; }
+
+private:
+    WebCLKernel(cl_kernel, PassRefPtr<WebCLContext>, WebCLProgram*, const String&);
+    bool isReleased() const { return !m_clKernel; }
+
+    WebCLProgram* m_program;
+    String m_kernelName;
+    WebCLKernelArgInfoProvider m_argumentInfoProvider;
+    cl_kernel m_clKernel;
+};
+
+} // namespace blink
+
+#endif // ENABLE(WEBCL)
+#endif // WebCLKernel_h
