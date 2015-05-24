6f0dd433e74f353b888e5c57bc63a3b6ec355fe1
hujiajie <jiajie.hu@intel.com>
2015-05-24 20:39:27 +0800

    [Blink] WebCL support for Crosswalk.
    WebCL 1.0 defines a JavaScript binding to the Khronos OpenCL standard
    for heterogeneous parallel computing. Its spec is available at:
    http://www.khronos.org/registry/webcl/specs/latest/1.0/
    
    This patch includes WebCL basic APIs support, dynamic loading mechanism
    etc. WebCL support is controlled by "ENABLE_WEBCL" flag, and it's enabled
    by default on Android. If you want to disable with WebCL,
    please add "-Denable_webcl=0" when running the "xwalk/gyp_xwalk" before building.
    
    Currently, Intel CPU/GPU, Qualcomm CPU/GPU and Power VR GPU are supported.
    But OpenCL library isn't always shipped with device by default, even it supports OpenCL.
    For example: Google Nexus series. It requires to install OpenCL library manually.
    
    TEST=WebCL conformances test on Asus MemoPad and Xiaomi3.
    
    Known issue:
    1. miss oilpan support.
    2. miss cl_khr_gl_sharing extension support.
    3. load library in render process.
    
    R=dongseong.hwang@intel.com, halton.huo@intel.com, kenneth.r.christiansen@intel.com
    
    BUG=XWALK-4254
    
    [M48: adapted to the following upstream changes:
     - https://codereview.chromium.org/1303153005
     - https://codereview.chromium.org/1362103002
     - https://codereview.chromium.org/1372463002
     - https://codereview.chromium.org/1441573002]

---
+++ third_party/WebKit/Source/modules/webcl/WebCLObject.h
@@ -0,0 +1,47 @@
+// Copyright (C) 2013 Samsung Electronics Corporation. All rights reserved.
+// Copyright (C) 2015 Intel Corporation All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebCLObject_h
+#define WebCLObject_h
+
+#if ENABLE(WEBCL)
+
+#include "core/webcl/WebCLException.h"
+
+#include <wtf/PassRefPtr.h>
+#include <wtf/RefCounted.h>
+#include <wtf/RefPtr.h>
+#include <wtf/WeakPtr.h>
+
+namespace blink {
+class ExceptionState;
+class WebCLContext;
+
+// WebCLObject is the base class of WebCommandQueue, WebCLProgram, WebCLKernel,
+// WebCLMemoryObject/WebCLBuffer/WebCLImage, WebCLEvent/WebCLUserEvent, WebCLSampler.
+// They are owned by WebCLContext through weakptr for lifecycle tracking.
+class WebCLObject : public RefCounted<WebCLObject> {
+public:
+    virtual ~WebCLObject();
+    WeakPtr<WebCLObject> createWeakPtr() { return m_weakFactory.createWeakPtr(); }
+    PassRefPtr<WebCLContext> context();
+
+    void setContext(PassRefPtr<WebCLContext> context);
+    virtual void release() { ASSERT_NOT_REACHED(); }
+
+protected:
+    explicit WebCLObject(PassRefPtr<WebCLContext> context);
+    // Some object isn't associated with WebContext in constructor by developer.
+    // but at runtime by OpenCL. Such as: WebCLEvent
+    WebCLObject();
+
+    WeakPtrFactory<WebCLObject> m_weakFactory;
+    RefPtr<WebCLContext> m_context;
+};
+
+} // namespace blink
+
+#endif // ENABLE(WEBCL)
+#endif // WebCLObject_h
