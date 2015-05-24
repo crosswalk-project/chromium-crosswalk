9d60a2873c1953380c498992f6710498bc1e3cdd
hujiajie <jiajie.hu@intel.com>
2015-05-24 20:39:27 +0800

    [Blink] WebCL support for Crosswalk.
    WebCL 1.0 defines a JavaScript binding to the Khronos OpenCL standard
    for heterogeneous parallel computing. Its spec is available at:
    http://www.khronos.org/registry/webcl/specs/latest/1.0/
    
    This patch includes WebCL basic APIs support, dynamic loading mechanism
    etc. WebCL support is controlled by "ENABLE_WEBCL" flag, and it's enabled
    by default on Android. If you want to disable with WebCL,
    please add "-Denable_webcl=0" when running the "xwalk/gyp_xwalk" before building.
    
    Currently, Intel CPU/GPU, Qualcomm CPU/GPU and Power VR GPU are supported.
    But OpenCL library isn't always shipped with device by default, even it supports OpenCL.
    For example: Google Nexus series. It requires to install OpenCL library manually.
    
    TEST=WebCL conformances test on Asus MemoPad and Xiaomi3.
    
    Known issue:
    1. miss oilpan support.
    2. miss cl_khr_gl_sharing extension support.
    3. load library in render process.
    
    R=dongseong.hwang@intel.com, halton.huo@intel.com, kenneth.r.christiansen@intel.com
    
    BUG=XWALK-4254
    
    [M48: adapted to the following upstream changes:
     - https://codereview.chromium.org/1303153005
     - https://codereview.chromium.org/1362103002
     - https://codereview.chromium.org/1372463002
     - https://codereview.chromium.org/1441573002]

---
+++ third_party/WebKit/Source/modules/webcl/WebCLDevice.h
@@ -0,0 +1,57 @@
+// Copyright (C) 2011 Samsung Electronics Corporation. All rights reserved.
+// Copyright (C) 2015 Intel Corporation All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebCLDevice_h
+#define WebCLDevice_h
+
+#if ENABLE(WEBCL)
+#include "modules/webcl/WebCLConfig.h"
+#include "modules/webcl/WebCLExtension.h"
+#include "modules/webcl/WebCLPlatform.h"
+
+#include <wtf/PassRefPtr.h>
+#include <wtf/RefCounted.h>
+
+namespace blink {
+
+class ExceptionState;
+class WebCL;
+class WebCLGetInfo;
+class WebCLPlatform;
+
+class WebCLDevice : public RefCounted<WebCLDevice>, public ScriptWrappable {
+    DEFINE_WRAPPERTYPEINFO();
+public:
+    ~WebCLDevice();
+    static PassRefPtr<WebCLDevice> create(cl_device_id);
+    static PassRefPtr<WebCLDevice> create(cl_device_id, WebCLPlatform* platform);
+
+    ScriptValue getInfo(ScriptState*, unsigned, ExceptionState&);
+
+    void cacheDeviceExtensions();
+    bool enableExtension(const String& name);
+    void getEnabledExtensions(HashSet<String>& extensions);
+    Vector<String> getSupportedExtensions();
+    unsigned getQueueProperties();
+    unsigned long long getMaxMemAllocSize();
+    unsigned getImage2DMaxWidth();
+    unsigned getImage2DMaxHeight();
+    unsigned getMaxWorkGroup();
+    Vector<unsigned> getMaxWorkItem();
+    PassRefPtr<WebCLPlatform> getPlatform() const { return m_platform; }
+    cl_device_id getDeviceId() { return m_clDeviceId; }
+
+private:
+    WebCLDevice(cl_device_id, WebCLPlatform* platform);
+
+    WebCLPlatform* m_platform;
+    WebCLExtension m_extension;
+    cl_device_id m_clDeviceId;
+};
+
+} // namespace blink
+
+#endif // ENABLE(WEBCL)
+#endif // WebCLDevice_h
