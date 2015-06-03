93dccbda78ceeea6d760f489a7c96c9fc7c8b627
Vaclav Brozek <vabr@chromium.org>
2015-06-03 15:47:59 +0200

    [Merge to M44] Move password_manager_util and password_bubble_experiment to components
    This merges https://codereview.chromium.org/1133613007 to branch 2403 (M44). Approval in http://crbug.com/493661#c6.
    
    Original description:
    ----------------------------------------------------
    BUG=486739
    
    Review URL: https://codereview.chromium.org/1133613007
    
    Cr-Commit-Position: refs/heads/master@{#330905}
    ----------------------------------------------------
    TBR=vabr@chromium.org
    BUG=493661
    (cherry picked from commit 205d8da3ff51ec789abcef187191b9a82519b9b2)
    
    Review URL: https://codereview.chromium.org/1164003003
    
    Cr-Commit-Position: refs/branch-heads/2403@{#185}
    Cr-Branched-From: f54b8097a9c45ed4ad308133d49f05325d6c5070-refs/heads/master@{#330231}

--- chrome/browser/ui/webui/options/password_manager_handler.cc
+++ chrome/browser/ui/webui/options/password_manager_handler.cc
@@ -19,11 +19,11 @@
 #include "chrome/browser/ui/ash/ash_util.h"
 #endif
 #include "chrome/browser/ui/passwords/manage_passwords_view_utils.h"
-#include "chrome/browser/ui/passwords/password_bubble_experiment.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/autofill/core/common/password_form.h"
+#include "components/password_manager/core/browser/password_bubble_experiment.h"
 #include "components/password_manager/core/common/experiments.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_source.h"
--- chrome/chrome_browser_ui.gypi
+++ chrome/chrome_browser_ui.gypi
@@ -201,8 +201,6 @@
       'browser/ui/passwords/manage_passwords_ui_controller.h',
       'browser/ui/passwords/manage_passwords_view_utils.cc',
       'browser/ui/passwords/manage_passwords_view_utils.h',
-      'browser/ui/passwords/password_bubble_experiment.cc',
-      'browser/ui/passwords/password_bubble_experiment.h',
       'browser/ui/passwords/password_manager_presenter.cc',
       'browser/ui/passwords/password_manager_presenter.h',
       'browser/ui/passwords/password_ui_view.h',
