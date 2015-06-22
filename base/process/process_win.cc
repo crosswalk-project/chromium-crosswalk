9979ca38505633f8b2cc2ecd74ac8b9a7103e881
Gabriel Charette <gab@chromium.org>
2015-06-22 17:47:52 -0400

    [Merge to M44] Use StartsWith rather than == to compare BackgroundRendererProcesses experiment group names.
    The other portion of this experiment in render_process_host_impl.cc was
    already fixed in M44 when doing the Mac implementation of this experiment.
    
    BUG=501885
    TBR=avi@chromium.org, thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/1188773007
    
    Cr-Commit-Position: refs/heads/master@{#335088}
    (cherry picked from commit 3820c66b1437b7a2de14a54332706a3903e1efa7)
    
     Conflicts:
    	content/browser/renderer_host/render_process_host_impl.cc (M44's version is already correct)
            base/process/process_win.cc (need to use old version of StartsWith to compile on M44).
    
    Review URL: https://codereview.chromium.org/1194073005.
    
    Cr-Commit-Position: refs/branch-heads/2403@{#372}
    Cr-Branched-From: f54b8097a9c45ed4ad308133d49f05325d6c5070-refs/heads/master@{#330231}

--- base/process/process_win.cc
+++ base/process/process_win.cc
@@ -9,6 +9,7 @@
 #include "base/metrics/field_trial.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/process/kill.h"
+#include "base/strings/string_util.h"
 #include "base/win/windows_version.h"
 
 namespace {
@@ -188,8 +189,10 @@ bool Process::SetProcessBackgrounded(bool value) {
     DWORD background_priority = IDLE_PRIORITY_CLASS;
     base::FieldTrial* trial =
         base::FieldTrialList::Find("BackgroundRendererProcesses");
-    if (trial && trial->group_name() == "AllowBelowNormalFromBrowser")
+    if (trial && StartsWithASCII(trial->group_name(),
+                                 "AllowBelowNormalFromBrowser", true)) {
       background_priority = BELOW_NORMAL_PRIORITY_CLASS;
+    }
 
     priority = value ? background_priority : NORMAL_PRIORITY_CLASS;
   }
