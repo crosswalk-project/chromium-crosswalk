e902fe18534fe154b24e404352edacf2bdd0de27
Xiaohan Wang <xhwang@chromium.org>
2015-06-22 13:18:17 -0700

    media: Add RAPPOR metrics for media player origin URLs.
    - Plumb RAPPOR reporting to MediaClient.
    - Add origin URL report for both MSE and SRC playback.
    - Move metrics reporting to a central place.
    - Remove redundant param in Create*WebMediaPlayer*() functions.
    
    TBR=nasko@chromium.org,ddorwin@chromium.org,holte@chromium.org
    BUG=499949
    
    Review URL: https://codereview.chromium.org/1175383004
    
    Cr-Commit-Position: refs/heads/master@{#334968}
    (cherry picked from commit 0acca44b3b1071de445a09484cdf1c1ffac729cc)
    
    Review URL: https://codereview.chromium.org/1198963008.
    
    Cr-Commit-Position: refs/branch-heads/2403@{#370}
    Cr-Branched-From: f54b8097a9c45ed4ad308133d49f05325d6c5070-refs/heads/master@{#330231}

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -758,7 +758,6 @@ class CONTENT_EXPORT RenderFrameImpl
   void InitializeUserMediaClient();
 
   blink::WebMediaPlayer* CreateWebMediaPlayerForMediaStream(
-      const blink::WebURL& url,
       blink::WebMediaPlayerClient* client);
 
   // Creates a factory object used for creating audio and video renderers.
@@ -805,7 +804,6 @@ class CONTENT_EXPORT RenderFrameImpl
 
 #if defined(OS_ANDROID)
   blink::WebMediaPlayer* CreateAndroidWebMediaPlayer(
-      const blink::WebURL& url,
       blink::WebMediaPlayerClient* client,
       media::MediaPermission* media_permission,
       blink::WebContentDecryptionModule* initial_cdm);
