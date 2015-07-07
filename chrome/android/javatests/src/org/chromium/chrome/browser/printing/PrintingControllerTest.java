5531f2b3445a80097b4ec1a4f5c35fab6e4d5e10
Lin Sun <lin.sun@intel.com>
2015-07-07 11:27:58 +0800

    [ICS Support] Revert "Remove ICS support code in chrome/ and components/precache/"
    This reverts commit acb0e1da5a1bec89c8ce40101641c3e8aa4ba60c.

--- chrome/android/javatests/src/org/chromium/chrome/browser/printing/PrintingControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/printing/PrintingControllerTest.java
@@ -239,6 +239,7 @@ public class PrintingControllerTest extends ChromeActivityTestCaseBase<ChromeAct
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void callLayoutOnUiThread(
             final PrintingControllerImpl controller,
             final PrintAttributes oldAttributes,
