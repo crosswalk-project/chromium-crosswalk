67abb0ffa2e743af91a91d07080b70969618bcd0
Olli Raula <olli.raula@intel.com>
2015-07-10 11:11:26 +0300

    [ICS Support] Revert "Remove unnecesary ApiCompatibilityUtils calls."
    This reverts commit 82aef33f7e4160c7ae578f52c8e694e91b7bb0b1.

--- ui/android/java/src/org/chromium/ui/DropdownAdapter.java
+++ ui/android/java/src/org/chromium/ui/DropdownAdapter.java
@@ -56,7 +56,7 @@ public class DropdownAdapter extends ArrayAdapter<DropdownItem> {
             LayoutInflater inflater =
                     (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             layout = inflater.inflate(R.layout.dropdown_item, null);
-            layout.setBackground(new DropdownDividerDrawable());
+            ApiCompatibilityUtils.setBackgroundForView(layout, new DropdownDividerDrawable());
         }
 
         DropdownDividerDrawable divider = (DropdownDividerDrawable) layout.getBackground();
@@ -70,10 +70,10 @@ public class DropdownAdapter extends ArrayAdapter<DropdownItem> {
             divider.setHeight(dividerHeight);
             if (mSeparators != null && mSeparators.contains(position)) {
                 divider.setColor(ApiCompatibilityUtils.getColor(mContext.getResources(),
-                        R.color.dropdown_dark_divider_color));
+                                 R.color.dropdown_dark_divider_color));
             } else {
                 divider.setColor(ApiCompatibilityUtils.getColor(mContext.getResources(),
-                        R.color.dropdown_divider_color));
+                                 R.color.dropdown_divider_color));
             }
         }
 
