01e15ce31576abd5a55a52b1350eec780df09a71
Olli Raula <olli.raula@intel.com>
2015-07-10 11:11:26 +0300

    [ICS Support] Revert "Remove unnecesary ApiCompatibilityUtils calls."
    This reverts commit 82aef33f7e4160c7ae578f52c8e694e91b7bb0b1.

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/ChromeSwitchPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/ChromeSwitchPreference.java
@@ -16,6 +16,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 
 /**
@@ -71,7 +72,7 @@ public class ChromeSwitchPreference extends SwitchPreference {
             int right = view.getPaddingRight();
             int top = view.getPaddingTop();
             int bottom = view.getPaddingBottom();
-            view.setBackground(DividerDrawable.create(getContext()));
+            ApiCompatibilityUtils.setBackgroundForView(view, DividerDrawable.create(getContext()));
             view.setPadding(left, top, right, bottom);
         }
 
