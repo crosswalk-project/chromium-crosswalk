415028e93813fba2c8fcecda6dd3bbe667c22cce
Xiyuan Xia <xiyuan@chromium.org>
2015-07-09 18:30:57 -0700

    Merge "Fix TrackTerminatedExtension UAF"
    > Extension instance could go away while TrackTerminatedExtension task
    > scheduled in NOTIFICATION_EXTENSION_PROCESS_TERMINATED notification
    > handler is still in-fly. Change TrackTerminatedExtension to take
    > extension id and check the Extension existence to avoid such situation.
    >
    > BUG=495682
    >
    > Review URL: https://codereview.chromium.org/1159783008
    >
    > Cr-Commit-Position: refs/heads/master@{#332948}
    > (cherry picked from commit e87def2eacbc7ec43afe5be94369645dbd958358)
    >
    > Clean up TrackTerminatedExtension UAF fix CL
    >
    > Review URL: https://codereview.chromium.org/1174773002
    >
    > Cr-Commit-Position: refs/heads/master@{#333603}
    > (cherry picked from commit a30a209a0a47118e363fd1e6ade20b253a52070b)
    
    Review URL: https://codereview.chromium.org/1230063003 .
    
    Cr-Commit-Position: refs/branch-heads/2403@{#482}
    Cr-Branched-From: f54b8097a9c45ed4ad308133d49f05325d6c5070-refs/heads/master@{#330231}

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -2112,16 +2112,22 @@ void ExtensionService::RegisterContentSettings(
               profile_->GetOriginalProfile() != profile_)));
 }
 
-void ExtensionService::TrackTerminatedExtension(const Extension* extension) {
+void ExtensionService::TrackTerminatedExtension(
+    const std::string& extension_id) {
+  extensions_being_terminated_.erase(extension_id);
+
+  const Extension* extension = GetInstalledExtension(extension_id);
+  if (!extension) {
+    return;
+  }
+
   // No need to check for duplicates; inserting a duplicate is a no-op.
   registry_->AddTerminated(make_scoped_refptr(extension));
-  extensions_being_terminated_.erase(extension->id());
   UnloadExtension(extension->id(), UnloadedExtensionInfo::REASON_TERMINATE);
 }
 
 void ExtensionService::TerminateExtension(const std::string& extension_id) {
-  const Extension* extension = GetInstalledExtension(extension_id);
-  TrackTerminatedExtension(extension);
+  TrackTerminatedExtension(extension_id);
 }
 
 void ExtensionService::UntrackTerminatedExtension(const std::string& id) {
@@ -2262,7 +2268,7 @@ void ExtensionService::Observe(int type,
           base::Bind(
               &ExtensionService::TrackTerminatedExtension,
               AsWeakPtr(),
-              host->extension()));
+              host->extension()->id()));
       break;
     }
     case content::NOTIFICATION_RENDERER_PROCESS_TERMINATED: {
