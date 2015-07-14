42c00dd2b55b3366f830c35a502f1b60d988563c
timav <timav@chromium.org>
2015-07-14 13:11:35 -0700

    Use more realistic audio samples for MediaCodecPlayer tests
    These samples are encoded in AAC, I switched audio factories
    to use AAC codec. These samples allow for more relaible PTS
    checks for audio.
    
    BUG=407577
    
    Review URL: https://codereview.chromium.org/1233553002
    
    Cr-Commit-Position: refs/heads/master@{#338727}

--- media/base/android/demuxer_stream_player_params.cc
+++ media/base/android/demuxer_stream_player_params.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "media/base/android/demuxer_stream_player_params.h"
+#include <iomanip>
 
 namespace media {
 
@@ -27,3 +28,44 @@ DemuxerData::DemuxerData() : type(DemuxerStream::UNKNOWN) {}
 DemuxerData::~DemuxerData() {}
 
 }  // namespace media
+
+std::ostream& operator<<(std::ostream& os, const media::AccessUnit& au) {
+  os << "status:" << au.status << (au.is_end_of_stream ? " EOS" : "")
+     << (au.is_key_frame ? " KEY_FRAME" : "") << " pts:" << au.timestamp
+     << " size:" << au.data.size();
+  return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const media::DemuxerConfigs& conf) {
+  os << "duration:" << conf.duration;
+
+  if (conf.audio_codec == media::kUnknownAudioCodec &&
+      conf.video_codec == media::kUnknownVideoCodec) {
+    os << " no audio, no video";
+    return os;
+  }
+
+  if (conf.audio_codec != media::kUnknownAudioCodec) {
+    os << " audio:" << media::AsString(conf.audio_codec)
+       << " channels:" << conf.audio_channels
+       << " rate:" << conf.audio_sampling_rate
+       << (conf.is_audio_encrypted ? " encrypted" : "")
+       << " delay (ns):" << conf.audio_codec_delay_ns
+       << " preroll (ns):" << conf.audio_seek_preroll_ns;
+
+    if (!conf.audio_extra_data.empty()) {
+      os << " extra:{" << std::hex;
+      for (uint8 byte : conf.audio_extra_data)
+        os << " " << std::setfill('0') << std::setw(2) << (int)byte;
+      os << "}" << std::dec;
+    }
+  }
+
+  if (conf.video_codec != media::kUnknownVideoCodec) {
+    os << " video:" << media::AsString(conf.video_codec) << " "
+       << conf.video_size.width() << "x" << conf.video_size.height()
+       << (conf.is_video_encrypted ? " encrypted" : "");
+  }
+
+  return os;
+}
