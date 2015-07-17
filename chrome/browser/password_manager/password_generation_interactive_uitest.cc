2159396cdb2b87c3b30494574eaf441a94980fb1
Gabriel Charette <gab@chromium.org>
2015-07-17 13:59:15 -0400

    Revert "[M44] [Password Generation] Save passwords even if heuristics determine submit failed"
    This reverts commit 3ed505d6650e9129045c5f5c547ba5dea3a7eb7f.
    
    TBR=vabr@chromium.org
    BUG=506307,511125
    
    Review URL: https://codereview.chromium.org/1236393004 .
    
    Cr-Commit-Position: refs/branch-heads/2403@{#529}
    Cr-Branched-From: f54b8097a9c45ed4ad308133d49f05325d6c5070-refs/heads/master@{#330231}

--- chrome/browser/password_manager/password_generation_interactive_uitest.cc
+++ chrome/browser/password_manager/password_generation_interactive_uitest.cc
@@ -3,10 +3,7 @@
 // found in the LICENSE file.
 
 #include "base/command_line.h"
-#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/password_manager/chrome_password_manager_client.h"
-#include "chrome/browser/password_manager/password_manager_test_base.h"
-#include "chrome/browser/password_manager/password_store_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/autofill/password_generation_popup_observer.h"
 #include "chrome/browser/ui/browser.h"
@@ -16,7 +13,6 @@
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/common/autofill_switches.h"
 #include "components/password_manager/core/browser/password_generation_manager.h"
-#include "components/password_manager/core/browser/test_password_store.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
@@ -50,8 +46,7 @@ class TestPopupObserver : public autofill::PasswordGenerationPopupObserver {
 
 }  // namespace
 
-class PasswordGenerationInteractiveTest :
-    public PasswordManagerBrowserTestBase {
+class PasswordGenerationInteractiveTest : public InProcessBrowserTest {
  public:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     // Make sure the feature is enabled.
@@ -63,33 +58,39 @@ class PasswordGenerationInteractiveTest :
   }
 
   void SetUpOnMainThread() override {
-    PasswordManagerBrowserTestBase::SetUpOnMainThread();
-
     // Disable Autofill requesting access to AddressBook data. This will cause
     // the tests to hang on Mac.
     autofill::test::DisableSystemServices(browser()->profile()->GetPrefs());
 
     // Set observer for popup.
     ChromePasswordManagerClient* client =
-        ChromePasswordManagerClient::FromWebContents(WebContents());
+        ChromePasswordManagerClient::FromWebContents(GetWebContents());
     client->SetTestObserver(&observer_);
 
-    NavigateToFile("/password/signup_form.html");
+    ASSERT_TRUE(embedded_test_server()->InitializeAndWaitUntilReady());
+    GURL url = embedded_test_server()->GetURL("/password/signup_form.html");
+    ui_test_utils::NavigateToURL(browser(), url);
   }
 
   void TearDownOnMainThread() override {
-    PasswordManagerBrowserTestBase::TearDownOnMainThread();
-
     // Clean up UI.
     ChromePasswordManagerClient* client =
-        ChromePasswordManagerClient::FromWebContents(WebContents());
+        ChromePasswordManagerClient::FromWebContents(GetWebContents());
     client->HidePasswordGenerationPopup();
+  }
+
+  content::WebContents* GetWebContents() {
+    return browser()->tab_strip_model()->GetActiveWebContents();
+  }
+
+  content::RenderViewHost* GetRenderViewHost() {
+    return GetWebContents()->GetRenderViewHost();
   }
 
   std::string GetFieldValue(const std::string& field_id) {
     std::string value;
     EXPECT_TRUE(content::ExecuteScriptAndExtractString(
-        RenderViewHost(),
+        GetRenderViewHost(),
         "window.domAutomationController.send("
         "    document.getElementById('" + field_id + "').value);",
         &value));
@@ -99,7 +100,7 @@ class PasswordGenerationInteractiveTest :
   std::string GetFocusedElement() {
     std::string focused_element;
     EXPECT_TRUE(content::ExecuteScriptAndExtractString(
-        RenderViewHost(),
+        GetRenderViewHost(),
         "window.domAutomationController.send("
         "    document.activeElement.id)",
         &focused_element));
@@ -108,7 +109,7 @@ class PasswordGenerationInteractiveTest :
 
   void FocusPasswordField() {
     ASSERT_TRUE(content::ExecuteScript(
-        RenderViewHost(),
+        GetRenderViewHost(),
         "document.getElementById('password_field').focus()"));
   }
 
@@ -116,7 +117,7 @@ class PasswordGenerationInteractiveTest :
     content::NativeWebKeyboardEvent event;
     event.windowsKeyCode = key;
     event.type = blink::WebKeyboardEvent::RawKeyDown;
-    RenderViewHost()->ForwardKeyboardEvent(event);
+    GetRenderViewHost()->ForwardKeyboardEvent(event);
   }
 
   bool GenerationPopupShowing() {
@@ -169,7 +170,7 @@ IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
   FocusPasswordField();
   EXPECT_TRUE(GenerationPopupShowing());
 
-  ASSERT_TRUE(content::ExecuteScript(RenderViewHost(),
+  ASSERT_TRUE(content::ExecuteScript(GetRenderViewHost(),
                                      "window.scrollTo(100, 0);"));
 
   EXPECT_FALSE(GenerationPopupShowing());
@@ -178,49 +179,15 @@ IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
 // Disabled due to flakiness due to resizes, see http://crbug.com/407998.
 IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
                        DISABLED_GenerationTriggeredInIFrame) {
-  NavigateToFile("/password/framed_signup_form.html");
+  GURL url = embedded_test_server()->GetURL(
+      "/password/framed_signup_form.html");
+  ui_test_utils::NavigateToURL(browser(), url);
 
   std::string focus_script =
       "var frame = document.getElementById('signup_iframe');"
       "var frame_doc = frame.contentDocument;"
       "frame_doc.getElementById('password_field').focus();";
 
-  ASSERT_TRUE(content::ExecuteScript(RenderViewHost(), focus_script));
+  ASSERT_TRUE(content::ExecuteScript(GetRenderViewHost(), focus_script));
   EXPECT_TRUE(GenerationPopupShowing());
-}
-
-// Disabled due to flakiness due to resizes, see http://crbug.com/407998.
-IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
-                       DISABLED_AutoSavingGeneratedPassword) {
-  password_manager::TestPasswordStore* password_store =
-      static_cast<password_manager::TestPasswordStore*>(
-          PasswordStoreFactory::GetForProfile(
-              browser()->profile(), ServiceAccessType::IMPLICIT_ACCESS).get());
-
-  FocusPasswordField();
-  EXPECT_TRUE(GenerationPopupShowing());
-  SendKeyToPopup(ui::VKEY_DOWN);
-  SendKeyToPopup(ui::VKEY_RETURN);
-
-  // Change username and submit.
-  NavigationObserver observer(WebContents());
-  std::string submit_script =
-      "document.getElementById('username_field').value = 'something';"
-      "document.getElementById('input_submit_button').click()";
-  ASSERT_TRUE(content::ExecuteScript(RenderViewHost(), submit_script));
-  observer.Wait();
-
-  // Spin the message loop to make sure the password store had a chance to save
-  // the password.
-  base::RunLoop run_loop;
-  run_loop.RunUntilIdle();
-  EXPECT_FALSE(password_store->IsEmpty());
-
-  // Make sure the username is correct.
-  password_manager::TestPasswordStore::PasswordMap stored_passwords =
-      password_store->stored_passwords();
-  EXPECT_EQ(1u, stored_passwords.size());
-  EXPECT_EQ(1u, stored_passwords.begin()->second.size());
-  EXPECT_EQ(base::UTF8ToUTF16("something"),
-            (stored_passwords.begin()->second)[0].username_value);
 }
