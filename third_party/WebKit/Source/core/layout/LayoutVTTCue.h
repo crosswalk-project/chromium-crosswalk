8ec26eafbff8c397f291e047bde804980685fec9
philipj@opera.com <philipj@opera.com>
2015-07-28 12:46:08 +0000

    Merge 199160 "Separate VTTCue from VTTCueBox and LayoutVTTCue"
    > Separate VTTCue from VTTCueBox and LayoutVTTCue
    > 
    > Both VTTCueBox and LayoutVTTCue had a VTTCue member, which made for
    > tricky lifetime considerations, in particular the VTTCue destructor
    > was removing the VTTCueBox from the DOM, so that the stale VTTCue*
    > pointers would never be accessed.
    > 
    > Instead, let VTTCue provide VTTCueBox with the information it needs,
    > which will in turn provide it to LayoutVTTCue. Stale information
    > should not be a problem, as all relevant changes to VTTCue will call
    > cueDidChange(), whichs ends up removing and re-inserting the cue.
    > 
    > Refactoring only, no web-observable changes are intended.
    > 
    > BUG=511174, 509911
    > R=fs@opera.com
    > 
    > Review URL: https://codereview.chromium.org/1240433007
    
    TBR=philipj@opera.com
    
    Review URL: https://codereview.chromium.org/1257273003
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2454@199578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutVTTCue.h
+++ third_party/WebKit/Source/core/layout/LayoutVTTCue.h
@@ -31,12 +31,15 @@
 
 namespace blink {
 
-class VTTCue;
 class VTTCueBox;
 
 class LayoutVTTCue final : public LayoutBlockFlow {
 public:
-    explicit LayoutVTTCue(VTTCueBox*);
+    LayoutVTTCue(ContainerNode*, float snapToLinesPosition);
+
+    // The computed line position for snap-to-lines layout, and NaN for
+    // non-snap-to-lines layout where no adjustment should take place.
+    float snapToLinesPosition() { return m_snapToLinesPosition; }
 
 private:
     virtual void layout() override;
@@ -44,7 +47,7 @@ private:
     void adjustForTopAndBottomMarginBorderAndPadding();
     void repositionCueSnapToLinesNotSet();
 
-    VTTCue* m_cue;
+    float m_snapToLinesPosition;
 };
 
 } // namespace blink
