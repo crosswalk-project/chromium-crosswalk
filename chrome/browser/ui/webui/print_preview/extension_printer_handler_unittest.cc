30b1422a851fb2a33c635a7c79f9a9c00e7afe4c
Reilly Grant <reillyg@chromium.org>
2015-08-04 16:40:34 -0700

    Merge M-45: Stablize 'usb_printers' extension manifest key.
    The new permissions flow for USB printers feature is trying to launch in
    M-45 and I forgot to stablize this before the branch point.
    
    BUG=484546
    TBR=vitalybuka@chromium.org,asargent@chromium.org
    
    Review URL: https://codereview.chromium.org/1269083004
    
    Cr-Commit-Position: refs/heads/master@{#341628}
    (cherry-picked from commit 28fd987718745889af0bf774d9fd9d58c3bb588a)
    
    Review URL: https://codereview.chromium.org/1265353002 .
    
    Cr-Commit-Position: refs/branch-heads/2454@{#227}
    Cr-Branched-From: 12bfc3360892ec53cd00fc239a47e5298beb063b-refs/heads/master@{#338390}

--- chrome/browser/ui/webui/print_preview/extension_printer_handler_unittest.cc
+++ chrome/browser/ui/webui/print_preview/extension_printer_handler_unittest.cc
@@ -20,7 +20,6 @@
 #include "chrome/browser/extensions/test_extension_environment.h"
 #include "chrome/browser/local_discovery/pwg_raster_converter.h"
 #include "chrome/browser/ui/webui/print_preview/extension_printer_handler.h"
-#include "chrome/common/extensions/features/feature_channel.h"
 #include "chrome/test/base/testing_profile.h"
 #include "device/core/device_client.h"
 #include "device/usb/mock_usb_device.h"
@@ -553,8 +552,6 @@ TEST_F(ExtensionPrinterHandlerTest, GetPrinters_Reset) {
 }
 
 TEST_F(ExtensionPrinterHandlerTest, GetUsbPrinters) {
-  extensions::ScopedCurrentChannel channel(chrome::VersionInfo::CHANNEL_DEV);
-
   scoped_refptr<MockUsbDevice> device0 =
       new MockUsbDevice(0, 0, "Google", "USB Printer", "");
   usb_service_.AddDevice(device0);
