9984991d77cf2681a9701af63d00a1cd8b03b686
Ian Wen <ianwen@chromium.org>
2015-08-14 14:10:27 -0700

    Fix Custom Action Bar for custom tabs on tablet
    Custom tabs on tablet is special in that it does not have a tab strip.
    Therefore ContextualMenuBar will calculate the wrong offset when
    animating the bar.
    
    BUG=510284
    
    Review URL: https://codereview.chromium.org/1292693002
    
    Cr-Commit-Position: refs/heads/master@{#343437}
    TBR=dtrainor@chromium.org
    
    Review URL: https://codereview.chromium.org/1298483004 .
    
    Cr-Commit-Position: refs/branch-heads/2454@{#321}
    Cr-Branched-From: 12bfc3360892ec53cd00fc239a47e5298beb063b-refs/heads/master@{#338390}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
@@ -195,6 +195,11 @@ public class CustomTabToolbar extends ToolbarLayout implements LocationBar,
     }
 
     @Override
+    public int getTabStripHeight() {
+        return 0;
+    }
+
+    @Override
     public ChromeTab getCurrentTab() {
         return ChromeTab.fromTab(getToolbarDataProvider().getTab());
     }
