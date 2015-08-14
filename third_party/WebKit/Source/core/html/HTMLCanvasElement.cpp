crosswalk-project/chromium-crosswalk

commit 3b11986ef4697910f0a9cd60b4327b8bf531f266

Removing dependency on SkDeferredCanvas from blink.
This change refactors Canvas2DLayerBridge to leverage the overdraw
optimization logic that is now built-in to SkCanvas (for copy on write)
and CanvasRenderingContext2D (for frame skipping), thus rendering
SkDeferredCAnvas unnecessary. A side-effect of this refactor it
replaces SkBitmaps with SkImages in many places.

The rebaselines are imperceptible pixel differences due to the slightly
differently formulated arithmetic in the StaticBitmapImage/SkImage
code paths. Use cases that resize images seem to be the most sensitive
to this change.

This change removes the Canvas2DLayerManager completely, so we will
need to follow-up with automatic flushing heuristics similar to
what is in RecordingImageBufferSurface in order to keep a cap on
resource consumption.

BUG=512991

Review URL: https://codereview.chromium.org/1259763002

git-svn-id: svn://svn.chromium.org/blink/trunk@199900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -474,18 +474,26 @@ ImageData* HTMLCanvasElement::toImageData(SourceDrawingBuffer sourceBuffer) cons
 
         m_context->paintRenderingResultsToCanvas(sourceBuffer);
         imageData = ImageData::create(m_size);
-        SkImageInfo imageInfo = SkImageInfo::Make(width(), height(), kRGBA_8888_SkColorType, kUnpremul_SkAlphaType);
-        buffer()->bitmap().readPixels(imageInfo, imageData->data()->data(), imageInfo.minRowBytes(), 0, 0);
+        RefPtr<SkImage> snapshot = buffer()->newSkImageSnapshot();
+        if (snapshot) {
+            SkImageInfo imageInfo = SkImageInfo::Make(width(), height(), kRGBA_8888_SkColorType, kUnpremul_SkAlphaType);
+            snapshot->readPixels(imageInfo, imageData->data()->data(), imageInfo.minRowBytes(), 0, 0);
+        }
         return imageData;
     }
 
     imageData = ImageData::create(m_size);
 
-    if (m_context) {
-        ASSERT(m_context->is2d());
+    if (!m_context)
+        return imageData;
+
+    ASSERT(m_context->is2d());
+    RefPtr<SkImage> snapshot = buffer()->newSkImageSnapshot();
+    if (snapshot) {
         SkImageInfo imageInfo = SkImageInfo::Make(width(), height(), kRGBA_8888_SkColorType, kUnpremul_SkAlphaType);
-        buffer()->bitmap().readPixels(imageInfo, imageData->data()->data(), imageInfo.minRowBytes(), 0, 0);
+        snapshot->readPixels(imageInfo, imageData->data()->data(), imageInfo.minRowBytes(), 0, 0);
     }
+
     return imageData;
 }
 
@@ -785,7 +793,7 @@ PassRefPtr<Image> HTMLCanvasElement::copiedImage(SourceDrawingBuffer sourceBuffe
     if (m_context->is3d())
         needToUpdate |= m_context->paintRenderingResultsToCanvas(sourceBuffer);
     if (needToUpdate && buffer()) {
-        m_copiedImage = buffer()->copyImage(CopyBackingStore, Unscaled);
+        m_copiedImage = buffer()->newImageSnapshot();
         updateExternallyAllocatedMemory();
     }
     return m_copiedImage;
@@ -840,7 +848,7 @@ void HTMLCanvasElement::didMoveToNewDocument(Document& oldDocument)
     HTMLElement::didMoveToNewDocument(oldDocument);
 }
 
-PassRefPtr<Image> HTMLCanvasElement::getSourceImageForCanvas(SourceImageMode mode, SourceImageStatus* status) const
+PassRefPtr<Image> HTMLCanvasElement::getSourceImageForCanvas(SourceImageStatus* status) const
 {
     if (!width() || !height()) {
         *status = ZeroSizeCanvasSourceImageStatus;
@@ -857,13 +865,11 @@ PassRefPtr<Image> HTMLCanvasElement::getSourceImageForCanvas(SourceImageMode mod
         return createTransparentImage(size());
     }
 
-    m_imageBuffer->willAccessPixels();
-
     if (m_context->is3d()) {
         m_context->paintRenderingResultsToCanvas(BackBuffer);
     }
 
-    RefPtr<SkImage> image = buffer()->newImageSnapshot();
+    RefPtr<SkImage> image = buffer()->newSkImageSnapshot();
     if (image) {
         *status = NormalSourceImageStatus;
         return StaticBitmapImage::create(image.release());

third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -153,7 +153,7 @@ public:
     void didChangeVisibilityState(PageVisibilityState) override;
 
     // CanvasImageSource implementation
-    PassRefPtr<Image> getSourceImageForCanvas(SourceImageMode, SourceImageStatus*) const override;
+    PassRefPtr<Image> getSourceImageForCanvas(SourceImageStatus*) const override;
     bool wouldTaintOrigin(SecurityOrigin*) const override;
     FloatSize elementSize() const override;
     bool isCanvasElement() const override { return true; }

third_party/WebKit/Source/modules/canvas2d/CanvasRenderingContext2D.cpp
@@ -1370,8 +1370,7 @@ void CanvasRenderingContext2D::drawImage(CanvasImageSource* imageSource,
     RefPtr<Image> image;
     SourceImageStatus sourceImageStatus = InvalidSourceImageStatus;
     if (!imageSource->isVideoElement()) {
-        SourceImageMode mode = canvas() == imageSource ? CopySourceImageIfVolatile : DontCopySourceImage; // Thunking for ==
-        image = imageSource->getSourceImageForCanvas(mode, &sourceImageStatus);
+        image = imageSource->getSourceImageForCanvas(&sourceImageStatus);
         if (sourceImageStatus == UndecodableSourceImageStatus)
             exceptionState.throwDOMException(InvalidStateError, "The HTMLImageElement provided is in the 'broken' state.");
         if (!image || !image->width() || !image->height())
@@ -1398,14 +1397,6 @@ void CanvasRenderingContext2D::drawImage(CanvasImageSource* imageSource,
 
     if (imageSource->isVideoElement())
         canvas()->buffer()->willDrawVideo();
-
-    // FIXME: crbug.com/447218
-    // We make the destination canvas fall out of display list mode by calling
-    // willAccessPixels. This is to prevent run-away memory consumption caused by SkSurface
-    // copyOnWrite when the source canvas is animated and consumed at a rate higher than the
-    // presentation frame rate of the destination canvas.
-    if (imageSource->isCanvasElement())
-        canvas()->buffer()->willAccessPixels();
 
     draw(
         [this, &imageSource, &image, &srcRect, dstRect](const SkPaint* paint) // draw lambda
@@ -1436,8 +1427,20 @@ void CanvasRenderingContext2D::drawImage(CanvasImageSource* imageSource,
             buffer->setHasExpensiveOp();
     }
 
-    if (sourceImageStatus == ExternalSourceImageStatus && isAccelerated() && canvas()->buffer())
-        canvas()->buffer()->flush();
+    if (imageSource->isCanvasElement()) {
+        if (static_cast<HTMLCanvasElement*>(imageSource)->is3D()) {
+            // WebGL to 2D canvas: must flush graphics context to prevent a race
+            // FIXME: crbug.com/516331 Fix the underlying synchronization issue so this flush can be eliminated.
+            canvas()->buffer()->flushGpu();
+        } else {
+            // FIXME: crbug.com/447218
+            // We make the destination canvas fall out of display list mode by calling
+            // flush. This is to prevent run-away memory consumption caused by SkSurface
+            // copyOnWrite when the source canvas is animated and consumed at a rate higher than the
+            // presentation frame rate of the destination canvas.
+            canvas()->buffer()->flush();
+        }
+    }
 
     if (canvas()->originClean() && wouldTaintOrigin(imageSource))
         canvas()->setOriginTainted();
@@ -1483,7 +1486,7 @@ CanvasPattern* CanvasRenderingContext2D::createPattern(const CanvasImageSourceUn
 
     SourceImageStatus status;
     CanvasImageSource* imageSourceInternal = toImageSourceInternal(imageSource);
-    RefPtr<Image> imageForRendering = imageSourceInternal->getSourceImageForCanvas(CopySourceImageIfVolatile, &status);
+    RefPtr<Image> imageForRendering = imageSourceInternal->getSourceImageForCanvas(&status);
 
     switch (status) {
     case NormalSourceImageStatus:
@@ -1500,7 +1503,6 @@ CanvasPattern* CanvasRenderingContext2D::createPattern(const CanvasImageSourceUn
     case IncompleteSourceImageStatus:
         return nullptr;
     default:
-    case ExternalSourceImageStatus: // should not happen when mode is CopySourceImageIfVolatile
         ASSERT_NOT_REACHED();
         return nullptr;
     }

third_party/WebKit/Source/platform/graphics/Canvas2DImageBufferSurface.h
@@ -56,7 +56,7 @@ public:
 
     // ImageBufferSurface implementation
     void finalizeFrame(const FloatRect &dirtyRect) override { m_layerBridge->finalizeFrame(dirtyRect); }
-    void willAccessPixels() override { m_layerBridge->willAccessPixels(); }
+    void willOverwriteCanvas() override { m_layerBridge->willOverwriteCanvas(); }
     SkCanvas* canvas() const override { return m_layerBridge->canvas(); }
     bool isValid() const override { return m_layerBridge && m_layerBridge->checkSurfaceValid(); }
     bool restore() override { return m_layerBridge->restoreSurface(); }
@@ -66,8 +66,11 @@ public:
     void setIsHidden(bool hidden) override { m_layerBridge->setIsHidden(hidden); }
     void setImageBuffer(ImageBuffer* imageBuffer) override { m_layerBridge->setImageBuffer(imageBuffer); }
     void didDraw(const FloatRect& rect) override { m_layerBridge->didDraw(); }
+    void flush() override { m_layerBridge->flush(); }
+    void flushGpu() override { m_layerBridge->flushGpu(); }
+    bool writePixels(const SkImageInfo& origInfo, const void* pixels, size_t rowBytes, int x, int y) override { return m_layerBridge->writePixels(origInfo, pixels, rowBytes, x, y); }
 
-    PassRefPtr<SkImage> newImageSnapshot() const override { return m_layerBridge->newImageSnapshot(); }
+    PassRefPtr<SkImage> newImageSnapshot() override { return m_layerBridge->newImageSnapshot(); }
 private:
     RefPtr<Canvas2DLayerBridge> m_layerBridge;
 };

commit 14cf421f3766af3aaa92ec58c538bd7be4ab2a13

Eliminate deferral overhead with canvas to canvas draws
This fixes a performance regression from r199900 which caused deferred
canvas operations to be flushed all the way down to the GrContext level
every time a canvas to canvas draw was performed. This was significantly
slowing down uses cases the use canvases as sprite atlases. This change
does more that fix the performance regression. By stoping the canvas
from switching back and forth between immediate and deferred rendering,
we get ~2x perf boost in cases where a GPU-accelerated canvas is used
as an atlas.

BUG=517504
TEST=tough_canvas_cases/canvas2d_balls_common/bouncing_balls.html?ball=canvas_sprite&back=canvas
NOTRY=true

Review URL: https://codereview.chromium.org/1297663002

git-svn-id: svn://svn.chromium.org/blink/trunk@200545 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -744,9 +744,10 @@ SkCanvas* HTMLCanvasElement::drawingCanvas() const
     return buffer() ? m_imageBuffer->canvas() : nullptr;
 }
 
-SkCanvas* HTMLCanvasElement::immediateDrawingCanvas() const
+void HTMLCanvasElement::disableDeferral() const
 {
-    return buffer() ? m_imageBuffer->immediateCanvas() : nullptr;
+    if (buffer())
+        m_imageBuffer->disableDeferral();
 }
 
 SkCanvas* HTMLCanvasElement::existingDrawingCanvas() const

third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -120,7 +120,7 @@ public:
     void paint(GraphicsContext*, const LayoutRect&);
 
     SkCanvas* drawingCanvas() const;
-    SkCanvas* immediateDrawingCanvas() const; // Guarantees draws will not be deferred. Use sparingly.
+    void disableDeferral() const;
     SkCanvas* existingDrawingCanvas() const;
 
     void setRenderingContext(PassOwnPtrWillBeRawPtr<CanvasRenderingContext>);

third_party/WebKit/Source/modules/canvas2d/CanvasRenderingContext2D.cpp
@@ -879,16 +879,12 @@ void CanvasRenderingContext2D::compositedDraw(const DrawFunc& drawFunc, SkCanvas
 }
 
 template<typename DrawFunc, typename ContainsFunc>
-bool CanvasRenderingContext2D::draw(const DrawFunc& drawFunc, const ContainsFunc& drawCoversClipBounds, CanvasDeferralMode deferralMode, const SkRect& bounds, CanvasRenderingContext2DState::PaintType paintType, CanvasRenderingContext2DState::ImageType imageType)
+bool CanvasRenderingContext2D::draw(const DrawFunc& drawFunc, const ContainsFunc& drawCoversClipBounds, const SkRect& bounds, CanvasRenderingContext2DState::PaintType paintType, CanvasRenderingContext2DState::ImageType imageType)
 {
     if (!state().isTransformInvertible())
         return false;
 
     SkIRect clipBounds;
-    // Deliberately not using 'deferralMode' in the call to drawingCanvas below because
-    // a) The call does not write anything so we do not care about the write mode here
-    // b) We want to avoid flushing before the call to checkOverdraw (below), otherwise
-    //    we could be supressing an overdraw optimization.
     if (!drawingCanvas()->getClipDeviceBounds(&clipBounds))
         return false;
 
@@ -901,12 +897,12 @@ bool CanvasRenderingContext2D::draw(const DrawFunc& drawFunc, const ContainsFunc
     }
 
     if (isFullCanvasCompositeMode(state().globalComposite()) || state().hasFilter()) {
-        compositedDraw(drawFunc, drawingCanvas(deferralMode), paintType, imageType);
+        compositedDraw(drawFunc, drawingCanvas(), paintType, imageType);
         didDraw(clipBounds);
     } else if (state().globalComposite() == SkXfermode::kSrc_Mode) {
         clearCanvas(); // takes care of checkOvewrdraw()
         const SkPaint* paint = state().getPaint(paintType, DrawForegroundOnly, imageType);
-        drawFunc(drawingCanvas(deferralMode), paint);
+        drawFunc(drawingCanvas(), paint);
         didDraw(clipBounds);
     } else {
         SkIRect dirtyRect;
@@ -914,7 +910,7 @@ bool CanvasRenderingContext2D::draw(const DrawFunc& drawFunc, const ContainsFunc
             const SkPaint* paint = state().getPaint(paintType, DrawShadowAndForeground, imageType);
             if (paintType != CanvasRenderingContext2DState::StrokePaintType && drawCoversClipBounds(clipBounds))
                 checkOverdraw(bounds, paint, imageType, ClipFill);
-            drawFunc(drawingCanvas(deferralMode), paint);
+            drawFunc(drawingCanvas(), paint);
             didDraw(dirtyRect);
         }
     }
@@ -956,7 +952,7 @@ void CanvasRenderingContext2D::drawPathInternal(const Path& path, CanvasRenderin
         [](const SkIRect& rect) // overdraw test lambda
         {
             return false;
-        }, AllowDeferredCanvas, bounds, paintType)) {
+        }, bounds, paintType)) {
         if (isPathExpensive(path)) {
             ImageBuffer* buffer = canvas()->buffer();
             if (buffer)
@@ -1013,7 +1009,7 @@ void CanvasRenderingContext2D::fillRect(float x, float y, float width, float hei
         [&rect, this](const SkIRect& clipBounds) // overdraw test lambda
         {
             return rectContainsTransformedRect(rect, clipBounds);
-        }, AllowDeferredCanvas, rect, CanvasRenderingContext2DState::FillPaintType);
+        }, rect, CanvasRenderingContext2DState::FillPaintType);
 }
 
 static void strokeRectOnCanvas(const FloatRect& rect, SkCanvas* canvas, const SkPaint* paint)
@@ -1050,7 +1046,7 @@ void CanvasRenderingContext2D::strokeRect(float x, float y, float width, float h
         [](const SkIRect& clipBounds) // overdraw test lambda
         {
             return false;
-        }, AllowDeferredCanvas, bounds, CanvasRenderingContext2DState::StrokePaintType);
+        }, bounds, CanvasRenderingContext2DState::StrokePaintType);
 }
 
 void CanvasRenderingContext2D::clipInternal(const Path& path, const String& windingRuleString)
@@ -1392,7 +1388,13 @@ void CanvasRenderingContext2D::drawImage(CanvasImageSource* imageSource,
     if (srcRect.isEmpty())
         return;
 
-    CanvasDeferralMode deferralMode = imageSource->isVideoElement() ? ForceImmediateCanvas : AllowDeferredCanvas;
+    // FIXME: crbug.com/521001
+    // We make the destination canvas fall out of display list mode by forcing
+    // immediate rendering. This is to prevent run-away memory consumption caused by SkSurface
+    // copyOnWrite when the source canvas is animated and consumed at a rate higher than the
+    // presentation frame rate of the destination canvas.
+    if (imageSource->isVideoElement() || imageSource->isCanvasElement())
+        canvas()->disableDeferral();
 
     validateStateStack();
 
@@ -1404,7 +1406,7 @@ void CanvasRenderingContext2D::drawImage(CanvasImageSource* imageSource,
         [this, &dstRect](const SkIRect& clipBounds) // overdraw test lambda
         {
             return rectContainsTransformedRect(dstRect, clipBounds);
-        }, deferralMode, dstRect, CanvasRenderingContext2DState::ImagePaintType,
+        }, dstRect, CanvasRenderingContext2DState::ImagePaintType,
         imageSource->isOpaque() ? CanvasRenderingContext2DState::OpaqueImage : CanvasRenderingContext2DState::NonOpaqueImage);
 
     validateStateStack();
@@ -1423,19 +1425,10 @@ void CanvasRenderingContext2D::drawImage(CanvasImageSource* imageSource,
             buffer->setHasExpensiveOp();
     }
 
-    if (imageSource->isCanvasElement()) {
-        if (static_cast<HTMLCanvasElement*>(imageSource)->is3D()) {
-            // WebGL to 2D canvas: must flush graphics context to prevent a race
-            // FIXME: crbug.com/516331 Fix the underlying synchronization issue so this flush can be eliminated.
-            canvas()->buffer()->flushGpu();
-        } else {
-            // FIXME: crbug.com/447218
-            // We make the destination canvas fall out of display list mode by calling
-            // flush. This is to prevent run-away memory consumption caused by SkSurface
-            // copyOnWrite when the source canvas is animated and consumed at a rate higher than the
-            // presentation frame rate of the destination canvas.
-            canvas()->buffer()->flush();
-        }
+    if (imageSource->isCanvasElement() && static_cast<HTMLCanvasElement*>(imageSource)->is3D()) {
+        // WebGL to 2D canvas: must flush graphics context to prevent a race
+        // FIXME: crbug.com/516331 Fix the underlying synchronization issue so this flush can be eliminated.
+        canvas()->buffer()->flushGpu();
     }
 
     if (canvas()->originClean() && wouldTaintOrigin(imageSource))
@@ -1446,8 +1439,9 @@ void CanvasRenderingContext2D::clearCanvas()
 {
     FloatRect canvasRect(0, 0, canvas()->width(), canvas()->height());
     checkOverdraw(canvasRect, 0, CanvasRenderingContext2DState::NoImage, ClipFill);
-    if (drawingCanvas())
-        drawingCanvas()->clear(m_hasAlpha ? SK_ColorTRANSPARENT : SK_ColorBLACK);
+    SkCanvas* c = drawingCanvas();
+    if (c)
+        c->clear(m_hasAlpha ? SK_ColorTRANSPARENT : SK_ColorBLACK);
 }
 
 bool CanvasRenderingContext2D::rectContainsTransformedRect(const FloatRect& rect, const SkIRect& transformedRect) const
@@ -1553,11 +1547,11 @@ void CanvasRenderingContext2D::didDraw(const SkIRect& dirtyRect)
     canvas()->didDraw(SkRect::Make(dirtyRect));
 }
 
-SkCanvas* CanvasRenderingContext2D::drawingCanvas(CanvasDeferralMode deferralMode) const
+SkCanvas* CanvasRenderingContext2D::drawingCanvas() const
 {
     if (isContextLost())
         return nullptr;
-    return deferralMode == ForceImmediateCanvas ? canvas()->immediateDrawingCanvas() : canvas()->drawingCanvas();
+    return canvas()->drawingCanvas();
 }
 
 ImageData* CanvasRenderingContext2D::createImageData(ImageData* imageData) const
@@ -2020,7 +2014,7 @@ void CanvasRenderingContext2D::drawTextInternal(const String& text, float x, flo
         {
             return false;
         },
-        AllowDeferredCanvas, textRunPaintInfo.bounds, paintType);
+        textRunPaintInfo.bounds, paintType);
 }
 
 void CanvasRenderingContext2D::inflateStrokeRect(FloatRect& rect) const

third_party/WebKit/Source/modules/canvas2d/CanvasRenderingContext2D.h
@@ -242,12 +242,7 @@ private:
     bool computeDirtyRect(const FloatRect& localBounds, const SkIRect& transformedClipBounds, SkIRect*);
     void didDraw(const SkIRect&);
 
-    enum CanvasDeferralMode {
-        AllowDeferredCanvas,
-        ForceImmediateCanvas,
-    };
-
-    SkCanvas* drawingCanvas(CanvasDeferralMode = AllowDeferredCanvas) const;
+    SkCanvas* drawingCanvas() const;
 
     void unwindStateStack();
     void realizeSaves();
@@ -258,7 +253,7 @@ private:
     bool shouldDrawImageAntialiased(const FloatRect& destRect) const;
 
     template<typename DrawFunc, typename ContainsFunc>
-    bool draw(const DrawFunc&, const ContainsFunc&, CanvasDeferralMode, const SkRect& bounds, CanvasRenderingContext2DState::PaintType, CanvasRenderingContext2DState::ImageType = CanvasRenderingContext2DState::NoImage);
+    bool draw(const DrawFunc&, const ContainsFunc&, const SkRect& bounds, CanvasRenderingContext2DState::PaintType, CanvasRenderingContext2DState::ImageType = CanvasRenderingContext2DState::NoImage);
     void drawPathInternal(const Path&, CanvasRenderingContext2DState::PaintType, SkPath::FillType = SkPath::kWinding_FillType);
     void drawImageInternal(SkCanvas*, CanvasImageSource*, Image*, const FloatRect& srcRect, const FloatRect& dstRect, const SkPaint*);
     void clipInternal(const Path&, const String& windingRuleString);

third_party/WebKit/Source/platform/graphics/Canvas2DImageBufferSurface.h
@@ -58,7 +58,7 @@ public:
     void finalizeFrame(const FloatRect &dirtyRect) override { m_layerBridge->finalizeFrame(dirtyRect); }
     void willOverwriteCanvas() override { m_layerBridge->willOverwriteCanvas(); }
     SkCanvas* canvas() override { return m_layerBridge->canvas(); }
-    SkCanvas* immediateCanvas() override { return m_layerBridge->immediateCanvas(); }
+    void disableDeferral() override { m_layerBridge->disableDeferral(); }
     bool isValid() const override { return m_layerBridge && m_layerBridge->checkSurfaceValid(); }
     bool restore() override { return m_layerBridge->restoreSurface(); }
     WebLayer* layer() const override { return m_layerBridge->layer(); }

third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridge.cpp
@@ -98,6 +98,7 @@ Canvas2DLayerBridge::Canvas2DLayerBridge(PassOwnPtr<WebGraphicsContext3DProvider
     , m_rateLimitingEnabled(false)
     , m_filterQuality(kLow_SkFilterQuality)
     , m_isHidden(false)
+    , m_isDeferralEnabled(true)
     , m_lastImageId(0)
     , m_lastFilter(GL_LINEAR)
     , m_opacityMode(opacityMode)
@@ -132,6 +133,7 @@ Canvas2DLayerBridge::~Canvas2DLayerBridge()
 
 void Canvas2DLayerBridge::startRecording()
 {
+    ASSERT(m_isDeferralEnabled);
     m_recorder = adoptPtr(new SkPictureRecorder);
     m_recorder->beginRecording(m_size.width(), m_size.height(), nullptr);
     if (m_imageBuffer) {
@@ -141,26 +143,36 @@ void Canvas2DLayerBridge::startRecording()
 
 SkCanvas* Canvas2DLayerBridge::canvas()
 {
+    if (!m_isDeferralEnabled)
+        return m_surface->getCanvas();
     return m_recorder->getRecordingCanvas();
 }
 
-SkCanvas* Canvas2DLayerBridge::immediateCanvas()
+void Canvas2DLayerBridge::disableDeferral()
 {
-    if (!m_surface)
-        return nullptr;
+    // Disabling deferral is permanent: once triggered by disableDeferral()
+    // we stay in immediate mode indefinitely. This is a performance heuristic
+    // that significantly helps a number of use cases. The rationale is that if
+    // immediate rendering was needed once, it is likely to be needed at least
+    // once per frame, which eliminates the possibility for inter-frame
+    // overdraw optimization. Furthermore, in cases where immediate mode is
+    // required multiple times per frame, the repeated flushing of deferred
+    // commands would cause significant overhead, so it is better to just stop
+    // trying to defer altogether.
+    if (!m_isDeferralEnabled)
+        return;
+
+    m_isDeferralEnabled = false;
     flushRecordingOnly();
-
+    m_recorder.clear();
     // install the current matrix/clip stack onto the immediate canvas
-    m_surface->getCanvas()->restoreToCount(m_initialSurfaceSaveCount);
     m_imageBuffer->resetCanvas(m_surface->getCanvas());
-
-    return m_surface->getCanvas();
 }
 
 void Canvas2DLayerBridge::setImageBuffer(ImageBuffer* imageBuffer)
 {
     m_imageBuffer = imageBuffer;
-    if (m_imageBuffer) {
+    if (m_imageBuffer && m_isDeferralEnabled) {
         m_imageBuffer->resetCanvas(m_recorder->getRecordingCanvas());
     }
 }
@@ -246,9 +258,9 @@ void Canvas2DLayerBridge::flushRecordingOnly()
     if (m_haveRecordedDrawCommands && m_surface) {
         TRACE_EVENT0("cc", "Canvas2DLayerBridge::flush");
         RefPtr<SkPicture> picture = adoptRef(m_recorder->endRecording());
-        m_surface->getCanvas()->restoreToCount(m_initialSurfaceSaveCount); // In case immediateCanvas() was used
         picture->playback(m_surface->getCanvas());
-        startRecording();
+        if (m_isDeferralEnabled)
+            startRecording();
         m_haveRecordedDrawCommands = false;
     }
 }
@@ -479,14 +491,14 @@ WebLayer* Canvas2DLayerBridge::layer() const
 
 void Canvas2DLayerBridge::didDraw()
 {
-    m_haveRecordedDrawCommands = true;
+    if (m_isDeferralEnabled)
+        m_haveRecordedDrawCommands = true;
 }
 
 void Canvas2DLayerBridge::finalizeFrame(const FloatRect &dirtyRect)
 {
     ASSERT(!m_destructionInProgress);
     m_layer->layer()->invalidateRect(enclosingIntRect(dirtyRect));
-
     m_framesPending++;
     if (m_framesPending > 1) {
         // Turn on the rate limiter if this layer tends to accumulate a

third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridge.h
@@ -65,7 +65,7 @@ public:
     void willOverwriteAllPixels();
     void willOverwriteCanvas();
     SkCanvas* canvas();
-    SkCanvas* immediateCanvas();
+    void disableDeferral();
     bool checkSurfaceValid();
     bool restoreSurface();
     WebLayer* layer() const;
@@ -108,6 +108,7 @@ private:
     bool m_rateLimitingEnabled;
     SkFilterQuality m_filterQuality;
     bool m_isHidden;
+    bool m_isDeferralEnabled;
 
     friend class Canvas2DLayerBridgeTest;
 

third_party/WebKit/Source/platform/graphics/ImageBuffer.cpp
@@ -93,11 +93,9 @@ SkCanvas* ImageBuffer::canvas() const
     return m_surface->canvas();
 }
 
-SkCanvas* ImageBuffer::immediateCanvas() const
+void ImageBuffer::disableDeferral() const
 {
-    if (!isSurfaceValid())
-        return nullptr;
-    return m_surface->immediateCanvas();
+    return m_surface->disableDeferral();
 }
 
 bool ImageBuffer::writePixels(const SkImageInfo& info, const void* pixels, size_t rowBytes, int x, int y)

third_party/WebKit/Source/platform/graphics/ImageBuffer.h
@@ -91,7 +91,7 @@ public:
     void resetCanvas(SkCanvas*) const;
 
     SkCanvas* canvas() const;
-    SkCanvas* immediateCanvas() const;
+    void disableDeferral() const;
 
     // Called at the end of a task that rendered a whole frame
     void finalizeFrame(const FloatRect &dirtyRect);

third_party/WebKit/Source/platform/graphics/ImageBufferSurface.h
@@ -58,8 +58,8 @@ class PLATFORM_EXPORT ImageBufferSurface {
 public:
     virtual ~ImageBufferSurface();
 
-    virtual SkCanvas* canvas() { return immediateCanvas(); }
-    virtual SkCanvas* immediateCanvas() = 0;
+    virtual SkCanvas* canvas() = 0;
+    virtual void disableDeferral() { }
     virtual const SkBitmap& deprecatedBitmapForOverwrite();
     virtual void willOverwriteCanvas() { }
     virtual void didDraw(const FloatRect& rect) { }

third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
@@ -113,12 +113,10 @@ SkCanvas* RecordingImageBufferSurface::canvas()
     return m_currentFrame->getRecordingCanvas();
 }
 
-SkCanvas* RecordingImageBufferSurface::immediateCanvas()
+void RecordingImageBufferSurface::disableDeferral()
 {
     if (!m_fallbackSurface)
         fallBackToRasterCanvas();
-
-    return m_fallbackSurface->canvas();
 }
 
 PassRefPtr<SkPicture> RecordingImageBufferSurface::getPicture()

third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.h
@@ -35,7 +35,7 @@ public:
 
     // Implementation of ImageBufferSurface interfaces
     SkCanvas* canvas() override;
-    SkCanvas* immediateCanvas() override;
+    void disableDeferral() override;
     PassRefPtr<SkPicture> getPicture() override;
     void flush() override;
     void didDraw(const FloatRect&) override;

third_party/WebKit/Source/platform/graphics/UnacceleratedImageBufferSurface.cpp
@@ -52,7 +52,7 @@ UnacceleratedImageBufferSurface::UnacceleratedImageBufferSurface(const IntSize& 
 
 UnacceleratedImageBufferSurface::~UnacceleratedImageBufferSurface() { }
 
-SkCanvas* UnacceleratedImageBufferSurface::immediateCanvas()
+SkCanvas* UnacceleratedImageBufferSurface::canvas()
 {
     return m_surface->getCanvas();
 }

third_party/WebKit/Source/platform/graphics/UnacceleratedImageBufferSurface.h
@@ -44,7 +44,7 @@ public:
     UnacceleratedImageBufferSurface(const IntSize&, OpacityMode = NonOpaque);
     ~UnacceleratedImageBufferSurface() override;
 
-    SkCanvas* immediateCanvas() override;
+    SkCanvas* canvas() override;
     bool isValid() const override;
     const SkBitmap& deprecatedBitmapForOverwrite() override;
 

third_party/WebKit/Source/platform/graphics/gpu/AcceleratedImageBufferSurface.h
@@ -44,7 +44,7 @@ public:
     AcceleratedImageBufferSurface(const IntSize&, OpacityMode = NonOpaque);
     ~AcceleratedImageBufferSurface() override { }
 
-    SkCanvas* immediateCanvas() override { return m_surface ? m_surface->getCanvas() : nullptr; }
+    SkCanvas* canvas() override { return m_surface ? m_surface->getCanvas() : nullptr; }
     bool isValid() const override { return m_surface; }
     bool isAccelerated() const override { return true; }
     PassRefPtr<SkImage> newImageSnapshot() override;