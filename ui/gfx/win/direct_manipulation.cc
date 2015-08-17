6ec3626818c25f3cfea54c9e32dddd65c7c2c603
Anantanarayanan Iyengar <ananta@chromium.org>
2015-08-17 15:42:15 -0700

    Remove the CHECK from the DirectManipulationHelper::Activate function as it is firing in Canary on Windows 10.
    Merging to M45
    
    Crash dump analysis reveals the error code to be E_UNEXPECTED. Some disassembling into the Direct Manipulation Activate
    function did not reveal a code path where the above error is returned. Based on the crash dump analysis it looks like
    Activate is called on a window which is visible but has 0 dimensions which could possibly be triggering this.
    
    In any case it is also possible that calling Activate multiple times on a window may be triggering this. I was unable to repro the crash though.
    
    Proposed fixes include :-
    1. Remove the CHECK from the DirectManipulationHelper::Activate function.
    2. Add a new function DirectManipulationHelper::Deactivate which deactivates Direct manipulation processing on the window.
       This is called from views and content when the window is hidden.
    
    BUG=520492
    TBR=sky
    
    Review URL: https://codereview.chromium.org/1295683003
    
    Cr-Commit-Position: refs/heads/master@{#343526}
    (cherry picked from commit ee20880b00c6381d4393fd9f1927459034871c63)
    
    Review URL: https://codereview.chromium.org/1297063003 .
    
    Cr-Commit-Position: refs/branch-heads/2454@{#338}
    Cr-Branched-From: 12bfc3360892ec53cd00fc239a47e5298beb063b-refs/heads/master@{#338390}

--- ui/gfx/win/direct_manipulation.cc
+++ ui/gfx/win/direct_manipulation.cc
@@ -89,8 +89,12 @@ void DirectManipulationHelper::SetBounds(const gfx::Rect& bounds) {
 
 void DirectManipulationHelper::Activate(HWND window) {
   DCHECK(::IsWindow(window));
-  HRESULT hr = manager_->Activate(window);
-  CHECK(SUCCEEDED(hr));
+  manager_->Activate(window);
+}
+
+void DirectManipulationHelper::Deactivate(HWND window) {
+  DCHECK(::IsWindow(window));
+  manager_->Deactivate(window);
 }
 
 void DirectManipulationHelper:: HandleMouseWheel(HWND window, UINT message,
