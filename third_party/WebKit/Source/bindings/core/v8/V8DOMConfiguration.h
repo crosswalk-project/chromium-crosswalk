crosswalk-project/chromium-crosswalk

commit fd3d29efff78b635b29efca9b64f3acc7c4e8ace

Adding allocator annotations to blink classes and structs.
Document about replacing:
https://docs.google.com/document/d/1e4OvVMFuPxtoLGr6VkAu7qTaHWS02qBv5ndQoW8v1Wg/edit

blink-dev thread:
https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/1YrpiUJ0etg

BUG=

Review URL: https://codereview.chromium.org/1278983003

git-svn-id: svn://svn.chromium.org/blink/trunk@200693 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/bindings/core/v8/V8DOMConfiguration.h
@@ -37,6 +37,8 @@
 namespace blink {
 
 class CORE_EXPORT V8DOMConfiguration final {
+    DISALLOW_ALLOCATION();
+    WTF_MAKE_NONCOPYABLE(V8DOMConfiguration);
 public:
     // The following Configuration structs and install methods are used for
     // setting multiple properties on ObjectTemplate / FunctionTemplate, used
@@ -64,6 +66,8 @@ public:
     // AttributeConfiguration translates into calls to SetAccessor() on either
     // the instance or the prototype ObjectTemplate, based on |instanceOrPrototypeConfiguration|.
     struct AttributeConfiguration {
+        WTF_MAKE_NONCOPYABLE(AttributeConfiguration);
+        DISALLOW_ALLOCATION();
         const char* const name;
         v8::AccessorNameGetterCallback getter;
         v8::AccessorNameSetterCallback setter;
@@ -84,6 +88,8 @@ public:
     // AccessorConfiguration translates into calls to SetAccessorProperty()
     // on prototype ObjectTemplate.
     struct AccessorConfiguration {
+        WTF_MAKE_NONCOPYABLE(AccessorConfiguration);
+        DISALLOW_ALLOCATION();
         const char* const name;
         v8::FunctionCallback getter;
         v8::FunctionCallback setter;
@@ -117,6 +123,8 @@ public:
     // object's constants. It sets the constant on both the FunctionTemplate and
     // the ObjectTemplate. PropertyAttributes is always ReadOnly.
     struct ConstantConfiguration {
+        WTF_MAKE_NONCOPYABLE(ConstantConfiguration);
+        DISALLOW_ALLOCATION();
         const char* const name;
         int ivalue;
         double dvalue;
@@ -142,6 +150,8 @@ public:
     // object's callbacks. It sets the method on both the FunctionTemplate or
     // the ObjectTemplate.
     struct MethodConfiguration {
+        WTF_MAKE_NONCOPYABLE(MethodConfiguration);
+        DISALLOW_ALLOCATION();
         v8::Local<v8::Name> methodName(v8::Isolate* isolate) const { return v8AtomicString(isolate, name); }
         v8::FunctionCallback callbackForWorld(const DOMWrapperWorld& world) const
         {
@@ -156,6 +166,8 @@ public:
     };
 
     struct SymbolKeyedMethodConfiguration {
+        WTF_MAKE_NONCOPYABLE(SymbolKeyedMethodConfiguration);
+        DISALLOW_ALLOCATION();
         v8::Local<v8::Name> methodName(v8::Isolate* isolate) const { return getSymbol(isolate); }
         v8::FunctionCallback callbackForWorld(const DOMWrapperWorld&) const
         {

commit 3be2fd754f41747f403630681719b93c41bca77e

Delete operator= of V8DOMConfiguration structures instead of NONCOPYABLE
This is for fixing gcc builds caused by r200693.
https://src.chromium.org/viewvc/blink?view=rev&revision=200693

BUG=

Review URL: https://codereview.chromium.org/1307503002

git-svn-id: svn://svn.chromium.org/blink/trunk@200898 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/bindings/core/v8/V8DOMConfiguration.h
@@ -66,7 +66,7 @@ public:
     // AttributeConfiguration translates into calls to SetAccessor() on either
     // the instance or the prototype ObjectTemplate, based on |instanceOrPrototypeConfiguration|.
     struct AttributeConfiguration {
-        WTF_MAKE_NONCOPYABLE(AttributeConfiguration);
+        AttributeConfiguration& operator=(const AttributeConfiguration&) = delete;
         DISALLOW_ALLOCATION();
         const char* const name;
         v8::AccessorNameGetterCallback getter;
@@ -88,7 +88,7 @@ public:
     // AccessorConfiguration translates into calls to SetAccessorProperty()
     // on prototype ObjectTemplate.
     struct AccessorConfiguration {
-        WTF_MAKE_NONCOPYABLE(AccessorConfiguration);
+        AccessorConfiguration& operator=(const AccessorConfiguration&) = delete;
         DISALLOW_ALLOCATION();
         const char* const name;
         v8::FunctionCallback getter;
@@ -123,7 +123,7 @@ public:
     // object's constants. It sets the constant on both the FunctionTemplate and
     // the ObjectTemplate. PropertyAttributes is always ReadOnly.
     struct ConstantConfiguration {
-        WTF_MAKE_NONCOPYABLE(ConstantConfiguration);
+        ConstantConfiguration& operator=(const ConstantConfiguration&) = delete;
         DISALLOW_ALLOCATION();
         const char* const name;
         int ivalue;
@@ -150,7 +150,7 @@ public:
     // object's callbacks. It sets the method on both the FunctionTemplate or
     // the ObjectTemplate.
     struct MethodConfiguration {
-        WTF_MAKE_NONCOPYABLE(MethodConfiguration);
+        MethodConfiguration& operator=(const MethodConfiguration&) = delete;
         DISALLOW_ALLOCATION();
         v8::Local<v8::Name> methodName(v8::Isolate* isolate) const { return v8AtomicString(isolate, name); }
         v8::FunctionCallback callbackForWorld(const DOMWrapperWorld& world) const
@@ -166,7 +166,7 @@ public:
     };
 
     struct SymbolKeyedMethodConfiguration {
-        WTF_MAKE_NONCOPYABLE(SymbolKeyedMethodConfiguration);
+        SymbolKeyedMethodConfiguration& operator=(const SymbolKeyedMethodConfiguration&) = delete;
         DISALLOW_ALLOCATION();
         v8::Local<v8::Name> methodName(v8::Isolate* isolate) const { return getSymbol(isolate); }
         v8::FunctionCallback callbackForWorld(const DOMWrapperWorld&) const