ef68fda6b360d3ead677748c6c436401ea6d4cf7
Yaron Friedman <yfriedman@chromium.org>
2015-09-11 16:52:47 -0400

    Fix crash in DownloadOverwriteInfoBarDelegate.
    When the DownloadOverwriteInfoBar is dismissed by user action (e.g.
    because a tab was launched only for the download), we shouldn't try and
    remove the infobar ourselves. Propagate the value through to ensure we
    don't double-free it.
    BUG=481758
    TBR=acleung
    
    Review URL: https://codereview.chromium.org/1326033002
    
    Cr-Commit-Position: refs/heads/master@{#347391}
    (cherry picked from commit 84fa1e834dac2ca70989b8f9ae9baea7e0c878ec)
    
    Review URL: https://codereview.chromium.org/1342493002 .
    
    Cr-Commit-Position: refs/branch-heads/2490@{#236}
    Cr-Branched-From: 7790a3535f2a81a03685eca31a32cf69ae0c114f-refs/heads/master@{#344925}

--- chrome/browser/android/download/chrome_download_delegate.cc
+++ chrome/browser/android/download/chrome_download_delegate.cc
@@ -48,13 +48,13 @@ static void DangerousDownloadValidated(
 }
 
 // static
-void ChromeDownloadDelegate::EnqueueDownloadManagerRequest(
+bool ChromeDownloadDelegate::EnqueueDownloadManagerRequest(
     jobject chrome_download_delegate,
     bool overwrite,
     jobject download_info) {
   JNIEnv* env = base::android::AttachCurrentThread();
 
-  Java_ChromeDownloadDelegate_enqueueDownloadManagerRequestFromNative(
+  return Java_ChromeDownloadDelegate_enqueueDownloadManagerRequestFromNative(
       env, chrome_download_delegate, overwrite, download_info);
 }
 
