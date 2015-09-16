0a1444ac9f508279f9e220bf401aaf1c8fc223dc
jochen@chromium.org <jochen@chromium.org>
2015-09-16 07:01:12 +0000

    Merge 201908 "Add access checks to V8WrapperInstationScope."
    > Add access checks to V8WrapperInstationScope.
    > 
    > We skip those access checks when dealing with Location, as V8Location has its own checks in place according to the Location's cross-origin policy (https://html.spec.whatwg.org/multipage/browsers.html#security-location).
    > 
    > BUG=455160
    > 
    > Review URL: https://codereview.chromium.org/1262353002
    
    TBR=epertoso@chromium.org
    
    Review URL: https://codereview.chromium.org/1345043004
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2454@202328 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/V8DOMWrapper.h
+++ third_party/WebKit/Source/bindings/core/v8/V8DOMWrapper.h
@@ -31,8 +31,10 @@
 #ifndef V8DOMWrapper_h
 #define V8DOMWrapper_h
 
+#include "bindings/core/v8/BindingSecurity.h"
 #include "bindings/core/v8/DOMDataStore.h"
 #include "bindings/core/v8/ScriptWrappable.h"
+#include "bindings/core/v8/V8Binding.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RawPtr.h"
 #include "wtf/text/AtomicString.h"
@@ -106,7 +108,7 @@ inline v8::Local<v8::Object> V8DOMWrapper::associateObjectWithWrapper(v8::Isolat
 
 class V8WrapperInstantiationScope {
 public:
-    V8WrapperInstantiationScope(v8::Local<v8::Object> creationContext, v8::Isolate* isolate)
+    V8WrapperInstantiationScope(v8::Local<v8::Object> creationContext, v8::Isolate* isolate, bool withSecurityCheck = true)
         : m_didEnterContext(false)
         , m_context(isolate->GetCurrentContext())
     {
@@ -119,6 +121,8 @@ public:
         // is different from the context that we are about to enter.
         if (contextForWrapper == m_context)
             return;
+        if (withSecurityCheck)
+            SecurityCheck(isolate, contextForWrapper);
         m_context = v8::Local<v8::Context>::New(isolate, contextForWrapper);
         m_didEnterContext = true;
         m_context->Enter();
@@ -134,6 +138,8 @@ public:
     v8::Local<v8::Context> context() const { return m_context; }
 
 private:
+    void SecurityCheck(v8::Isolate*, v8::Local<v8::Context> contextForWrapper);
+
     bool m_didEnterContext;
     v8::Local<v8::Context> m_context;
 };
