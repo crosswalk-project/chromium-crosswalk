c82b0ab685fef68670557b095dfc960f2086417f
Mike West <mkwst@google.com>
2015-09-18 11:01:53 +0200

    Merge "Allow 'chrome-extension:' URLs to bypass content settings (1/2)" to M46.
    > We changed the behavior of 'Document::firstPartyForCookies' to return an
    > empty URL in the case where any URL in the ancestor chain doesn't match
    > the current document's URL. Previously, we'd simply return the top-level
    > document's URL.
    >
    > This means that the content-settings bypass check in
    > 'ContentSettingsObserver::IsWhitelistedForContentSettings' sees an empty
    > URL as opposed to a 'chrome-extension://' URL for cases in which an
    > extension loads a resource, so content settings are applied as per usual.
    > This breaks things fairly badly for folks who have turned on third-party
    > cookie blocking.
    >
    > In these patches, we introduce a new scheme registry for those schemes
    > which ought to override Blink's concept of "first-party" when they're
    > loaded into the top-level browsing context.
    >
    > Patch 1 (Blink): https://codereview.chromium.org/1305253012
    > Patch 2 (Chromium): [This patch]
    >
    > BUG=527963
    > R=jochen@chromium.org
    >
    > Review URL: https://codereview.chromium.org/1332563006
    
    Cr-Commit-Position: refs/heads/master@{#348194}
    (cherry picked from commit cfed7a300606c5eedb858433b37db5ffadf3b2cc)
    
    Note: Moved the call of this method from Dispatcher::WebKitInitialized()
    to Dispatcher::Dispatcher(), which is where the similar methods were at
    this point in history.
    
    TBR=amineer@chromium.org
    
    Review URL: https://codereview.chromium.org/1351313002 .
    
    Cr-Commit-Position: refs/branch-heads/2490@{#319}
    Cr-Branched-From: 7790a3535f2a81a03685eca31a32cf69ae0c114f-refs/heads/master@{#344925}

--- extensions/renderer/dispatcher.cc
+++ extensions/renderer/dispatcher.cc
@@ -278,6 +278,13 @@ Dispatcher::Dispatcher(DispatcherDelegate* delegate)
       extension_scheme);
   WebSecurityPolicy::registerURLSchemeAsBypassingContentSecurityPolicy(
       extension_resource_scheme);
+
+  // Extension resources, when loaded as the top-level document, should
+  // bypass Blink's strict first-party origin checks.
+  WebSecurityPolicy::registerURLSchemeAsFirstPartyWhenTopLevel(
+      extension_scheme);
+  WebSecurityPolicy::registerURLSchemeAsFirstPartyWhenTopLevel(
+      extension_resource_scheme);
 }
 
 Dispatcher::~Dispatcher() {
