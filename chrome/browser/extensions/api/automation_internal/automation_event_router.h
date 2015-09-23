8f0c8408526f22b662e5d9c8818561fbdb79f69f
David Tseng <dtseng@chromium.org>
2015-09-23 15:21:39 -0700

    Merge to m46: Fixes to AutomationEventRouter and dispatching to appropriate profiles.
    BUG=532602,515538
    
    Review URL: https://codereview.chromium.org/1348833002
    Review URL: https://codereview.chromium.org/1268163002
    
    Cr-Commit-Position: refs/heads/master@{#349264}
    (cherry picked from commit 8d79eb8530c150ec56350ee9ce95407b3f0ac3e5)
    Cr-Commit-Position: refs/heads/master@{#348570}
    (cherry picked from commit 36eee6c86d20119bab09d7cbe3ab42622a794531)
    
    Review URL: https://codereview.chromium.org/1366643004 .
    
    Cr-Commit-Position: refs/branch-heads/2490@{#367}
    Cr-Branched-From: 7790a3535f2a81a03685eca31a32cf69ae0c114f-refs/heads/master@{#344925}

--- chrome/browser/extensions/api/automation_internal/automation_event_router.h
+++ chrome/browser/extensions/api/automation_internal/automation_event_router.h
@@ -14,6 +14,8 @@
 #include "content/public/browser/notification_registrar.h"
 #include "extensions/common/extension.h"
 
+class Profile;
+
 namespace content {
 class BrowserContext;
 }  // namespace content
@@ -32,14 +34,16 @@ class AutomationEventRouter : public content::NotificationObserver {
   // wants to receive automation events from the accessibility tree indicated
   // by |source_ax_tree_id|. Automation events are forwarded from now on
   // until the listener process dies.
-  void RegisterListenerForOneTree(int listener_process_id,
+  void RegisterListenerForOneTree(const ExtensionId& extension_id,
+                                  int listener_process_id,
                                   int listener_routing_id,
                                   int source_ax_tree_id);
 
   // Indicates that the listener at |listener_process_id|, |listener_routing_id|
   // wants to receive automation events from all accessibility trees because
   // it has Desktop permission.
-  void RegisterListenerWithDesktopPermission(int listener_process_id,
+  void RegisterListenerWithDesktopPermission(const ExtensionId& extension_id,
+                                             int listener_process_id,
                                              int listener_routing_id);
 
   void DispatchAccessibilityEvent(
@@ -56,16 +60,19 @@ class AutomationEventRouter : public content::NotificationObserver {
     AutomationListener();
     ~AutomationListener();
 
+    ExtensionId extension_id;
     int routing_id;
     int process_id;
     bool desktop;
     std::set<int> tree_ids;
+    bool is_active_profile;
   };
 
   AutomationEventRouter();
   ~AutomationEventRouter() override;
 
   void Register(
+      const ExtensionId& extension_id,
       int listener_process_id,
       int listener_routing_id,
       int source_ax_tree_id,
@@ -76,8 +83,22 @@ class AutomationEventRouter : public content::NotificationObserver {
                const content::NotificationSource& source,
                const content::NotificationDetails& details) override;
 
+  // Called when the user switches profiles or when a listener is added
+  // or removed. The purpose is to ensure that multiple instances of the
+  // same extension running in different profiles don't interfere with one
+  // another, so in that case only the one associated with the active profile
+  // is marked as active.
+  //
+  // This is needed on Chrome OS because ChromeVox loads into the login profile
+  // in addition to the active profile.  If a similar fix is needed on other
+  // platforms, we'd need an equivalent of SessionStateObserver that works
+  // everywhere.
+  void UpdateActiveProfile();
+
   content::NotificationRegistrar registrar_;
   std::vector<AutomationListener> listeners_;
+
+  Profile* active_profile_;
 
   friend struct DefaultSingletonTraits<AutomationEventRouter>;
 
