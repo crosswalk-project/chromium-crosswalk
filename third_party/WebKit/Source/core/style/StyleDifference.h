6f441ac58a494c339d2e83219224ee08ba5035ef
Philip Rogers <pdr@chromium.org>
2015-09-25 20:09:08 -0700

    Merge 72f85dacbbef4c117efdb8e353036873d167b989 to branch 2490
    This patch carefully merges 72f85dacbbef4c117efdb8e353036873d167b989
    to branch 2490.
    
    TBR=chrishtr
    BUG=529944
    
    Review URL: https://codereview.chromium.org/1369893002 .
    
    Cr-Commit-Position: refs/branch-heads/2490@{#413}
    Cr-Branched-From: 7790a3535f2a81a03685eca31a32cf69ae0c114f-refs/heads/master@{#344925}

--- third_party/WebKit/Source/core/style/StyleDifference.h
+++ third_party/WebKit/Source/core/style/StyleDifference.h
@@ -16,8 +16,9 @@ public:
         OpacityChanged = 1 << 1,
         ZIndexChanged = 1 << 2,
         FilterChanged = 1 << 3,
-        // The object needs to issue paint invalidations if it contains text or properties dependent on color (e.g., border or outline).
-        TextOrColorChanged = 1 << 4,
+        // The object needs to issue paint invalidations if it is affected by text decorations or properties dependent on color (e.g., border or outline).
+        TextDecorationOrColorChanged = 1 << 4,
+        // If you add a value here, be sure to update the number of bits on m_propertySpecificDifferences.
     };
 
     StyleDifference()
@@ -74,8 +75,8 @@ public:
     bool filterChanged() const { return m_propertySpecificDifferences & FilterChanged; }
     void setFilterChanged() { m_propertySpecificDifferences |= FilterChanged; }
 
-    bool textOrColorChanged() const { return m_propertySpecificDifferences & TextOrColorChanged; }
-    void setTextOrColorChanged() { m_propertySpecificDifferences |= TextOrColorChanged; }
+    bool textDecorationOrColorChanged() const { return m_propertySpecificDifferences & TextDecorationOrColorChanged; }
+    void setTextDecorationOrColorChanged() { m_propertySpecificDifferences |= TextDecorationOrColorChanged; }
 
 private:
     enum PaintInvalidationType {
