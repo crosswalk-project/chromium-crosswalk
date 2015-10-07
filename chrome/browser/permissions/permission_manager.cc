f9442ec3921549edc53e24b1a29e09582849f343
felt <felt@chromium.org>
2015-10-07 16:41:19 -0700

    Merge "Fix extension geolocation permission requests"
    BUG=534631, 457091
    TBR=mlamouri@chromium.org
    
    Review URL: https://codereview.chromium.org/1390403002 .
    
    Cr-Commit-Position: refs/branch-heads/2490@{#513}
    Cr-Branched-From: 7790a3535f2a81a03685eca31a32cf69ae0c114f-refs/heads/master@{#344925}

--- chrome/browser/permissions/permission_manager.cc
+++ chrome/browser/permissions/permission_manager.cc
@@ -274,11 +274,9 @@ void PermissionManager::UnsubscribePermissionStatusChange(int subscription_id) {
 
 bool PermissionManager::IsPermissionBubbleManagerMissing(
     content::WebContents* web_contents) {
-  // Can't be missing if it isn't needed to begin with.
-  if (!PermissionBubbleManager::Enabled())
-    return false;
-
-  return PermissionBubbleManager::FromWebContents(web_contents) == nullptr;
+  // TODO(felt): Remove this method entirely. Leaving it to make a minimal
+  // last-minute merge to 46. See crbug.com/457091 and crbug.com/534631.
+  return false;
 }
 
 void PermissionManager::OnContentSettingChanged(
