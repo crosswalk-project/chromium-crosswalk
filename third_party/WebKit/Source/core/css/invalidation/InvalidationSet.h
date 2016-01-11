e61ee7ecab43e75ac3fe2903632ab5d2c3b4debe
Eric Willigers <ericwilligers@chromium.org>
2016-01-12 10:50:30 +1100

    CSS Invalidation: Avoid virtual functions in InvalidationSet
    Virtual functions were unnecessarily added to InvalidationSet by
    https://codereview.chromium.org/1317533002
    
    These have a memory cost when there are many style rules and/or
    many elements scheduled for style invalidation.
    
    BUG=556531
    
    Review URL: https://codereview.chromium.org/1480973002
    
    Cr-Commit-Position: refs/heads/master@{#363166}
    (cherry picked from commit 580ea704c5158d858ad9ba20eb3783f8883d8689)
    
    Review URL: https://codereview.chromium.org/1580503004 .
    
    Cr-Commit-Position: refs/branch-heads/2564@{#532}
    Cr-Branched-From: 1283eca15bd9f772387f75241576cde7bdec7f54-refs/heads/master@{#359700}

--- third_party/WebKit/Source/core/css/invalidation/InvalidationSet.h
+++ third_party/WebKit/Source/core/css/invalidation/InvalidationSet.h
@@ -56,10 +56,8 @@ enum InvalidationType {
 class CORE_EXPORT InvalidationSet : public RefCounted<InvalidationSet> {
     WTF_MAKE_NONCOPYABLE(InvalidationSet);
 public:
-    virtual ~InvalidationSet() {}
-
-    virtual bool isDescendantInvalidationSet() const { return false; }
-    virtual bool isSiblingInvalidationSet() const { return false; }
+    bool isDescendantInvalidationSet() const { return m_type == InvalidateDescendants; }
+    bool isSiblingInvalidationSet() const { return m_type == InvalidateSiblings; }
 
     static void cacheTracingFlag();
 
@@ -98,12 +96,21 @@ public:
     const HashSet<AtomicString>& tagNameSetForTesting() const { ASSERT(m_tagNames); return *m_tagNames; }
     const HashSet<AtomicString>& attributeSetForTesting() const { ASSERT(m_attributes); return *m_attributes; }
 
+    void deref()
+    {
+        if (!derefBase())
+            return;
+        destroy();
+    }
+
 protected:
-    InvalidationSet();
+    InvalidationSet(InvalidationType);
 
     void combine(const InvalidationSet& other);
 
 private:
+    void destroy();
+
     HashSet<AtomicString>& ensureClassSet();
     HashSet<AtomicString>& ensureIdSet();
     HashSet<AtomicString>& ensureTagNameSet();
@@ -114,6 +121,8 @@ private:
     OwnPtr<HashSet<AtomicString>> m_ids;
     OwnPtr<HashSet<AtomicString>> m_tagNames;
     OwnPtr<HashSet<AtomicString>> m_attributes;
+
+    unsigned m_type : 1;
 
     // If true, all descendants might be invalidated, so a full subtree recalc is required.
     unsigned m_allDescendantsMightBeInvalid : 1;
@@ -138,15 +147,14 @@ public:
         return adoptRef(new DescendantInvalidationSet);
     }
 
-    bool isDescendantInvalidationSet() const final { return true; }
-
     void combine(const DescendantInvalidationSet& other)
     {
         InvalidationSet::combine(other);
     }
 
 private:
-    DescendantInvalidationSet() {}
+    DescendantInvalidationSet()
+        : InvalidationSet(InvalidateDescendants) {}
 };
 
 class CORE_EXPORT SiblingInvalidationSet final : public InvalidationSet {
@@ -155,8 +163,6 @@ public:
     {
         return adoptRef(new SiblingInvalidationSet);
     }
-
-    bool isSiblingInvalidationSet() const final { return true; }
 
     void combine(const SiblingInvalidationSet& other);
 
