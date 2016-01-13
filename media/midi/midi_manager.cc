d364f29e442010ce19182375fd011776b5bc7008
Oliver Chang <ochang@chromium.org>
2016-01-13 12:34:05 -0800

    Prevent a race condition with MidiHost IPC sending.
    EndSession() is moved from MidiHost's destructor into OnChannelClosing() before
    the destruction process starts (at which point it is unsafe to call any
    functions on the MidiHost/MidiManagerClient).
    
    Also moves a function call on a MidiManagerClient in MidiManager into the AutoLock
    scope.
    
    BUG=576383
    R=toyoshim@chromium.org,agoode@chromium.org
    
    Review URL: https://codereview.chromium.org/1576323002
    
    Cr-Commit-Position: refs/heads/master@{#369091}
    (cherry picked from commit bd648ad6116ad3eb09356fb8c34fb006dbe86d4c)
    
    Review URL: https://codereview.chromium.org/1584793003 .
    
    Cr-Commit-Position: refs/branch-heads/2564@{#554}
    Cr-Branched-From: 1283eca15bd9f772387f75241576cde7bdec7f54-refs/heads/master@{#359700}

--- media/midi/midi_manager.cc
+++ media/midi/midi_manager.cc
@@ -125,11 +125,14 @@ void MidiManager::StartSession(MidiManagerClient* client) {
         pending_clients_.insert(client);
       }
     }
+
+    if (completion == Completion::COMPLETE_SYNCHRONOUSLY) {
+      client->CompleteStartSession(result);
+      return;
+    }
   }
 
-  if (completion == Completion::COMPLETE_SYNCHRONOUSLY) {
-    client->CompleteStartSession(result);
-  } else if (completion == Completion::INVOKE_INITIALIZATION) {
+  if (completion == Completion::INVOKE_INITIALIZATION) {
     // Lazily initialize the MIDI back-end.
     TRACE_EVENT0("midi", "MidiManager::StartInitialization");
     // CompleteInitialization() will be called asynchronously when platform
@@ -142,7 +145,8 @@ void MidiManager::EndSession(MidiManagerClient* client) {
   ReportUsage(Usage::SESSION_ENDED);
 
   // At this point, |client| can be in the destruction process, and calling
-  // any method of |client| is dangerous.
+  // any method of |client| is dangerous. Calls on clients *must* be protected
+  // by |lock_| to prevent race conditions.
   base::AutoLock auto_lock(lock_);
   clients_.erase(client);
   pending_clients_.erase(client);
@@ -153,6 +157,8 @@ void MidiManager::AccumulateMidiBytesSent(MidiManagerClient* client, size_t n) {
   if (clients_.find(client) == clients_.end())
     return;
 
+  // Continue to hold lock_ here in case another thread is currently doing
+  // EndSession.
   client->AccumulateMidiBytesSent(n);
 }
 
