b94d12ff0904c8888675a8bfd6190b0b6165a492
Jackie Quinn <jyquinn@chromium.org>
2016-01-21 07:54:51 -0800

    Call stopLoading when opening native application
    Previously, abortLoad was called, which did not fully handle
    session management or returning to native pages such as
    the new tab page.
    
    BUG=579766
    
    Review URL: https://codereview.chromium.org/1612743002
    
    Cr-Commit-Position: refs/heads/master@{#370613}
    (cherry picked from commit 94eade2c798535c64cd286820064348aa255c5a8)
    
    Review URL: https://codereview.chromium.org/1615933002 .
    
    Cr-Commit-Position: refs/branch-heads/2564@{#597}
    Cr-Branched-From: 1283eca15bd9f772387f75241576cde7bdec7f54-refs/heads/master@{#359700}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -2790,7 +2790,7 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
       // that errors reported following the 'NO' reply can be safely ignored.
       if ([self shouldClosePageOnNativeApplicationLoad])
         [_delegate webPageOrderedClose];
-      [self abortLoad];
+      [self stopLoading];
       [_openedApplicationURL addObject:request.URL];
       return NO;
     }
