e8a6061afac9ace39af31bd4b92aa50bca94d832
Aleksandar Stojiljkovic <aleksandar.stojiljkovic@intel.com>
2016-01-25 15:17:26 +0200

    XWALK-4992 [windows] App does not support gamepad
    While chromium is bundling xinput1_3.dll from DirectX redist, using whatever
    platform suports. Logic about what dll is in use for different platforms is
    copied from Xinput.h.
    In order to target all of the Windows versions with the same binary, chosen to
    keep dynamic linking approach. Otherwise, there would be a problem with
    XInputEnable not available in xinput dll before Windows8 and deprecated on
    Windows 10.
    
    Snippet from <ProgramFiles>\Windows Kits\10\Include\10.0.10586.0\um\Xinput.h:
    
    \#if(_WIN32_WINNT >= _WIN32_WINNT_WIN8)
    \#define XINPUT_DLL_A  "xinput1_4.dll"
    \#define XINPUT_DLL_W L"xinput1_4.dll"
    \#else
    \#define XINPUT_DLL_A  "xinput9_1_0.dll"
    \#define XINPUT_DLL_W L"xinput9_1_0.dll"
    \#endif
    
    For detailed information on Windows library loading and search path priorities,
    check https://msdn.microsoft.com documentation on LoadLibrary().
    
    Upstreaming the patch planned, too.

--- content/browser/gamepad/gamepad_platform_data_fetcher_win.h
+++ content/browser/gamepad/gamepad_platform_data_fetcher_win.h
@@ -49,10 +49,10 @@ class GamepadPlatformDataFetcherWin : public GamepadDataFetcher {
   typedef DWORD (WINAPI *XInputGetStateFunc)(
       DWORD dwUserIndex, XINPUT_STATE* pState);
 
-  // Get functions from dynamically loaded xinput1_3.dll. We don't use
-  // DELAYLOAD because the import library for Win8 SDK pulls xinput1_4 which
-  // isn't redistributable. Returns true if loading was successful. We include
-  // xinput1_3.dll with Chrome.
+  // Get functions from dynamically loaded xinputX_Y.dll. We don't use
+  // DELAYLOAD because XInputEnable is not available on all versions (it is
+  // marked as deprecated on Win10) and thus the symbol is resolved in runtime.
+  // Returns true if loading was successful.
   bool GetXInputDllFunctions();
 
   // Scan for connected XInput and DirectInput gamepads.
@@ -71,7 +71,6 @@ class GamepadPlatformDataFetcherWin : public GamepadDataFetcher {
 
   // Function pointers to XInput functionality, retrieved in
   // |GetXinputDllFunctions|.
-  XInputEnableFunc xinput_enable_;
   XInputGetCapabilitiesFunc xinput_get_capabilities_;
   XInputGetStateFunc xinput_get_state_;
 
