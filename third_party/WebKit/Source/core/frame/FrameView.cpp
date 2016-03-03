e10085291ad10dec622b236d8b2d991b441ec102
Alex Mineer <amineer@chromium.org>
2016-03-02 16:10:36 -0800

    Revert "Add more RELEASE_ASSERT(!needsLayout()) in FrameView::updateStyleAndLayoutIfNeededRecursive()"
    This reverts commit c75c1462aac85b247ef8125eb42e197235f3074d.
    
    Temporary revert to allow for a clean revert of a previous patch.
    Will reland shortly.
    
    Cr-Commit-Position: refs/branch-heads/2661@{#50}
    Cr-Branched-From: ef6f6ae5e4c96622286b563658d5cd62a6cf1197-refs/heads/master@{#378081}

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -2639,8 +2639,6 @@ void FrameView::updateStyleAndLayoutIfNeededRecursive()
     if (needsLayout())
         layout();
 
-    RELEASE_ASSERT(!needsLayout());
-
     // FIXME: Calling layout() shouldn't trigger script execution or have any
     // observable effects on the frame tree but we're not quite there yet.
     WillBeHeapVector<RefPtrWillBeMember<FrameView>> frameViews;
@@ -2653,8 +2651,6 @@ void FrameView::updateStyleAndLayoutIfNeededRecursive()
 
     for (const auto& frameView : frameViews)
         frameView->updateStyleAndLayoutIfNeededRecursive();
-
-    RELEASE_ASSERT(!needsLayout());
 
     // When SVG filters are invalidated using Document::scheduleSVGFilterLayerUpdateHack() they may trigger an
     // extra style recalc. See PaintLayer::filterNeedsPaintInvalidation().
