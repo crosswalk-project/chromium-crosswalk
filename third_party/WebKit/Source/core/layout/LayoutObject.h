1fb49bb47cbcb17f44198fc2a906928adf4dbaff
Koji Ishii <kojii@chromium.org>
2016-03-04 13:16:31 +0800

    Fix SubtreeLayoutScope not to schedule relayout
    This patch fixes SubtreeLayoutScope::setNeedsLayout() and
    setChildNeedsLayout() not to schedule relayout when they call
    markContainerChainForLayout().
    
    The signature of markContainerChainForLayout() allows to schedule
    relayout even when SubtreeLayoutScope exists. To not allow scheduling
    relayout while we're in layout, this patch changes the signature.
    
    BUG=590620
    
    Review URL: https://codereview.chromium.org/1755543002
    
    Cr-Commit-Position: refs/heads/master@{#378639}
    (cherry picked from commit 6e396c50a4630c1bd065aaf19244cf8c1fdcd6d1)
    
    Review URL: https://codereview.chromium.org/1768493002 .
    
    Cr-Commit-Position: refs/branch-heads/2661@{#76}
    Cr-Branched-From: ef6f6ae5e4c96622286b563658d5cd62a6cf1197-refs/heads/master@{#378081}

--- third_party/WebKit/Source/core/layout/LayoutObject.h
+++ third_party/WebKit/Source/core/layout/LayoutObject.h
@@ -798,7 +798,8 @@ public:
 
     Element* offsetParent() const;
 
-    void markContainerChainForLayout(bool scheduleRelayout = true, SubtreeLayoutScope* = nullptr);
+    void markContainerChainForLayout(bool scheduleRelayout = true);
+    void markContainerChainForLayout(SubtreeLayoutScope*);
     void setNeedsLayout(LayoutInvalidationReasonForTracing, MarkingBehavior = MarkContainerChain, SubtreeLayoutScope* = nullptr);
     void setNeedsLayoutAndFullPaintInvalidation(LayoutInvalidationReasonForTracing, MarkingBehavior = MarkContainerChain, SubtreeLayoutScope* = nullptr);
     void clearNeedsLayout();
@@ -1560,6 +1561,8 @@ private:
 
     void setNeedsOverflowRecalcAfterStyleChange();
 
+    void markContainerChainForLayout(bool scheduleRelayout, SubtreeLayoutScope*);
+
     // FIXME: This should be 'markContaingBoxChainForOverflowRecalc when we make LayoutBox
     // recomputeOverflow-capable. crbug.com/437012 and crbug.com/434700.
     inline void markContainingBlocksForOverflowRecalc();
@@ -1967,7 +1970,7 @@ inline void LayoutObject::setNeedsLayout(LayoutInvalidationReasonForTracing reas
             "data",
             InspectorLayoutInvalidationTrackingEvent::data(this, reason));
         if (markParents == MarkContainerChain && (!layouter || layouter->root() != this))
-            markContainerChainForLayout(true, layouter);
+            markContainerChainForLayout(layouter);
     }
 }
 
@@ -2004,7 +2007,7 @@ inline void LayoutObject::setChildNeedsLayout(MarkingBehavior markParents, Subtr
     setNormalChildNeedsLayout(true);
     // FIXME: Replace MarkOnlyThis with the SubtreeLayoutScope code path and remove the MarkingBehavior argument entirely.
     if (!alreadyNeededLayout && markParents == MarkContainerChain && (!layouter || layouter->root() != this))
-        markContainerChainForLayout(true, layouter);
+        markContainerChainForLayout(layouter);
 }
 
 inline void LayoutObject::setNeedsPositionedMovementLayout()
