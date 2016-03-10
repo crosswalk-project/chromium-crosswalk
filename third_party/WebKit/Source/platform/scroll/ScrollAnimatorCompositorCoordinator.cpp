0051c1ce7c155d282712a789f9b2b28c620a9035
Yash Malik <ymalik@google.com>
2016-03-09 21:52:17 -0500

    Takeover MT initiated animations from the compositor
    This is a followup CL to https://crrev.com/1734063003/. It finishes a
    MT initiated animation running on the compositor, on the MT.
    
    BUG=581875
    
    Review URL: https://codereview.chromium.org/1770443002
    
    Cr-Commit-Position: refs/heads/master@{#379868}
    (cherry picked from commit d0eaee288867a6e00e4a9c3fa65d28e4de9e922a)
    
    TBR=
    
    Review URL: https://codereview.chromium.org/1780573005 .
    
    Cr-Commit-Position: refs/branch-heads/2661@{#167}
    Cr-Branched-From: ef6f6ae5e4c96622286b563658d5cd62a6cf1197-refs/heads/master@{#378081}

--- third_party/WebKit/Source/platform/scroll/ScrollAnimatorCompositorCoordinator.cpp
+++ third_party/WebKit/Source/platform/scroll/ScrollAnimatorCompositorCoordinator.cpp
@@ -65,6 +65,7 @@ bool ScrollAnimatorCompositorCoordinator::hasAnimationThatRequiresService() cons
     case RunState::WaitingToSendToCompositor:
     case RunState::RunningOnMainThread:
     case RunState::RunningOnCompositorButNeedsUpdate:
+    case RunState::RunningOnCompositorButNeedsTakeover:
     case RunState::WaitingToCancelOnCompositor:
         return true;
     }
@@ -115,6 +116,7 @@ void ScrollAnimatorCompositorCoordinator::cancelAnimation()
     case RunState::WaitingToCancelOnCompositor:
     case RunState::PostAnimationCleanup:
         break;
+    case RunState::RunningOnCompositorButNeedsTakeover:
     case RunState::WaitingToSendToCompositor:
         if (m_compositorAnimationId) {
             // We still have a previous animation running on the compositor.
@@ -129,6 +131,30 @@ void ScrollAnimatorCompositorCoordinator::cancelAnimation()
     case RunState::RunningOnCompositorButNeedsUpdate:
     case RunState::RunningOnCompositor:
         m_runState = RunState::WaitingToCancelOnCompositor;
+
+        // Get serviced the next time compositor updates are allowed.
+        scrollableArea()->registerForAnimation();
+    }
+}
+
+void ScrollAnimatorCompositorCoordinator::takeoverCompositorAnimation()
+{
+    switch (m_runState) {
+    case RunState::Idle:
+    case RunState::WaitingToCancelOnCompositor:
+    case RunState::PostAnimationCleanup:
+    case RunState::RunningOnCompositorButNeedsTakeover:
+    case RunState::WaitingToSendToCompositor:
+    case RunState::RunningOnMainThread:
+        break;
+    case RunState::RunningOnCompositorButNeedsUpdate:
+    case RunState::RunningOnCompositor:
+        // We call abortAnimation that makes changes to the animation running on
+        // the compositor. Thus, this function should only be called when in
+        // CompositingClean state.
+        abortAnimation();
+
+        m_runState = RunState::RunningOnCompositorButNeedsTakeover;
 
         // Get serviced the next time compositor updates are allowed.
         scrollableArea()->registerForAnimation();
@@ -154,6 +180,7 @@ void ScrollAnimatorCompositorCoordinator::compositorAnimationFinished(
         break;
     case RunState::RunningOnCompositor:
     case RunState::RunningOnCompositorButNeedsUpdate:
+    case RunState::RunningOnCompositorButNeedsTakeover:
     case RunState::WaitingToCancelOnCompositor:
         m_runState = RunState::PostAnimationCleanup;
         // Get serviced the next time compositor updates are allowed.
