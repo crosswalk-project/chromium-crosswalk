742ba1b04062fb97cf4f32662bc2d5af03edd9e3
Yan, Shaobo <shaobo.yan@intel.com>
2016-03-14 17:38:38 +0800

    Partially fix ScrollTo behaviour caused by async compositor.
    BUG=XWALK-3112

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1800,6 +1800,8 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Screen
     public void scrollBy(float dxPix, float dyPix, boolean useLastFocalEventLocation) {
         if (mNativeContentViewCore == 0) return;
         if (dxPix == 0 && dyPix == 0) return;
+        mRenderCoordinates.setScrollX(mRenderCoordinates.getScrollXPix() + dxPix);
+        mRenderCoordinates.setScrollY(mRenderCoordinates.getScrollYPix() + dyPix);
         long time = SystemClock.uptimeMillis();
         // It's a very real (and valid) possibility that a fling may still
         // be active when programatically scrolling. Cancelling the fling in
