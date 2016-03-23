9c48e88ab90a5f4eb56dd4c65da66e4c0b1db138
glevin <glevin@chromium.org>
2016-03-23 10:59:25 -0700

    Revert "Fix shelf overflow bubble position"
    This reverts commit 751830a3891a5b643715b276d550e1318bfc4b7b.
    
    R=glevin@chromium.org
    BUG=597308
    
    Review URL: https://codereview.chromium.org/1828773002 .
    
    Cr-Commit-Position: refs/branch-heads/2661@{#362}
    Cr-Branched-From: ef6f6ae5e4c96622286b563658d5cd62a6cf1197-refs/heads/master@{#378081}

--- ash/shelf/overflow_bubble_view.cc
+++ ash/shelf/overflow_bubble_view.cc
@@ -41,9 +41,6 @@ OverflowBubbleView::~OverflowBubbleView() {
 
 void OverflowBubbleView::InitOverflowBubble(views::View* anchor,
                                             ShelfView* shelf_view) {
-  shelf_view_ = shelf_view;
-  AddChildView(shelf_view_);
-
   SetAnchorView(anchor);
   set_arrow(GetBubbleArrow());
   set_background(NULL);
@@ -57,6 +54,9 @@ void OverflowBubbleView::InitOverflowBubble(views::View* anchor,
   SetPaintToLayer(true);
   SetFillsBoundsOpaquely(false);
   layer()->SetMasksToBounds(true);
+
+  shelf_view_ = shelf_view;
+  AddChildView(shelf_view_);
 
   set_parent_window(Shell::GetContainer(
       anchor->GetWidget()->GetNativeWindow()->GetRootWindow(),
