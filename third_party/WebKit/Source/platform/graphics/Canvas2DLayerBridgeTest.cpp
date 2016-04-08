06bb6cf8eb3c04254e4f5187a644ddb086b7afcc
Justin Novosad <junov@chromium.org>
2016-04-08 15:05:42 -0400

    Disable 2D canvas switch to CPU rendering while backgrounded
    The switch to software rendering that happens when actively drawing to
    a canvas while the containing page is in the background was cause the
    compositing layer to get into a corrupted state.  This fix disables
    the feature until a permanent solution can be found
    
    BUG=591029
    
    Review URL: https://codereview.chromium.org/1857763002
    
    Cr-Commit-Position: refs/heads/master@{#384990}
    (cherry picked from commit 30569cfdd5902ae953a0a9e07d19d828c57f5f6f)
    
    Review URL: https://codereview.chromium.org/1877473002 .
    
    Cr-Commit-Position: refs/branch-heads/2661@{#532}
    Cr-Branched-From: ef6f6ae5e4c96622286b563658d5cd62a6cf1197-refs/heads/master@{#378081}

--- third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridgeTest.cpp
+++ third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridgeTest.cpp
@@ -630,7 +630,7 @@ void postAndWaitRenderingTask(const WebTraceLocation& location, WebThread* testT
     doneEvent->wait();
 }
 
-#if CANVAS2D_HIBERNATION_ENABLED
+#if CANVAS2D_HIBERNATION_ENABLED && CANVAS2D_BACKGROUND_RENDER_SWITCH_TO_CPU
 TEST_F(Canvas2DLayerBridgeTest, BackgroundRenderingWhileHibernating)
 #else
 TEST_F(Canvas2DLayerBridgeTest, DISABLED_BackgroundRenderingWhileHibernating)
@@ -683,7 +683,7 @@ TEST_F(Canvas2DLayerBridgeTest, DISABLED_BackgroundRenderingWhileHibernating)
     ::testing::Mock::VerifyAndClearExpectations(&mainMock);
 }
 
-#if CANVAS2D_HIBERNATION_ENABLED
+#if CANVAS2D_HIBERNATION_ENABLED && CANVAS2D_BACKGROUND_RENDER_SWITCH_TO_CPU
 TEST_F(Canvas2DLayerBridgeTest, BackgroundRenderingWhileHibernatingWithDeferredRenderingDisabled)
 #else
 TEST_F(Canvas2DLayerBridgeTest, DISABLED_BackgroundRenderingWhileHibernatingWithDeferredRenderingDisabled)
@@ -744,7 +744,7 @@ TEST_F(Canvas2DLayerBridgeTest, DISABLED_BackgroundRenderingWhileHibernatingWith
     postAndWaitDestroyBridgeTask(BLINK_FROM_HERE, testThread.get(), &bridge);
 }
 
-#if CANVAS2D_HIBERNATION_ENABLED
+#if CANVAS2D_HIBERNATION_ENABLED && CANVAS2D_BACKGROUND_RENDER_SWITCH_TO_CPU
 TEST_F(Canvas2DLayerBridgeTest, DisableDeferredRenderingWhileHibernating)
 #else
 TEST_F(Canvas2DLayerBridgeTest, DISABLED_DisableDeferredRenderingWhileHibernating)
@@ -1052,7 +1052,7 @@ TEST_F(Canvas2DLayerBridgeTest, DISABLED_PrepareMailboxWhileHibernating)
     postAndWaitDestroyBridgeTask(BLINK_FROM_HERE, testThread.get(), &bridge);
 }
 
-#if CANVAS2D_HIBERNATION_ENABLED
+#if CANVAS2D_HIBERNATION_ENABLED && CANVAS2D_BACKGROUND_RENDER_SWITCH_TO_CPU
 TEST_F(Canvas2DLayerBridgeTest, PrepareMailboxWhileBackgroundRendering)
 #else
 TEST_F(Canvas2DLayerBridgeTest, DISABLED_PrepareMailboxWhileBackgroundRendering)
