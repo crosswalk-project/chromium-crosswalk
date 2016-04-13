43d41bf62871baa0f34d521afc48ecfccbb97902
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2016-04-13 14:18:47 +0300

    Fix calling of AudioOutputStream::AudioSourceCallback API
    Before this change calling of AudioOutputStream::AudioSourceCallback
    method without stream position ended up in an empty implementation.
    
    BUG=XWALK-6703

--- media/audio/simple_sources.h
+++ media/audio/simple_sources.h
@@ -37,7 +37,8 @@ class MEDIA_EXPORT SineWaveAudioSource
   // Implementation of AudioSourceCallback.
   int OnMoreData(AudioBus* audio_bus,
                  uint32_t total_bytes_delay,
-                 uint32_t frames_skipped) override;
+                 uint32_t frames_skipped,
+                 const StreamPosition& position) override;
   void OnError(AudioOutputStream* stream) override;
 
   // The number of OnMoreData() and OnError() calls respectively.
@@ -64,7 +65,8 @@ class MEDIA_EXPORT FileSource : public AudioOutputStream::AudioSourceCallback,
   // Implementation of AudioSourceCallback.
   int OnMoreData(AudioBus* audio_bus,
                  uint32_t total_bytes_delay,
-                 uint32_t frames_skipped) override;
+                 uint32_t frames_skipped,
+                 const StreamPosition& position) override;
   void OnError(AudioOutputStream* stream) override;
 
  private:
@@ -97,7 +99,8 @@ class BeepingSource : public AudioOutputStream::AudioSourceCallback {
   // Implementation of AudioSourceCallback.
   int OnMoreData(AudioBus* audio_bus,
                  uint32_t total_bytes_delay,
-                 uint32_t frames_skipped) override;
+                 uint32_t frames_skipped,
+                 const StreamPosition& position) override;
   void OnError(AudioOutputStream* stream) override;
 
   static void BeepOnce();
