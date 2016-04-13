4f87b062ae2f8f18fa9a6aa93875f7fa840d6a66
Jason Kersey <kerz@google.com>
2016-04-12 17:06:00 -0700

    isUploadEnabledForTests() does not exist on 2661.
    TBR=amineer,asvitkine
    BUG=602703
    
    Review URL: https://codereview.chromium.org/1879223003 .
    
    Cr-Commit-Position: refs/branch-heads/2661@{#574}
    Cr-Branched-From: ef6f6ae5e4c96622286b563658d5cd62a6cf1197-refs/heads/master@{#378081}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferencesManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferencesManager.java
@@ -383,7 +383,7 @@ public class PrivacyPreferencesManager implements CrashReportingPermissionManage
      */
     @Override
     public boolean isUmaUploadPermitted() {
-        return isNetworkAvailable() && (allowUploadCrashDump() || isUploadEnabledForTests());
+        return isNetworkAvailable() && allowUploadCrashDump();
     }
 
     /**
