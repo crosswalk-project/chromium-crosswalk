627e8ababa85e8cc0e83e3e84702fccd0a782dc5
Ben Ruthig <bruthig@chromium.org>
2016-04-28 10:53:57 -0400

    Expand the Material Design hover as it fades out when a ripple is triggered.
    BUG=582253
    TEST=manual
    
    Review URL: https://codereview.chromium.org/1890243002
    
    Cr-Commit-Position: refs/heads/master@{#389377}
    (cherry picked from commit caf80a39907860be5a649a81e5418721faac2f98)
    
    Review URL: https://codereview.chromium.org/1924663007 .
    
    Cr-Commit-Position: refs/branch-heads/2704@{#285}
    Cr-Branched-From: 6e53600def8f60d8c632fadc70d7c1939ccea347-refs/heads/master@{#386251}

--- ui/views/animation/ink_drop_host_view.cc
+++ ui/views/animation/ink_drop_host_view.cc
@@ -5,6 +5,7 @@
 #include "ui/views/animation/ink_drop_host_view.h"
 
 #include "ui/gfx/color_palette.h"
+#include "ui/gfx/geometry/size_conversions.h"
 #include "ui/views/animation/ink_drop_hover.h"
 #include "ui/views/animation/square_ink_drop_animation.h"
 
@@ -13,6 +14,17 @@ namespace views {
 // Default sizes for ink drop effects.
 const int kInkDropSize = 24;
 const int kInkDropLargeCornerRadius = 4;
+
+// The scale factor to compute the large ink drop size.
+const float kLargeInkDropScale = 1.333f;
+
+namespace {
+
+gfx::Size CalculateLargeInkDropSize(const gfx::Size small_size) {
+  return gfx::ScaleToCeiledSize(gfx::Size(small_size), kLargeInkDropScale);
+}
+
+}  // namespace
 
 // static
 const int InkDropHostView::kInkDropSmallCornerRadius = 2;
@@ -35,12 +47,10 @@ void InkDropHostView::RemoveInkDropLayer(ui::Layer* ink_drop_layer) {
 }
 
 scoped_ptr<InkDropAnimation> InkDropHostView::CreateInkDropAnimation() const {
-  gfx::Size large_drop(ink_drop_size_.width() * 4 / 3,
-                       ink_drop_size_.height() * 4 / 3);
-
   scoped_ptr<InkDropAnimation> animation(new SquareInkDropAnimation(
-      large_drop, kInkDropLargeCornerRadius, ink_drop_size_,
-      kInkDropSmallCornerRadius, GetInkDropCenter(), GetInkDropBaseColor()));
+      CalculateLargeInkDropSize(ink_drop_size_), kInkDropLargeCornerRadius,
+      ink_drop_size_, kInkDropSmallCornerRadius, GetInkDropCenter(),
+      GetInkDropBaseColor()));
   return animation;
 }
 
@@ -48,6 +58,7 @@ scoped_ptr<InkDropHover> InkDropHostView::CreateInkDropHover() const {
   scoped_ptr<InkDropHover> hover(
       new InkDropHover(ink_drop_size_, kInkDropSmallCornerRadius,
                        GetInkDropCenter(), GetInkDropBaseColor()));
+  hover->set_explode_size(CalculateLargeInkDropSize(ink_drop_size_));
   return hover;
 }
 
