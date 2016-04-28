586346cc515ffc40f1ada1d04b633c060517a9db
Yan, Shaobo <shaobo.yan@intel.com>
2016-04-29 04:36:55 +0800

    [Blink] WebCL support for Crosswalk.
    WebCL 1.0 defines a JavaScript binding to the Khronos OpenCL standard
    for heterogeneous parallel computing. Its spec is available at:
    http://www.khronos.org/registry/webcl/specs/latest/1.0/
    
    This patch includes WebCL basic APIs support, dynamic loading mechanism
    etc. WebCL support is controlled by "ENABLE_WEBCL" flag, and it's enabled
    by default on Android. If you want to disable with WebCL,
    please add "-Denable_webcl=0" when running the "xwalk/gyp_xwalk" before building.
    
    Currently, Intel CPU/GPU, Qualcomm CPU/GPU and Power VR GPU are supported.
    But OpenCL library isn't always shipped with device by default, even it supports OpenCL.
    For example: Google Nexus series. It requires to install OpenCL library manually.
    
    TEST=WebCL conformances test on Asus MemoPad and Xiaomi3.
    
    Known issue:
    1. miss oilpan support.
    2. miss cl_khr_gl_sharing extension support.
    3. load library in render process.
    
    R=dongseong.hwang@intel.com, halton.huo@intel.com, kenneth.r.christiansen@intel.com
    
    BUG=XWALK-4254
    
    [M48: adapted to the following upstream changes:
     - https://codereview.chromium.org/1303153005
     - https://codereview.chromium.org/1362103002
     - https://codereview.chromium.org/1372463002
     - https://codereview.chromium.org/1441573002]
    
    [M49: adapted to the following changes:
    WebCL: Only set ENABLE_WEBCL in the relevant files.
    WebCL: Remove "config.h" in WebCL Related Implementation and Fix
    Compile Error
    WebCL: Using gyp to enable or disable WebCL. BUG=XWALK-6200
    WebCL: Fix calling convention of callbacks.
    WebCL: Build WebCL for all platforms and enable it during runtime.
    WebCL: Use the CPU() macro provided by WTF for CPU arch detection.
    WebCL: Fix build with component=shared_library.]
    
    [M50: adapted to the following changes:
    WebCL: Modified corresponding class to final for warning error
    elimation.
    WebCL: Change WebCLCallback parent class from RefCounted to
    GarbageCollectedFinalized for idl generation rule changed. Now the
    upstream generate callbakc interface with oilpan support.
    WebCL: Due to WebCLCallback is oilpan supported, Modifined RefPtr to
    Persistent or Member, Vector to HeapVector/PersistentHeapVector in
    corresponding non-garbage-collected class to elimate compile error.]

---
+++ third_party/WebKit/Source/modules/webcl/DOMWindowWebCL.h
@@ -0,0 +1,40 @@
+// Copyright (C) 2015 Intel Corporation All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DOMWindowWebCL_h
+#define DOMWindowWebCL_h
+
+#include "core/frame/DOMWindowProperty.h"
+#include "platform/Supplementable.h"
+
+namespace blink {
+
+class DOMWindow;
+class LocalDOMWindow;
+class WebCL;
+
+class DOMWindowWebCL final : public GarbageCollectedFinalized<DOMWindowWebCL>, public HeapSupplement<LocalDOMWindow>, public DOMWindowProperty {
+    USING_GARBAGE_COLLECTED_MIXIN(DOMWindowWebCL);
+public:
+    static DOMWindowWebCL& from(LocalDOMWindow&);
+    static WebCL* webcl(DOMWindow&);
+
+    void willDestroyGlobalObjectInFrame() override;
+    void willDetachGlobalObjectFromFrame() override;
+
+    DECLARE_TRACE();
+
+private:
+    explicit DOMWindowWebCL(LocalDOMWindow&);
+
+    WebCL* webcl();
+    static const char* supplementName();
+
+    Member<LocalDOMWindow> m_window;
+    RefPtr<WebCL> m_webcl;
+};
+
+} // namespace blink
+
+#endif // DOMWindowWebCL_h
