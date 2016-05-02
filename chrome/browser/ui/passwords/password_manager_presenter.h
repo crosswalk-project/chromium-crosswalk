3877e40f5ac8fc8a45aabab1be84a2472abaadff
Vasilii Sukhanov <vasilii@chromium.org>
2016-05-02 13:33:00 +0200

    [Password Manager] Add federations to sort key on password page
    If federations are not included into sort key, entries with the same origin, username and password, but different federation will be collapsed into one entry. That is not correct.
    
    BUG=604750
    
    Review-Url: https://codereview.chromium.org/1927753003
    Cr-Commit-Position: refs/heads/master@{#390457}
    (cherry picked from commit 7c82fec6686c0ee3b38bb70d86d0f0c1837127c9)
    
    Review URL: https://codereview.chromium.org/1936053002 .
    
    Cr-Commit-Position: refs/branch-heads/2704@{#329}
    Cr-Branched-From: 6e53600def8f60d8c632fadc70d7c1939ccea347-refs/heads/master@{#386251}

--- chrome/browser/ui/passwords/password_manager_presenter.h
+++ chrome/browser/ui/passwords/password_manager_presenter.h
@@ -26,6 +26,8 @@ struct PasswordForm;
 using DuplicatesMap =
     std::multimap<std::string, std::unique_ptr<autofill::PasswordForm>>;
 
+enum class PasswordEntryType { SAVED, BLACKLISTED };
+
 class PasswordUIView;
 
 class Profile;
@@ -79,14 +81,14 @@ class PasswordManagerPresenter
 
   // Sort entries of |list| based on sort key. The key is the concatenation of
   // origin, entry type (non-Android credential, Android w/ affiliated web realm
-  // or Android w/o affiliated web realm). If |username_and_password_in_key|,
-  // username and password are also included in sort key. If there are several
-  // forms with the same key, all such forms but the first one are
+  // or Android w/o affiliated web realm). If |entry_type == SAVED|,
+  // username, password and federation are also included in sort key. If there
+  // are several forms with the same key, all such forms but the first one are
   // stored in |duplicates| instead of |list|.
   void SortEntriesAndHideDuplicates(
       std::vector<std::unique_ptr<autofill::PasswordForm>>* list,
       DuplicatesMap* duplicates,
-      bool username_and_password_in_key);
+      PasswordEntryType entry_type);
 
   // A short class to mediate requests to the password store.
   class ListPopulater : public password_manager::PasswordStoreConsumer {
