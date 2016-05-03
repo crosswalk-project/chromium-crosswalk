b18931f9693da894c393eafc70febed1e44389c4
Ben Ruthig <bruthig@chromium.org>
2016-05-03 15:38:34 -0400

    Enabled tests to control material design ink drop animations.
    The InkDropAnimationControllerImpl choreographs the complex interaction of the
    InkDropAnimations and InkDropHover animations. This interaction relies on
    internal observations of when the different animations start and finish. This
    change enables tests to control when these animations start and finish and allow
    tests to validate states that was previously impossible to inspect when using
    the ui::ScopedAnimationDurationScaleMode::ZERO_DURATION value.
    
    BUG=604415
    TEST=InkDropAnimationControllerImplTest.*
    
    Review-Url: https://codereview.chromium.org/1913243002
    Cr-Commit-Position: refs/heads/master@{#390416}
    (cherry picked from commit e75c5ce7fa1226bb678850cf6221ce4f503f16ab)
    
    Review URL: https://codereview.chromium.org/1944043002 .
    
    Cr-Commit-Position: refs/branch-heads/2704@{#359}
    Cr-Branched-From: 6e53600def8f60d8c632fadc70d7c1939ccea347-refs/heads/master@{#386251}

--- ui/views/animation/ink_drop_animation_controller_impl.h
+++ ui/views/animation/ink_drop_animation_controller_impl.h
@@ -18,6 +18,10 @@ class Timer;
 }  // namespace base
 
 namespace views {
+namespace test {
+class InkDropAnimationControllerImplTestApi;
+}  // namespace test
+
 class InkDropAnimation;
 class InkDropHost;
 class InkDropHover;
@@ -41,8 +45,7 @@ class VIEWS_EXPORT InkDropAnimationControllerImpl
   void SetHovered(bool is_hovered) override;
 
  private:
-  friend class InkDropAnimationControllerFactoryTest;
-  friend class InkDropAnimationControllerImplTest;
+  friend class test::InkDropAnimationControllerImplTestApi;
 
   // Destroys |ink_drop_animation_| if it's targeted to the HIDDEN state.
   void DestroyHiddenTargetedAnimations();
