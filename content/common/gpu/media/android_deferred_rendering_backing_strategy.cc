988a874dfd930b387b045c440ca0f74b63e3729f
liberato <liberato@chromium.org>
2016-05-12 00:16:35 -0700

    Roll-up merge to M51 of AVDA::ShouldCopyPictures fixes.
    Roll-up of https://codereview.chromium.org/1965473002/ and
    https://codereview.chromium.org/1807173004/ .
    
    https://codereview.chromium.org/1965473002/ :
    
    Fix workaround for Galaxy Tab in AVDA.
    
    Previously, the workaround for turning off texture copy on some
    Galaxy Tabs did nothing, since it was done after the check for
    the workaound in FeatureInfo.  Since FeatureInfo is present, but
    isn't set correctly for Galaxy Tab (bad gpu strings), it returned
    success before the workaround was reached.
    
    This CL checks FeatureInfo after the Galaxy Tab workaround.
    
    https://codereview.chromium.org/1807173004/ :
    
    Added additional Galaxy Tab devices to AVDA workaround.
    
    Based on crash reports, widened the Galaxy tab workaround list for
    copying pictures in AVDA to include SM-J105, SM-J111, SM-J120,
    SM-T280, SM-T285, and SM-J320.
    
    BUG=591600
    NOTRY=true
    NOPRESUBMIT=true
    TBR=dalecurtis@chromium.org
    
    Review-Url: https://codereview.chromium.org/1972063002
    Cr-Commit-Position: refs/branch-heads/2704@{#517}
    Cr-Branched-From: 6e53600def8f60d8c632fadc70d7c1939ccea347-refs/heads/master@{#386251}

--- content/common/gpu/media/android_deferred_rendering_backing_strategy.cc
+++ content/common/gpu/media/android_deferred_rendering_backing_strategy.cc
@@ -441,28 +441,34 @@ bool AndroidDeferredRenderingBackingStrategy::ShouldCopyPictures() const {
           state_provider_->GetGlDecoder().get()) {
     if (gpu::gles2::ContextGroup* group = gl_decoder->GetContextGroup()) {
       if (gpu::gles2::FeatureInfo* feature_info = group->feature_info()) {
-        return !feature_info->workarounds().avda_dont_copy_pictures;
+        if (feature_info->workarounds().avda_dont_copy_pictures)
+          return false;
       }
     }
   }
 
   // Samsung Galaxy Tab A, J3, and J1 Mini all like to crash on Lollipop in
-  // glEGLImageTargetTexture2DOES .  Exact models were SM-T280, SM-J320F,
-  // and SM-j105H.
+  // glEGLImageTargetTexture2DOES .  These include SM-J105, SM-J111, SM-J120,
+  // SM-T280, SM-T285, and SM-J320 with various suffixes.  All run lollipop and
+  // and have a Mali-400 gpu.
+  // For these devices, we must check based on the brand / model
+  // number, since the strings used by FeatureInfo aren't populated.
   if (base::android::BuildInfo::GetInstance()->sdk_int() <= 22) {  // L MR1
     const std::string brand(
         base::ToLowerASCII(base::android::BuildInfo::GetInstance()->brand()));
     if (brand == "samsung") {
       const std::string model(
           base::ToLowerASCII(base::android::BuildInfo::GetInstance()->model()));
-      if (model.find("sm-t280") != std::string::npos ||
-          model.find("sm-j320f") != std::string::npos ||
-          model.find("sm-j105") != std::string::npos)
+      if (model.find("sm-j105") != std::string::npos ||
+          model.find("sm-j111") != std::string::npos ||
+          model.find("sm-j120") != std::string::npos ||
+          model.find("sm-t280") != std::string::npos ||
+          model.find("sm-t285") != std::string::npos ||
+          model.find("sm-j320") != std::string::npos)
         return false;
     }
   }
 
-  // Assume so.
   return true;
 }
 
