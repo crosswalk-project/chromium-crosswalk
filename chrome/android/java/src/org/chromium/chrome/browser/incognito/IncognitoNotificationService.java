aeb6605639d596f681b14dc4e44a25bc56b05e80
Ted Choc <tedchoc@google.com>
2016-05-23 11:33:37 -0700

    Fix the incognito notification if you are in the tab switcher.
    The problem is the default close all action allows the behavior
    to be delegated out (in particular to the tab switcher UI) that
    delays actually closing the tabs.  By removing the ability to
    delegate the close all, we ensure the tab count reaches 0 as
    we expect in the code.
    
    BUG=613524
    
    Review-Url: https://codereview.chromium.org/1996883003
    Cr-Commit-Position: refs/heads/master@{#395178}
    
    Review URL: https://codereview.chromium.org/2008603002 .
    
    Cr-Commit-Position: refs/branch-heads/2704@{#640}
    Cr-Branched-From: 6e53600def8f60d8c632fadc70d7c1939ccea347-refs/heads/master@{#386251}

--- chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationService.java
@@ -102,7 +102,12 @@ public class IncognitoNotificationService extends IntentService {
                 if (incognitoCount == 0) {
                     IncognitoNotificationManager.dismissIncognitoNotification();
                 }
+            }
+        });
 
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
                 // Now ensure that the snapshots in recents are all cleared for Tabbed activities
                 // to remove any trace of incognito mode.
                 if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
@@ -208,7 +213,8 @@ public class IncognitoNotificationService extends IntentService {
                             ChromeTabbedActivity tabbedActivity = (ChromeTabbedActivity) activity;
                             if (tabbedActivity.isActivityDestroyed()) continue;
 
-                            tabbedActivity.getTabModelSelector().getModel(true).closeAllTabs();
+                            tabbedActivity.getTabModelSelector().getModel(true).closeAllTabs(
+                                    false, false);
                             selectorIndexes.add(TabWindowManager.getInstance().getIndexForWindow(
                                     tabbedActivity));
                         }
