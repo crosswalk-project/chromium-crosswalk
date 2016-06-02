c31f0153a7045f5338935e26984eb41903a6f285
Ian Clelland <iclelland@google.com>
2016-06-01 23:49:24 -0400

    [Origin Trials] Install origin trial bindings on V8 context conditionally
    This patch removes the call-time check for origin-trial-enabled features,
    replacing it with code that installs features into the V8 context as trial
    tokens are encountered. This means that experimental features which were previously polluting the global namespaces are now completely unavailable except when enabled by origin trials. (The specific case of pollution caused by Durable Storage is removed from test expectations in this patch)
    
    Origin trials are now initialized after the V8 context, by calling initializeOriginTrials. This calls, through a series of function pointers, the bindings initialization code for core, modules, and optionally internals (for tests).
    
    This patch implements the design at https://docs.google.com/document/d/1vwKD8sUrr5mtktvEwvO_f2KUsNhpcgLMixYXhD8K4KY
    
    BUG=584367
    
    Review-Url: https://codereview.chromium.org/2005433002
    Cr-Commit-Position: refs/heads/master@{#396860}
    (cherry picked from commit e3f3c54cae7eea91719344411dbfe89a717f2e9f)
    
    Review URL: https://codereview.chromium.org/2032613005 .
    
    Cr-Commit-Position: refs/branch-heads/2743@{#185}
    Cr-Branched-From: 2b3ae3b8090361f8af5a611712fc1a5ab2de53cb-refs/heads/master@{#394939}

--- third_party/WebKit/Source/core/origin_trials/OriginTrialContext.h
+++ third_party/WebKit/Source/core/origin_trials/OriginTrialContext.h
@@ -72,6 +72,13 @@ public:
     // token validator object that is normally used (e.g. for testing).
     bool isFeatureEnabled(const String& featureName, String* errorMessage, WebTrialTokenValidator* = nullptr);
 
+    // Installs JavaScript bindings for any features which should be enabled by
+    // the current set of trial tokens. This method is idempotent; only features
+    // which have been enabled since the last time it was run will be installed.
+    // If the V8 context for the host execution context has not been
+    // initialized, then this method will return without doing anything.
+    void initializePendingFeatures();
+
     void setFeatureBindingsInstalled(const String& featureName);
     bool featureBindingsInstalled(const String& featureName);
 
