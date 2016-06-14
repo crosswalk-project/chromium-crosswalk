6895d9eea0c14482c02bb6b27e756f3fd2abdff2
Peter Kasting <pkasting@chromium.org>
2016-06-14 15:30:38 -0700

    Add heuristics to limit showing of new backspace UI bubble.
    This restricts the bubble to showing 5 times, and only showing when users
    trigger backspace twice within three seconds (which likely indicates "hey, why
    isn't the browser going back").
    
    The bubble is also immediately hidden (by fading out) if the user successfully
    navigates back or forward (by any means) while it's showing.
    
    BUG=610039,615760
    TEST=New "press alt-left to go back" UI should not appear when pressing backspace (to attempt to go back) unless backspace is pressed twice within three seconds.  After the UI is shown 5 times, it should not appear again.
    TBR=phajdan.jr
    
    Review-Url: https://codereview.chromium.org/2041293002
    Cr-Commit-Position: refs/heads/master@{#399114}
    (cherry picked from commit 36aa72944195f579454c1d3abea5045219fd685a)
    
    Review URL: https://codereview.chromium.org/2065283002 .
    
    Cr-Commit-Position: refs/branch-heads/2743@{#358}
    Cr-Branched-From: 2b3ae3b8090361f8af5a611712fc1a5ab2de53cb-refs/heads/master@{#394939}

--- chrome/browser/ui/views/new_back_shortcut_bubble.cc
+++ chrome/browser/ui/views/new_back_shortcut_bubble.cc
@@ -31,8 +31,7 @@ const int kShowDurationMs = 3800;
 }
 
 NewBackShortcutBubble::NewBackShortcutBubble(
-    ExclusiveAccessBubbleViewsContext* context,
-    bool forward)
+    ExclusiveAccessBubbleViewsContext* context)
     : bubble_view_context_(context),
       animation_(new gfx::SlideAnimation(this)),
       view_(new SubtleNotificationView(nullptr)),
@@ -40,7 +39,6 @@ NewBackShortcutBubble::NewBackShortcutBubble(
           bubble_view_context_->GetBubbleParentView(),
           view_,
           false)) {
-  UpdateContent(forward);
 }
 
 NewBackShortcutBubble::~NewBackShortcutBubble() {
@@ -48,6 +46,10 @@ NewBackShortcutBubble::~NewBackShortcutBubble() {
   // ~ExclusiveAccessBubbleViews.
   popup_->Close();
   base::MessageLoop::current()->DeleteSoon(FROM_HERE, popup_);
+}
+
+bool NewBackShortcutBubble::IsVisible() const {
+  return popup_->IsVisible();
 }
 
 void NewBackShortcutBubble::UpdateContent(bool forward) {
@@ -81,6 +83,11 @@ void NewBackShortcutBubble::UpdateContent(bool forward) {
   hide_timeout_.Start(FROM_HERE,
                       base::TimeDelta::FromMilliseconds(kShowDurationMs), this,
                       &NewBackShortcutBubble::OnTimerElapsed);
+}
+
+void NewBackShortcutBubble::Hide() {
+  hide_timeout_.Stop();
+  OnTimerElapsed();
 }
 
 void NewBackShortcutBubble::AnimationProgressed(
--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -343,6 +343,7 @@ extern const char kDefaultBrowserSettingEnabled[];
 extern const char kShowUpdatePromotionInfoBar[];
 #endif
 extern const char kUseCustomChromeFrame[];
+extern const char kBackShortcutBubbleShownCount[];
 #if defined(ENABLE_PLUGINS)
 extern const char kContentSettingsPluginWhitelist[];
 #endif
