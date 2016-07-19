e43c2f45f70c6ad3012850e519c95d9707af2677
srikumarb <sbonda@nvidia.com>
2016-07-19 17:35:29 -0500

    Update MediaResourceGetter.java

--- content/public/android/java/src/org/chromium/content/browser/MediaResourceGetter.java
+++ content/public/android/java/src/org/chromium/content/browser/MediaResourceGetter.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2015, NVIDIA CORPORATION. All rights reserved.
 // Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
