36696dff8591b5626ad74b9c0eed200c0444838c
Theresa Wellington <twellington@google.com>
2016-07-19 17:27:32 -0700

    Disable Android FRE in demo mode
    BUG=625032
    TBR=tedchoc@chromium.org,nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/2157393003 .
    
    Review-Url: https://codereview.chromium.org/2113243004
    Cr-Original-Commit-Position: refs/heads/master@{#404002}
    Cr-Commit-Position: refs/branch-heads/2785@{#231}
    Cr-Branched-From: 68623971be0cfc492a2cb0427d7f478e7b214c24-refs/heads/master@{#403382}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
@@ -62,7 +62,8 @@ public abstract class FirstRunFlowSequencer  {
      * Once finished, calls onFlowIsKnown().
      */
     public void start() {
-        if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)) {
+        if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
+                || ApiCompatibilityUtils.isDemoUser(mActivity)) {
             onFlowIsKnown(null);
             return;
         }
@@ -219,7 +220,8 @@ public abstract class FirstRunFlowSequencer  {
      */
     public static Intent checkIfFirstRunIsNecessary(Context context, boolean fromChromeIcon) {
         // If FRE is disabled (e.g. in tests), proceed directly to the intent handling.
-        if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)) {
+        if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
+                || ApiCompatibilityUtils.isDemoUser(context)) {
             return null;
         }
 
