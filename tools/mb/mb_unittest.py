73a5c3676f396421ad41fcb89812a9f7e9f6f797
Dirk Pranke <dpranke@chromium.org>
2016-07-27 17:03:36 -0700

    Implement multi-phase build support in MB.
    The PGO builders need to run multiple compiles in a single build,
    each with different arguments. This was not previously possible
    with MB, but this CL adds support for specifying a list of configs
    per builder, and adds a --phase command line arg to indicate which
    phase of the build should be generated.
    
    It is an error to specify --phase on a builder that isn't
    expected to have multiple phases. Also phases are 1-based (phase 1,
    phase 2, etc.).
    
    Also, this CL cleans up some TODOs that I no longer plan to fix.
    
    TBR=sebmarchand@chromium.org
    BUG=595947
    
    Review URL: https://codereview.chromium.org/2181183005 .
    
    Review-Url: https://codereview.chromium.org/2160353003
    Cr-Original-Commit-Position: refs/heads/master@{#406955}
    Cr-Commit-Position: refs/branch-heads/2743@{#704}
    Cr-Branched-From: 2b3ae3b8090361f8af5a611712fc1a5ab2de53cb-refs/heads/master@{#394939}

--- tools/mb/mb_unittest.py
+++ tools/mb/mb_unittest.py
@@ -105,13 +105,6 @@ class FakeFile(object):
 
 TEST_CONFIG = """\
 {
-  'configs': {
-    'gyp_rel_bot': ['gyp', 'rel', 'goma'],
-    'gn_debug_goma': ['gn', 'debug', 'goma'],
-    'gyp_debug': ['gyp', 'debug', 'fake_feature1'],
-    'gn_rel_bot': ['gn', 'rel', 'goma'],
-    'gyp_crosscompile': ['gyp', 'crosscompile'],
-  },
   'masters': {
     'chromium': {},
     'fake_master': {
@@ -121,7 +114,17 @@ TEST_CONFIG = """\
       'fake_gn_debug_builder': 'gn_debug_goma',
       'fake_gyp_builder': 'gyp_debug',
       'fake_gn_args_bot': '//build/args/bots/fake_master/fake_gn_args_bot.gn',
+      'fake_multi_phase': ['gn_phase_1', 'gn_phase_2'],
     },
+  },
+  'configs': {
+    'gyp_rel_bot': ['gyp', 'rel', 'goma'],
+    'gn_debug_goma': ['gn', 'debug', 'goma'],
+    'gyp_debug': ['gyp', 'debug', 'fake_feature1'],
+    'gn_rel_bot': ['gn', 'rel', 'goma'],
+    'gyp_crosscompile': ['gyp', 'crosscompile'],
+    'gn_phase_1': ['gn', 'phase_1'],
+    'gn_phase_2': ['gn', 'phase_2'],
   },
   'mixins': {
     'crosscompile': {
@@ -136,6 +139,14 @@ TEST_CONFIG = """\
     'goma': {
       'gn_args': 'use_goma=true',
       'gyp_defines': 'goma=1',
+    },
+    'phase_1': {
+      'gn_args': 'phase=1',
+      'gyp_args': 'phase=1',
+    },
+    'phase_2': {
+      'gn_args': 'phase=2',
+      'gyp_args': 'phase=2',
     },
     'rel': {
       'gn_args': 'is_debug=false',
@@ -463,6 +474,35 @@ class UnitTest(unittest.TestCase):
     finally:
       sys.stdout = orig_stdout
 
+  def test_multiple_phases(self):
+    # Check that not passing a --phase to a multi-phase builder fails.
+    mbw = self.check(['lookup', '-m', 'fake_master', '-b', 'fake_multi_phase'],
+                     ret=1)
+    self.assertIn('Must specify a build --phase', mbw.out)
+
+    # Check that passing a --phase to a single-phase builder fails.
+    mbw = self.check(['lookup', '-m', 'fake_master', '-b', 'fake_gn_builder',
+                      '--phase', '1'],
+                     ret=1)
+    self.assertIn('Must not specify a build --phase', mbw.out)
+
+    # Check different ranges; 0 and 3 are out of bounds, 1 and 2 should work.
+    mbw = self.check(['lookup', '-m', 'fake_master', '-b', 'fake_multi_phase',
+                      '--phase', '0'], ret=1)
+    self.assertIn('Phase 0 out of bounds', mbw.out)
+
+    mbw = self.check(['lookup', '-m', 'fake_master', '-b', 'fake_multi_phase',
+                      '--phase', '1'], ret=0)
+    self.assertIn('phase = 1', mbw.out)
+
+    mbw = self.check(['lookup', '-m', 'fake_master', '-b', 'fake_multi_phase',
+                      '--phase', '2'], ret=0)
+    self.assertIn('phase = 2', mbw.out)
+
+    mbw = self.check(['lookup', '-m', 'fake_master', '-b', 'fake_multi_phase',
+                      '--phase', '3'], ret=1)
+    self.assertIn('Phase 3 out of bounds', mbw.out)
+
   def test_validate(self):
     mbw = self.fake_mbw()
     self.check(['validate'], mbw=mbw, ret=0)
@@ -471,6 +511,7 @@ class UnitTest(unittest.TestCase):
     mbw = self.fake_mbw()
     mbw.files[mbw.default_config] = TEST_BAD_CONFIG
     self.check(['validate'], mbw=mbw, ret=1)
+
 
 
 if __name__ == '__main__':
