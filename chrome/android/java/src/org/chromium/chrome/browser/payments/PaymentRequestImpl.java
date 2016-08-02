f3d399a063e61c09c8a99d12bcfc9105ac71c669
Rouslan Solomakhin <rouslan@chromium.org>
2016-08-02 16:08:35 -0700

    [Merge M-53] Always close payments UI after disconnecting from the renderer.
    BUG=633604
    
    Review-Url: https://codereview.chromium.org/2199923004
    Cr-Commit-Position: refs/heads/master@{#409325}
    (cherry picked from commit bf7ab24213d1c6b4d477d3ef0957f6d25d8677d0)
    
    Review URL: https://codereview.chromium.org/2205133002 .
    
    Cr-Commit-Position: refs/branch-heads/2785@{#473}
    Cr-Branched-From: 68623971be0cfc492a2cb0427d7f478e7b214c24-refs/heads/master@{#403382}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
@@ -816,7 +816,6 @@ public class PaymentRequestImpl implements PaymentRequest, PaymentRequestUI.Clie
     @Override
     public void onDismiss() {
         disconnectFromClientWithDebugMessage("Dialog dismissed");
-        closeUI(false);
     }
 
     private void disconnectFromClientWithDebugMessage(String debugMessage) {
@@ -827,6 +826,7 @@ public class PaymentRequestImpl implements PaymentRequest, PaymentRequestUI.Clie
         Log.d(TAG, debugMessage);
         if (mClient != null) mClient.onError(reason);
         closeClient();
+        closeUI(false);
     }
 
     @Override
