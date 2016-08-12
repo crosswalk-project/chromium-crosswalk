3eeb3eab582355e6176af768920759fcb349b2fc
twellington <twellington@chromium.org>
2016-08-11 18:57:05 -0700

    Revert of Separate data use reporting logic in ExternalDataUseObserver (patchset #1 id:1 of https://codereview.chromium.org/2237253002/ )
    Reason for revert:
    Fails to compile
    
    Original issue's description:
    > Separate data use reporting logic in ExternalDataUseObserver
    >
    > ExternalDataUseReporter class is created with the data use reporting
    > logic. ExternalDataUseReporter is created in IO thread and moved to UI
    > thread. It is owned by ExternalDataUseObserver.
    >
    > BUG=570884
    >
    > Review-Url: https://codereview.chromium.org/2145863002
    > Cr-Commit-Position: refs/heads/master@{#406041}
    > (cherry picked from commit 7ef8efba12b3ce0c6101175451130ab5c48fce1c)
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/420db8b477d2ab04c5899b56ddf140c8f85435da
    
    TBR=rajendrant@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=570884
    
    Review-Url: https://codereview.chromium.org/2242463003
    Cr-Commit-Position: refs/branch-heads/2785@{#572}
    Cr-Branched-From: 68623971be0cfc492a2cb0427d7f478e7b214c24-refs/heads/master@{#403382}

--- chrome/browser/android/data_usage/data_use_tab_model.h
+++ chrome/browser/android/data_usage/data_use_tab_model.h
@@ -221,7 +221,6 @@ class DataUseTabModel {
  private:
   friend class DataUseTabModelTest;
   friend class ExternalDataUseObserverTest;
-  friend class ExternalDataUseReporterTest;
   friend class TabDataUseEntryTest;
   friend class TestDataUseTabModel;
   FRIEND_TEST_ALL_PREFIXES(DataUseTabModelTest,
