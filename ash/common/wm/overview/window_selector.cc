8b67fa253af23fe7fa638ac27a95594cdc3b526d
Mitsuru Oshima <oshima@chromium.org>
2016-08-17 16:54:17 -0700

    Don't reposition if shutdown is in progress
    BUG=634572
    
    Review-Url: https://codereview.chromium.org/2249853009
    Cr-Commit-Position: refs/heads/master@{#412675}
    (cherry picked from commit bbe0bdc738e50b89b5bffb25e431e6b6e75a4249)
    
    Review URL: https://codereview.chromium.org/2252833004 .
    
    Cr-Commit-Position: refs/branch-heads/2785@{#647}
    Cr-Branched-From: 68623971be0cfc492a2cb0427d7f478e7b214c24-refs/heads/master@{#403382}

--- ash/common/wm/overview/window_selector.cc
+++ ash/common/wm/overview/window_selector.cc
@@ -379,6 +379,7 @@ void WindowSelector::Init(const WindowList& windows) {
 // may cause other, unrelated classes, (ie PanelLayoutManager) to make indirect
 // calls to restoring_minimized_windows() on a partially destructed object.
 void WindowSelector::Shutdown() {
+  is_shut_down_ = true;
   ResetFocusRestoreWindow(true);
   RemoveAllObservers();
 
