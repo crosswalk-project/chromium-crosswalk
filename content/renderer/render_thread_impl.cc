b3d944286ca79ccbcdfb693818fff72908577152
David Reveman <reveman@chromium.org>
2016-09-21 20:09:10 -0400

    base: Allow renderer thread priorities to be changed.
    This provides a mechanism for a renderer to change the priority of
    its threads and as a result also the cpuset they are assigned to.
    
    A ChildProcessHost IPC message from the renderer is used to request
    a thread priority change and have the browser process identify the
    renderer thread that is requesting a priority change by checking all
    its threads to find a thread with NSpid field in
    /proc/[pid]/task/[thread_id]/status that matches the namespace tid
    from the renderer.
    
    This is currently limited to Linux and ChromeOS but follow up work
    will investigate the possibility and benefits of doing the same on
    other platforms.
    
    Note: Thread priorities in the renderer are already adjusted in a
    similar way on Android as the sandbox is not strict enough to
    prevent this on Android today.
    
    BUG=chrome-os-partner:56550
    TEST=
    
    Review-Url: https://codereview.chromium.org/2334533002
    Cr-Commit-Position: refs/heads/master@{#419649}
    (cherry picked from commit 7b97c3240fb3da0826f244f9b592fc1761fb6554)
    
    Review URL: https://codereview.chromium.org/2357983004 .
    
    Cr-Commit-Position: refs/branch-heads/2840@{#483}
    Cr-Branched-From: 1ae106dbab4bddd85132d5b75c670794311f4c57-refs/heads/master@{#414607}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -854,6 +854,13 @@ void RenderThreadImpl::Init(
   GetRemoteInterfaces()->GetInterface(
       mojo::GetProxy(&storage_partition_service_));
 
+#if defined(OS_LINUX)
+  ChildProcess::current()->SetIOThreadPriority(base::ThreadPriority::DISPLAY);
+  ChildThreadImpl::current()->SetThreadPriority(
+      categorized_worker_pool_->background_worker_thread_id(),
+      base::ThreadPriority::BACKGROUND);
+#endif
+
   is_renderer_suspended_ = false;
 }
 
@@ -1130,6 +1137,10 @@ void RenderThreadImpl::InitializeCompositorThread() {
   compositor_task_runner_->PostTask(
       FROM_HERE,
       base::Bind(base::IgnoreResult(&ThreadRestrictions::SetIOAllowed), false));
+#if defined(OS_LINUX)
+  ChildThreadImpl::current()->SetThreadPriority(compositor_thread_->threadId(),
+                                                base::ThreadPriority::DISPLAY);
+#endif
 
   SynchronousInputHandlerProxyClient* synchronous_input_handler_proxy_client =
       nullptr;
