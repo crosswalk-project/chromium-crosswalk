33dfe341f4e398cf5219848882fed91331043359
Peter Wen <wnwen@google.com>
2016-09-30 15:35:56 -0400

    Fix build due to missing variable.
    PathUtils removed the context parameter in ToT and I didn't realize
    it when merging 8283f2ea4fff862febec4432f7951dea2711545f.
    
    TBR=dfalcantara@chromium.org
    BUG=651901
    
    Review URL: https://codereview.chromium.org/2383853002 .
    
    Cr-Commit-Position: refs/branch-heads/2840@{#603}
    Cr-Branched-From: 1ae106dbab4bddd85132d5b75c670794311f4c57-refs/heads/master@{#414607}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabbedModeTabPersistencePolicy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabbedModeTabPersistencePolicy.java
@@ -375,7 +375,7 @@ public class TabbedModeTabPersistencePolicy implements TabPersistencePolicy {
             if (mDestroyed) return null;
 
             mTabFileNames = getOrCreateStateDirectory().list();
-            String thumbnailDirectory = PathUtils.getThumbnailCacheDirectory(mContext);
+            String thumbnailDirectory = PathUtils.getThumbnailCacheDirectory(null);
             mThumbnailFileNames = new File(thumbnailDirectory).list();
 
             mOtherTabIds = new SparseBooleanArray();
