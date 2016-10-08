ee7ca0fdc96ab4ba61d0be876c8e779adced302d
Robert Hogan <robhogan@gmail.com>
2016-10-08 12:56:00 +0100

    Don't repeat headers if at least one row of content doesn't fit
    The spec (https://drafts.csswg.org/css-tables-3/#repeated-headers) tells us that
    we shouldn't bother repeating headers if a row of content doesn't fit. It's not
    clear what to do in the situation where it's just the first page that the row
    doesn't fit. I think we ultimately want to do the same as Edge/IE and still
    repeat the headers on subsequent pages.
    
    That would require a second layout pass to achieve so for now just drop the
    repeating headers if we can't fit a row on the first page.
    
    BUG=624814
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2326303002
    Cr-Commit-Position: refs/heads/master@{#418838}
    (cherry picked from commit 8bf47bb4e1d0ea9599bc443906fc26c3ce86f152)
    
    Review URL: https://codereview.chromium.org/2400303004 .
    
    Cr-Commit-Position: refs/branch-heads/2840@{#694}
    Cr-Branched-From: 1ae106dbab4bddd85132d5b75c670794311f4c57-refs/heads/master@{#414607}

--- third_party/WebKit/Source/core/layout/LayoutTableSection.cpp
+++ third_party/WebKit/Source/core/layout/LayoutTableSection.cpp
@@ -1000,10 +1000,11 @@ void LayoutTableSection::layoutRows()
             rowLayoutObject->updateLayerTransformAfterLayout();
             if (isPaginated) {
                 paginationStrutOnRow = paginationStrutForRow(rowLayoutObject, LayoutUnit(m_rowPos[r]));
+                rowLayoutObject->setPaginationStrut(LayoutUnit(paginationStrutOnRow));
                 if (paginationStrutOnRow) {
                     // If there isn't room for at least one content row on a page with a header group, then
                     // we won't repeat the header on each page.
-                    if (!r && table()->header() && table()->sectionAbove(this) == table()->header())
+                    if (!r && table()->header() && table()->sectionAbove(this) == table()->header() && table()->header()->getPaginationBreakability() != AllowAnyBreaks)
                         state.setHeightOffsetForTableHeaders(state.heightOffsetForTableHeaders() - table()->header()->logicalHeight());
                     // If we have a header group we will paint it at the top of each page, move the rows
                     // down to accomodate it.
@@ -1716,7 +1717,7 @@ void LayoutTableSection::setLogicalPositionForCell(LayoutTableCell* cell, unsign
     cell->setLogicalLocation(cellLocation);
 }
 
-bool LayoutTableSection::hasRepeatingHeaderGroup() const
+bool LayoutTableSection::isRepeatingHeaderGroup() const
 {
     if (getPaginationBreakability() == LayoutBox::AllowAnyBreaks)
         return false;
@@ -1733,7 +1734,7 @@ bool LayoutTableSection::hasRepeatingHeaderGroup() const
     // If the first row of the section after the header group doesn't fit on the page, then
     // don't repeat the header on each page. See https://drafts.csswg.org/css-tables-3/#repeated-headers
     LayoutTableSection* sectionBelow = table()->sectionBelow(this);
-    if (sectionBelow && sectionBelow->firstRow() && sectionBelow->paginationStrutForRow(sectionBelow->firstRow(), sectionBelow->logicalTop()))
+    if (sectionBelow && sectionBelow->firstRow() && sectionBelow->firstRow()->paginationStrut())
         return false;
 
     return true;
@@ -1746,7 +1747,7 @@ bool LayoutTableSection::mapToVisualRectInAncestorSpace(const LayoutBoxModelObje
     // Repeating table headers are painted once per fragmentation page/column. This does not go through the regular fragmentation machinery,
     // so we need special code to expand the invalidation rect to contain all positions of the header in all columns.
     // Note that this is in flow thread coordinates, not visual coordinates. The enclosing LayoutFlowThread will convert to visual coordinates.
-    if (table()->header() == this && hasRepeatingHeaderGroup())
+    if (table()->header() == this && isRepeatingHeaderGroup())
         rect.setHeight(table()->logicalHeight());
     return LayoutTableBoxComponent::mapToVisualRectInAncestorSpace(ancestor, rect, flags);
 }
